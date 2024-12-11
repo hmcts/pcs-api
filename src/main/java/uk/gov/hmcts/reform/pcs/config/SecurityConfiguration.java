0cc8c5fd78f122aa217d8cdb07119c9613cc93bf
adusumillipraveen <praveen.adusumilli@hmcts.net>
2024-12-11 09:50:21 +0000

    Add a onceperrequest filter

---
+++ src/main/java/uk/gov/hmcts/reform/pcs/config/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package uk.gov.hmcts.reform.pcs.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import uk.gov.hmcts.reform.authorisation.filters.ServiceAuthFilter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@ConfigurationProperties(prefix = "security")
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    private final List<String> anonymousPaths = new ArrayList<>();
+    private final ServiceAuthFilter serviceAuthFilter;
+
+    @Autowired
+    public SecurityConfiguration(ServiceAuthFilter serviceAuthFilter) {
+        super();
+        this.serviceAuthFilter = serviceAuthFilter;
+    }
+
+    public List<String> getAnonymousPaths() {
+        return anonymousPaths;
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(anonymousPaths.toArray(String[]::new));
+    }
+
+    @Bean
+    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .addFilterBefore(serviceAuthFilter, AbstractPreAuthenticatedProcessingFilter.class)
+            .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(STATELESS))
+            .httpBasic(httpBasic -> httpBasic.disable())
+            .formLogin(formLogin -> formLogin.disable())
+            .logout(logout -> logout.disable())
+            .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
