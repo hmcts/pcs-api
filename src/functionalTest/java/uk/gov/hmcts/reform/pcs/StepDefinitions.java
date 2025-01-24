21804d631eca6aeebb1ce54755e9899ddbd91b5c
marianadpereira <mariana.pereira@HMCTS.NET>
2025-01-24 14:02:37 +0000

    adding sample test, dependencies and reporting

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/pcs/StepDefinitions.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.reform.pcs;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.Suite;
+import org.junit.runner.RunWith;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+
+
+@Suite
+@IncludeEngines("cucumber")
+@ConfigurationParameter(
+    key = PLUGIN_PROPERTY_NAME,
+    value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+)
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//    plugin = {
+//        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+//    }
+//)
+public class StepDefinitions {
+
+    @Given("repo is created")
+    public void repo_is_created() {
+    }
+
+    @When("framework is implemented")
+    public void framework_is_implemented() {
+    }
+
+    @Then("{string} is printed")
+    public void is_printed(String string) {
+        System.out.println(string);
+    }
+}
