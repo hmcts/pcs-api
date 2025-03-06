efac7c967493816126f5af51ec826800e79bf376
adusumillipraveen <praveen.adusumilli@hmcts.net>
2025-03-06 17:01:35 +0000

    fix

--- src/main/java/uk/gov/hmcts/reform/pcs/hearings/config/HearingsJMSConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/hearings/config/HearingsJMSConfiguration.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.pcs.config;
 
-import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -12,11 +11,6 @@ import org.springframework.jms.annotation.JmsListener;
 public class HearingsJMSConfiguration {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HearingsJMSConfiguration.class);
-
-    @PostConstruct
-    public void init() {
-        LOGGER.info("JMS Listener initialized and started.");
-    }
 
     @JmsListener(destination = "${spring.jms.servicebus.hearings-topic}",
         containerFactory = "topicJmsListenerContainerFactory",
