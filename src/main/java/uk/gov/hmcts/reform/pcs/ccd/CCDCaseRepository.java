c6a9ddbd32c8f4ea7871fd2be128e5e1a7315a26
Alex McAusland <a.m@user.com>
2025-03-10 15:20:14 +0000

    hibernate repo

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/CCDCaseRepository.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/CCDCaseRepository.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.DecentralisedCaseRepository;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
+import uk.gov.hmcts.reform.pcs.repository.PCSCaseRepository;
 
 import java.io.StringWriter;
 import java.io.Writer;
@@ -17,18 +18,25 @@ public class CCDCaseRepository extends DecentralisedCaseRepository<PCSCase> {
     @Autowired
     private PebbleEngine pebl;
 
+    @Autowired
+    private PCSCaseRepository repository;
+
     @Override
     public PCSCase getCase(long caseRef, String roleAssignments) {
-        return PCSCase.builder().
-            exampleTabMarkdown(renderExampleTab())
+        // Load the case from our database.
+        var pcsCase = repository.getReferenceById(caseRef);
+        // Translate it into the CCD model.
+        return
+            PCSCase.builder().
+                exampleTabMarkdown(renderExampleTab(pcsCase.getCaseDescription()))
             .build();
     }
     @SneakyThrows
-    private String renderExampleTab() {
+    private String renderExampleTab(String description) {
         PebbleTemplate compiledTemplate = pebl.getTemplate("example");
         Writer writer = new StringWriter();
 
-        compiledTemplate.evaluate(writer, Map.of());
+        compiledTemplate.evaluate(writer, Map.of("description", description));
         return writer.toString();
     }
 }
--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/PCSCase.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/PCSCase.java
@@ -8,15 +8,13 @@ import uk.gov.hmcts.ccd.sdk.api.CCD;
 import java.util.List;
 
 /**
- * The main domain model representing a possessions case.
+ * The CCD domain model representing a possessions case.
  */
 @Builder
 @Data
 public class PCSCase {
     @CCD(label = "Description of this case")
     private String caseDescription;
-
-    private List<Party> parties;
 
     @External
     private String exampleTabMarkdown;
