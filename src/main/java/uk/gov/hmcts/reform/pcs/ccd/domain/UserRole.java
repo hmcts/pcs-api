76cb8ed020b1f9f8208e76f725b606d1395cd0f2
Alex McAusland <a.m@user.com>
2025-03-12 18:09:24 +0000

    use Set for userrole permissions

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/UserRole.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/UserRole.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import uk.gov.hmcts.ccd.sdk.api.HasRole;
+import uk.gov.hmcts.ccd.sdk.api.Permission;
+
+import java.util.Set;
 
 /**
  * All the different roles for a PCS case.
@@ -12,9 +15,13 @@ import uk.gov.hmcts.ccd.sdk.api.HasRole;
 @Getter
 public enum UserRole implements HasRole {
 
-    CASE_WORKER("caseworker-civil", "CRU");
+    CASE_WORKER("caseworker-civil", Permission.CRU);
 
     @JsonValue
     private final String role;
-    private final String caseTypePermissions;
+    private final Set<Permission> caseTypePermissions;
+
+    public String getCaseTypePermissions() {
+        return Permission.toString(caseTypePermissions);
+    }
 }
