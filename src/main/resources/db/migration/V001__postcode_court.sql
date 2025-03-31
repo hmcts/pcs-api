f63d27001e2d39940e0a4c510b0432146482a9f2
adusumillipraveen <praveen.adusumilli@hmcts.net>
2025-03-31 10:43:26 +0100

    fix naming

--- src/main/resources/db/migration/V001__postcode_court.sql
+++ src/main/resources/db/migration/V001__postcode_court.sql
@@ -1,8 +1,8 @@
 CREATE TABLE postcode_court_mapping (
     postcode VARCHAR(20) PRIMARY KEY,
     epimid INT NOT NULL,
-    legislativecountry VARCHAR(80) NOT NULL,
-    effectivefrom DATE,
-    effectiveto DATE,
+    legislative_country VARCHAR(80) NOT NULL,
+    effective_from DATE,
+    effective_to DATE,
     audit JSONB NOT NULL
 );
