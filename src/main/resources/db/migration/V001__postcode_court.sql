996d4b1d744d63a01f55cf06b90cd685496c1ac3
adusumillipraveen <praveen.adusumilli@hmcts.net>
2025-03-31 11:21:59 +0100

    change to timestamp

--- src/main/resources/db/migration/V001__postcode_court.sql
+++ src/main/resources/db/migration/V001__postcode_court.sql
@@ -2,7 +2,7 @@ CREATE TABLE postcode_court_mapping (
     postcode VARCHAR(20) PRIMARY KEY,
     epimid INT NOT NULL,
     legislative_country VARCHAR(80) NOT NULL,
-    effective_from DATE,
-    effective_to DATE,
+    effective_from TIMESTAMP,
+    effective_to TIMESTAMP,
     audit JSONB NOT NULL
 );
