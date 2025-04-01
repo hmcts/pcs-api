562b2ab821c8a944962fbc37020e646350988ecd
trevor@trevorhinson.com <trevor@promolli.com>
2025-04-01 14:17:23 +0100

    Refactor Postcode entity for updated data model and JSON handling
    Renamed fields to align with standard naming conventions and improved clarity. Updated data types, added JSON serialization for the audit field, and introduced a helper method for setting the audit field. Enhanced @Column configurations to support database changes.

--- src/main/java/uk/gov/hmcts/reform/pcs/postalcode/domain/Postcode.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/postalcode/domain/Postcode.java
@@ -1,12 +1,17 @@
 package uk.gov.hmcts.reform.pcs.postalcode.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import uk.gov.hmcts.reform.pcs.audit.Audit;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,26 +20,37 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @ToString
-@EqualsAndHashCode(of = "epimid")
-public class Postcode {
+@EqualsAndHashCode(of = "postCode")
+public class PostCode {
 
     @Id
-    @Column(name = "epimid", nullable = false)
-    private int epimid;
-
     @Column(name = "postcode", length = 20, nullable = false)
-    private String postcode;
+    private String postCode;
 
-    @Column(name = "legislativecountry", length = 80, nullable = false)
+    @Column(name = "epimid", nullable = false)
+    private int epimId;
+
+    @Column(name = "legislative_country", length = 80, nullable = false)
     private String legislativeCountry;
 
-    @Column(name = "effectivefrom")
-    private LocalDate effectiveFrom;
+    @Column(name = "effective_from")
+    private LocalDateTime effectiveFrom;
 
-    @Column(name = "effectiveto")
-    private LocalDate effectiveTo;
+    @Column(name = "effective_to")
+    private LocalDateTime effectiveTo;
 
-    @Column(name = "audit", nullable = false)
+    @Column(name = "audit", nullable = false, columnDefinition = "jsonb")
+    @JdbcTypeCode(SqlTypes.JSON)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String audit;
+
+    public void setAudit(Audit auditObject) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            this.audit = objectMapper.writeValueAsString(auditObject);
+        } catch (Exception e) {
+            throw new RuntimeException("Error serializing Audit object to JSON", e);
+        }
+    }
 
 }
