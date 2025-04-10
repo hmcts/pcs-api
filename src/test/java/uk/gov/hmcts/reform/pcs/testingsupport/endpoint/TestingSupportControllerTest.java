e79ce15ffbcd13d47faf3a97b1fa9b3aac5a7f39
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2025-04-10 14:45:05 +0100

    Addressing comment

--- src/test/java/uk/gov/hmcts/reform/pcs/testingsupport/endpoint/TestingSupportControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/testingsupport/endpoint/TestingSupportControllerTest.java
@@ -21,7 +21,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class TestingSupportTest {
+class TestingSupportControllerTest {
 
     @Mock
     private NotificationService notificationService;
