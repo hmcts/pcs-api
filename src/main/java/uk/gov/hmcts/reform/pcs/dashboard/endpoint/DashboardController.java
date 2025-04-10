1901fa61e2a07c84f8894f859f9f186bb753e081
sstewart <scott.stewart@solirius.com>
2025-04-10 15:20:01 +0100

    HDPI-499: Return mock data for dashboard notifications

--- src/main/java/uk/gov/hmcts/reform/pcs/dashboard/endpoint/DashboardController.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/dashboard/endpoint/DashboardController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.pcs.dashboard.model.DashboardNotification;
+import uk.gov.hmcts.reform.pcs.dashboard.service.DashboardNotificationService;
 
 import java.util.List;
 
@@ -23,8 +24,9 @@ import java.util.List;
 @AllArgsConstructor
 public class DashboardController {
 
+    private final DashboardNotificationService dashboardNotificationService;
+
     @GetMapping(value = "/{caseReference}/notifications",
-        consumes = MediaType.APPLICATION_JSON_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE
     )
     @Operation(summary = "Get the active dashboard notifications for a case")
@@ -53,7 +55,7 @@ public class DashboardController {
     public ResponseEntity<List<DashboardNotification>> getNotificationsForCase(
         @PathVariable("caseReference") Long caseReference) {
 
-        List<DashboardNotification> notifications = List.of();
+        List<DashboardNotification> notifications = dashboardNotificationService.getNotifications(caseReference);
 
         return ResponseEntity.ok(notifications);
     }
