33d6efeec335fa656026cd66a5b617e7025de346
Cebrail Ergisi <cebrail.ergisi@gmail.com>
2025-04-11 13:39:10 +0100

    Fix checkstyle issues

--- src/main/java/uk/gov/hmcts/reform/pcs/dashboard/endpoint/DashboardController.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/dashboard/endpoint/DashboardController.java
@@ -41,8 +41,7 @@ public class DashboardController {
         content = {
             @Content(
                 mediaType = "application/json",
-                array = @ArraySchema(schema = @Schema(implementation = DashboardNotification.class))
-            )
+                array = @ArraySchema(schema = @Schema(implementation = DashboardNotification.class)))
         })
     @ApiResponse(
         responseCode = "401",
@@ -76,8 +75,7 @@ public class DashboardController {
         content = {
             @Content(
                 mediaType = "application/json",
-                array = @ArraySchema(schema = @Schema(implementation = TaskGroup.class)) // Using TaskGroup
-            )
+                array = @ArraySchema(schema = @Schema(implementation = TaskGroup.class)))
         })
     @ApiResponse(
         responseCode = "401",
