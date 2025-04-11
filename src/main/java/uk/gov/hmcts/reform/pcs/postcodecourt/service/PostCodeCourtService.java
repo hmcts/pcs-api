e8a5b8584c0761ff7be6fa5bec4dcd67ae516608
trevor@trevorhinson.com <trevor@promolli.com>
2025-04-11 12:03:43 +0100

    Enable Flyway, add logging to service, and relocate migration.
    Flyway migrations are now enabled in the configuration. Added debug logging to `PostCodeCourtService` for easier tracing of court lookups. Relocated test data migration script to the correct main resources directory.

--- src/main/java/uk/gov/hmcts/reform/pcs/postcodecourt/service/PostCodeCourtService.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/postcodecourt/service/PostCodeCourtService.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.pcs.postcodecourt.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.pcs.postcodecourt.domain.PostCodeCourt;
 import uk.gov.hmcts.reform.pcs.postcodecourt.repository.PostCodeCourtRepository;
@@ -9,12 +10,15 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class PostCodeCourtService {
 
     private final PostCodeCourtRepository postCodeCourtRepository;
 
     public List<PostCodeCourt> getEpimIdByPostCode(String postcode) {
-        return postCodeCourtRepository.findByIdPostCode(postcode);
+        List<PostCodeCourt> byIdPostCode = postCodeCourtRepository.findByIdPostCode(postcode);
+        log.debug("Found {} courts for postcode {}", byIdPostCode.size(), postcode);
+        return byIdPostCode;
     }
 
 }
