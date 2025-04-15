309f3b928635dab912e4cca68e7f2d0f9fb2e97c
trevor@trevorhinson.com <trevor@promolli.com>
2025-04-15 12:39:12 +0100

    Remove custom JPA configurations and update dependencies
    Eliminated `EntityManagerConfig` and `JpaPropertiesConfig` as they are no longer required. Simplified `FlywayConfiguration` and adjusted annotations for better integration with Spring Boot defaults. Updated `testcontainers:junit-jupiter` dependency to align versions.

--- src/main/java/uk/gov/hmcts/reform/pcs/config/FlywayConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/config/FlywayConfiguration.java
@@ -1,30 +1,33 @@
 package uk.gov.hmcts.reform.pcs.config;
 
+
 import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.core.annotation.Order;
 import uk.gov.hmcts.reform.pcs.data.migration.FlywayNoOpStrategy;
 
+/**
+ * Flyway configuration.
+ */
+@AutoConfigureAfter({
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
+@AutoConfigureBefore({
+    FlywayAutoConfiguration.class
+})
 @Configuration
+@ConditionalOnClass(Flyway.class)
 @ConditionalOnProperty(prefix = "spring.flyway", name = "enabled", matchIfMissing = true)
-@Order(1)
 public class FlywayConfiguration {
-
-    @Bean
-    @Lazy
-    public Flyway flyway(DataSourceProperties dataSourceProperties, FlywayProperties properties) {
-        Flyway flyway = Flyway.configure()
-                .dataSource(dataSourceProperties.getUrl(),
-                        dataSourceProperties.getUsername(), dataSourceProperties.getPassword())
-                .locations(properties.getLocations())
-                .load();
-        flyway.migrate();
-        return flyway;
-    }
 
     /**
      * Bean for FlywayMigrationStrategy.
