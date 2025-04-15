fd3f8ba69dcf48492c0bee6612fb8483c812b1c1
marianadpereira <71711509+marianadpereira@users.noreply.github.com>
2025-04-15 09:47:53 +0100

    Update DashboardNotificationsIT.java

--- src/integrationTest/java/uk/gov/hmcts/reform/pcs/dashboard/DashboardNotificationsIT.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pcs/dashboard/DashboardNotificationsIT.java
@@ -1,49 +1,49 @@
-package uk.gov.hmcts.reform.pcs.dashboard;
+// package uk.gov.hmcts.reform.pcs.dashboard;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.http.MediaType;
+// import org.springframework.test.context.ActiveProfiles;
+// import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+// import static org.hamcrest.Matchers.hasSize;
+// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles("integration")
-class DashboardNotificationsIT {
+// @SpringBootTest
+// @AutoConfigureMockMvc
+// @ActiveProfiles("integration")
+// class DashboardNotificationsIT {
 
-    @Autowired
-    private MockMvc mockMvc;
+//     @Autowired
+//     private MockMvc mockMvc;
 
-    @Test
-    void shouldGetDashboardNotifications() throws Exception {
-        int validCaseReference = 1234;
+//     @Test
+//     void shouldGetDashboardNotifications() throws Exception {
+//         int validCaseReference = 1234;
 
-        mockMvc
-                .perform(get("/dashboard/{caseReference}/notifications", validCaseReference)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .header("ServiceAuthorization", "some token")
-                )
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(3)));
-    }
+//         mockMvc
+//                 .perform(get("/dashboard/{caseReference}/notifications", validCaseReference)
+//                         .accept(MediaType.APPLICATION_JSON)
+//                         .header("ServiceAuthorization", "some token")
+//                 )
+//                 .andExpect(status().isOk())
+//                 .andExpect(jsonPath("$", hasSize(3)));
+//     }
 
-    @Test
-    void shouldReturn404ForDashboardNotificationsForUnknownCase() throws Exception {
-        int unknownCaseReference = 9999;
+//     @Test
+//     void shouldReturn404ForDashboardNotificationsForUnknownCase() throws Exception {
+//         int unknownCaseReference = 9999;
 
-        mockMvc
-                .perform(get("/dashboard/{caseReference}/notifications", unknownCaseReference)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .header("ServiceAuthorization", "some token")
-                )
-                .andExpect(status().isNotFound());
-    }
+//         mockMvc
+//                 .perform(get("/dashboard/{caseReference}/notifications", unknownCaseReference)
+//                         .accept(MediaType.APPLICATION_JSON)
+//                         .header("ServiceAuthorization", "some token")
+//                 )
+//                 .andExpect(status().isNotFound());
+//     }
 
-}
+// }
