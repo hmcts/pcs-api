e1e4e25e69910c3fbb58479060d3abf541d8f655
trevor@trevorhinson.com <trevor@promolli.com>
2025-04-16 10:30:40 +0100

    Remove unused JPA repository configuration.
    The `@EnableJpaRepositories` annotation was removed as it is no longer required. This cleanup reduces unnecessary configuration and ensures the application focuses only on relevant dependencies.

--- src/main/java/uk/gov/hmcts/reform/pcs/Application.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/Application.java
@@ -3,9 +3,8 @@ package uk.gov.hmcts.reform.pcs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import uk.gov.hmcts.reform.idam.client.IdamApi;
 import org.springframework.jms.annotation.EnableJms;
+import uk.gov.hmcts.reform.idam.client.IdamApi;
 import uk.gov.hmcts.reform.pcs.hearings.service.api.HmcHearingApi;
 
 @SpringBootApplication(
@@ -21,7 +20,6 @@ import uk.gov.hmcts.reform.pcs.hearings.service.api.HmcHearingApi;
     }
 )
 @EnableJms
-@EnableJpaRepositories(basePackages = "uk.gov.hmcts.reform.pcs")
 public class Application {
 
     public static void main(final String[] args) {
