c2497d72f35ea7f21e791e43850a48b211ae2173
asifshaik-moj <asif.shaik@hmcts.net>
2025-04-23 17:53:02 +0100

    HDPI-352: Implemented review comments

--- src/test/java/uk/gov/hmcts/reform/pcs/postcodecourt/service/PostCodeCourtServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/postcodecourt/service/PostCodeCourtServiceTest.java
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class PostCodeCourtEntityServiceTest {
+class PostCodeCourtServiceTest {
 
     @Mock
     private PostCodeCourtRepository postCodeCourtRepository;
