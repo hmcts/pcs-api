4390bbc1c1b8cfbc5d67175b448bb58175abbd56
adusumillipraveen <praveen.adusumilli@hmcts.net>
2025-04-24 14:39:07 +0100

    try upload

---
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/config/HighLevelDataSetupApp.java
@@ -0,0 +1,86 @@
+package uk.gov.hmcts.reform.pcs.ccd.config;
+
+import java.util.List;
+import java.util.Locale;
+import javax.crypto.AEADBadTagException;
+import javax.net.ssl.SSLException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.hmcts.befta.dse.ccd.CcdEnvironment;
+import uk.gov.hmcts.befta.dse.ccd.CcdRoleConfig;
+import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
+import uk.gov.hmcts.befta.exception.ImportException;
+import uk.gov.hmcts.befta.util.BeftaUtils;
+import uk.gov.hmcts.reform.pcs.ccd.CaseType;
+
+public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
+
+    private static final Logger logger = LoggerFactory.getLogger(HighLevelDataSetupApp.class);
+
+    private static final CcdRoleConfig[] CCD_ROLES = {
+        new CcdRoleConfig("caseworker-civil", "PUBLIC"),
+    };
+
+
+    private final CcdEnvironment environment;
+
+    public HighLevelDataSetupApp(CcdEnvironment dataSetupEnvironment) {
+        super(dataSetupEnvironment);
+        environment = dataSetupEnvironment;
+    }
+
+    public static void main(String[] args) throws Throwable {
+        DataLoaderToDefinitionStore.main(HighLevelDataSetupApp.class, args);
+    }
+
+    @Override
+    public void addCcdRoles() {
+        for (CcdRoleConfig roleConfig : CCD_ROLES) {
+            try {
+                logger.info("\n\nAdding CCD Role {}.", roleConfig);
+                addCcdRole(roleConfig);
+                logger.info("\n\nAdded CCD Role {}.", roleConfig);
+            } catch (Exception e) {
+                logger.error("\n\nCouldn't add CCD Role {}.\n\n", roleConfig, e);
+                if (!shouldTolerateDataSetupFailure()) {
+                    throw e;
+                }
+            }
+        }
+    }
+
+    @Override
+    protected List<String> getAllDefinitionFilesToLoadAt(String definitionsPath) {
+        String environmentName = environment.name().toLowerCase(Locale.UK);
+        return List.of(
+            "build/definitions/CCD_Definition_" + CaseType.getCaseTypeId() + "_" + environmentName + ".xlsx"
+        );
+    }
+
+    @Override
+    public void createRoleAssignments() {
+        // Do not create role assignments.
+        BeftaUtils.defaultLog("Will NOT create role assignments!");
+    }
+
+    @Override
+    protected boolean shouldTolerateDataSetupFailure() {
+        var env = getDataSetupEnvironment();
+        return CcdEnvironment.PERFTEST == env || CcdEnvironment.DEMO == env || CcdEnvironment.ITHC == env;
+    }
+
+    @Override
+    protected boolean shouldTolerateDataSetupFailure(Throwable e) {
+        int httpStatusCode504 = 504;
+        if (e instanceof ImportException importException) {
+            return importException.getHttpStatusCode() == httpStatusCode504;
+        }
+        if (e instanceof SSLException) {
+            return true;
+        }
+        if (e instanceof AEADBadTagException) {
+            return true;
+        }
+        return shouldTolerateDataSetupFailure();
+    }
+}
