3aed8b1f28b5711d059d51eea9def448c24dcaff
adusumillipraveen <praveen.adusumilli@hmcts.net>
2025-04-25 12:22:42 +0100

    change case type name as well

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/CaseType.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/CaseType.java
@@ -18,22 +18,26 @@ public class CaseType implements CCDConfig<PCSCase, State, UserRole> {
 
     private static final String CASE_TYPE_ID = "PCS";
     public static final String CASE_TYPE_NAME = "Civil Possessions";
-    public static final String CASE_TYPE_DESCRIPTION = "Civil Possessions Claims";
+    public static final String CASE_TYPE_DESCRIPTION = "Civil Possessions Case Type";
     public static final String JURISDICTION_ID = "CIVIL";
     public static final String JURISDICTION_NAME = "Civil Possessions";
-    public static final String JURISDICTION_DESCRIPTION = "Civil Possessions Claims";
+    public static final String JURISDICTION_DESCRIPTION = "Civil Possessions Jurisdiction";
 
     public static String getCaseTypeId() {
+        return withChangeId(CASE_TYPE_ID, "-");
+    }
+
+    private static String withChangeId(String base, String separator) {
         return ofNullable(getenv().get("CHANGE_ID"))
-            .map(num -> CASE_TYPE_ID + "_" + num)
-            .orElse(CASE_TYPE_ID);
+            .map(changeId -> base + separator + changeId)
+            .orElse(base);
     }
 
     @Override
     public void configure(final ConfigBuilder<PCSCase, State, UserRole> builder) {
         builder.setCallbackHost(getenv().getOrDefault("CASE_API_URL", "http://localhost:3206"));
 
-        builder.caseType(getCaseTypeId(), CASE_TYPE_NAME, CASE_TYPE_DESCRIPTION);
+        builder.caseType(getCaseTypeId(), withChangeId(CASE_TYPE_NAME," "), CASE_TYPE_DESCRIPTION);
         builder.jurisdiction(JURISDICTION_ID, JURISDICTION_NAME, JURISDICTION_DESCRIPTION);
 
         var label = "Applicant Forename";
