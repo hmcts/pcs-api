50f8f4fda8e4ef4d868c726add1757e3c8dd3722
jaina-sol <jonathan.aina@hmcts.net>
2025-05-08 01:48:31 +0100

    Add method to find notifications by provider notification ID

--- src/main/java/uk/gov/hmcts/reform/pcs/notify/repository/NotificationRepository.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/notify/repository/NotificationRepository.java
@@ -3,7 +3,9 @@ package uk.gov.hmcts.reform.pcs.notify.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uk.gov.hmcts.reform.pcs.notify.domain.CaseNotification;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface NotificationRepository extends JpaRepository<CaseNotification, UUID> {
+    Optional<CaseNotification> findByProviderNotificationId(UUID providerNotificationId);
 }
