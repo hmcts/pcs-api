956578179bdbf93e5e8a2d88c52ebef7722d8b06
sstewart <scott.stewart@solirius.com>
2025-05-09 14:09:43 +0100

    HDPI-661: Use trigger DDL compatible with Postgres 11
    Postgres 11 is still used in the DB pods in the preview environment

--- src/main/resources/db/migration/R__postcode_court_triggers.sql
+++ src/main/resources/db/migration/R__postcode_court_triggers.sql
@@ -11,6 +11,17 @@ BEGIN
 END;
 $func$;
 
-CREATE OR REPLACE TRIGGER postcode_court_mapping_trigger
+
+/*
+  CREATE OR REPLACE TRIGGER was only added in Postgres 14 and at the time of
+  writing the preview environment uses Postgres 11, (even though Azure DBs
+  for pcs-api are on 16).
+
+  When the preview environment is using Postgres 14+ then the DROP/CREATE can be
+  changed to a CREATE OR REPLACE
+*/
+DROP TRIGGER IF EXISTS postcode_court_mapping_trigger ON postcode_court_mapping;
+
+CREATE TRIGGER postcode_court_mapping_trigger
   BEFORE INSERT OR UPDATE ON postcode_court_mapping
   FOR EACH ROW EXECUTE FUNCTION postcode_court_mapping_trigger_func();
