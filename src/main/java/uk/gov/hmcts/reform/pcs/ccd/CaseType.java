9627bfa09517a6093392502fc18421a544785341
libanAbdirahman1 <liban.abdirahman@hmcts.net>
2025-06-11 11:17:04 +0100

    updating filters to show cases

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/CaseType.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/CaseType.java
@@ -48,27 +48,27 @@ public class CaseType implements CCDConfig<PCSCase, State, UserRole> {
         var addressLabel = "Property Address";
         var claimantLabel = "Claimant Information";
         builder.searchInputFields()
-            .field(PCSCase::getApplicantForename, nameLabel);
+            .field(PCSCase::getApplicantForename, nameLabel, UserRole.CASE_WORKER);
 
         builder.searchCasesFields()
-            .field(PCSCase::getApplicantForename, nameLabel);
+            .field(PCSCase::getApplicantForename, nameLabel, UserRole.CASE_WORKER.getRole());
 
         builder.searchResultFields()
-            .field(PCSCase::getApplicantForename, nameLabel)
-            .field(PCSCase::getPropertyAddress, addressLabel);
+            .field(PCSCase::getApplicantForename, nameLabel, UserRole.CASE_WORKER)
+            .field(PCSCase::getPropertyAddress, addressLabel, UserRole.CASE_WORKER);
 
         builder.workBasketInputFields()
-            .field(PCSCase::getApplicantForename, nameLabel);
+            .field(PCSCase::getApplicantForename, nameLabel, UserRole.CASE_WORKER);
 
         builder.workBasketResultFields()
-            .field(PCSCase::getApplicantForename, nameLabel)
-            .field(PCSCase::getPropertyAddress, addressLabel);
+            .field(PCSCase::getApplicantForename, nameLabel, UserRole.CASE_WORKER)
+            .field(PCSCase::getPropertyAddress, addressLabel, UserRole.CASE_WORKER);
 
         builder.tab("claimantInformation", claimantLabel)
-            .field(PCSCase::getApplicantForename);
+            .field(PCSCase::getApplicantForename, UserRole.CASE_WORKER.getRole());
 
         builder.tab("propertyAddress", "Property Address")
-            .field(PCSCase::getPropertyAddress);
+            .field(PCSCase::getPropertyAddress, UserRole.CASE_WORKER.getRole());
 
     }
 }
