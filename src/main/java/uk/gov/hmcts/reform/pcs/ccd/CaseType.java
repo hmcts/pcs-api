75feb19b5e970d3466dc90562a5c0a4275b8eed1
sstewart <scott.stewart@solirius.com>
2025-06-12 12:47:07 +0100

    HDPI-856: Revert temp change to test unmodified case type

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/CaseType.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/CaseType.java
@@ -1,13 +1,14 @@
 package uk.gov.hmcts.reform.pcs.ccd;
 
+import static java.lang.System.getenv;
+import static java.util.Optional.ofNullable;
+
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
 import uk.gov.hmcts.reform.pcs.ccd.domain.State;
 import uk.gov.hmcts.reform.pcs.ccd.domain.UserRole;
-
-import static java.lang.System.getenv;
 
 /**
  * Setup some common possessions case type configuration.
@@ -23,11 +24,17 @@ public class CaseType implements CCDConfig<PCSCase, State, UserRole> {
     private static final String JURISDICTION_DESCRIPTION = "Civil Possessions Jurisdiction";
 
     public static String getCaseType() {
-        return CASE_TYPE_ID;
+        return withChangeId(CASE_TYPE_ID, "-");
     }
 
     public static String getCaseTypeName() {
-        return CASE_TYPE_NAME;
+        return withChangeId(CASE_TYPE_NAME, " ");
+    }
+
+    private static String withChangeId(String base, String separator) {
+        return ofNullable(getenv().get("CHANGE_ID"))
+            .map(changeId -> base + separator + changeId)
+            .orElse(base);
     }
 
     @Override
