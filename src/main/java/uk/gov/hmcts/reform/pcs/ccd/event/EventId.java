b07ddc0dd25c38a45a3c6cd7316490ae69f8d25c
sstewart <scott.stewart@solirius.com>
2025-06-16 16:59:39 +0100

    HDPI-856: Add temporary event fo Caseworker to create events

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/EventId.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/EventId.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.pcs.ccd.event;
 public enum EventId {
 
     caseworkerUpdateApplication,
+    caseworkerCreateApplication,
     citizenCreateApplication,
     citizenSubmitApplication,
     citizenUpdateApplication
