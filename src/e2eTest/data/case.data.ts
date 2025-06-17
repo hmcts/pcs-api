f9d916750d0672237c38afbe05ec16b9696ffc39
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-06-17 14:52:00 +0100

    Temporarily adding the PR number in e2e test for verification.

--- src/e2eTest/data/case.data.ts
+++ src/e2eTest/data/case.data.ts
@@ -1,7 +1,7 @@
 
 export const caseData = {
   jurisdiction: 'Possessions',
-  caseType: 'Civil Possessions',
+  caseType: 'Civil Possessions 261',
   event: 'Make a claim',
   postcode: 'W3 7RX',
   addressIndex: 1,
