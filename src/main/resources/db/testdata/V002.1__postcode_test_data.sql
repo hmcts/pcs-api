44ff4f15fa95a63f6db1032b511869baa2f50091
libanAbdirahman1 <liban.abdirahman@hmcts.net>
2025-06-18 16:04:58 +0100

    Added more test data

--- src/main/resources/db/testdata/V002.1__postcode_test_data.sql
+++ src/main/resources/db/testdata/V002.1__postcode_test_data.sql
@@ -11,7 +11,12 @@ INSERT INTO postcode_court_mapping (postcode, epimid, legislative_country, effec
     ('SW1 1AA', 817113, 'England', '2025-06-16', NULL, '{"created_by": "admin", "change_reason": "initial insert"}'::jsonb),
     ('SW1 1AA', 817114, 'England', '2025-06-01', NULL, '{"created_by": "admin", "change_reason": "initial insert"}'::jsonb),
     ('W95', 20263, 'England', '2025-06-01', '2025-06-10', '{"created_by": "admin", "change_reason": "initial insert"}'::jsonb),
-    ('W95', 20262, 'England', '2025-06-01', '2035-07-31', '{"created_by": "admin", "change_reason": "initial insert"}'::jsonb),
+    ('W95', 20262, 'England', '2025-06-11', '2035-07-31', '{"created_by": "admin", "change_reason": "initial insert"}'::jsonb),
     ('DN5 51P', 999985, 'England', '2025-06-01', '2035-11-10', '{"created_by": "admin", "change_reason": "initial insert"}'::jsonb),
-    ('DN5 51P', 999984, 'England', '2025-06-01', NULL, '{"created_by": "admin", "change_reason": "initial insert"}'::jsonb),
-    ('SL6', 144641, 'England', '2025-06-01', '2035-11-10', '{"created_by": "admin", "change_reason": "initial insert"}'::jsonb);
+    ('DN5 51P', 999984, 'England', '2025-01-01', NULL, '{"created_by": "admin", "change_reason": "initial insert"}'::jsonb),
+    ('SL6', 144641, 'England', '2025-06-01', '2035-11-10', '{"created_by": "admin", "change_reason": "initial insert"}'::jsonb),
+    ('M13 9PZ', 817114, 'England', '2025-01-01', '2025-02-10', '{"created_by": "admin", "change_reason": "initial insert"}'::jsonb),
+    ('M13 9P', 484482, 'England', '2025-03-01', '2025-04-10', '{"created_by": "admin", "change_reason": "initial insert"}'::jsonb),
+    ('M13 9', 20262, 'England', '2025-06-01', '2035-07-31', '{"created_by": "admin", "change_reason": "initial insert"}'::jsonb),
+    ('M13', 144641, 'England', '2025-06-01', NULL, '{"created_by": "admin", "change_reason": "initial insert"}'::jsonb);
+
