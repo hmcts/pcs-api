543fcc3464d04ba758780b332c14cc24097c2a78
sstewart <scott.stewart@solirius.com>
2025-06-24 11:33:25 +0100

    HDPI-660: Replace nunjucks with templated string following PR feedback

--- tools/postcode-to-epims/src/sql-generator.js
+++ tools/postcode-to-epims/src/sql-generator.js
@@ -1,26 +1,42 @@
 import { DateTime } from 'luxon';
 import fs from 'fs';
-import nunjucks from 'nunjucks'
+import { userInfo } from 'os';
 
-const sqlTemplate = `{% macro toSqlDate(dateTime) -%}
-    {%- if dateTime -%} '{{ dateTime.toSQLDate() }}' {%- else -%} NULL {%- endif -%}
-{%- endmacro %}
-BEGIN;
-TRUNCATE postcode_court_mapping;
-INSERT INTO postcode_court_mapping (postcode, epimid, legislative_country, effective_from, effective_to, audit)
-VALUES
-{%- for mapping in mappings %}
-   ('{{mapping.postCode}}', {{mapping.epimId}}, '{{mapping.legislativeCountry}}', {{ toSqlDate(mapping.effectiveFrom) }}, {{ toSqlDate(mapping.effectiveTo) }}, '{"generated": "{{ generatedAt }}" }')
-    {%- if loop.last -%} ; {%- else -%} , {%- endif -%}
-{%- endfor %}
-COMMIT;`;
+function convertToSqlDateString(dateTime) {
+  if (!dateTime) {
+    return 'null';
+  }
+
+  return `'${dateTime.toSQLDate()}'`;
+}
+
+function isLastElementIndex(elementIndex, mappings) {
+  return elementIndex === mappings.length - 1;
+}
 
 export const writeMappingsToSql = (mappings, sqlOutputFile) => {
-  const generatedSql = nunjucks.renderString(sqlTemplate, {
-    mappings,
-    generatedAt: DateTime.utc().toISO()
-  });
+  const lines = [];
 
-  fs.writeFileSync(sqlOutputFile, generatedSql);
+  lines.push('BEGIN;');
+  lines.push('TRUNCATE postcode_court_mapping;');
+  lines.push('INSERT INTO postcode_court_mapping (postcode, epimid, legislative_country, effective_from, effective_to, audit)');
+  lines.push('VALUES');
+
+  const currentDateTime = DateTime.utc();
+
+  for (let i = 0; i < mappings.length; i++) {
+    const mapping = mappings[i];
+    const effectiveFrom = convertToSqlDateString(mapping.effectiveFrom);
+    const effectiveTo = convertToSqlDateString(mapping.effectiveTo);
+    const auditJson = `{"created_by": "${userInfo().username}", "generated": "${currentDateTime.toISO()}"}`;
+
+    const lineEnding = isLastElementIndex(i, mappings) ? ';' : ','
+    lines.push(`  ('${mapping.postCode}', ${mapping.epimId}, '${mapping.legislativeCountry}', `
+     + `${effectiveFrom}, ${effectiveTo}, '${auditJson}')${lineEnding}`);
+  }
+
+  lines.push('COMMIT;');
+
+  fs.writeFileSync(sqlOutputFile, lines.join('\n'));
   console.log(`SQL statements written to ${sqlOutputFile}`);
 }
