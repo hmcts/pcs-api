3edd92c084a9fffc8549186d60971dc3f9a59c80
sstewart <scott.stewart@solirius.com>
2025-06-25 15:28:10 +0100

    Revert "HDPI-956: Mark case fields as @External"
    This reverts commit 09817383c1a6baa468eb4ff50ac2d54d18b647c6.

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/PCSCase.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/PCSCase.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.pcs.ccd.domain;
 
 import lombok.Builder;
 import lombok.Data;
-import uk.gov.hmcts.ccd.sdk.External;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.AddressUK;
 import uk.gov.hmcts.reform.pcs.ccd.accesscontrol.CaseworkerAccess;
@@ -19,21 +18,18 @@ public class PCSCase {
         label = "Applicant's forename",
         access = {CitizenAccess.class, CaseworkerAccess.class}
     )
-    @External
     private String applicantForename;
 
     @CCD(
         label = "Applicant's surname",
         access = {CitizenAccess.class, CaseworkerAccess.class}
     )
-    @External
     private String applicantSurname;
 
     @CCD(
         label = "Property address",
         access = {CitizenAccess.class, CaseworkerAccess.class}
     )
-    @External
     private AddressUK propertyAddress;
 
 }
