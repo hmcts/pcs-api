33fe90a94f999e50e693131bddf278cff945a13b
scottstewart-sl <scott.stewart@solirius.com>
2025-06-27 09:58:44 +0100

    HDPI-660: Add script to validate and convert postcode/ePIMS mappings (#301)
    Co-authored-by: marianadpereira <71711509+marianadpereira@users.noreply.github.com>

---
+++ tools/postcode-to-epims/src/csv-parser.js
@@ -0,0 +1,69 @@
+import fs from 'fs';
+import {parse} from 'csv-parse/sync';
+import { DateTime } from 'luxon';
+
+function convertToDateTime(sourceDateString) {
+  if (!sourceDateString || sourceDateString === 'null') {
+    return null;
+  }
+
+  return DateTime.fromFormat(sourceDateString, 'dd/MM/yyyy');
+}
+
+export const readCsvData = (csvSourceFile) => {
+  let csvData;
+  try {
+    csvData = fs.readFileSync(csvSourceFile);
+  } catch (e) {
+    console.warn(`Unable to read file at ${csvSourceFile}`)
+    process.exit(2);
+  }
+
+  const rows = parse(csvData, {bom: true});
+
+  if (rows.length < 2) {
+    console.warn('No data rows found in CSV');
+    process.exit(3);
+  }
+
+  // Remove the header row
+  rows.shift();
+  return rows;
+}
+
+function convertRowsToMappings(rows, warnings) {
+  let warningRowNumber = 2; // Row 1 is the header row in the sourcr CSV
+  return rows.map(row => {
+    const mapping = {};
+
+    let sourceEffectiveFrom;
+    let sourceEffectiveTo;
+    [mapping.postCode, mapping.epimId, mapping.legislativeCountry, sourceEffectiveFrom, sourceEffectiveTo] = row;
+
+    mapping.effectiveFrom = convertToDateTime(sourceEffectiveFrom);
+    if (!mapping.effectiveFrom) {
+      warnings.push(`Row ${warningRowNumber}: Missing mandatory Effective From date. Row will be omitted.`);
+      mapping.excludeFromSql = true;
+    } else if (!mapping.effectiveFrom.isValid) {
+      mapping.excludeFromSql = true;
+      warnings.push(`Row ${warningRowNumber}: Invalid Effective From date: ${sourceEffectiveFrom}. Row will be omitted.`);
+    }
+
+    mapping.effectiveTo = convertToDateTime(sourceEffectiveTo);
+    if (mapping.effectiveTo && !mapping.effectiveTo.isValid) {
+      warnings.push(`Row ${warningRowNumber}: Invalid Effective To date ${sourceEffectiveTo}`);
+    }
+
+    if (mapping.effectiveFrom && mapping.effectiveTo && mapping.effectiveFrom > mapping.effectiveTo) {
+      warnings.push(`Row ${warningRowNumber}: Effective From date ${sourceEffectiveFrom} is after Effective To date ${sourceEffectiveTo}`);
+    }
+
+    warningRowNumber++;
+    return mapping;
+  });
+}
+
+export const parseCsv = (csvSourceFile, warnings) => {
+  const rows = readCsvData(csvSourceFile);
+  return convertRowsToMappings(rows, warnings);
+}
