3070ffe44c4c3d89b766f2ab2bb5eb413228a372
scottstewart-sl <scott.stewart@solirius.com>
2025-06-27 10:51:50 +0100

    Hdpi 956 decentralised case in preview (#287)
    Co-authored-by: Alex McAusland <a.m@user.com>
    Co-authored-by: Isha Shrivastava <isha.shrivastava1@hmcts.net>
    Co-authored-by: hmcts-jenkins-j-to-z <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
    Co-authored-by: SameenaHMCTS <sameena.javeed@hmcts.net>

---
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/EventId.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.reform.pcs.ccd.event;
+
+public enum EventId {
+
+    caseworkerUpdateApplication,
+    citizenCreateApplication,
+    citizenSubmitApplication,
+    citizenUpdateApplication,
+    createTestApplication
+
+}
---
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/CCDCaseRepositoryTest.java
@@ -0,0 +1,99 @@
+package uk.gov.hmcts.reform.pcs.ccd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import uk.gov.hmcts.ccd.sdk.type.AddressUK;
+import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
+import uk.gov.hmcts.reform.pcs.ccd.entity.AddressEntity;
+import uk.gov.hmcts.reform.pcs.ccd.entity.PcsCaseEntity;
+import uk.gov.hmcts.reform.pcs.ccd.repository.PcsCaseRepository;
+import uk.gov.hmcts.reform.pcs.exception.CaseNotFoundException;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CCDCaseRepositoryTest {
+
+    private static final long CASE_REFERENCE = 1234L;
+
+    @Mock
+    private PcsCaseRepository pcsCaseRepository;
+    @Mock
+    private ModelMapper modelMapper;
+
+    private CCDCaseRepository underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new CCDCaseRepository(pcsCaseRepository, modelMapper);
+    }
+
+    @Test
+    void shouldThrowExceptionForUnknownCaseReference() {
+        // Given
+        when(pcsCaseRepository.findByCaseReference(CASE_REFERENCE)).thenReturn(Optional.empty());
+
+        // When
+        Throwable throwable = catchThrowable(() -> underTest.getCase(CASE_REFERENCE));
+
+        // Then
+        assertThat(throwable)
+            .isInstanceOf(CaseNotFoundException.class)
+            .hasMessage("No case found with reference %s", CASE_REFERENCE);
+    }
+
+    @Test
+    void shouldReturnCaseWithNoPropertyAddress() {
+        // Given
+        String expectedForename = "Test forename";
+        String expectedSurname = "Test surname";
+
+        PcsCaseEntity pcsCaseEntity = mock(PcsCaseEntity.class);
+        when(pcsCaseEntity.getApplicantForename()).thenReturn(expectedForename);
+        when(pcsCaseEntity.getApplicantSurname()).thenReturn(expectedSurname);
+
+        when(pcsCaseRepository.findByCaseReference(CASE_REFERENCE)).thenReturn(Optional.of(pcsCaseEntity));
+
+        // When
+        PCSCase pcsCase = underTest.getCase(CASE_REFERENCE);
+
+        // Then
+        assertThat(pcsCase.getApplicantForename()).isEqualTo(expectedForename);
+        assertThat(pcsCase.getApplicantSurname()).isEqualTo(expectedSurname);
+        assertThat(pcsCase.getPropertyAddress()).isNull();
+    }
+
+    @Test
+    void shouldMapPropertyAddress() {
+        // Given
+        PcsCaseEntity pcsCaseEntity = mock(PcsCaseEntity.class);
+        AddressEntity addressEntity = mock(AddressEntity.class);
+        when(pcsCaseEntity.getPropertyAddress()).thenReturn(addressEntity);
+
+        AddressUK addressUK = stubAddressEntityModelMapper(addressEntity);
+
+        when(pcsCaseRepository.findByCaseReference(CASE_REFERENCE)).thenReturn(Optional.of(pcsCaseEntity));
+
+        // When
+        PCSCase pcsCase = underTest.getCase(CASE_REFERENCE);
+
+        // Then
+        assertThat(pcsCase.getPropertyAddress()).isEqualTo(addressUK);
+    }
+
+    private AddressUK stubAddressEntityModelMapper(AddressEntity addressEntity) {
+        AddressUK addressUK = mock(AddressUK.class);
+        when(modelMapper.map(addressEntity, AddressUK.class)).thenReturn(addressUK);
+        return addressUK;
+    }
+
+}
