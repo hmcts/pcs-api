e9a455a432daa638b737b29faa316062c8968fe4
libanAbdirahman1 <liban.abdirahman@hmcts.net>
2025-07-04 09:13:23 +0100

    Updating HighLevelDataSetupApp to import gen app case type

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/config/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/config/HighLevelDataSetupApp.java
@@ -8,6 +8,7 @@ import uk.gov.hmcts.befta.dse.ccd.CcdRoleConfig;
 import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
 import uk.gov.hmcts.befta.exception.ImportException;
 import uk.gov.hmcts.befta.util.BeftaUtils;
+import uk.gov.hmcts.reform.pcs.ccd.GeneralApplicationCaseType;
 import uk.gov.hmcts.reform.pcs.ccd.PCSCaseType;
 
 import javax.crypto.AEADBadTagException;
@@ -57,7 +58,8 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
     protected List<String> getAllDefinitionFilesToLoadAt(String definitionsPath) {
         String environmentName = environment.name().toLowerCase(Locale.UK);
         return List.of(
-            "build/definitions/CCD_Definition_" + PCSCaseType.getCaseType() + "_" + environmentName + ".xlsx"
+            "build/definitions/CCD_Definition_" + PCSCaseType.getCaseType() + "_" + environmentName + ".xlsx",
+            "build/definitions/CCD_Definition_" + GeneralApplicationCaseType.getCaseType() + "_" + environmentName + ".xlsx"
         );
     }
 
