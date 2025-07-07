26f0310f87774a4981be76db0ad1bf9acf75d140
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-07-07 18:13:38 +0100

    Testing report during failure

--- src/e2eTest/data/case.data.ts
+++ src/e2eTest/data/case.data.ts
@@ -4,7 +4,7 @@ export const caseData = {
   caseType: process.env.CHANGE_ID
     ? `Civil Possessions ${process.env.CHANGE_ID}`
     : 'Civil Possessions',
-  event: 'Make a claim xxx',
+  event: 'Make a claim',
   postcode: 'W3 7RX',
   addressIndex: 1,
   addressLine2: 'address2',
