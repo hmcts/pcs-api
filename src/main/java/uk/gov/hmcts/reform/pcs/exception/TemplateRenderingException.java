5a7eb08e8f98deb279ebfe8018874f9d7e76eeb2
sstewart <scott.stewart@solirius.com>
2025-07-08 16:49:51 +0100

    HDPI-1245: Remove used exception class and Pebble template

--- src/main/java/uk/gov/hmcts/reform/pcs/exception/TemplateRenderingException.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.pcs.exception;
-
-public class TemplateRenderingException extends RuntimeException {
-
-    public TemplateRenderingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-}
