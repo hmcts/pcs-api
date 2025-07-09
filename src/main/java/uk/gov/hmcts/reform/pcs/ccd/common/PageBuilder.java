eedd2abe150f9299c1474886d524c9ba6b447483
Jake Gowler <jakecgowler@gmail.com>
2025-07-09 14:11:39 +0100

    Small change to how we configure ExUI journeys to clean the code a little for extended journeys

---
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/common/PageBuilder.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.reform.pcs.ccd.common;
+
+import uk.gov.hmcts.ccd.sdk.api.Event.EventBuilder;
+import uk.gov.hmcts.ccd.sdk.api.FieldCollection.FieldCollectionBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.MidEvent;
+import uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole;
+import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
+import uk.gov.hmcts.reform.pcs.ccd.domain.State;
+
+public class PageBuilder {
+
+    private final EventBuilder<PCSCase, UserRole, State> eventBuilder;
+
+    public PageBuilder(EventBuilder<PCSCase, UserRole, State> eventBuilder) {
+        this.eventBuilder = eventBuilder;
+    }
+
+    public FieldCollectionBuilder<PCSCase, State, EventBuilder<PCSCase, UserRole, State>> page(final String id) {
+        return eventBuilder.fields().page(id);
+    }
+
+    public FieldCollectionBuilder<PCSCase, State, EventBuilder<PCSCase, UserRole, State>> page(
+        final String id,
+        final MidEvent<PCSCase, State> callback) {
+
+        return eventBuilder.fields().page(id, callback);
+    }
+}
--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/CreateTestCase.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/CreateTestCase.java
@@ -4,11 +4,16 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.Event.EventBuilder;
 import uk.gov.hmcts.ccd.sdk.api.EventPayload;
 import uk.gov.hmcts.ccd.sdk.api.Permission;
+import uk.gov.hmcts.reform.pcs.ccd.common.CcdPageConfiguration;
+import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
 import uk.gov.hmcts.reform.pcs.ccd.domain.State;
 import uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole;
+import uk.gov.hmcts.reform.pcs.ccd.page.createtestcase.ClaimantInformation;
+import uk.gov.hmcts.reform.pcs.ccd.page.createtestcase.MakeAClaim;
 import uk.gov.hmcts.reform.pcs.ccd.service.PcsCaseService;
 
 import static uk.gov.hmcts.reform.pcs.ccd.event.EventId.createTestApplication;
@@ -17,24 +22,23 @@ import static uk.gov.hmcts.reform.pcs.ccd.event.EventId.createTestApplication;
 @AllArgsConstructor
 public class CreateTestCase implements CCDConfig<PCSCase, State, UserRole> {
 
+    private static final CcdPageConfiguration makeAClaim = new MakeAClaim();
+    private static final CcdPageConfiguration claimantInformation = new ClaimantInformation();
+
     private final PcsCaseService pcsCaseService;
 
     @Override
     public void configure(ConfigBuilder<PCSCase, State, UserRole> configBuilder) {
-        configBuilder
-            .decentralisedEvent(createTestApplication.name(), this::submit, this::start)
-            .initialState(State.CASE_ISSUED)
-            .name("Make a claim")
-            .grant(Permission.CRUD, UserRole.PCS_CASE_WORKER)
-            .fields()
-            .page("Make a claim")
-            .pageLabel("What is the address of the property you're claiming possession of?")
-            .label("lineSeparator", "---")
-            .mandatory(PCSCase::getPropertyAddress)
-            .page("claimant information")
-            .pageLabel("Please enter applicant's name")
-                .mandatory(PCSCase::getApplicantForename)
-                .done();
+        EventBuilder<PCSCase, UserRole, State> eventBuilder =
+            configBuilder
+                .decentralisedEvent(createTestApplication.name(), this::submit, this::start)
+                .initialState(State.CASE_ISSUED)
+                .name("Make a claim")
+                .grant(Permission.CRUD, UserRole.PCS_CASE_WORKER);
+
+        PageBuilder pageBuilder = new PageBuilder(eventBuilder);
+        makeAClaim.addTo(pageBuilder);
+        claimantInformation.addTo(pageBuilder);
     }
 
     private PCSCase start(EventPayload<PCSCase, State> eventPayload) {
