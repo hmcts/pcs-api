5f3490e8496edeac1b5b79ac0d9e439753e7ec2b
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2025-07-14 13:46:36 +0100

    execution is successful locally

--- src/e2eTest/tests/searchCase.spec.ts
+++ src/e2eTest/tests/searchCase.spec.ts
@@ -1,62 +1,68 @@
 // src/e2eTest/tests/searchCase.spec.ts
-import { test } from '@playwright/test';
-import { parentSuite } from 'allure-js-commons';
-import { getSessionVariable } from '../utils/controller';
+import {test} from '@playwright/test';
+import {parentSuite} from 'allure-js-commons';
+import {getSessionVariable} from '../utils/controller';
+import configData from "@config/test.config";
 import {
   initializeExecutor,
   performAction,
   performActions,
   performValidation
 } from '@utils/controller';
-import { caseData } from '@data/case.data';
+import {caseData} from '@data/case.data';
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({page}, testInfo) => {
+
   initializeExecutor(page);
-  await parentSuite('Create Case');
+  await parentSuite('Case Creation');
+  await performAction('navigateToUrl', configData.manageCasesBaseURL);
   await performAction('login', 'exuiUser');
+
+  await testInfo.attach('Page URL', {
+    body: page.url(),
+    contentType: 'text/plain',
+  });
 });
 
-test.afterEach(async () => {
+/*test.afterEach(async () => {
   await performAction('click', 'Sign out');
-});
+});*/
 
-async function createCaseWithAddress(){
+async function createCaseWithAddress() {
 
   await performAction('click', 'Create case');
 
-  await performActions(
-    'Case option selection',
-    ['select', 'Jurisdiction', caseData.jurisdiction],
-    ['select', 'Case type', caseData.caseType],
-    ['select', 'Event', caseData.event]
-  );
+  await performActions('Case option selection'
+    , ['select', 'Jurisdiction', caseData.jurisdiction]
+    , ['select', 'Case type', caseData.caseType]
+    , ['select', 'Event', caseData.event]);
 
   await performAction('click', 'Start');
 
-  await performActions(
-    'Find Address based on postcode',
-    ['fill', 'Enter a UK postcode', caseData.postcode],
-    ['click', 'Find address'],
-    ['select', 'Select an address', caseData.addressIndex],
-    ['fill', 'Address Line 2', caseData.addressLine2],
-    ['fill', 'Address Line 3', caseData.addressLine3],
-    ['fill', 'County', caseData.county]
-  );
+  await performActions('Find Address based on postcode'
+    , ['fill', 'Enter a UK postcode', caseData.postcode]
+    , ['click', 'Find address']
+    , ['select', 'Select an address', caseData.addressIndex]
+    , ['fill', 'Address Line 2', caseData.addressLine2]
+    , ['fill', 'Address Line 3', caseData.addressLine3]
+    , ['fill', 'County', caseData.county]);
 
   await performAction('click', 'Continue');
+
   await performAction('fill', "Applicant's forename", caseData.applicantFirstName);
+
   await performAction('click', 'Submit');
 
-  await performValidation('bannerAlert', {message: 'Case #.* has been created.'});
+  await performValidation("bannerAlert", {message: "Case #.* has been created."});
+  await performAction('caseNumber', 'case number');
+
 }
+
 async function searchCase(caseNumber: string) {
-  await performActions(
-    'Search for case',
-    ['select', 'Jurisdiction', caseData.jurisdiction],
-    ['select', 'Case type', caseData.caseType],
-    ['fill', 'case number', caseNumber],
-    ['click', 'Apply']
-  );
+  await performAction('select', 'Jurisdiction', caseData.jurisdiction),
+    await performAction('select', 'Case type', caseData.caseType),
+    await performAction('fill', 'Case Number', caseNumber),
+    await performAction('click', 'Apply')
 }
 
 test.describe('Search case by case number @Master @nightly', () => {
@@ -66,8 +72,9 @@ test.describe('Search case by case number @Master @nightly', () => {
     if (!caseNumber) {
       throw new Error('Case number not found in session variable');
     } else {
-      searchCase(caseNumber);
-      await performValidation('Search Case', 'case number', {caseNumber});
+      await performAction('click', 'Case list');
+      await searchCase(caseNumber);
+      await performValidation('text', 'caseNumber', {content: caseNumber});
     }
   });
   test('Search for case via find case', async ({}) => {
@@ -78,6 +85,6 @@ test.describe('Search case by case number @Master @nightly', () => {
     }
     await performAction('click', 'Find case');
     await searchCase(caseNumber);
-    await performValidation('Search Case', 'case number', {caseNumber});
+    await performValidation('text', 'caseNumber', {content: caseNumber});
   });
 });
--- src/e2eTest/utils/validations/element-validations/text.validation.ts
+++ src/e2eTest/utils/validations/element-validations/text.validation.ts
@@ -1,15 +1,20 @@
 // validations/text.validation.ts
-import { Page, expect } from '@playwright/test';
+import {Page, expect, test} from '@playwright/test';
 import {IValidation, ValidationData} from "../../interfaces/validation.interface";
 
 export class TextValidation implements IValidation {
   async validate(page: Page, fieldName: string, data: ValidationData): Promise<void> {
-    const locator = page.locator(`[data-testid="${fieldName}"]`);
-
-    if ('expected' in data) {
-      await expect(locator).toHaveText(String(data.expected));
-    } else {
-      throw new Error('TextValidation requires "expected" property in data');
+    const textLocator = page.locator(`a[aria-label$="${data.content}"]`);
+    const text = (await textLocator.innerText())?.trim();
+    if (!text) {
+      throw new Error('Table column is not present or empty.');
     }
+    await test.step(`Found number: "${text}"`, async () => {
+      if (!data) {
+        throw new Error('table column requires case number');
+      }
+      console.log(`Table column ${data.content}`);
+      expect(text, `Table column should exactly match`).toBe(data.content);
+    });
   }
 }
