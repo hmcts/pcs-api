7ee9be506149d8a25538d1f0972be9e43eac0fe3
Gautham059 <gautham.bhikkumalla@hmcts.net>
2025-07-15 13:24:08 +0100

    Validations added to validate form labels and values in a table (#353)

---
+++ src/e2eTest/utils/actions/element-actions/clickTab.action.ts
@@ -0,0 +1,9 @@
+import { Page } from '@playwright/test';
+import { IAction } from '../../interfaces/action.interface';
+
+export class ClickTabAction implements IAction {
+  async execute(page: Page, tabName: string): Promise<void> {
+    const locator = page.locator(`div.mat-tab-label .mat-tab-label-content:has-text("${tabName}")`);
+    await locator.click();
+  }
+}
