c5035c5dee9fedc792abcde949c1eafad256baf9
scottstewart-sl <scott.stewart@solirius.com>
2025-07-15 10:08:18 +0100

    HDPI-1245: Add user's PCQ ID to case persistence (#340)
    Co-authored-by: elvianihuseyin2 <elviani.huseyin2@hmcts.net>

---
+++ src/main/java/uk/gov/hmcts/reform/pcs/security/SecurityContextService.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.reform.pcs.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
+import uk.gov.hmcts.reform.pcs.exception.SecurityContextException;
+import uk.gov.hmcts.reform.pcs.idam.User;
+
+import java.util.UUID;
+
+@Service
+public class SecurityContextService {
+
+    /**
+     * Gets the current user ID from the {@link SecurityContext}.
+     * @return The user ID for the user making the current request
+     * @throws SecurityContextException if the security principal is not set or is not a {@link User} type
+     */
+    public UUID getCurrentUserId() {
+        UserInfo userDetails = getCurrentUserDetails();
+        return userDetails != null ? UUID.fromString(userDetails.getUid()) : null;
+    }
+
+    /**
+     * Gets the current user details from the {@link SecurityContext}.
+     * @return The user details for the user making the current request
+     * @throws SecurityContextException if the security principal is not set or is not a {@link User} type
+     */
+    public UserInfo getCurrentUserDetails() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null) {
+            throw new SecurityContextException("No authentication instance found");
+        }
+
+        if (authentication.getPrincipal() instanceof User user) {
+            return user.getUserDetails();
+        } else {
+            throw new SecurityContextException("Authentication principal is null or not of the expected type");
+        }
+    }
+
+}
