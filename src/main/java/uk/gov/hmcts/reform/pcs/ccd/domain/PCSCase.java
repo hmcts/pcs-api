75b96dc668d159045d8e865f78cc2a42ac042681
Liban A <liban.abdirahman@hmcts.net>
2025-07-17 19:23:42 +0100

    Removing unused import to fix checkstyle issue

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/PCSCase.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/PCSCase.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.pcs.ccd.domain;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.ToString;
 import uk.gov.hmcts.ccd.sdk.External;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.AddressUK;
