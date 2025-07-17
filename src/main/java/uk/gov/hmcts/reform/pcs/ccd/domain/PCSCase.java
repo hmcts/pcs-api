24b32944ea48e78dd7a5ac630d38167a889031f5
Liban A <liban.abdirahman@hmcts.net>
2025-07-17 19:01:51 +0100

    Removing page heading and markdown fields from logs

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/PCSCase.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/PCSCase.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.pcs.ccd.domain;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
 import uk.gov.hmcts.ccd.sdk.External;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.AddressUK;
@@ -69,8 +70,10 @@ public class PCSCase {
     )
     private PaymentType paymentType;
 
+    @ToString.Exclude
     private String pageHeadingMarkdown;
 
+    @ToString.Exclude
     private String claimPaymentTabMarkdown;
 
 }
