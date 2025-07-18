403486b03d75e436e33933ff0e49440a74a82189
arun <arun.kumar@hmcts.net>
2025-07-18 12:45:42 +0100

    HDPI-1256 renamed table from court_eligibility to eligibility_whitelisted_epim

--- src/main/resources/db/migration/V008__court_eligibility_table.sql
+++ src/main/resources/db/migration/V008__court_eligibility_table.sql
@@ -1,8 +1,8 @@
-CREATE TABLE court_eligibility (
+CREATE TABLE eligibility_whitelisted_epim (
     epims_id INT NOT NULL,
     eligible_from DATE NOT NULL,
     audit JSONB NOT NULL,
     PRIMARY KEY (epims_id)
 );
 
-CREATE INDEX idx_court_eligibility_eligible_from ON court_eligibility(eligible_from); 
+CREATE INDEX idx_eligibility_whitelisted_epim_eligible_from ON eligibility_whitelisted_epim(eligible_from); 
