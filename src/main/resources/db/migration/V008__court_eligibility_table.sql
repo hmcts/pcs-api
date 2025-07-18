e6be2fa30ca48d0d43b012953d9e079b82d7b31b
arun <arun.kumar@hmcts.net>
2025-07-18 10:21:00 +0100

    HDPI-1256 renamed epimid column to epims_id in court_eligibility table

--- src/main/resources/db/migration/V008__court_eligibility_table.sql
+++ src/main/resources/db/migration/V008__court_eligibility_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE court_eligibility (
-    epimid INT NOT NULL,
+    epims_id INT NOT NULL,
     eligible_from DATE NOT NULL,
     audit JSONB NOT NULL,
-    PRIMARY KEY (epimid)
+    PRIMARY KEY (epims_id)
 );
 
 CREATE INDEX idx_court_eligibility_eligible_from ON court_eligibility(eligible_from); 
--- src/main/resources/db/testdata/V008.1__court_eligibility_test_data.sql
+++ src/main/resources/db/testdata/V008.1__court_eligibility_test_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO court_eligibility (epimid, eligible_from, audit) VALUES
+INSERT INTO court_eligibility (epims_id, eligible_from, audit) VALUES
     (20262, '2025-07-14', '{"generated, R1, V1": "2025-07-08T10:02:39.968Z"}'::jsonb),
     (28837, '2025-07-14', '{"generated, R1, V1": "2025-07-08T10:02:39.968Z"}'::jsonb),
     (144641, '2025-07-14', '{"generated, R1, V1": "2025-07-08T10:02:39.968Z"}'::jsonb),
