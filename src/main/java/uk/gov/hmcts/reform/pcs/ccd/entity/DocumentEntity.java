b0f914491e5c0de8ea7e9a18793a99d18a570096
fudge88 <f.akhlaq@outlook.com>
2025-07-21 15:35:30 +0100

    Changed the relation from  manyToOne relationship to oneToOne

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/entity/DocumentEntity.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/entity/DocumentEntity.java
@@ -13,7 +13,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import uk.gov.hmcts.reform.pcs.ccd.entity.PcsCaseEntity;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
