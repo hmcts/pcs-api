59a047fd517b3d7f50a863c84f6f5fc3dad1b4ba
Liban A <liban.abdirahman@hmcts.net>
2025-07-21 14:57:55 +0100

    Revert "Merge remote-tracking branch 'origin' into HDPI-1277-Trigger-case-management-venue-allocation-upon-payment"
    This reverts commit 204a7924cd4296b0d15ce0ea60f8b31e68daae54, reversing
    changes made to 66b0dd15dbfbde33371267985fde0a205bfe5091.

--- src/main/java/uk/gov/hmcts/reform/pcs/postcodecourt/repository/PostCodeCourtRepository.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/postcodecourt/repository/PostCodeCourtRepository.java
@@ -2,11 +2,8 @@ package uk.gov.hmcts.reform.pcs.postcodecourt.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.pcs.postcodecourt.entity.PostCodeCourtEntity;
-import uk.gov.hmcts.reform.pcs.postcodecourt.model.LegislativeCountry;
-import uk.gov.hmcts.reform.pcs.postcodecourt.model.PostcodeCourtMapping;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -15,40 +12,11 @@ import java.util.List;
 public interface PostCodeCourtRepository extends JpaRepository<PostCodeCourtEntity, Integer> {
 
     @Query("""
-            SELECT new uk.gov.hmcts.reform.pcs.postcodecourt.model.PostcodeCourtMapping(
-                 p.id.postCode,
-                 p.legislativeCountry,
-                 p.id.epimsId,
-                 p.effectiveFrom,
-                 p.effectiveTo,
-                 e.eligibleFrom
-            )
-            FROM PostCodeCourtEntity p LEFT JOIN CourtEligibilityEntity e ON e.epimsId = p.id.epimsId
-            WHERE p.id.postCode IN :postcodes
-        """)
-    List<PostcodeCourtMapping> findByPostCodeIn(@Param("postcodes") List<String> postcodes);
-
-    @Query("""
-            SELECT new uk.gov.hmcts.reform.pcs.postcodecourt.model.PostcodeCourtMapping(
-                 p.id.postCode,
-                 p.legislativeCountry,
-                 p.id.epimsId,
-                 p.effectiveFrom,
-                 p.effectiveTo,
-                 e.eligibleFrom
-            )
-            FROM PostCodeCourtEntity p LEFT JOIN CourtEligibilityEntity e ON e.epimsId = p.id.epimsId
-            WHERE (p.legislativeCountry = :legislativeCountry) AND (p.id.postCode IN :postcodes)
-        """)
-    List<PostcodeCourtMapping> findByPostCodeIn(@Param("postcodes") List<String> postcodes,
-                                                @Param("legislativeCountry") LegislativeCountry legislativeCountry);
-
-    @Query("""
             SELECT p FROM PostCodeCourtEntity p
             WHERE p.id.postCode IN :postcodes
               AND p.effectiveFrom <= :currentDate
               AND (p.effectiveTo IS NULL OR p.effectiveTo >= :currentDate)
         """)
-    List<PostCodeCourtEntity> findActiveByPostCodeIn(List<String> postcodes, LocalDate currentDate);
+    List<PostCodeCourtEntity> findByIdPostCodeIn(List<String> postcodes, LocalDate currentDate);
 
 }
