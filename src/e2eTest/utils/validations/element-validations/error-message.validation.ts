7683b66e07fb6bc2833c9f03137fb34bf5dbbd1b
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-07-21 18:13:45 +0100

    1st commit with customat and element validations/actions.

--- src/e2eTest/utils/validations/element-validations/error-message.validation.ts
+++ src/e2eTest/utils/validations/element-validations/error-message.validation.ts
@@ -1,4 +1,3 @@
-// validations/error-message.validation.ts
 import { Page, expect } from '@playwright/test';
 import { IValidation, ValidationData } from '../../interfaces/validation.interface';
 
@@ -9,12 +8,10 @@ interface ErrorMessageValidationData extends ValidationData {
 
 export class ErrorMessageValidation implements IValidation {
   async validate(page: Page, _fieldName: string, data: ErrorMessageValidationData): Promise<void> {
-    // Validate error summary heading
-    const headingLocator = page.locator('h2.govuk-error-summary__title');
-    await expect(headingLocator).toHaveText(data.header);
 
-    // Validate link text in the error list
-    const linkLocator = page.locator('div.govuk-error-summary__body a.validation-error');
-    await expect(linkLocator).toHaveText(data.errorHasLink);
+    await expect(page.locator('h2.govuk-error-summary__title')).toHaveText(data.header);
+
+    await expect(page.locator('a.validation-error', { hasText: data.errorHasLink })).toHaveText(data.errorHasLink);
   }
 }
+
