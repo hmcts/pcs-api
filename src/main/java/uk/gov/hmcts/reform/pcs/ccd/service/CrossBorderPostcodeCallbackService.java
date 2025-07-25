18fbae8cf9e3c79dd78548143edea9b1085cc517
Andrew Ajube <andrew.ajube@hmcts.net>
2025-07-25 09:33:00 +0100

    Create CrossBorderPostcodeCallbackService for CCD integration

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/service/CrossBorderPostcodeCallbackService.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/service/CrossBorderPostcodeCallbackService.java
@@ -3,10 +3,14 @@ package uk.gov.hmcts.reform.pcs.ccd.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
 import uk.gov.hmcts.reform.pcs.ccd.exception.CrossBorderPostcodeException;
 import uk.gov.hmcts.reform.pcs.postcodecourt.model.EligibilityResult;
+import uk.gov.hmcts.reform.pcs.postcodecourt.model.EligibilityStatus;
 import uk.gov.hmcts.reform.pcs.postcodecourt.model.LegislativeCountry;
 import uk.gov.hmcts.reform.pcs.postcodecourt.service.EligibilityService;
+
+import java.util.stream.Collectors;
 
 /**
  * Service to handle cross-border postcode callback logic for CCD mid-event callbacks.
@@ -57,6 +61,42 @@ public class CrossBorderPostcodeCallbackService {
         } catch (Exception e) {
             log.error("Error validating selected country for postcode: {}", postcode, e);
             throw new CrossBorderPostcodeException("Failed to validate selected country", e);
+        }
+    }
+
+    /**
+     * Populates the crossBorderCountries field in the PCSCase with the actual countries
+     * returned from the API. This method is designed to be called from CCD mid-event callbacks.
+     *
+     * @param pcsCase The case to update
+     * @param postcode The postcode to check
+     * @return The updated PCSCase with crossBorderCountries populated
+     */
+    public PCSCase populateCrossBorderCountries(PCSCase pcsCase, String postcode) {
+        log.info("Populating cross-border countries for postcode: {}", postcode);
+        
+        try {
+            EligibilityResult result = eligibilityService.checkEligibility(postcode, null);
+            
+            if (result.getStatus() == EligibilityStatus.LEGISLATIVE_COUNTRY_REQUIRED 
+                && result.getLegislativeCountries() != null) {
+                
+                String countryList = result.getLegislativeCountries().stream()
+                    .map(LegislativeCountry::getLabel)
+                    .collect(Collectors.joining(" and "));
+                
+                pcsCase.setCrossBorderCountries(countryList);
+                log.info("Set cross-border countries for postcode {}: {}", postcode, countryList);
+            } else {
+                // Clear the field if not cross-border
+                pcsCase.setCrossBorderCountries(null);
+                log.info("Cleared cross-border countries for postcode {} (not cross-border)", postcode);
+            }
+            
+            return pcsCase;
+        } catch (Exception e) {
+            log.error("Error populating cross-border countries for postcode: {}", postcode, e);
+            throw new CrossBorderPostcodeException("Failed to populate cross-border countries", e);
         }
     }
 }
