f88bad27cf1c6c7945b008bb30370451ab9c693b
fudge88 <f.akhlaq@outlook.com>
2025-07-25 09:31:17 +0100

    HDPI-1304: POC - Document upload for evidence. saving document link to PSC_Case table.

--- src/main/resources/db/migration/V011__create_document_table.sql
+++ src/main/resources/db/migration/V011__create_document_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE documents (
     id UUID PRIMARY KEY,
     case_id UUID UNIQUE NOT NULL REFERENCES pcs_case(id),
-    file_name VARCHAR(255),
+    file_name VARCHAR(100),
     file_path VARCHAR(500),
     content_type VARCHAR(100),
     uploaded_at TIMESTAMP NOT NULL
