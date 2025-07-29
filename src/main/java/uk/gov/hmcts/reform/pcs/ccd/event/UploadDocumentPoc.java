5eb814b1f2efc43f839a6d253bf08c19e7d71a47
fudge88 <f.akhlaq@outlook.com>
2025-07-29 13:01:14 +0100

    HDPI-1304: POC - Document upload POC - added a few steps to the flow taking in user name and address.

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/UploadDocumentPoc.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/UploadDocumentPoc.java
@@ -41,7 +41,7 @@ public class UploadDocumentPoc implements CCDConfig<PCSCase, State, UserRole> {
 
     private PCSCase start(EventPayload<PCSCase, State> eventPayload) {
         PCSCase caseData = eventPayload.caseData();
-        caseData.setApplicantForename("Enter your name here");
+        caseData.setApplicantForename("Enter your name");
         return caseData;
     }
 
