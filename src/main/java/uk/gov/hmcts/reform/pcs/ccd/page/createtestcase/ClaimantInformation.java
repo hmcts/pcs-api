f9465592a015f9435ba955e0ef38ab2c6993f071
Jake Gowler <jakecgowler@gmail.com>
2025-07-30 14:23:47 +0100

    Fixing Toby's mistakes

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/createtestcase/ClaimantInformation.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/createtestcase/ClaimantInformation.java
@@ -3,8 +3,8 @@ package uk.gov.hmcts.reform.pcs.ccd.page.createtestcase;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.pcs.ccd.common.CcdPageConfiguration;
 import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
-//import uk.gov.hmcts.reform.pcs.clients.ProfessionalOrganisationRetriever;
-//import uk.gov.hmcts.reform.pcs.dto.OrganisationDto;
+// import uk.gov.hmcts.reform.pcs.clients.ProfessionalOrganisationRetriever;
+// import uk.gov.hmcts.reform.pcs.dto.OrganisationDto;
 
 @Component
 public class ClaimantInformation implements CcdPageConfiguration {
@@ -15,9 +15,9 @@ public class ClaimantInformation implements CcdPageConfiguration {
             .page("Organisation Details")
             .pageLabel("Organisation Details")
             .label("OrganisationQuestion", "Are you a part of this Organisation?");
-//            .complex(PCSCase::getOrganisationPolicy)
-//                .complex(OrganisationPolicy::getOrganisation)
-//                    .optional(Organisation::getOrganisationId)
-//        .done();
+        // .complex(PCSCase::getOrganisationPolicy)
+        // .complex(OrganisationPolicy::getOrganisation)
+        // .optional(Organisation::getOrganisationId)
+        // .done();
     }
 }
