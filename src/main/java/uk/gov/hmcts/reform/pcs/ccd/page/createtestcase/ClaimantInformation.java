d675f083688edc0400bd11169ce895609899d7df
Toby Plunkett <toby.plunkett@HMCTS.NET>
2025-07-30 15:04:04 +0100

    HDPI-1310 readding complex values

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/createtestcase/ClaimantInformation.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/createtestcase/ClaimantInformation.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.pcs.ccd.page.createtestcase;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.pcs.ccd.common.CcdPageConfiguration;
 import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
+import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
 // import uk.gov.hmcts.reform.pcs.clients.ProfessionalOrganisationRetriever;
 // import uk.gov.hmcts.reform.pcs.dto.OrganisationDto;
 
@@ -14,10 +15,10 @@ public class ClaimantInformation implements CcdPageConfiguration {
         pageBuilder
             .page("Organisation Details")
             .pageLabel("Organisation Details")
-            .label("OrganisationQuestion", "Are you a part of this Organisation?");
-        // .complex(PCSCase::getOrganisationPolicy)
+            .label("OrganisationQuestion", "Are you a part of this Organisation?")
+            .complex(PCSCase::getOrganisationPolicy)
         // .complex(OrganisationPolicy::getOrganisation)
         // .optional(Organisation::getOrganisationId)
-        // .done();
+            .done();
     }
 }
