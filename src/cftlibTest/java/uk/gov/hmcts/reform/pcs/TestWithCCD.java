d2a15c3d23473641ffed5f6f26a523fd5b3aa82c
Toby Plunkett <toby.plunkett@HMCTS.NET>
2025-07-31 17:14:04 +0100

    HDPI-1369 readd test

--- src/cftlibTest/java/uk/gov/hmcts/reform/pcs/TestWithCCD.java
+++ src/cftlibTest/java/uk/gov/hmcts/reform/pcs/TestWithCCD.java
@@ -2,13 +2,22 @@ package uk.gov.hmcts.reform.pcs;
 
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.ccd.sdk.type.AddressUK;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.ccd.client.model.Event;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.pcs.ccd.CaseType;
+import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
+import uk.gov.hmcts.reform.pcs.ccd.domain.PaymentStatus;
 import uk.gov.hmcts.rse.ccd.lib.test.CftlibTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -30,5 +39,30 @@ public class TestWithCCD extends CftlibTest {
         idamToken = idamClient.getAccessToken("caseworker@pcs.com", "password");
         s2sToken = generateDummyS2SToken("ccd_gw");
         userId = idamClient.getUserInfo(idamToken).getUid();
+    }
+
+    @Test
+    public void createsTestCase() {
+        var r = ccdApi.startCase(idamToken, s2sToken, CaseType.getCaseType(), "createTestApplication");
+        PCSCase caseData = PCSCase.builder()
+            .applicantForename("Foo")
+            .propertyAddress(AddressUK.builder()
+                                 .addressLine1("123 Baker Street")
+                                 .addressLine2("Marylebone")
+                                 .postTown("London")
+                                 .county("Greater London")
+                                 .postCode("NW1 6XE")
+                                 .build())
+            .paymentStatus(PaymentStatus.UNPAID)
+            .build();
+        var content = CaseDataContent.builder()
+            .data(caseData)
+            .event(Event.builder().id("createTestApplication").build())
+            .eventToken(r.getToken())
+            .build();
+        caseDetails = ccdApi.submitForCaseworker(idamToken, s2sToken, userId,
+                                                 "PCS", CaseType.getCaseType(), false, content
+        );
+        assertThat(caseDetails.getId()).isNotNull();
     }
 }
