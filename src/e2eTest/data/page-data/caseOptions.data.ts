2d65c6b270d1eb25b3b4db18ab02831d7188f790
ashajayaprakash <asha.jayaprakash@hmcts.net>
2025-07-31 17:36:13 +0100

    reverted change_id

--- src/e2eTest/data/page-data/caseOptions.data.ts
+++ src/e2eTest/data/page-data/caseOptions.data.ts
@@ -1,34 +1,28 @@
 export const pageIdentifier =
-{
-  title : 'Case Options',
-  mainHeader : 'some header'
-};
+    {
+        title: 'Case Options',
+        mainHeader: 'some header'
+    };
 
 export const caseOption = {
-  jurisdiction:
-    {
-      posessions: 'Possessions'
-    },
-  caseType:
-    {
-      civilPosessions: 'Civil Possessions 394'
-
-    },
-
-  // caseType:
-  //   {
-  //     civilPosessions: process.env.CHANGE_ID
-  //       ? `Civil Possessions ${process.env.CHANGE_ID}`
-  //       : 'Civil Possessions'
-  //   },
-  event:
-    {
-      makeAPosessionClaim: 'Make a claim'
-    },
+    jurisdiction:
+        {
+            posessions: 'Possessions'
+        },
+    caseType:
+        {
+            civilPosessions: process.env.CHANGE_ID
+                ? `Civil Possessions ${process.env.CHANGE_ID}`
+                : 'Civil Possessions'
+        },
+    event:
+        {
+            makeAPosessionClaim: 'Make a claim'
+        },
 
 }
 
 export const errorMessages = {
-  header: 'There is a problem',
-  errorMessage: 'An address is required',
+    header: 'There is a problem',
+    errorMessage: 'An address is required',
 }
