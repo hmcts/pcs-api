787baf17986f6cab3429fcade8484a729f69b8d1
Liban A <liban.abdirahman@hmcts.net>
2025-08-04 10:19:38 +0100

    Increased contact email column length

--- src/main/resources/db/migration/V012__contact_preferences.sql
+++ src/main/resources/db/migration/V012__contact_preferences.sql
@@ -1,6 +1,6 @@
 ALTER TABLE public.party
   ADD COLUMN contact_address_id UUID,
-  ADD COLUMN contact_email VARCHAR(50),
+  ADD COLUMN contact_email VARCHAR(100),
   ADD COLUMN contact_phone_number VARCHAR(20),
   ADD CONSTRAINT fk_contact_address
     FOREIGN KEY (contact_address_id)
