638f4d1c051c4ad956c41a016c4357fe6cadf381
Toby Plunkett <toby.plunkett@HMCTS.NET>
2025-08-04 11:24:37 +0100

    HDPI-1369 Add MidEvent

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/createtestcase/ClaimantInformation.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/createtestcase/ClaimantInformation.java
@@ -1,11 +1,17 @@
 package uk.gov.hmcts.reform.pcs.ccd.page.createtestcase;
 
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.ccd.sdk.type.Organisation;
 import uk.gov.hmcts.ccd.sdk.type.OrganisationPolicy;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.reform.pcs.ccd.common.CcdPageConfiguration;
 import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
+import uk.gov.hmcts.reform.pcs.ccd.domain.State;
+
+import java.util.List;
 
 @Component
 public class ClaimantInformation implements CcdPageConfiguration {
@@ -13,13 +19,18 @@ public class ClaimantInformation implements CcdPageConfiguration {
     @Override
     public void addTo(PageBuilder pageBuilder) {
         pageBuilder
-            .page("Organisation Details")
+            .page("Organisation Details", this::midEvent)
             .pageLabel("Organisation Details")
-            .label("OrganisationQuestion", "Your claimant anme registered with My HMCTS is:")
+            .label("OrganisationQuestion", "Your claimant name registered with My HMCTS is:")
             .complex(PCSCase::getOrganisationPolicy)
             .complex(OrganisationPolicy::getOrganisation)
-                .mandatory(Organisation::getOrganisationName)
-                .done();
-
+            .done();
     }
-}
+
+    private AboutToStartOrSubmitResponse<PCSCase, State> midEvent(CaseDetails<PCSCase, State> details,
+                                                                  CaseDetails<PCSCase, State> detailsBefore) {
+
+        return AboutToStartOrSubmitResponse.<PCSCase, State>builder()
+            .errors(List.of("You're not eligible for this online service"))
+            .build();
+    }}
