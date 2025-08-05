b700651699c05877762f3ad4410a38343d3ce12f
Toby Plunkett <toby.plunkett@HMCTS.NET>
2025-08-05 10:52:19 +0100

    HDPI-1369 Fix test

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/createtestcase/MakeAClaim.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/createtestcase/MakeAClaim.java
@@ -2,6 +2,9 @@ package uk.gov.hmcts.reform.pcs.ccd.page.createtestcase;
 
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.ccd.sdk.type.Organisation;
+import uk.gov.hmcts.ccd.sdk.type.OrganisationPolicy;
+import uk.gov.hmcts.reform.pcs.ccd.ShowConditions;
 import uk.gov.hmcts.reform.pcs.ccd.common.CcdPageConfiguration;
 import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
@@ -15,7 +18,12 @@ public class MakeAClaim implements CcdPageConfiguration {
             .page("Make a claim", this::midEvent)
             .pageLabel("What is the address of the property you're claiming possession of?")
             .label("lineSeparator", "---")
-            .mandatory(PCSCase::getPropertyAddress);
+            .mandatory(PCSCase::getPropertyAddress)
+            .complex(PCSCase::getOrganisationPolicy)
+                .complex(OrganisationPolicy::getOrganisation)
+                .readonly(Organisation::getOrganisationName, ShowConditions.NEVER_SHOW)
+                .done();
+
     }
 
     private AboutToStartOrSubmitResponse<PCSCase, State> midEvent(CaseDetails<PCSCase, State> details,
@@ -24,7 +32,6 @@ public class MakeAClaim implements CcdPageConfiguration {
         details.getData().setShortenedName(details.getData().getOrganisationPolicy()
             .getOrganisation().getOrganisationName());
 
-        return AboutToStartOrSubmitResponse.<PCSCase, State>builder()
-            .build();
+        return AboutToStartOrSubmitResponse.<PCSCase, State>builder().build();
     }
 }
