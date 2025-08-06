4f7248b0f1ab4ff2919fedcde51a0c035457a569
marianadpereira <mariana.pereira@HMCTS.NET>
2025-08-06 11:39:04 +0100

    changing grant for token generation

--- src/functionalTest/java/uk/gov/hmcts/reform/pcs/functional/config/AuthConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/pcs/functional/config/AuthConfig.java
@@ -2,7 +2,7 @@ package uk.gov.hmcts.reform.pcs.functional.config;
 
 public class AuthConfig {
     public static final String CLIENT_ID = "pcs-api";
-    public static final String GRANT_TYPE = "password";
-    public static final String SCOPE = "profile openid roles";
+    public static final String GRANT_TYPE = "client_credentials";
+    public static final String SCOPE = "profile roles";
     public static final String ENDPOINT = "/o/token";
 }
