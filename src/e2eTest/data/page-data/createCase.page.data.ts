c1b18b871b10ae08ea3fa61ffbc518cd77dc2462
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2025-08-06 17:03:52 +0100

    jenkins file updated to run tests

--- src/e2eTest/data/page-data/createCase.page.data.ts
+++ src/e2eTest/data/page-data/createCase.page.data.ts
@@ -1,25 +1,27 @@
-
 export const caseOptions = {
-    title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
-    mainHeader: 'Create Case',
-    jurisdiction:
-        {
-            posessions: 'Possessions'
-        },
-    caseType:
-        {
-            civilPosessions: process.env.CHANGE_ID
-                ? `Civil Possessions ${process.env.CHANGE_ID}`
-                : 'Civil Possessions'
-        },
-    event:
-        {
-            makeAPosessionClaim: 'Make a claim'
-        },
+  title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
+  mainHeader: 'Create Case',
+  jurisdiction:
+    {
+      posessions: 'Possessions'
+    },
+  /*caseType:
+      {
+          civilPosessions: process.env.CHANGE_ID
+              ? `Civil Possessions ${process.env.CHANGE_ID}`
+              : 'Civil Possessions'
+      },*/
+  caseType: {
+    civilPosessions: 412
+  },
+  event:
+    {
+      makeAPosessionClaim: 'Make a claim'
+    },
 
 }
 
 export const errorMessages = {
-    header: 'There is a problem',
-    errorMessage: 'An address is required',
+  header: 'There is a problem',
+  errorMessage: 'An address is required',
 }
