500ef3c497ee7f213e72b83c994cbd5962b1a4ca
Gautham059 <gautham.bhikkumalla@hmcts.net>
2025-08-08 12:46:31 +0100

    Skipping tests

--- src/e2eTest/tests/eligibilityCheck.spec.ts
+++ src/e2eTest/tests/eligibilityCheck.spec.ts
@@ -40,7 +40,8 @@ async function selectCountryRadioBtn(country: string) {
   await performAction('clickButton', borderPostcode.continue);
 }
 
-test.describe.skip('[Eligibility Checks for Cross border postcodes]  @Master @nightly', async () => {
+test.describe.skip('[Eligibility Checks for Cross border postcodes] @nightly', async () => {
+  //Skipping these tests until the postcode data insertion is handled in AAT via automation
   test('Verify cross border postcode eligibility check redirection and content for England and Wales', async ({page}) => {
       await inputAddressDetails(borderPostcode.englandWalesPostcode)
       await performValidation('text', {
