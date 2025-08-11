d2473f0bfef3446af2a52b82b9493653170486fd
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2025-08-11 15:44:21 +0100

    HDPI-1354 scripts added

--- src/e2eTest/utils/actions/element-actions/clickRadioButton.action.ts
+++ src/e2eTest/utils/actions/element-actions/clickRadioButton.action.ts
@@ -3,9 +3,9 @@ import {Page} from '@playwright/test';
 import {IAction} from '../../interfaces/action.interface';
 
 export class ClickRadioButton implements IAction {
-  async execute(page: Page, fieldName: string): Promise<void> {
-    const locator = page
-      .locator(`input[type="radio"] + label:has-text("${fieldName}")`);
-    await locator.check();
-  }
+    async execute(page: Page, fieldName: string): Promise<void> {
+        const locator = page
+            .locator(`input[type="radio"] + label:has-text("${fieldName}")`);
+        await locator.click();
+    }
 }
