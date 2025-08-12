93edeaae698897382324d45fba50d27b0bc9a028
AshaJayaprakash <asha.jayaprakash@hmcts.net>
2025-08-12 13:44:42 +0100

    Hdpi 1317 selecting type of claimant automation (#411)
    Co-authored-by: SameenaHMCTS <sameena.javeed@hmcts.net>
    Co-authored-by: marianadpereira <71711509+marianadpereira@users.noreply.github.com>
    Co-authored-by: Gautham059 <gautham.bhikkumalla@hmcts.net>

--- src/e2eTest/utils/actions/custom-actions/navigateToUrl.action.ts
+++ src/e2eTest/utils/actions/custom-actions/navigateToUrl.action.ts
@@ -1,8 +1,8 @@
 import { IAction } from '../../interfaces/action.interface';
 import {Page, test} from '@playwright/test';
 
-export class navigateToUrl implements IAction {
-  async execute(page: Page, url: string): Promise<void> {
+export class NavigateToUrl implements IAction {
+  async execute(page: Page, action: string, url: string): Promise<void> {
     await test.step(`Navigate to Manage Case URL: ${url}`, async () => {
       await page.goto(url);
     });
