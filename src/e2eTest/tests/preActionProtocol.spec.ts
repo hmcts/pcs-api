d0e741f5410210aaebe1ea6f99a99045832c3da3
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2025-08-12 09:35:32 +0100

    additional step part of 1355 is removed

--- src/e2eTest/tests/preActionProtocol.spec.ts
+++ src/e2eTest/tests/preActionProtocol.spec.ts
@@ -11,55 +11,53 @@ import {preActionProtocol} from '@data/page-data/preActionProtocol.page.data';
 import {mediationAndSettlement} from '@data/page-data/mediationAndSettlement.page.data';
 
 test.beforeEach(async ({page}) => {
-  initializeExecutor(page);
-  await parentSuite('Pre Action Protocol');
-  await performAction('navigateToUrl', configData.manageCasesBaseURL);
-  await performAction('createUserAndLogin', ['caseworker-pcs', 'caseworker']);
-  await performAction('clickButton', 'Create case');
-  await selectJurisdictionCaseTypeEvent()
+    initializeExecutor(page);
+    await parentSuite('Pre Action Protocol');
+    await performAction('navigateToUrl', configData.manageCasesBaseURL);
+    await performAction('createUserAndLogin', ['caseworker-pcs', 'caseworker']);
+    await performAction('clickButton', 'Create case');
+    await selectJurisdictionCaseTypeEvent()
 });
 
 async function selectJurisdictionCaseTypeEvent() {
-  await performActions('Case option selection'
-    , ['select', 'Jurisdiction', createCase.caseOption.jurisdiction.possessions]
-    , ['select', 'Case type', createCase.caseOption.caseType.civilPossessions]
-    , ['select', 'Event', createCase.caseOption.event.makeAPossessionClaim]);
-  await performAction('clickButton', 'Start');
+    await performActions('Case option selection'
+        , ['select', 'Jurisdiction', createCase.caseOption.jurisdiction.possessions]
+        , ['select', 'Case type', createCase.caseOption.caseType.civilPossessions]
+        , ['select', 'Event', createCase.caseOption.event.makeAPossessionClaim]);
+    await performAction('clickButton', 'Start');
 }
 
 async function inputAddressDetails(postcode: string) {
-  await performActions('Enter Address Manually'
-    , ['inputText', 'Enter a UK postcode', postcode]
-    , ['clickButton', 'Find address']
-    , ['select', 'Select an address', addressDetails.propertyAddressSection.addressIndex]
-    , ['inputText', 'Address Line 2', addressDetails.propertyAddressSection.addressLine2]
-    , ['inputText', 'Address Line 3', addressDetails.propertyAddressSection.addressLine3]
-    , ['inputText', 'County', addressDetails.propertyAddressSection.englandCounty]);
-  await performAction('clickButton', 'Continue');
+    await performActions('Enter Address Manually'
+        , ['inputText', 'Enter a UK postcode', postcode]
+        , ['clickButton', 'Find address']
+        , ['select', 'Select an address', addressDetails.propertyAddressSection.addressIndex]
+        , ['inputText', 'Address Line 2', addressDetails.propertyAddressSection.addressLine2]
+        , ['inputText', 'Address Line 3', addressDetails.propertyAddressSection.addressLine3]
+        , ['inputText', 'County', addressDetails.propertyAddressSection.englandCounty]);
+    await performAction('clickButton', 'Continue');
 }
+
 test.describe.skip('[Pre Action Protocol page] @nightly', () => {
-  test('Verify that the Pre-action Protocol page displays required pre-requisite information and user navigates to mediation and settlement page', async ({page}) => {
-    await inputAddressDetails(caseData.postcode);
-    await performAction('inputText', "Applicant's forename", applicantDetails.applicantFirstName);
-    await performAction('clickButton', 'Continue');
-    await performValidation('text', {
-      text: groundsForPossession.mainHeader,
-      elementType: 'heading'
+    test('Verify that the Pre-action Protocol page displays required pre-requisite information and user navigates to mediation and settlement page', async ({page}) => {
+        await inputAddressDetails(caseData.postcode);
+        await performAction('inputText', "Applicant's forename", applicantDetails.applicantFirstName);
+        await performAction('clickButton', 'Continue');
+        await performValidation('text', {
+            text: groundsForPossession.mainHeader,
+            elementType: 'heading'
+        });
+        await performAction('clickRadioButton', groundsForPossession.groundsForPossessionsOptions.yes);
+        await performAction('clickButton', 'Continue');
+        await performValidation('text', {
+            text: preActionProtocol.mainHeader,
+            elementType: 'heading'
+        });
+        await performAction('clickRadioButton', preActionProtocol.preActionProtocolOptions.yes);
+        await performAction('clickButton', 'Continue');
+        await performValidation('text', {
+            text: mediationAndSettlement.mainHeader,
+            elementType: 'heading'
+        });
     });
-    await performAction('clickRadioButton', groundsForPossession.groundsForPossessionsOptions.yes);
-    await performAction('clickButton', 'Continue');
-    await performValidation('text', {
-      text: preActionProtocol.mainHeader,
-      elementType: 'heading'
-    });
-    await page.waitForLoadState()
-    await performAction('clickRadioButton', preActionProtocol.preActionProtocolOptions.yes);
-    await performAction('clickButton', 'Continue');
-    await performValidation('text', {
-      text: mediationAndSettlement.mainHeader,
-      elementType: 'heading'
-    });
-    await performAction('clickRadioButton', 'Yes');
-    await performAction('clickButton', 'Continue');
-  });
 });
