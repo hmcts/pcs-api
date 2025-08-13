255c2be24826e88415df7d63aecaa35ecebbc569
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2025-08-13 17:30:14 +0100

    Possession text is corrected in groundsForPossession.page.data.ts

--- src/e2eTest/data/page-data/preActionProtocol.page.data.ts
+++ src/e2eTest/data/page-data/preActionProtocol.page.data.ts
@@ -1,6 +1,6 @@
 export const preActionProtocol = {
   title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
-  mainHeader: 'Grounds for possession',
+  mainHeader: 'Pre-action Protocol',
   preActionProtocolParagraph: 'Registered providers of social housing should follow the pre-action protocol before making a housing possession claim. You should have:',
   preActionProtocolInlineText: 'Your case could be delayed or rejected if you have not followed the pre-action protocol and completed all the steps.',
   elementTypeRadio:'radio',
