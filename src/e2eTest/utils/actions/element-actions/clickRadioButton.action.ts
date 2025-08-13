16f0504045bd63fc330e426994cf36aa6ffb02f9
ashajayaprakash <asha.jayaprakash@hmcts.net>
2025-08-13 08:49:04 +0100

    Jenkins update

--- src/e2eTest/utils/actions/element-actions/clickRadioButton.action.ts
+++ src/e2eTest/utils/actions/element-actions/clickRadioButton.action.ts
@@ -2,14 +2,6 @@ import {Page} from '@playwright/test';
 
 import {IAction} from '../../interfaces/action.interface';
 
-// export class ClickRadioButton implements IAction {
-//   async execute(page: Page, action: string, fieldName: string): Promise<void> {
-//     const locator = page
-//       .locator(`input[type="radio"] + label:has-text("${fieldName}")`);
-//     await locator.click();
-//   }
-// }
-
 export class ClickRadioButton implements IAction {
   async execute(
     page: Page,
