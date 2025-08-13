5258d963ea03e11c0a4dc93621b80ef095d35968
ashajayaprakash <asha.jayaprakash@hmcts.net>
2025-08-13 11:20:58 +0100

    added skip

--- src/e2eTest/data/page-data/createCase.page.data.ts
+++ src/e2eTest/data/page-data/createCase.page.data.ts
@@ -4,10 +4,14 @@ export const createCase =
     mainHeader: 'Create Case',
     possessionsJurisdiction: 'Possessions',
     caseType:
-        {
-          civilPossessions: process.env.CHANGE_ID
-            ? `Civil Possessions ${process.env.CHANGE_ID}`
-            : 'Civil Possessions'
-        },
+      {
+        civilPossessions: 'Civil Possessions 316'
+      },
+    // caseType:
+    //     {
+    //       civilPossessions: process.env.CHANGE_ID
+    //         ? `Civil Possessions ${process.env.CHANGE_ID}`
+    //         : 'Civil Possessions'
+    //     },
     makeAPossessionClaimEvent: 'Make a claim'
   };
