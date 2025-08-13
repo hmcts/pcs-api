6d5e1b070a8a6d8908fe24bc8b4a7144137a7cbc
ashajayaprakash <asha.jayaprakash@hmcts.net>
2025-08-13 10:59:58 +0100

    skip removed

--- src/e2eTest/tests/createCase.spec.ts
+++ src/e2eTest/tests/createCase.spec.ts
@@ -26,7 +26,7 @@ test.beforeEach(async ({ page }, testInfo) => {
   await performAction('selectJurisdictionCaseTypeEvent');
 });
 
-test.describe.skip('[Create Case Flow With Address and Claimant Type]  @Master @nightly', async () => {
+test.describe('[Create Case Flow With Address and Claimant Type]  @Master @nightly', async () => {
   test('England - Successful case creation', async () => {
     await performAction('selectAddress', {postcode: addressDetails.englandPostcode,
       addressIndex: addressDetails.addressIndex});
