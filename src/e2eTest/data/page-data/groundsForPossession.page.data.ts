d7256003358cfc98ad9b65f3e45c3a18498e90fe
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2025-08-14 09:50:49 +0100

    mainHeader is updated to reflect placeholder in groundsForPossession.page.data.ts Capital P is changed p in preActionProtocol.page.data.ts

--- src/e2eTest/data/page-data/groundsForPossession.page.data.ts
+++ src/e2eTest/data/page-data/groundsForPossession.page.data.ts
@@ -1,6 +1,6 @@
 export const groundsForPossession = {
   title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
-  mainHeader: 'Grounds for possession',
+  mainHeader: 'Grounds for possession (placeholder)',
   groundsForPossessionSectionInlineText: 'Are you claiming possession because of rent arrears or breach of the tenancy (ground1)?',
   elementTypeRadio: 'radio',
   groundsForPossessionsOptions:
