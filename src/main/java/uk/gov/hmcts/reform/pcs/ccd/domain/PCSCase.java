7de7265b47e935dea4594598b3641d9eb0e76bf0
adusumillipraveen <praveen.adusumilli@hmcts.net>
2025-08-15 17:41:09 +0100

    test doc type check

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/PCSCase.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/PCSCase.java
@@ -1,33 +1,26 @@
 package uk.gov.hmcts.reform.pcs.ccd.domain;
 
-import java.util.List;
+import static uk.gov.hmcts.ccd.sdk.type.FieldType.Collection;
+import static uk.gov.hmcts.ccd.sdk.type.FieldType.DynamicRadioList;
+import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
+import java.util.List;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.ccd.sdk.External;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.AddressUK;
+import uk.gov.hmcts.ccd.sdk.type.ComponentLauncher;
 import uk.gov.hmcts.ccd.sdk.type.Document;
 import uk.gov.hmcts.ccd.sdk.type.FieldType;
-import static uk.gov.hmcts.ccd.sdk.type.FieldType.DynamicRadioList;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
-import uk.gov.hmcts.ccd.sdk.type.ComponentLauncher;
 import uk.gov.hmcts.reform.pcs.ccd.accesscontrol.CaseworkerAccess;
 import uk.gov.hmcts.reform.pcs.ccd.accesscontrol.CitizenAccess;
-import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
 import uk.gov.hmcts.reform.pcs.ccd.type.DynamicStringList;
 import uk.gov.hmcts.reform.pcs.postcodecourt.model.LegislativeCountry;
-
-
-import java.util.List;
-
-import static uk.gov.hmcts.ccd.sdk.type.FieldType.DynamicRadioList;
-
-import static uk.gov.hmcts.ccd.sdk.type.FieldType.Collection;
 
 /**
  * The main domain model representing a possessions case.
@@ -195,7 +188,8 @@ public class PCSCase {
         label = "Supporting documents",
         typeOverride = Collection,
         typeParameterOverride = "Document",
-        access = {CitizenAccess.class, CaseworkerAccess.class}
+        access = {CitizenAccess.class, CaseworkerAccess.class},
+        regex = ".pdf"
     )
     @JsonProperty("supportingDocuments")
     private List<ListValue<Document>> supportingDocuments;
