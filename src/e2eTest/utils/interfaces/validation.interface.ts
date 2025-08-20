2081030bf3564f4e3798d6036f549523dbc0ecbb
ashajayaprakash <asha.jayaprakash@hmcts.net>
2025-08-20 10:48:48 +0100

    renamed variable

--- src/e2eTest/utils/interfaces/validation.interface.ts
+++ src/e2eTest/utils/interfaces/validation.interface.ts
@@ -1,4 +1,5 @@
 import { Page } from '@playwright/test';
+
 export type validationData = Record<string, string | number | boolean | string[] | object>;
 export interface IValidation {
   validate(page: Page, fieldName?: string | validationData, data?: string | validationData): Promise<void>;
--- src/e2eTest/utils/validations/element-validations/error-message.validation.ts
+++ src/e2eTest/utils/validations/element-validations/error-message.validation.ts
@@ -2,14 +2,14 @@ import {Page, expect} from '@playwright/test';
 import {IValidation, validationData} from '../../interfaces/validation.interface';
 
 export class ErrorMessageValidation implements IValidation {
-  async validate(page: Page, fieldName: string, headerAndMessage: string | validationData): Promise<void> {
+  async validate(page: Page, fieldName: string, error: string | validationData): Promise<void> {
     let errorMessage;
-    if (typeof headerAndMessage === 'string') {
-      errorMessage = page.locator(`a.validation-error:has-text("${fieldName}")`);
+    if (typeof error === 'string') {
+      errorMessage = page.locator(`a.validation-error:has-text("${error}")`);
     } else {
       errorMessage = page.locator(`
-        h3.error-summary-heading:has-text("${headerAndMessage.header}") + p:has-text("${headerAndMessage.message}"),
-        h3.error-summary-heading:has-text("${headerAndMessage.header}") ~ #errors li:has-text("${headerAndMessage.message}")
+        h3.error-summary-heading:has-text("${error.header}") + p:has-text("${error.message}"),
+        h3.error-summary-heading:has-text("${error.header}") ~ #errors li:has-text("${error.message}")
       `);
     }
     await expect(errorMessage).toBeVisible();
