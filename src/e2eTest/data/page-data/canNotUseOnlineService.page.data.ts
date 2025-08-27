95371e4b928eb26b8e2c4204fabc3a726e80bbc3
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2025-08-27 12:28:48 +0100

    line break is fixed

--- src/e2eTest/data/page-data/canNotUseOnlineService.page.data.ts
+++ src/e2eTest/data/page-data/canNotUseOnlineService.page.data.ts
@@ -5,8 +5,10 @@ export const canNotUseOnlineService = {
     claimsInScotlandLink: 'local sheriff court (opens in new tab)',
     claimsInNorthernIrelandLink: 'Enforcement of Judgments Office (EJO) (opens in new tab)',
     propertyPossessionsFullListLink: 'View the full list of property possessions forms (opens in a new tab)',
-    basedOnPostcodeContent: 'Based on the postcode you provided, we cannot determine which court your claim should be assigned to, so you cannot use this online service.',
-    claimsInEnglandContent: 'For other types of claims in England – fill in form N5 and the correct particulars of claim form.',
+    basedOnPostcodeContent: 'Based on the postcode you provided, we cannot determine which court your claim should be ' 
+        + 'assigned to, so you cannot use this online service.',
+    claimsInEnglandContent: 'For other types of claims in England – fill in form N5 and the correct particulars of ' 
+        + 'claim form.',
     claimsInWalesContent: 'For claims in Wales - Use form N5 Wales and the correct particulars of claim form.',
     forOtherTypesOfClaims: 'For other types of claims  – fill in form N5 and the correct particulars of claim form.',
     formN5Wales: 'Use form N5 Wales and the correct particulars of claim form.'
