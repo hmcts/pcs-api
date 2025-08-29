7e6f678d3cf69aa64bfd16c9070358ea34c6263e
ashajayaprakash <asha.jayaprakash@hmcts.net>
2025-08-29 11:01:59 +0100

    addressed review comment

--- src/e2eTest/utils/actions/element-actions/clickRadioButton.action.ts
+++ src/e2eTest/utils/actions/element-actions/clickRadioButton.action.ts
@@ -2,11 +2,7 @@ import { Page } from '@playwright/test';
 import { actionRecord, IAction } from '@utils/interfaces/action.interface';
 
 export class ClickRadioButton implements IAction {
-  async execute(
-    page: Page,
-    action: string,
-  params: string | actionRecord
-): Promise<void> {
+  async execute(page: Page, action: string, params: string | actionRecord): Promise<void> {
     const radioButton = typeof params === 'string'
       ? page.locator(`input[type="radio"] + label:has-text("${params}")`)
   : page.locator(`legend:has-text("${params.question}")`)
--- src/e2eTest/utils/actions/element-actions/uploadFile.action.ts
+++ src/e2eTest/utils/actions/element-actions/uploadFile.action.ts
@@ -3,11 +3,7 @@ import path from 'path';
 import { IAction } from '@utils/interfaces/action.interface';
 
 export class UploadFileAction implements IAction {
-  async execute(
-    page: Page,
-    action: string,
-    params: string
-  ): Promise<void> {
+  async execute(page: Page, action: string, params: string): Promise<void> {
     const fileInput = page.locator('input[type="file"].form-control.bottom-30');
     const filePath = path.resolve(__dirname, '../../../data/inputFiles', params);
     await fileInput.last().setInputFiles(filePath);
