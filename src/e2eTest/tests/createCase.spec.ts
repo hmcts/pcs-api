8ea9ae56f0e71ca0d90d26aa17987e591e98f645
Gautham059 <gautham.bhikkumalla@hmcts.net>
2025-08-29 15:40:58 +0100

    Skipping all the tests

--- src/e2eTest/tests/createCase.spec.ts
+++ src/e2eTest/tests/createCase.spec.ts
@@ -30,7 +30,7 @@ test.beforeEach(async ({page}, testInfo) => {
   await performAction('housingPossessionClaim');
 });
 
-test.describe('[Create Case Flow With Address and Claimant Type]  @Master @nightly', async () => {
+test.describe.skip('[Create Case Flow With Address and Claimant Type]  @Master @nightly', async () => {
   test('England - Successful case creation', async () => {
     await performAction('selectAddress', {
       postcode: addressDetails.englandPostcode,
