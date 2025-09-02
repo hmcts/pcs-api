4a97b5993655b2e5645715e9f2e20274a762f8a9
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-09-02 15:33:29 +0100

    Test Automation - HDPI-1429 - reverted the chnages to config

--- src/e2eTest/utils/validations/element-validations/text.validation.ts
+++ src/e2eTest/utils/validations/element-validations/text.validation.ts
@@ -3,7 +3,6 @@ import {IValidation, validationData} from "../../interfaces/validation.interface
 
 export class TextValidation implements IValidation {
   async validate(page: Page, fieldName: string, data: validationData): Promise<void> {
-    let locator;
     switch (data.elementType) {
       case 'link':
         data.elementType = 'a';
@@ -14,11 +13,6 @@ export class TextValidation implements IValidation {
       case 'heading':
         data.elementType = 'h1.govuk-heading-l';
         break;
-      case 'subHeader':
-        locator = page
-          .locator('h2, h2.govuk-heading-l, h2.govuk-heading-m')
-          .filter({ hasText: String(data.text) });
-        break;
       case 'paragraph':
         data.elementType = 'p';
         break;
@@ -28,10 +22,7 @@ export class TextValidation implements IValidation {
       case 'listItem':
         data.elementType = 'li';
     }
-    if (!locator) {
-      locator = page.locator(`${data.elementType}:has-text("${data.text}")`).first();
-    }
-
+    const locator = page.locator(`${data.elementType}:has-text("${data.text}")`).first()
     await expect(locator).toHaveText(String(data.text));
   }
 }
