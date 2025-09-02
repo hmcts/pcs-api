eac250c995c5bb58c4cb2c7343a1000be15abf2b
Fahra Akhlaq <f.akhlaq@outlook.com>
2025-09-02 11:50:24 +0100

    added list in import

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/PCSCase.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/PCSCase.java
@@ -29,6 +29,7 @@ import uk.gov.hmcts.reform.pcs.postcodecourt.model.LegislativeCountry;
 
 
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.Collection;
+import java.util.List;
 
 /**
  * The main domain model representing a possessions case.
