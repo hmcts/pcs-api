5068912c902ffe740d6ff871761b04f241e22e67
Jake Gowler <jakecgowler@gmail.com>
2025-09-03 11:22:18 +0100

    HDPI-1728 - Enabling swagger publish

---
+++ src/test/java/uk/gov/hmcts/reform/pcs/config/SwaggerGeneratorTest.java
@@ -0,0 +1,46 @@
+package uk.gov.hmcts.reform.pcs.config;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Built-in feature which saves service's swagger specs in temporary directory.
+ * Each travis run on master should automatically save and upload (if updated) documentation.
+ */
+@SpringJUnitWebConfig
+@SpringBootTest
+@AutoConfigureMockMvc
+class SwaggerGeneratorTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @DisplayName("Generate swagger documentation")
+    @Test
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    void generateDocs() throws Exception {
+        byte[] specs = mvc.perform(MockMvcRequestBuilders.get("/v3/api-docs")
+                                        .accept(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsByteArray();
+
+        try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/pcs-api.json"))) {
+            outputStream.write(specs);
+        }
+
+    }
+}
