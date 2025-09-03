e66b9d56baa522f21eb0ad944a7a826f35eb1f29
ashajayaprakash <asha.jayaprakash@hmcts.net>
2025-09-03 11:15:12 +0100

    temporary postcode and jenkins updates

--- src/e2eTest/tests/createCase.spec.ts
+++ src/e2eTest/tests/createCase.spec.ts
@@ -33,7 +33,7 @@ test.beforeEach(async ({page}, testInfo) => {
   await performAction('housingPossessionClaim');
 });
 
-test.describe.skip('[Create Case Flow With Address and Claimant Type]  @Master @nightly', async () => {
+test.describe('[Create Case Flow With Address and Claimant Type]  @Master @nightly', async () => {
   test('England - Successful case creation', async () => {
     await performAction('selectAddress', {
       postcode: addressDetails.englandPostcode,
@@ -131,7 +131,7 @@ test.describe.skip('[Create Case Flow With Address and Claimant Type]  @Master @
       ['formLabelValue', 'Country', addressDetails.country]);
   });
 
-  test('England - Unsuccessful case creation journey due to claimant type not in scope of Release1 @R1only', async () => {
+  test.skip('England - Unsuccessful case creation journey due to claimant type not in scope of Release1 @R1only', async () => {
     await performAction('selectAddress', {
       postcode: addressDetails.englandPostcode,
       addressIndex: addressDetails.addressIndex
@@ -149,7 +149,7 @@ test.describe.skip('[Create Case Flow With Address and Claimant Type]  @Master @
     await performAction('clickButton', 'Cancel');
   });
 
-  test('Wales - Unsuccessful case creation journey due to claimant type not in scope of Release1 @R1only', async () => {
+  test.skip('Wales - Unsuccessful case creation journey due to claimant type not in scope of Release1 @R1only', async () => {
     await performAction('selectAddress', {
       postcode: addressDetails.walesPostcode,
       addressIndex: addressDetails.addressIndex
@@ -167,7 +167,7 @@ test.describe.skip('[Create Case Flow With Address and Claimant Type]  @Master @
     await performAction('clickButton', 'Cancel');
   });
 
-  test('Unsuccessful case creation journey due to claim type not in scope of Release1 @R1only', async () => {
+  test.skip('Unsuccessful case creation journey due to claim type not in scope of Release1 @R1only', async () => {
     await performAction('selectAddress', {
       postcode: addressDetails.englandPostcode,
       addressIndex: addressDetails.addressIndex
@@ -186,41 +186,44 @@ test.describe.skip('[Create Case Flow With Address and Claimant Type]  @Master @
     await performAction('clickButton', 'Cancel');
   });
 
-    test('Defendant 1\'s correspondence address is not known', async () => {
-      await performAction('enterTestAddressManually');
-      await performAction('selectLegislativeCountry', legislativeCountry.wales);
-      await performAction('selectClaimantType', claimantType.registeredCommunityLandlord);
-      await performAction('selectClaimType', claimType.no);
-      await performAction('selectClaimantName', claimantName.yes);
-      await performAction('selectContactPreferences', {
-        notifications: contactPreferences.yes,
-        correspondenceAddress: contactPreferences.yes,
-        phoneNumber: contactPreferences.no
-      });
-      await performAction('defendantDetails', {
-        name: defendantDetails.no,
-        correspondenceAddress: defendantDetails.no,
-        email: defendantDetails.no,
-      });
-      await performValidation('mainHeader', groundsForPossession.mainHeader);
-      await performAction('selectGroundsForPossission', groundsForPossession.yes);
-      await performAction('selectPreActionProtocol', preActionProtocol.yes);
-      await performAction('selectMediationAndSettlement', {
-        attemptedMediationWithDefendantsOption: mediationAndSettlement.yes,
-        settlementWithDefendantsOption: mediationAndSettlement.no,
-      });
-      await performValidation('mainHeader', checkingNotice.mainHeader);
-      await performAction('selectNoticeOfYourIntention', checkingNotice.no);
-      await performValidation('mainHeader', rentDetails.mainHeader);
-      await performAction('clickButton', rentDetails.continue);
-      await performAction('clickButton', 'Save and continue');
-      await performValidation('bannerAlert', 'Case #.* has been created.');
-      await performAction('clickTab', 'Property Details');
-      await performValidations('address information entered',
-        ['formLabelValue', 'Building and Street', addressDetails.buildingAndStreet],
-        ['formLabelValue', 'Address Line 2', addressDetails.addressLine2],
-        ['formLabelValue', 'Town or City', addressDetails.townOrCity],
-        ['formLabelValue', 'Postcode/Zipcode', addressDetails.postcode],
-        ['formLabelValue', 'Country', addressDetails.country]);
+  test('Defendant 1\'s correspondence address is not known', async () => {
+    await performAction('enterTestAddressManually');
+    await performAction('selectLegislativeCountry', legislativeCountry.wales);
+    await performAction('selectClaimantType', claimantType.registeredCommunityLandlord);
+    await performAction('selectClaimType', claimType.no);
+    await performAction('selectClaimantName', claimantName.yes);
+    await performAction('selectContactPreferences', {
+      notifications: contactPreferences.yes,
+      correspondenceAddress: contactPreferences.yes,
+      phoneNumber: contactPreferences.no
     });
+    await performAction('defendantDetails', {
+      name: defendantDetails.no,
+      correspondenceAddress: defendantDetails.no,
+      email: defendantDetails.no,
+    });
+    await performAction('selectTenancyOrLicenceDetails', {
+      tenancyOrLicenceType: tenancyLicenceDetails.assuredTenancy});
+    await performValidation('mainHeader', groundsForPossession.mainHeader);
+    await performAction('selectGroundsForPossission', groundsForPossession.yes);
+    await performAction('selectPreActionProtocol', preActionProtocol.yes);
+    await performAction('selectMediationAndSettlement', {
+      attemptedMediationWithDefendantsOption: mediationAndSettlement.yes,
+      settlementWithDefendantsOption: mediationAndSettlement.no,
+    });
+
+    await performValidation('mainHeader', checkingNotice.mainHeader);
+    await performAction('selectNoticeOfYourIntention', checkingNotice.no);
+    await performValidation('mainHeader', rentDetails.mainHeader);
+    await performAction('clickButton', rentDetails.continue);
+    await performAction('clickButton', 'Save and continue');
+    await performValidation('bannerAlert', 'Case #.* has been created.');
+    await performAction('clickTab', 'Property Details');
+    await performValidations('address information entered',
+      ['formLabelValue', 'Building and Street', addressDetails.buildingAndStreet],
+      ['formLabelValue', 'Address Line 2', addressDetails.addressLine2],
+      ['formLabelValue', 'Town or City', addressDetails.townOrCity],
+      ['formLabelValue', 'Postcode/Zipcode', addressDetails.postcode],
+      ['formLabelValue', 'Country', addressDetails.country]);
+  });
 });
