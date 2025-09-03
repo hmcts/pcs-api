bf035021d6ce475ee966f3549f534b6990910d71
Gautham059 <gautham.bhikkumalla@hmcts.net>
2025-09-03 09:51:54 +0100

    Support re-login of an IDAM user for e2e tests (#514)
    Co-authored-by: SameenaHMCTS <sameena.javeed@hmcts.net>

--- src/e2eTest/tests/createCase.spec.ts
+++ src/e2eTest/tests/createCase.spec.ts
@@ -21,7 +21,7 @@ test.beforeEach(async ({page}, testInfo) => {
   initializeExecutor(page);
   await parentSuite('Case Creation');
   await performAction('navigateToUrl', configData.manageCasesBaseURL);
-  await performAction('createUserAndLogin', ['caseworker-pcs', 'caseworker']);
+  await performAction('createUserAndLogin', 'claimant', ['caseworker-pcs', 'caseworker']);
   await testInfo.attach('Page URL', {
     body: page.url(),
     contentType: 'text/plain',
