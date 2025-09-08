78c2bc116a4bde146dada98207f855c82a421c2f
arun <arun.kumar@hmcts.net>
2025-09-08 14:18:19 +0100

    Fix HTML rendering issue in RentArrears page
    - Remove section tabindex wrappers that were causing HTML to display as raw text
    - Keep core HTML elements (paragraphs, lists, headings) for proper rendering
    - Maintain GOV.UK styling classes for consistent appearance
    - Ensure HTML content renders properly in browser instead of showing as plain text
    - Fix affects rent statement help content, headings, and third-party payments sections

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/createpossessionclaim/RentArrears.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/createpossessionclaim/RentArrears.java
@@ -17,30 +17,26 @@ public class RentArrears implements CcdPageConfiguration {
                 .label("rentArrears-rentStatement-separator", "---")
                 .label("rentArrears-rentStatement-heading",
                         """
-                        <section tabindex="0">
-                            <h2 class="govuk-heading-m">Rent statement</h2>
-                        </section>
+                        <h2 class="govuk-heading-m">Rent statement</h2>
                         """)
                 .label("rentArrears-rentStatement-help",
                         """
-                        <section tabindex="0">
-                            <p class="govuk-body govuk-!-margin-bottom-2"><strong>Upload the rent statement</strong></p>
+                        <p class="govuk-body govuk-!-margin-bottom-2"><strong>Upload the rent statement</strong></p>
 
-                            <p class="govuk-body">The rent statement must show:</p>
-                            <ul class="govuk-list govuk-list--bullet">
-                              <li>every date when a payment was supposed to be made</li>
-                              <li>the amount that was due on each of those dates</li>
-                              <li>the actual payments that were made, and when they were made</li>
-                              <li>the total rent arrears</li>
-                            </ul>
+                        <p class="govuk-body">The rent statement must show:</p>
+                        <ul class="govuk-list govuk-list--bullet">
+                          <li>every date when a payment was supposed to be made</li>
+                          <li>the amount that was due on each of those dates</li>
+                          <li>the actual payments that were made, and when they were made</li>
+                          <li>the total rent arrears</li>
+                        </ul>
 
-                            <p class="govuk-body">It must cover the time period of either:</p>
-                            <ul class="govuk-list govuk-list--bullet">
-                              <li>from the first date the defendants missed a payment, or</li>
-                              <li>the last two years of payments, if the first date of their missed payment was more 
-                              than two years ago</li>
-                            </ul>
-                        </section>
+                        <p class="govuk-body">It must cover the time period of either:</p>
+                        <ul class="govuk-list govuk-list--bullet">
+                          <li>from the first date the defendants missed a payment, or</li>
+                          <li>the last two years of payments, if the first date of their missed payment was more 
+                          than two years ago</li>
+                        </ul>
                         """)
                 .optional(PCSCase::getRentStatementDocuments)
 
@@ -48,11 +44,9 @@ public class RentArrears implements CcdPageConfiguration {
                 .label("rentArrears-totalArrears-separator", "---")
                 .label("rentArrears-totalArrears-heading",
                         """
-                        <section tabindex="0">
-                            <h2 class="govuk-heading-m govuk-!-margin-bottom-0">Rent arrears</h2>
-                            <h3 class="govuk-heading-s govuk-!-margin-top-0 govuk-!-margin-bottom-0">
-                            How much are the total rent arrears as shown on the rent statement?</h3>
-                        </section>
+                        <h2 class="govuk-heading-m govuk-!-margin-bottom-0">Rent arrears</h2>
+                        <h3 class="govuk-heading-s govuk-!-margin-top-0 govuk-!-margin-bottom-0">
+                        How much are the total rent arrears as shown on the rent statement?</h3>
                         """)
                 .mandatory(PCSCase::getTotalRentArrears)
 
@@ -60,20 +54,16 @@ public class RentArrears implements CcdPageConfiguration {
                 .label("rentArrears-thirdPartyPayments-separator", "---")
                 .label("rentArrears-thirdPartyPayments-heading",
                         """
-                        <section tabindex="0">
-                            <h2 class="govuk-heading-m govuk-!-margin-bottom-0">For the period shown on the rent
-                            statement, have any rent payments been paid by someone other than the defendants?</h2>
-                        </section>
+                        <h2 class="govuk-heading-m govuk-!-margin-bottom-0">For the period shown on the rent
+                        statement, have any rent payments been paid by someone other than the defendants?</h2>
                         """)
                 .mandatory(PCSCase::getThirdPartyPayments)
 
                 // Sources (select all that apply) + hint
                 .label("rentArrears-thirdPartyPayments-sources",
                         """
-                        <section tabindex="0">
-                            <h3 class="govuk-heading-s govuk-!-margin-bottom-1">Where have the payments come from?</h3>
-                            <div class="govuk-hint govuk-!-margin-bottom-2">Select all that apply</div>
-                        </section>
+                        <h3 class="govuk-heading-s govuk-!-margin-bottom-1">Where have the payments come from?</h3>
+                        <div class="govuk-hint govuk-!-margin-bottom-2">Select all that apply</div>
                         """,
                         "thirdPartyPayments=\"YES\"")
                 .optional(PCSCase::getThirdPartyPaymentSources, "thirdPartyPayments=\"YES\"")
