3769c14f8c173d6306bcb0f8b021ca32cb97ae40
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-09-09 20:33:08 +0100

    Updated as per latest Inputtext

--- src/e2eTest/utils/actions/element-actions/clickTab.action.ts
+++ src/e2eTest/utils/actions/element-actions/clickTab.action.ts
@@ -1,17 +1,9 @@
 import { Page } from '@playwright/test';
-import { actionRecord, IAction} from '../../interfaces/action.interface';
+import { IAction } from '../../interfaces/action.interface';
 
-// export class ClickTabAction implements IAction {
-//   async execute(page: Page, action: string, tabName: string): Promise<void> {
-//     const locator =     page.locator(`xpath=//h2[normalize-space()="${tabName}"]/ancestor::ccd-field-read/following::ccd-field-write[1]//textarea[not(@disabled) and normalize-space()]`).fill(reasonsForPossession.detailsAboutYourReason);
-//
-//   }
-// }
 export class ClickTabAction implements IAction {
-  async execute(page: Page, action: string, tabName: actionRecord): Promise<void> {
-    const textarea = page
-      .locator(`fieldset:has(h2:text-is("${tabName.text}")) textarea:visible:enabled`).nth(Number(tabName.index));
-    await textarea.fill(tabName.testReason);
+  async execute(page: Page, action: string, tabName: string): Promise<void> {
+    const locator = page.locator(`div.mat-tab-label .mat-tab-label-content:has-text("${tabName}")`);
+    await locator.click();
   }
 }
-
