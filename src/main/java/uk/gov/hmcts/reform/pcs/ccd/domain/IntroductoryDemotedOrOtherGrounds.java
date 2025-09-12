46f7050764b455f63f0c9f7c43f3fa1bb81d4e93
guygrewal77 <163517291+guygrewal77@users.noreply.github.com>
2025-09-12 14:46:05 +0100

    HDPI-1657 Fixed checkstyle warnings

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/IntroductoryDemotedOrOtherGrounds.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/IntroductoryDemotedOrOtherGrounds.java
@@ -8,11 +8,11 @@ import uk.gov.hmcts.ccd.sdk.api.HasLabel;
 @Getter
 public enum IntroductoryDemotedOrOtherGrounds implements HasLabel {
 
-		RENT_ARREARS("Rent Arrears"),
-		ANTI_SOCIAL("Antisocial behaviour"),
-		TENANCY_BREACH("Breach of the tenancy"),
-		ABSOLUTE_GROUNDS("Absolute grounds"),
-		OTHER("Other");
+    RENT_ARREARS("Rent Arrears"),
+    ANTI_SOCIAL("Antisocial behaviour"),
+    TENANCY_BREACH("Breach of the tenancy"),
+    ABSOLUTE_GROUNDS("Absolute grounds"),
+    OTHER("Other");
 
-		private final String label;
+    private final String label;
 }
