53086c757eaabc2b53509b4ab770ad2804206815
Gautham059 <gautham.bhikkumalla@hmcts.net>
2025-09-12 11:09:43 +0100

    Work in progress

--- src/e2eTest/utils/actions/element-actions/inputText.action.ts
+++ src/e2eTest/utils/actions/element-actions/inputText.action.ts
@@ -2,11 +2,7 @@ import {Page} from '@playwright/test';
 import {IAction} from '../../interfaces/action.interface';
 
 export class InputTextAction implements IAction {
-  async execute(page: Page, action: string, fieldName: string, value?: string): Promise<void> {
-    if (!value) {
-      throw new Error('inputText action requires a value');
-    }
-
+  async execute(page: Page, action: string, fieldName: string, value: string): Promise<void> {
     const locator = page.locator(`:has-text("${fieldName}") + input,
            label:has-text("${fieldName}") ~ textarea,
            label:has-text("${fieldName}") ~ input,
