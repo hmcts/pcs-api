0b3e6dd4445a5eb5b901e66b5634b9da796a8b88
Aqib <aqib.hussain@solirius.com>
2025-09-15 18:31:31 +0100

    Updated script

--- src/main/resources/db/migration/V019__claim_ground.sql
+++ src/main/resources/db/migration/V019__claim_ground.sql
@@ -2,5 +2,5 @@ CREATE TABLE claim_ground (
     id UUID PRIMARY KEY,
     claim_id UUID NOT NULL REFERENCES claim (id),
     grounds_id VARCHAR(80) NOT NULL,
-    claims_reason_text VARCHAR(255)
+    claims_reason_text VARCHAR(500)
 );
