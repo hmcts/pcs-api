e85e88b3394b0b3eade8f138b9671faadc0b51ea
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-09-16 14:42:20 +0100

    Updated the enterReasonForPossession as per the review comment.

--- src/e2eTest/data/page-data/createCase.page.data.ts
+++ src/e2eTest/data/page-data/createCase.page.data.ts
@@ -4,10 +4,14 @@ export const createCase =
     mainHeader: 'Create Case',
     possessionsJurisdiction: 'Possessions',
     caseType:
+    // {
+    //   civilPossessions: process.env.CHANGE_ID
+    //     ? `Civil Possessions ${process.env.CHANGE_ID}`
+    //     : 'Civil Possessions'
+    // },
       {
-        civilPossessions: process.env.CHANGE_ID
-          ? `Civil Possessions ${process.env.CHANGE_ID}`
-          : 'Civil Possessions'
+        civilPossessions:
+          'Civil Possessions 541'
       },
     makeAPossessionClaimEvent: 'Make a claim'
   };
