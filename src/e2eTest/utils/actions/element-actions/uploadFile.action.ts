460dea2c7c2597a44f170e1b636a4293fa7ccf45
madhavi1203 <Madhavi.Atmakuri1@HMCTS.NET>
2025-09-18 08:49:16 +0100

    HDPI-1542-QA: Details of rent arrears (#568)

--- src/e2eTest/utils/actions/element-actions/uploadFile.action.ts
+++ src/e2eTest/utils/actions/element-actions/uploadFile.action.ts
@@ -1,11 +1,23 @@
-import { Page } from '@playwright/test';
+import {Page} from '@playwright/test';
 import path from 'path';
-import { IAction } from '@utils/interfaces/action.interface';
+import {actionData, IAction} from '@utils/interfaces/action.interface';
+import {performAction} from '@utils/controller';
 
 export class UploadFileAction implements IAction {
-  async execute(page: Page, action: string, params: string): Promise<void> {
+  async execute(page: Page, action: string, files: actionData): Promise<void> {
+    if (typeof files == 'string') {
+      await this.uploadFile(page, files);
+    } else if (Array.isArray(files)) {
+      for (const file of files) {
+        await this.uploadFile(page, file);
+      }
+    }
+  }
+
+  private async uploadFile(page: Page, file: string): Promise<void> {
+    await performAction('clickButton', 'Add new');
     const fileInput = page.locator('input[type="file"].form-control.bottom-30');
-    const filePath = path.resolve(__dirname, '../../../data/inputFiles', params);
+    const filePath = path.resolve(__dirname, '../../../data/inputFiles', file);
     await fileInput.last().setInputFiles(filePath);
   }
 }
