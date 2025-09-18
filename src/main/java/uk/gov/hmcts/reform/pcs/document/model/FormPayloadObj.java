00a5eee24fa6eea7012b51e37b3ee585a4379d95
fudge88 <f.akhlaq@outlook.com>
2025-09-18 10:55:20 +0100

    HDPI-1648: amended formpayloadobj bug

--- src/main/java/uk/gov/hmcts/reform/pcs/document/model/FormPayloadObj.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/document/model/FormPayloadObj.java
@@ -1,16 +1,13 @@
 package uk.gov.hmcts.reform.pcs.document.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.docassembly.domain.FormPayload;
 
+
 @Data
 @Component
 public class FormPayloadObj implements FormPayload {
-    @JsonProperty("applicantName")
     private String applicantName;
-
-    @JsonProperty("caseNumber")
     private String caseNumber;
 }
