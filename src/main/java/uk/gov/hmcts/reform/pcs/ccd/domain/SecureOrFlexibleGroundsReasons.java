24137923c40b7dcdb3ac9bac9e3b72ebf406dbcc
Liban A <liban.abdirahman@hmcts.net>
2025-09-22 08:49:00 +0100

    Making mandatory error more descriptive by adding grounds to the labels

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/SecureOrFlexibleGroundsReasons.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/SecureOrFlexibleGroundsReasons.java
@@ -17,7 +17,7 @@ import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
 public class SecureOrFlexibleGroundsReasons {
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Breach of the tenancy (ground 1))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -26,7 +26,8 @@ public class SecureOrFlexibleGroundsReasons {
     private String breachOfTenancyGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Nuisance, annoyance, "
+                + "illegal or immoral use of the property (ground 2))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -35,7 +36,7 @@ public class SecureOrFlexibleGroundsReasons {
     private String nuisanceOrImmoralUseGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Domestic violence (ground 2A))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -44,7 +45,7 @@ public class SecureOrFlexibleGroundsReasons {
     private String domesticViolenceGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Offence during a riot (ground 2ZA))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -53,7 +54,8 @@ public class SecureOrFlexibleGroundsReasons {
     private String riotOffenceGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Deterioration in the condition "
+                + "of the property (ground 3))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -62,7 +64,7 @@ public class SecureOrFlexibleGroundsReasons {
     private String propertyDeteriorationGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Deterioration of furniture (ground 4))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -71,7 +73,7 @@ public class SecureOrFlexibleGroundsReasons {
     private String furnitureDeteriorationGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Tenancy obtained by false statement (ground 5))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -80,7 +82,8 @@ public class SecureOrFlexibleGroundsReasons {
     private String tenancyByFalseStatementGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Premium paid in connection "
+                + "with mutual exchange (ground 6))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -89,7 +92,7 @@ public class SecureOrFlexibleGroundsReasons {
     private String premiumMutualExchangeGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Unreasonable conduct in tied accommodation (ground 7))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -98,7 +101,8 @@ public class SecureOrFlexibleGroundsReasons {
     private String unreasonableConductGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Refusal to move back to main"
+                + " home after works completed (ground 8))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -107,7 +111,8 @@ public class SecureOrFlexibleGroundsReasons {
     private String refusalToMoveBackGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Tied accommodation needed for another "
+                + "employee (ground 12))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -116,7 +121,7 @@ public class SecureOrFlexibleGroundsReasons {
     private String tiedAccommodationGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Adapted accommodation (ground 13))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -125,7 +130,8 @@ public class SecureOrFlexibleGroundsReasons {
     private String adaptedAccommodationGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Housing association special circumstances"
+                + " accommodation (ground 14))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -134,7 +140,7 @@ public class SecureOrFlexibleGroundsReasons {
     private String housingAssocSpecialGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Special needs accommodation (ground 15))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -143,7 +149,7 @@ public class SecureOrFlexibleGroundsReasons {
     private String specialNeedsAccommodationGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Under occupying after succession (ground 15A))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -152,7 +158,7 @@ public class SecureOrFlexibleGroundsReasons {
     private String underOccupancySuccessionGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Antisocial behaviour)",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -161,7 +167,7 @@ public class SecureOrFlexibleGroundsReasons {
     private String antiSocialGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Overcrowding (ground 9))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -170,7 +176,7 @@ public class SecureOrFlexibleGroundsReasons {
     private String overcrowdingGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Landlord's works (ground 10))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -179,7 +185,7 @@ public class SecureOrFlexibleGroundsReasons {
     private String landlordWorksGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Property sold for redevelopment (ground 10A))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
@@ -188,7 +194,7 @@ public class SecureOrFlexibleGroundsReasons {
     private String propertySoldGround;
 
     @CCD(
-        label = "Give details about your reason for possession",
+        label = "Give details about your reason for possession (Charitable landlords (ground 11))",
         hint = "You'll be able to upload documents to support or further explain your reasons later on",
         typeOverride = TextArea,
         max = 500,
