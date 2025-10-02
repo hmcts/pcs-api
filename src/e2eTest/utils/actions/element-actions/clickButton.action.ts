24fa3ff4ec5e0bc4d7bf491483dccf9a6fde0e45
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-10-02 17:47:45 +0100

    Updated with different scenarios

--- src/e2eTest/utils/actions/element-actions/clickButton.action.ts
+++ src/e2eTest/utils/actions/element-actions/clickButton.action.ts
@@ -3,11 +3,17 @@ import { IAction } from '../../interfaces/action.interface';
 
 export class ClickButtonAction implements IAction {
   async execute(page: Page, action: string, fieldName: string): Promise<void> {
-    const locator = page.locator(`button:text-is("${fieldName}"),
-                                  [value="${fieldName}"],
-                                  :has-text("${fieldName}") + button,
-                                  :has-text("${fieldName}") ~ button,
-                                  a >> text=${fieldName}`);
-    await locator.first().click();
+    const buttonLocator = page.locator(
+      `button:text-is("${fieldName}"),
+       [value="${fieldName}"],
+       :has-text("${fieldName}") + button,
+       :has-text("${fieldName}") ~ button,
+       a >> text=${fieldName}`
+    );
+
+    await Promise.all([
+      buttonLocator.first().click(),
+      page.waitForURL('**', { waitUntil: 'networkidle' }).catch(() => {}),
+    ]);
   }
 }
