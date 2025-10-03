16404c286a7fcfc78b0c16091b4959e155bef20e
“elvianihuseyin2” <elviani.huseyin2@hmcts.net>
2025-10-03 16:20:07 +0100

    HDPI-1917: fix unit test

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/resumepossessionclaim/AlternativesToPossessionOptions.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/resumepossessionclaim/AlternativesToPossessionOptions.java
@@ -68,10 +68,14 @@ public class AlternativesToPossessionOptions implements CcdPageConfiguration {
         boolean showDemotionPage = altToPossessions.contains(AlternativesToPossession.DEMOTION_OF_TENANCY)
             && !altToPossessions.contains(AlternativesToPossession.SUSPENSION_OF_RIGHT_TO_BUY);
 
-        caseData.getSuspensionOfRightToBuy()
-            .setShowSuspensionOfRightToBuyHousingActsPage(YesOrNo.from(showSuspensionPage));
-        caseData.getDemotionOfTenancy()
-            .setShowDemotionOfTenancyHousingActsPage(YesOrNo.from(showDemotionPage));
+        if (caseData.getSuspensionOfRightToBuy() != null) {
+            caseData.getSuspensionOfRightToBuy()
+                .setShowSuspensionOfRightToBuyHousingActsPage(YesOrNo.from(showSuspensionPage));
+        }
+        if (caseData.getDemotionOfTenancy() != null) {
+            caseData.getDemotionOfTenancy()
+                .setShowDemotionOfTenancyHousingActsPage(YesOrNo.from(showDemotionPage));
+        }
 
         return AboutToStartOrSubmitResponse.<PCSCase, State>builder()
             .data(caseData)
