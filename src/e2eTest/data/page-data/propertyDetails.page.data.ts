dc50d06e2e8af7ab4fbf883fb0d59c1f193ecbcd
Lovelin <lovelin.marcus@hmcts.net>
2025-10-03 13:38:14 +0100

    HDPI-2212-Refactor-createCase.action.ts (#679)

---
+++ src/e2eTest/data/page-data/propertyDetails.page.data.ts
@@ -0,0 +1,8 @@
+export const propertyDetails = {
+  title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
+  buildingAndStreetLabel: 'Building and Street',
+  addressLine2Label: 'Address Line 2',
+  townOrCityLabel: 'Town or City',
+  postcodeZipcodeLabel: 'Postcode/Zipcode',
+  countryLabel: 'Country'
+};
