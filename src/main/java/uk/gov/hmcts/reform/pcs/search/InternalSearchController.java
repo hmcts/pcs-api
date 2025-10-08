434905207954d08e9fcf47d16c8b26a5b285b336
Toby Plunkett <toby.plunkett@HMCTS.NET>
2025-10-08 10:02:42 +0100

    HDPI-1962 - checkstyle

--- src/main/java/uk/gov/hmcts/reform/pcs/search/InternalSearchController.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/search/InternalSearchController.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
