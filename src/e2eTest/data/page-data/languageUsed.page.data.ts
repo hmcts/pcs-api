4f5d5b37c850c40e5a017b3bdd24ae3952163693
Gautham059 <gautham.bhikkumalla@hmcts.net>
2025-10-08 15:59:17 +0100

    Changed title

--- src/e2eTest/data/page-data/languageUsed.page.data.ts
+++ src/e2eTest/data/page-data/languageUsed.page.data.ts
@@ -1,6 +1,6 @@
 export const languageUsed = {
   title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
-  mainHeader: 'Additional reasons for possession',
+  mainHeader: 'Language used',
   no: 'No',
   continue: 'Continue',
 };
