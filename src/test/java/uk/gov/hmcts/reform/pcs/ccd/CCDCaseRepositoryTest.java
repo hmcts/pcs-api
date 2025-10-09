739eab88340d2b3426b25a001ff13721d7556d8e
ashajayaprakash <asha.jayaprakash@hmcts.net>
2025-10-09 08:58:21 +0100

    undo accidental change

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/CCDCaseRepositoryTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/CCDCaseRepositoryTest.java
@@ -120,15 +120,6 @@ class CCDCaseRepositoryTest {
     }
 
     @Test
-    void shouldSetPageHeadingMarkdownWhenCaseIsRetrieved() {
-        // When
-        PCSCase pcsCase = underTest.getCase(CASE_REFERENCE, STATE);
-
-        // Then
-        assertThat(pcsCase.getPageHeadingMarkdown()).isNotBlank();
-    }
-
-    @Test
     void shouldMapPropertyAddress() {
         // Given
         AddressEntity addressEntity = mock(AddressEntity.class);
