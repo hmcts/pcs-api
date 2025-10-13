a59503d0eb74cb4e582f9ef04a0a7bea21ee6e63
guygrewal77 <163517291+guygrewal77@users.noreply.github.com>
2025-10-13 09:39:23 +0100

    HDPI-2376 Updated labels

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcement/EnforcementApplicationPage.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcement/EnforcementApplicationPage.java
@@ -8,12 +8,9 @@ public class EnforcementApplicationPage implements CcdPageConfiguration {
     @Override
     public void addTo(PageBuilder pageBuilder) {
         pageBuilder
-                .page("Enforce the order")
+                .page("enforcementApplicationPage")
                 .pageLabel("Your application")
                 .label(
-                        "EnforcementApplicationPage-info",
-                        """
-                        ---
-                        """);
+                        "enforcementApplicationPage-content", "---");
     }
 }
