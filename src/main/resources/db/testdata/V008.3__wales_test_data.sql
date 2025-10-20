9b2b1fefa6827ea45b07d1698f712f2692391a29
arun <arun.kumar@hmcts.net>
2025-10-20 16:24:12 +0100

    HDPI-2367: Remove wales test data SQL file
    - Removed V008.3__wales_test_data.sql from repository

--- src/main/resources/db/testdata/V008.3__wales_test_data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO postcode_court_mapping (postcode, epims_id, legislative_country, effective_from, effective_to, audit) VALUES
-    ('CF11 1BB', 28837, 'Wales', '2025-01-01', NULL, '{"created_by": "admin", "change_reason": "wales test data"}'::jsonb)
-ON CONFLICT (postcode, epims_id) DO NOTHING; 
-
-
