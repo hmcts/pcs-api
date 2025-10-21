c24dcc8616d6b5cc5b8da16d7c6853fb9e7270cc
Alex McAusland <a.m@user.com>
2025-10-21 11:55:32 +0100

    reinstate elasticsearch indexing as a runtime dependency

--- src/main/java/uk/gov/hmcts/reform/pcs/Application.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
 import uk.gov.hmcts.reform.pcs.feesandpay.api.FeesRegisterApi;
 import uk.gov.hmcts.reform.pcs.hearings.service.api.HmcHearingApi;
@@ -25,6 +26,7 @@ import uk.gov.hmcts.reform.pcs.location.service.api.LocationReferenceApi;
     }
 )
 @EnableJms
+@EnableScheduling
 public class Application {
 
     public static void main(final String[] args) {
