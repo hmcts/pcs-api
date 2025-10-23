a8fe170c4f01b0f68f33d84f8ec3254394bf7908
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-10-23 20:47:57 +0100

    Access token generation is removed from global config and converted as perform action.

--- src/e2eTest/data/page-data/signInOrCreateAnAccount.page.data.ts
+++ src/e2eTest/data/page-data/signInOrCreateAnAccount.page.data.ts
@@ -3,5 +3,10 @@ export const signInOrCreateAnAccount = {
   mainHeader: 'Sign in or create an account',
   emailAddressLabel: 'Email address',
   passwordLabel: 'Password',
-  signInButton: 'Sign in'
+  signInButton: 'Sign in',
+  acceptAdditionalCookies: 'Accept additional cookies',
+  rejectAdditionalCookies: 'Reject additional cookies',
+  acceptAnalyticsCookies: 'Accept analytics cookies',
+  rejectAnalyticsCookies: 'Reject analytics cookies',
+  hideThisCookieMessage: 'Hide this cookie message'
 }
