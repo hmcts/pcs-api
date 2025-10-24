f0ca7e89515aede30c5acdcbf8b7ac168de0887d
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-10-24 12:47:56 +0100

    updated logic to handle cookies

--- src/e2eTest/utils/actions/custom-actions/handleCookieConsent.action.ts
+++ src/e2eTest/utils/actions/custom-actions/handleCookieConsent.action.ts
@@ -1,18 +1,53 @@
-import {actionRecord, IAction} from '../../interfaces/action.interface';
-import {customTimeout} from '../../../playwright.config';
-import {Page} from '@playwright/test';
+import {Page, test, Locator} from '@playwright/test';
+import {IAction, actionRecord} from '../../interfaces/action.interface';
+import { SHORT_TIMEOUT, waitForLoadStateTimeout } from '../../../playwright.config';
 
 export class handleCookieConsentAction implements IAction {
+
   async execute(page: Page, action: string, actionText: actionRecord): Promise<void> {
-    const button = page.getByRole('button', { name: `${actionText.accept}` });
-    try {
-      await button.waitFor({ state: 'visible', timeout: customTimeout });
-      await button.click();
-      if(actionText.hide) {
-        await page.getByRole('button', { name: `${actionText.hide}` }).click();
+
+    const acceptButtonName = actionText.accept as string;
+    const hideButtonName = actionText.hide as string | undefined;
+    await test.step(`Handle cookie consent (if present): ${acceptButtonName}`, async () => {
+
+      let consentBanner: Locator;
+      let acceptBtn: Locator;
+      let successBanner: Locator | undefined;
+      let hideBtn: Locator | undefined;
+
+      if (acceptButtonName === "Accept additional cookies") {
+        consentBanner = page.locator('#cm_cookie_notification');
+        acceptBtn = consentBanner.getByRole('button', {name: acceptButtonName});
+
+        if (hideButtonName) {
+          successBanner = page.locator('#accept-all-cookies-success');
+          hideBtn = successBanner.getByRole('button', {name: hideButtonName});
+        }
+
+      } else if (acceptButtonName === "Accept analytics cookies") {
+        consentBanner = page.locator('xuilib-cookie-banner');
+        acceptBtn = consentBanner.getByRole('button', {name: acceptButtonName});
       }
-    } catch {
-      console.log(`Cookie consent button not found or not visible: ${actionText.accept}`);
-    }
+        else {
+        return;
+      }
+      try {
+        await consentBanner.waitFor({ state: 'attached', timeout: 5000 });
+        await acceptBtn.click({ timeout: 5000 });
+
+        await consentBanner.waitFor({ state: 'hidden', timeout: SHORT_TIMEOUT });
+
+        if (successBanner && hideBtn) {
+          await successBanner.waitFor({ state: 'visible', timeout: SHORT_TIMEOUT });
+          await hideBtn.click({ timeout: SHORT_TIMEOUT });
+          await successBanner.waitFor({ state: 'hidden', timeout: SHORT_TIMEOUT });
+        }
+
+        await page.waitForLoadState('domcontentloaded', {timeout: waitForLoadStateTimeout});
+
+      } catch (err) {
+        console.warn(`Cookie consent not found or interaction failed. Continuing. Error}`);
+      }
+    });
   }
 }
