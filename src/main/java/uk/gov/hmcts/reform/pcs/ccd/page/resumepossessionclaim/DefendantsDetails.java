5c2eaafb1e2a12ccc3da5f68eacd1762e28c8bca
sstewart <scott.stewart@solirius.com>
2025-10-24 12:04:01 +0100

    HDPI-2510: Extract defendant validation into its own class

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/resumepossessionclaim/DefendantsDetails.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/resumepossessionclaim/DefendantsDetails.java
@@ -5,24 +5,22 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.ccd.sdk.type.AddressUK;
-import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.reform.pcs.ccd.common.CcdPageConfiguration;
 import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
 import uk.gov.hmcts.reform.pcs.ccd.domain.DefendantDetails;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
 import uk.gov.hmcts.reform.pcs.ccd.domain.State;
 import uk.gov.hmcts.reform.pcs.ccd.domain.VerticalYesNo;
-import uk.gov.hmcts.reform.pcs.ccd.service.AddressValidator;
+import uk.gov.hmcts.reform.pcs.ccd.service.DefendantValidator;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @AllArgsConstructor
 @Component
 public class DefendantsDetails implements CcdPageConfiguration {
 
-    private final AddressValidator addressValidator;
+    private final DefendantValidator defendantValidator;
 
     @Override
     public void addTo(PageBuilder pageBuilder) {
@@ -65,11 +63,11 @@ public class DefendantsDetails implements CcdPageConfiguration {
 
         DefendantDetails defendantDetails = caseData.getDefendant1();
         List<String> validationErrors
-            = new ArrayList<>(validateDefendant1(defendantDetails, additionalDefendantsProvided));
+            = new ArrayList<>(defendantValidator.validateDefendant1(defendantDetails, additionalDefendantsProvided));
 
         if (additionalDefendantsProvided) {
-            List<String> additionalValidationErrors = validateAdditionalDefendants(caseData.getAdditionalDefendants());
-            validationErrors.addAll(additionalValidationErrors);
+            validationErrors
+                .addAll(defendantValidator.validateAdditionalDefendants(caseData.getAdditionalDefendants()));
         }
 
         if (!validationErrors.isEmpty()) {
@@ -85,35 +83,6 @@ public class DefendantsDetails implements CcdPageConfiguration {
             .data(caseData)
             .build();
 
-    }
-
-    private List<String> validateDefendant1(DefendantDetails defendantDetails, boolean additionalDefendantsProvided) {
-        String sectionHint = additionalDefendantsProvided ? "defendant 1" : "";
-        return validateDefendant(defendantDetails, sectionHint);
-    }
-
-    private List<String> validateAdditionalDefendants(List<ListValue<DefendantDetails>> additionalDefendants) {
-        List<String> validationErrors = new ArrayList<>();
-
-        for (int i = 0; i < additionalDefendants.size(); i++) {
-            DefendantDetails defendantDetails = additionalDefendants.get(i).getValue();
-            String sectionHint = "additional defendant %d".formatted(i + 1);
-            List<String> defendantValidationErrors = validateDefendant(defendantDetails, sectionHint);
-
-            validationErrors.addAll(defendantValidationErrors);
-        }
-
-        return validationErrors;
-    }
-
-    private List<String> validateDefendant(DefendantDetails defendantDetails, String sectionHint) {
-        if (defendantDetails.getAddressKnown() == VerticalYesNo.YES
-            && defendantDetails.getAddressSameAsPossession() == VerticalYesNo.NO) {
-
-            return addressValidator.validateAddressFields(defendantDetails.getCorrespondenceAddress(), sectionHint);
-        } else {
-            return Collections.emptyList();
-        }
     }
 
 }
--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/page/createpossessionclaim/DefendantsDetailsTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/page/createpossessionclaim/DefendantsDetailsTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.ccd.sdk.type.AddressUK;
+import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.reform.pcs.ccd.domain.DefendantCircumstances;
 import uk.gov.hmcts.reform.pcs.ccd.domain.DefendantDetails;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
@@ -17,14 +17,17 @@ import uk.gov.hmcts.reform.pcs.ccd.domain.State;
 import uk.gov.hmcts.reform.pcs.ccd.domain.VerticalYesNo;
 import uk.gov.hmcts.reform.pcs.ccd.page.BasePageTest;
 import uk.gov.hmcts.reform.pcs.ccd.page.resumepossessionclaim.DefendantsDetails;
-import uk.gov.hmcts.reform.pcs.ccd.service.AddressValidator;
+import uk.gov.hmcts.reform.pcs.ccd.service.DefendantValidator;
 
 import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.argumentSet;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.pcs.ccd.util.ListValueUtils.wrapListItems;
 
@@ -32,19 +35,18 @@ import static uk.gov.hmcts.reform.pcs.ccd.util.ListValueUtils.wrapListItems;
 class DefendantsDetailsTest extends BasePageTest {
 
     @Mock
-    private AddressValidator addressValidator;
+    private DefendantValidator defendantValidator;
 
     @BeforeEach
     void setUp() {
-        setPageUnderTest(new DefendantsDetails(addressValidator));
+        setPageUnderTest(new DefendantsDetails(defendantValidator));
     }
 
     @Test
-    void shouldValidateAddressWithNoSectionHintWhenSingleDefendant() {
+    void shouldValidateSingleDefendant() {
         // Given
         DefendantCircumstances defendantCircumstances = mock(DefendantCircumstances.class);
-        AddressUK defendant1Address = mock(AddressUK.class);
-        DefendantDetails defendant1 = createDefendantWithAddress(defendant1Address);
+        DefendantDetails defendant1 = mock(DefendantDetails.class);
 
         PCSCase caseData = PCSCase.builder()
             .defendant1(defendant1)
@@ -53,7 +55,7 @@ class DefendantsDetailsTest extends BasePageTest {
             .build();
 
         List<String> expectedValidationErrors = List.of("error 1", "error 2");
-        when(addressValidator.validateAddressFields(defendant1Address, ""))
+        when(defendantValidator.validateDefendant1(defendant1, false))
             .thenReturn(expectedValidationErrors);
 
         // When
@@ -61,46 +63,44 @@ class DefendantsDetailsTest extends BasePageTest {
 
         // Then
         assertThat(response.getErrors()).isEqualTo(expectedValidationErrors);
+        verify(defendantValidator, never()).validateAdditionalDefendants(anyList());
     }
 
     @Test
-    void shouldValidateAddressesWithSectionHintsWhenAdditionalDefendants() {
+    void shouldValidateMultipleDefendants() {
         // Given
         DefendantCircumstances defendantCircumstances = mock(DefendantCircumstances.class);
-        AddressUK defendant1Address = mock(AddressUK.class);
-        DefendantDetails defendant1 = createDefendantWithAddress(defendant1Address);
+        DefendantDetails defendant1 = mock(DefendantDetails.class);
+        DefendantDetails additionalDefendant1 = mock(DefendantDetails.class);
+        DefendantDetails additionalDefendant2 = mock(DefendantDetails.class);
 
-        AddressUK additionalDefendant1Address = mock(AddressUK.class);
-        DefendantDetails additionalDefendant1 = createDefendantWithAddress(additionalDefendant1Address);
-
-        AddressUK additionalDefendant2Address = mock(AddressUK.class);
-        DefendantDetails additionalDefendant2 = createDefendantWithAddress(additionalDefendant2Address);
+        List<ListValue<DefendantDetails>> additionalDefendants = wrapListItems(List.of(
+            additionalDefendant1,
+            additionalDefendant2
+        ));
 
         PCSCase caseData = PCSCase.builder()
             .defendant1(defendant1)
             .defendantCircumstances(defendantCircumstances)
             .addAnotherDefendant(VerticalYesNo.YES)
-            .additionalDefendants(wrapListItems(List.of(additionalDefendant1, additionalDefendant2)))
+            .additionalDefendants(additionalDefendants)
             .build();
 
         String errorMessage1 = "error 1";
         String errorMessage2 = "error 2";
         String errorMessage3 = "error 3";
-        String errorMessage4 = "error 4";
-        when(addressValidator.validateAddressFields(defendant1Address, "defendant 1"))
+
+        when(defendantValidator.validateDefendant1(defendant1, true))
             .thenReturn(List.of(errorMessage1, errorMessage2));
 
-        when(addressValidator.validateAddressFields(additionalDefendant1Address, "additional defendant 1"))
+        when(defendantValidator.validateAdditionalDefendants(additionalDefendants))
             .thenReturn(List.of(errorMessage3));
-
-        when(addressValidator.validateAddressFields(additionalDefendant2Address, "additional defendant 2"))
-            .thenReturn(List.of(errorMessage4));
 
         // When
         AboutToStartOrSubmitResponse<PCSCase, State> response = callMidEventHandler(caseData);
 
         // Then
-        assertThat(response.getErrors()).containsExactly(errorMessage1, errorMessage2, errorMessage3, errorMessage4);
+        assertThat(response.getErrors()).containsExactly(errorMessage1, errorMessage2, errorMessage3);
     }
 
     @ParameterizedTest
@@ -130,14 +130,6 @@ class DefendantsDetailsTest extends BasePageTest {
             argumentSet("No additional defendants", VerticalYesNo.NO, "defendant's"),
             argumentSet("Additional defendants", VerticalYesNo.YES, "defendants'")
         );
-    }
-
-    private static DefendantDetails createDefendantWithAddress(AddressUK address) {
-        return DefendantDetails.builder()
-            .addressKnown(VerticalYesNo.YES)
-            .addressSameAsPossession(VerticalYesNo.NO)
-            .correspondenceAddress(address)
-            .build();
     }
 
 }
