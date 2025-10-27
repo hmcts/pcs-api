668bd924afe65c2d268b59c541e200dec35006ad
ashajayaprakash <asha.jayaprakash@hmcts.net>
2025-10-27 14:20:05 +0000

    fixed relogin failure

--- src/e2eTest/tests/createCase.spec.ts
+++ src/e2eTest/tests/createCase.spec.ts
@@ -611,8 +611,7 @@ test.describe('[Create Case - England] @Master @nightly', async () => {
     await performAction('selectClaimType', claimType.no);
     await performAction('selectClaimantName', claimantName.yes);
     await performAction('clickButtonAndVerifyPageNavigation', claimantName.continue, contactPreferences.mainHeader);
-    await performAction('clickButtonAndWaitForElement', 'Sign out', 'Sign in or create an account');
-    await performAction('reloginAndFindTheCase', user.claimantSolicitor);
+    await performAction('clickButton', contactPreferences.cancel);
     await performAction('clickButtonAndVerifyPageNavigation', resumeClaim.continue, resumeClaimOptions.mainHeader);
     await performAction('selectResumeClaimOption', resumeClaimOptions.yes);
     await performValidation('radioButtonChecked', claimantType.england.registeredProviderForSocialHousing, true);
