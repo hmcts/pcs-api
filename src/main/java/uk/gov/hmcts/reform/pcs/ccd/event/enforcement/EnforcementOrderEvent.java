6e84a0b7bb359d7b6a81a2059a52d8c0e16e8182
tvr-solirius <trevor.hinson@solirius.com>
2025-10-27 11:52:29 +0000

    Add CheckYourAnswersPlaceHolder page to EnforcementOrderEvent and update common page content punctuation.

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/enforcement/EnforcementOrderEvent.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/enforcement/EnforcementOrderEvent.java
@@ -13,6 +13,7 @@ import uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole;
 import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
 import uk.gov.hmcts.reform.pcs.ccd.domain.State;
+import uk.gov.hmcts.reform.pcs.ccd.page.enforcement.CheckYourAnswersPlaceHolder;
 import uk.gov.hmcts.reform.pcs.ccd.page.enforcement.EnforcementApplicationPage;
 import uk.gov.hmcts.reform.pcs.ccd.page.enforcement.EvictionDelayWarningPage;
 import uk.gov.hmcts.reform.pcs.ccd.page.enforcement.EvictionRisksPosedPage;
@@ -51,8 +52,8 @@ public class EnforcementOrderEvent implements CCDConfig<PCSCase, State, UserRole
                 .add(new LivingInThePropertyPage())
                 .add(new EvictionDelayWarningPage())
                 .add(new EvictionRisksPosedPage())
-                .add(new EvictionVulnerableAdultsChildrenPage());
-
+                .add(new EvictionVulnerableAdultsChildrenPage())
+                .add(new CheckYourAnswersPlaceHolder());
     }
 
     private PCSCase start(EventPayload<PCSCase, State> eventPayload) {
