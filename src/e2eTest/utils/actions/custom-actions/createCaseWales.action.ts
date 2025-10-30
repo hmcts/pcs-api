8d640e6d61d63844239238ecaa2d95efab25b182
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2025-10-30 10:46:44 +0000

    HDPI-2365 scripts added

--- src/e2eTest/utils/actions/custom-actions/createCaseWales.action.ts
+++ src/e2eTest/utils/actions/custom-actions/createCaseWales.action.ts
@@ -3,11 +3,13 @@ import {Page} from '@playwright/test';
 import {performAction, performActions, performValidation} from '@utils/controller';
 import {claimantDetailsWales} from '@data/page-data/claimantDetailsWales.page.data';
 import {caseNumber, CreateCaseAction} from "@utils/actions/custom-actions/createCase.action";
+import {occupationContractOrLicenceDetailsWales} from '@data/page-data/occupationContractOrLicenceDetailsWales.page.data';
 
 export class CreateCaseWalesAction extends CreateCaseAction implements IAction {
   async execute(page: Page, action: string, fieldName: actionData | actionRecord, data?: actionData): Promise<void> {
     const actionsMap = new Map<string, () => Promise<void>>([
-      ['selectClaimantDetails', () => this.selectClaimantDetails(fieldName as actionRecord)]
+      ['selectClaimantDetails', () => this.selectClaimantDetails(fieldName as actionRecord)],
+      ['selectOccupationContractOrLicenceDetails', () => this.selectOccupationContractOrLicenceDetails(fieldName as actionRecord)]
     ]);
     const actionToPerform = actionsMap.get(action);
     if (!actionToPerform) throw new Error(`No action found for '${action}'`);
@@ -35,5 +37,24 @@ export class CreateCaseWalesAction extends CreateCaseAction implements IAction {
         ['inputText', claimantDetailsWales.yearLabel, claimantDetailsWales.yearInput]);
     }
     await performAction('clickButton', claimantDetailsWales.continue);
+  }
+
+  private async selectOccupationContractOrLicenceDetails(occupationContractData: actionRecord) {
+    await performValidation('text', {elementType: 'paragraph', text: 'Case number: ' + caseNumber});
+    await performAction('clickRadioButton', {question: occupationContractOrLicenceDetailsWales.occupationContractOrLicenceType, option: occupationContractData.occupationContractType});
+    if (occupationContractData.occupationContractType === occupationContractOrLicenceDetailsWales.other) {
+      await performAction('inputText', occupationContractOrLicenceDetailsWales.giveDetailsOfTypeOfOccupationContractAgreement, occupationContractOrLicenceDetailsWales.detailsOfLicence);
+    }
+    if (occupationContractData.day && occupationContractData.month && occupationContractData.year) {
+      await performActions(
+        'Enter Date',
+        ['inputText', occupationContractOrLicenceDetailsWales.dayLabel, occupationContractOrLicenceDetailsWales.day],
+        ['inputText', occupationContractOrLicenceDetailsWales.monthLabel, occupationContractOrLicenceDetailsWales.month],
+        ['inputText', occupationContractOrLicenceDetailsWales.yearLabel, occupationContractOrLicenceDetailsWales.year]);
+    }
+    if (occupationContractData.files) {
+      await performAction('uploadFile', occupationContractData.files);
+    }
+    await performAction('clickButton', occupationContractOrLicenceDetailsWales.continue);
   }
 }
