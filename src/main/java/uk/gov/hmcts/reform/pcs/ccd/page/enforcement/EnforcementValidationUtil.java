652ddf8f8241556f4546f3061d24bfe6466b5519
guygrewal77 <163517291+guygrewal77@users.noreply.github.com>
2025-10-30 11:38:15 +0000

    HDPI-2303 - Error message formatting changes aligned with other screens

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcement/EnforcementValidationUtil.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcement/EnforcementValidationUtil.java
@@ -1,14 +1,18 @@
 package uk.gov.hmcts.reform.pcs.ccd.page.enforcement;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class EnforcementValidationUtil {
 
-    private EnforcementValidationUtil() {
-    }
-
     private static final String ERROR_MESSAGE_TEMPLATE = "In '%s', you have entered more than the "
-            + "maximum number of characters (%d)";
+            + "maximum number of characters (%s)";
 
     static String getCharacterLimitErrorMessage(String label, int charLimit) {
-        return String.format(ERROR_MESSAGE_TEMPLATE, label, charLimit);
+        return String.format(ERROR_MESSAGE_TEMPLATE, label, NumberFormat.getInstance(Locale.UK).format(charLimit));
     }
 }
