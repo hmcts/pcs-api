368dac737060689be32cb0773f2437dd51d1df12
aqibh25 <85311661+aqibh25@users.noreply.github.com>
2025-11-03 14:08:35 +0000

    HDPI 2241: Highlight potential risk screens (#754)
    Co-authored-by: guygrewal77 <163517291+guygrewal77@users.noreply.github.com>
    Co-authored-by: Aqib <aqib.hussain@solirius.com>
    Co-authored-by: tvr-hmcts <trevor.hinson1@hmcts.net>
    Co-authored-by: tvr-solirius <trevor.hinson@solirius.com>
    Co-authored-by: srinijg <srinivasan.joghee@hmcts.net>
    Co-authored-by: Lovelin <lovelin.marcus@hmcts.net>

---
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcement/EvictionVulnerableAdultsChildrenPage.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.pcs.ccd.page.enforcement;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.pcs.ccd.common.CcdPageConfiguration;
+import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
+
+
+@AllArgsConstructor
+@Component
+public class EvictionVulnerableAdultsChildrenPage implements CcdPageConfiguration {
+
+    @Override
+    public void addTo(PageBuilder pageBuilder) {
+        pageBuilder
+            .page("evictionVulnerableAdultsChildrenPage")
+            .pageLabel("Vulnerable adults and children at the property (placeholder)")
+            .label("evictionVulnerableAdultsChildrenPage-line-separator", "---");
+    }
+}
