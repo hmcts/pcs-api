9f12da8a47c3656249579879595940bfc018036e
Toby Plunkett <toby.plunkett@HMCTS.NET>
2025-11-05 14:54:15 +0000

    HDPI-2621 Attempt using 'Label' instead of 'String' within complex type

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/DefendantsDOBComplexLabel.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/DefendantsDOBComplexLabel.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.api.Label;
-import uk.gov.hmcts.ccd.sdk.type.FieldType;
 
 import java.time.LocalDate;
 
