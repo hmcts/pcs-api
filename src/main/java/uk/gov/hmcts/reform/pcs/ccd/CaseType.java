de72205068b2dc1cac0b44b40fc8c2ead3591bd8
sstewart <scott.stewart@solirius.com>
2025-11-05 14:03:07 +0000

    HDPI-2730: Add property address to case title

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/CaseType.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/CaseType.java
@@ -78,6 +78,6 @@ public class CaseType implements CCDConfig<PCSCase, State, UserRole> {
 
         builder.tab("hidden", "HiddenFields")
             .showCondition(NEVER_SHOW)
-            .field(PCSCase::getPageHeadingMarkdown);
+            .field(PCSCase::getCaseTitleMarkdown);
     }
 }
