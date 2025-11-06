f31a8bf19dad6c425dcaf4a5379c58bc3fe2f91d
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-11-06 10:41:01 +0000

    Updated content for startTheService page

---
+++ src/e2eTest/data/page-data-figma/startTheService.page.data.ts
@@ -0,0 +1,24 @@
+export const startTheService = {
+  makeAClaimMainHeader: `Make a claim`,
+  mainHeader: `Make a housing possession claim online`,
+  youCanUseThisOnlineServiceParagraph: `You can use this online service if you\'re a registered provider of social`
+  + `housing or a community landlord and the property you want to claim possession of is in England or Wales.`,
+  thisServiceIsAlsoAvailableParagraph: `This service is also available`,
+  inWelshLink: `in Welsh (Cymraeg).`,
+  theClaimFeeIs404Paragraph: `The claim fee is £404. You can pay by card or through Payment By Account (PBA).`,
+  yourClaimWillBeSavedPraragraph: `Your claim will be saved as you answer the questions, so you’ll be able to close and return to your draft.`,
+  subHeader: `What you’ll need`,
+  beforeYouStartParagraph: `Before you start, make sure you have the following information:`,
+  detailsOfTenanacyListItem: `details of the tenancy, contract, licence or mortgage agreement`,
+  theDefendantsDetailsListItem: `the defendants’ details (the people you’re making the claim against)`,
+  yourReasonForMakingPossesionClaimListItem: `your reasons for making a possession claim`,
+  copiesOfgAnyReleventDocumentsListItem: `copies of any relevant documents. You can either upload documents now or `
+  + `closer to the hearing date. Any documents you upload now will be included in the pack of documents that a judge `
+  + `will receive before the hearing (the bundle)`,
+  onceYouHaveFinishedAnsweringParagraph: `Once you\`ve finished answering the questions, you can either:`,
+  signSubmitPayListItem: `sign, submit and pay for your claim now, or`,
+  saveItAsADraftListItem: `save it as a draft. You or someone else can then return to sign, submit and pay at a later date`,
+  cancelLink: `Cancel`,
+  continueButton: `Continue`,
+  previousButton: `Previous`,
+};
