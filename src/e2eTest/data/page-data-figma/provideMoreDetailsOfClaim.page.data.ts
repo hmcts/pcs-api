082a1231c59469d5c439e42aa7de82571fe4fb5b
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-11-06 11:12:37 +0000

    Updated content for address page

---
+++ src/e2eTest/data/page-data-figma/provideMoreDetailsOfClaim.page.data.ts
@@ -0,0 +1,10 @@
+export const provideMoreDetailsOfClaim = {
+  caseCreatedAlert: 'Case #1762-4267-5332-7587 has been created.',
+  caseNumberParagraph: `Case number:`,
+  propertyAddressParagraph: `Property address:`,
+  subHeader: 'Provide more details about your claim',
+  youAnswersWillBeSavedMarkDown: 'Your answers will be saved from this point so you can return to your draft later.',
+  nextStepsTab: 'Next steps',
+  continueButton: 'Continue',
+  cancelLink: `Cancel`,
+}
