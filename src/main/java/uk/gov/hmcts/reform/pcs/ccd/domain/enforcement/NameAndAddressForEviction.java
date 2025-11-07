61319d5331ac2d5829a3b689b630610b9051d675
elvianihuseyin2 <elviani.huseyin2@hmcts.net>
2025-11-07 16:49:57 +0000

    HDPI-2956: fixing the jenkins errors

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/enforcement/NameAndAddressForEviction.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/enforcement/NameAndAddressForEviction.java
@@ -1,12 +1,16 @@
 package uk.gov.hmcts.reform.pcs.ccd.domain.enforcement;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.reform.pcs.ccd.domain.VerticalYesNo;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NameAndAddressForEviction {
 
     @CCD(label = "You can choose the defendants you want to evict on the next page")
