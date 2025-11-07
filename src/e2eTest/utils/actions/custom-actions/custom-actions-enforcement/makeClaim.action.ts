6c33e7d609a619e8c8fc1ec3c654c5e5b10ce583
srinijg <srinivasan.joghee@hmcts.net>
2025-11-07 12:23:35 +0000

    fixed imports

--- src/e2eTest/utils/actions/custom-actions/custom-actions-enforcement/makeClaim.action.ts
+++ src/e2eTest/utils/actions/custom-actions/custom-actions-enforcement/makeClaim.action.ts
@@ -1,14 +1,14 @@
-import { Page } from "@playwright/test";
-import { initializeExecutor } from "@utils/controller";
-import { performAction, performValidation } from "@utils/controller";
-import { actionData, actionRecord, IAction } from "@utils/interfaces/action.interface";
+import { Page } from '@playwright/test';
+import { initializeExecutor } from '@utils/controller';
+import { performAction, performValidation } from '@utils/controller';
+import { actionData, actionRecord, IAction } from '@utils/interfaces/action.interface';
 import {
   home, addressDetails, additionalReasonsForPossession, alternativesToPossession, applications, checkYourAnswers, claimantCircumstances, claimantName,
   claimantType, claimingCosts, claimType, completeYourClaim, contactPreferences, defendantCircumstances, defendantDetails,
   groundsForPossession, languageUsed, mediationAndSettlement, noticeOfYourIntention, preActionProtocol,
   provideMoreDetailsOfClaim, statementOfTruth, tenancyLicenceDetails, underlesseeOrMortgageeEntitledToClaim,
   wantToUploadDocuments
-} from "@data/page-data";
+} from '@data/page-data';
 
 export class MakeClaimAction implements IAction {
   async execute(
