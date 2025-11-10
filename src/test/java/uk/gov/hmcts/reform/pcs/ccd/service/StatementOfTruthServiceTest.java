4bfee3ee6c25706e345933741d099325b7e312c1
arun <arun.kumar@hmcts.net>
2025-11-10 18:10:34 +0000

    HDPI-2036: Remove unused import from StatementOfTruthServiceTest

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/service/StatementOfTruthServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/service/StatementOfTruthServiceTest.java
@@ -11,7 +11,6 @@ import uk.gov.hmcts.reform.pcs.ccd.domain.StatementOfTruthCompletedBy;
 import uk.gov.hmcts.reform.pcs.ccd.model.StatementOfTruth;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
