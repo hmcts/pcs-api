f6d82d69b0ba40f30669585d62feafc77e30f1b2
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-11-10 12:22:43 +0000

    Updated the keys as per the format

--- src/e2eTest/data/page-data-figma/provideMoreDetailsOfClaim.page.data.ts
+++ src/e2eTest/data/page-data-figma/provideMoreDetailsOfClaim.page.data.ts
@@ -1,10 +1,9 @@
 export const provideMoreDetailsOfClaim = {
-  caseCreatedAlert: 'Case #1762-4267-5332-7587 has been created.',
   caseNumberParagraph: `Case number:`,
-  propertyAddressParagraph: `Property address:`,
-  subHeader: 'Provide more details about your claim',
-  youAnswersWillBeSavedMarkDown: 'Your answers will be saved from this point so you can return to your draft later.',
+  //propertyAddressParagraph: `Property address:`,
   nextStepsTab: 'Next steps',
+  subHeader: 'Provide more details about your claim',
+  youAnswersWillBeSavedParagraph: 'Your answers will be saved from this point so you can return to your draft later.',
   continueButton: 'Continue',
   cancelLink: `Cancel`,
 }
