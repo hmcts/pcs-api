3bd288f7a491af529c7021ee5b824632058c41e3
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-11-11 18:33:36 +0000

    Add Test Tagging Mechanism for PR/Preview Builds

--- src/e2eTest/tests/tests-enforcement/enforcement.spec.ts
+++ src/e2eTest/tests/tests-enforcement/enforcement.spec.ts
@@ -30,7 +30,7 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe('[Enforcement - Warrant of Possession] @Master @nightly', async () => {
-  test('Apply for a Warrant of Possession - risk to Bailiff [Yes]', async () => {
+  test('Apply for a Warrant of Possession - risk to Bailiff [Yes] @PR', async () => {
     await performAction('select', caseSummary.nextStepEventList, caseSummary.enforceTheOrderEvent);
     await performAction('clickButtonAndVerifyPageNavigation', caseSummary.go, yourApplication.mainHeader);
     await performAction('selectApplicationType', {
