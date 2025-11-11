d9f1a642d8605022431eb307830c06ecb4140a83
guygrewal77 <163517291+guygrewal77@users.noreply.github.com>
2025-11-11 13:33:50 +0000

    HDPI-2337 - Removed redundant Repository methods.

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/repository/enforcement/EnforcementDataRepository.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/repository/enforcement/EnforcementDataRepository.java
@@ -3,14 +3,7 @@ package uk.gov.hmcts.reform.pcs.ccd.repository.enforcement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uk.gov.hmcts.reform.pcs.ccd.entity.enforcement.EnforcementDataEntity;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface EnforcementDataRepository extends JpaRepository<EnforcementDataEntity, UUID> {
-
-    Optional<EnforcementDataEntity> findById(UUID enforcementCaseId);
-
-    boolean existsById(UUID enforcementCaseId);
-
-    void deleteById(UUID enforcementCaseId);
 }
