6fe6066d971da594cb6a5f22a9c0a20a9520efca
fudge88 <f.akhlaq@outlook.com>
2025-11-11 15:51:03 +0000

    merge from main

--- src/main/java/uk/gov/hmcts/reform/pcs/Application.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/Application.java
@@ -7,7 +7,6 @@ import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentClientApi;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
-import uk.gov.hmcts.reform.pcs.feesandpay.api.FeesRegisterApi;
 import uk.gov.hmcts.reform.pcs.hearings.service.api.HmcHearingApi;
 import uk.gov.hmcts.reform.pcs.idam.api.IdamTokenApi;
 import uk.gov.hmcts.reform.pcs.location.service.api.LocationReferenceApi;
@@ -29,7 +28,6 @@ import uk.gov.hmcts.reform.pcs.reference.api.RdProfessionalApi;
         IdamApi.class,
         IdamTokenApi.class,
         RdProfessionalApi.class,
-        FeesRegisterApi.class,
         CaseDocumentClientApi.class
     }
 )
