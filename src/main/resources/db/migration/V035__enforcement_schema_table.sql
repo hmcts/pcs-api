66c7a5a92d15f460480631bfa9eccbce6c26d65d
guygrewal77 <163517291+guygrewal77@users.noreply.github.com>
2025-11-12 11:54:42 +0000

    HDPI-2337 - Removed unnecessary index on column which is also a PK.

--- src/main/resources/db/migration/V035__enforcement_schema_table.sql
+++ src/main/resources/db/migration/V035__enforcement_schema_table.sql
@@ -5,5 +5,3 @@ CREATE TABLE enforcement.enf_case (
     case_id uuid references public.pcs_case (id),
     submitted_enforcement_data JSONB
 );
-
-CREATE INDEX enf_case_enforcement_case_id_idx ON enforcement.enf_case (enforcement_case_id);
