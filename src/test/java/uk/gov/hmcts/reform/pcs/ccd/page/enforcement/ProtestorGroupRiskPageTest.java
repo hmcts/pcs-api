f56fd1664fc14be168b1a72c480cb28afa3686a8
Jake Gowler <jakecgowler@gmail.com>
2025-11-14 11:39:03 +0000

    Suppression for issue CVE-2025-7962 (#954)
    Co-authored-by: tvr-solirius <trevor.hinson@solirius.com>

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcement/ProtestorGroupRiskPageTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcement/ProtestorGroupRiskPageTest.java
@@ -70,7 +70,7 @@ class ProtestorGroupRiskPageTest extends BasePageTest {
         // Then
         String expectedError = String.format(CHARACTER_LIMIT_ERROR_TEMPLATE,
                                              RiskCategory.PROTEST_GROUP_MEMBER.getText(),
-                                             RISK_CATEGORY_EXTRA_LONG_TEXT_LIMIT);
+                                             "6,800");
 
         assertThat(response.getErrors()).containsExactly(expectedError);
     }
