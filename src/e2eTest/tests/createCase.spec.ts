fa388e1ce3b17df8ca9a71eed36994055c8e328c
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-11-15 11:19:09 +0000

    4 Tests with 2 worker

--- src/e2eTest/tests/createCase.spec.ts
+++ src/e2eTest/tests/createCase.spec.ts
@@ -23,7 +23,7 @@ test.beforeEach(async ({page}) => {
   await performAction('housingPossessionClaim');
 });
 
-test.describe('[Create Case - England] @regression @PR', async () => {
+test.describe('[Create Case - England] @regression', async () => {
   test('England - Assured tenancy with Rent arrears and other possession grounds @PR', async () => {
     await performAction('selectAddress', {
       postcode: addressDetails.englandCourtAssignedPostcodeTextInput,
