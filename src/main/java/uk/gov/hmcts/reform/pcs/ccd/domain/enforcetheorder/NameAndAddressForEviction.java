ecbc2127064fae8b8deafccee9a9e79d521fad7c
tvr-solirius <trevor.hinson@solirius.com>
2025-11-18 15:53:59 +0000

    - Remove unused CheckYourAnswersPlaceHolder class - Rename `enforcement` package to `enforcetheorder` for improved clarity and consistency

---
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/enforcetheorder/NameAndAddressForEviction.java
@@ -0,0 +1,15 @@
+package uk.gov.hmcts.reform.pcs.ccd.domain.enforcetheorder;
+
+import lombok.Builder;
+import lombok.Data;
+import uk.gov.hmcts.ccd.sdk.api.CCD;
+import uk.gov.hmcts.reform.pcs.ccd.domain.VerticalYesNo;
+
+@Builder
+@Data
+public class NameAndAddressForEviction {
+
+    @CCD(label = "You can choose the defendants you want to evict on the next page")
+    private VerticalYesNo correctNameAndAddress;
+
+}
---
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/event/enforcetheorder/EnforcementOrderEventTest.java
@@ -0,0 +1,93 @@
+package uk.gov.hmcts.reform.pcs.ccd.event.enforcetheorder;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.type.AddressUK;
+import uk.gov.hmcts.ccd.sdk.type.ListValue;
+import uk.gov.hmcts.reform.pcs.ccd.domain.DefendantDetails;
+import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
+import uk.gov.hmcts.reform.pcs.ccd.event.BaseEventTest;
+import uk.gov.hmcts.reform.pcs.ccd.page.enforcetheorder.AdditionalInformationPage;
+import uk.gov.hmcts.reform.pcs.ccd.page.enforcetheorder.AggressiveAnimalsRiskPage;
+import uk.gov.hmcts.reform.pcs.ccd.page.enforcetheorder.CriminalAntisocialRiskPage;
+import uk.gov.hmcts.reform.pcs.ccd.page.enforcetheorder.FirearmsPossessionRiskPage;
+import uk.gov.hmcts.reform.pcs.ccd.page.enforcetheorder.PoliceOrSocialServicesRiskPage;
+import uk.gov.hmcts.reform.pcs.ccd.page.enforcetheorder.PropertyAccessDetailsPage;
+import uk.gov.hmcts.reform.pcs.ccd.page.enforcetheorder.ProtestorGroupRiskPage;
+import uk.gov.hmcts.reform.pcs.ccd.page.enforcetheorder.VerbalOrWrittenThreatsRiskPage;
+import uk.gov.hmcts.reform.pcs.ccd.page.enforcetheorder.ViolentAggressiveRiskPage;
+import uk.gov.hmcts.reform.pcs.ccd.page.enforcetheorder.VulnerableAdultsChildrenPage;
+import uk.gov.hmcts.reform.pcs.ccd.util.AddressFormatter;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class EnforcementOrderEventTest extends BaseEventTest {
+
+    @Mock
+    private final AddressFormatter addressFormatter = new AddressFormatter();
+    @Mock
+    private ViolentAggressiveRiskPage violentAggressiveRiskPage;
+    @Mock
+    private VerbalOrWrittenThreatsRiskPage verbalOrWrittenThreatsRiskPage;
+    @Mock
+    private ProtestorGroupRiskPage protestorGroupRiskPage;
+    @Mock
+    private PoliceOrSocialServicesRiskPage policeOrSocialServicesRiskPage;
+    @Mock
+    private FirearmsPossessionRiskPage firearmsPossessionRiskPage;
+    @Mock
+    private CriminalAntisocialRiskPage criminalAntisocialRiskPage;
+    @Mock
+    private AggressiveAnimalsRiskPage aggressiveAnimalsRiskPage;
+    @Mock
+    private PropertyAccessDetailsPage propertyAccessDetailsPage;
+    @Mock
+    private VulnerableAdultsChildrenPage vulnerableAdultsChildrenPage;
+    @Mock
+    private AdditionalInformationPage additionalInformationPage;
+
+    @BeforeEach
+    void setUp() {
+        setEventUnderTest(new EnforcementOrderEvent(addressFormatter, violentAggressiveRiskPage,
+                                                    verbalOrWrittenThreatsRiskPage, protestorGroupRiskPage,
+                                                    policeOrSocialServicesRiskPage, firearmsPossessionRiskPage,
+                                                    criminalAntisocialRiskPage, aggressiveAnimalsRiskPage,
+                                                    propertyAccessDetailsPage, vulnerableAdultsChildrenPage,
+                                                    additionalInformationPage));
+    }
+
+    @Test
+    void shouldReturnCaseDataInStartCallback() {
+        // Given
+        AddressUK propertyAddress = mock(AddressUK.class);
+        String expectedFormattedPropertyAddress = "expected formatted property address";
+        when(addressFormatter.formatAddressWithHtmlLineBreaks(propertyAddress))
+            .thenReturn(expectedFormattedPropertyAddress);
+
+        String firstName = "Test";
+        String lastName = "Testing";
+        DefendantDetails defendantDetails = DefendantDetails.builder().firstName(firstName).lastName(lastName).build();
+        PCSCase caseData = PCSCase.builder()
+            .allDefendants(List.of(ListValue.<DefendantDetails>builder().value(defendantDetails).build()))
+            .propertyAddress(propertyAddress).build();
+
+        // When
+        PCSCase result = callStartHandler(caseData);
+
+        // Then
+        assertThat(result).isNotNull();
+        assertThat(result.getFormattedPropertyAddress()).isEqualTo(expectedFormattedPropertyAddress);
+        assertThat(result.getAllDefendants()).hasSize(1);
+        assertThat(result.getDefendant1().getFirstName()).isEqualTo(firstName);
+        assertThat(result.getDefendant1().getLastName()).isEqualTo(lastName);
+    }
+
+}
