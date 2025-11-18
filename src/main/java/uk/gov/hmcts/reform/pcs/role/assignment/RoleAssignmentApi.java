87dbf35e047c81efff4e2ca39a68d00eedf11c22
Toby Plunkett <toby.plunkett@HMCTS.NET>
2025-11-18 16:35:38 +0000

    HDPI-1523 assign role on submit (hardcoded endpoint)

--- src/main/java/uk/gov/hmcts/reform/pcs/role/assignment/RoleAssignmentApi.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/role/assignment/RoleAssignmentApi.java
@@ -7,14 +7,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import uk.gov.hmcts.reform.pcs.role.assignment.DTO.CaseUserListDTO;
 
-@FeignClient(name = "role-assignment", url = "${role-assignment.url}")
+@FeignClient(name = "role-assignment", url = "http://ccd-data-store-api-aat.service.core-compute-aat.internal")
 public interface RoleAssignmentApi {
 
     @PostMapping(
-        value = "http://ccd-data-store-api-aat.service.core-compute-aat.internal/case-users",
+        value = "/case-users",
         produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public String assignRole(
+     String assignRole(
+        @RequestHeader("Authorization") String authorization,
         @RequestHeader("ServiceAuthorization") String serviceAuth,
         @RequestBody CaseUserListDTO request);
 }
--- src/main/java/uk/gov/hmcts/reform/pcs/role/assignment/RoleAssignmentService.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/role/assignment/RoleAssignmentService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
+import uk.gov.hmcts.reform.pcs.idam.IdamService;
 import uk.gov.hmcts.reform.pcs.reference.service.OrganisationDetailsService;
 import uk.gov.hmcts.reform.pcs.role.assignment.DTO.CaseUserDTO;
 import uk.gov.hmcts.reform.pcs.role.assignment.DTO.CaseUserListDTO;
@@ -22,17 +23,21 @@ public class RoleAssignmentService {
     private final SecurityContextService securityContextService;
     private final OrganisationDetailsService organisationDetailsService;
     private final AuthTokenGenerator authTokenGenerator;
+    private final IdamService idamService;
 
     @Value("${role-assignment.role-id}")
     private String caseRole;
 
 
     public void assignRole(String caseReference, PCSCase pcsCase) {
+        //Auth for request
+        String userAuthorisation = idamService.getSystemUserAuthorisation();
+        String s2sToken = authTokenGenerator.generate();
+
+        //Data for CaseUserDTO
         UUID userId = securityContextService.getCurrentUserId();
         //      String orgId = organisationDetailsService.getOrganisationIdentifier(userId.toString());
         String orgId = "E71FH4Q";
-
-        String s2sToken = authTokenGenerator.generate();
 
         CaseUserDTO caseUserDTO = CaseUserDTO.builder()
             .caseId(caseReference)
@@ -46,7 +51,7 @@ public class RoleAssignmentService {
 
         CaseUserListDTO caseUserListDTO = CaseUserListDTO.builder().caseUsers(caseUserList).build();
 
-        String response = roleAssignmentApi.assignRole(s2sToken, caseUserListDTO);
+        String response = roleAssignmentApi.assignRole(userAuthorisation, s2sToken, caseUserListDTO);
         log.error("Role assignment response {}", response);
     }
 }
