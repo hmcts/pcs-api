25f96c712a8eaea5912b65402429a8086ae5746e
srinijg <srinivasan.joghee@hmcts.net>
2025-11-20 18:23:38 +0000

    fixed errors

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/page/resumepossessionclaim/DefendantsDetailsTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/page/resumepossessionclaim/DefendantsDetailsTest.java
@@ -3,120 +3,132 @@ package uk.gov.hmcts.reform.pcs.ccd.page.resumepossessionclaim;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.hmcts.ccd.sdk.api.Event;
-import uk.gov.hmcts.ccd.sdk.type.AddressUK;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
+import uk.gov.hmcts.reform.pcs.ccd.domain.DefendantCircumstances;
 import uk.gov.hmcts.reform.pcs.ccd.domain.DefendantDetails;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
-import uk.gov.hmcts.reform.pcs.ccd.domain.enforcement.EnforcementOrder;
-import uk.gov.hmcts.reform.pcs.ccd.event.BaseEventTest;
-import uk.gov.hmcts.reform.pcs.ccd.service.enforcement.EnforcementOrderService;
-import uk.gov.hmcts.reform.pcs.ccd.page.builder.SavingPageBuilder;
-import uk.gov.hmcts.reform.pcs.ccd.page.builder.SavingPageBuilderFactory;
-import uk.gov.hmcts.reform.pcs.ccd.page.enforcement.AdditionalInformationPage;
-import uk.gov.hmcts.reform.pcs.ccd.page.enforcement.AggressiveAnimalsRiskPage;
-import uk.gov.hmcts.reform.pcs.ccd.page.enforcement.CriminalAntisocialRiskPage;
-import uk.gov.hmcts.reform.pcs.ccd.page.enforcement.FirearmsPossessionRiskPage;
-import uk.gov.hmcts.reform.pcs.ccd.page.enforcement.PoliceOrSocialServicesRiskPage;
-import uk.gov.hmcts.reform.pcs.ccd.page.enforcement.PropertyAccessDetailsPage;
-import uk.gov.hmcts.reform.pcs.ccd.page.enforcement.ProtestorGroupRiskPage;
-import uk.gov.hmcts.reform.pcs.ccd.page.enforcement.VerbalOrWrittenThreatsRiskPage;
-import uk.gov.hmcts.reform.pcs.ccd.page.enforcement.ViolentAggressiveRiskPage;
-import uk.gov.hmcts.reform.pcs.ccd.page.enforcement.VulnerableAdultsChildrenPage;
-import uk.gov.hmcts.reform.pcs.ccd.util.AddressFormatter;
+import uk.gov.hmcts.reform.pcs.ccd.domain.State;
+import uk.gov.hmcts.reform.pcs.ccd.domain.VerticalYesNo;
+import uk.gov.hmcts.reform.pcs.ccd.page.BasePageTest;
+import uk.gov.hmcts.reform.pcs.ccd.service.DefendantValidator;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.junit.jupiter.params.provider.Arguments.argumentSet;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-import static uk.gov.hmcts.reform.pcs.ccd.event.EventId.enforceTheOrder;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.pcs.ccd.util.ListValueUtils.wrapListItems;
 
 @ExtendWith(MockitoExtension.class)
-class EnforcementOrderEventTest extends BaseEventTest {
+class DefendantsDetailsTest extends BasePageTest {
 
     @Mock
-    private final AddressFormatter addressFormatter = new AddressFormatter();
-    @Mock
-    private ViolentAggressiveRiskPage violentAggressiveRiskPage;
-    @Mock
-    private VerbalOrWrittenThreatsRiskPage verbalOrWrittenThreatsRiskPage;
-    @Mock
-    private ProtestorGroupRiskPage protestorGroupRiskPage;
-    @Mock
-    private PoliceOrSocialServicesRiskPage policeOrSocialServicesRiskPage;
-    @Mock
-    private FirearmsPossessionRiskPage firearmsPossessionRiskPage;
-    @Mock
-    private CriminalAntisocialRiskPage criminalAntisocialRiskPage;
-    @Mock
-    private AggressiveAnimalsRiskPage aggressiveAnimalsRiskPage;
-    @Mock
-    private PropertyAccessDetailsPage propertyAccessDetailsPage;
-    @Mock
-    private VulnerableAdultsChildrenPage vulnerableAdultsChildrenPage;
-    @Mock
-    private AdditionalInformationPage additionalInformationPage;
-    @Mock
-    private SavingPageBuilderFactory savingPageBuilderFactory;
-    @Mock
-    private EnforcementOrderService enforcementOrderService;
+    private DefendantValidator defendantValidator;
 
-    @SuppressWarnings("unchecked")
     @BeforeEach
     void setUp() {
-        SavingPageBuilder savingPageBuilder = mock(SavingPageBuilder.class);
-        when(savingPageBuilder.add(any())).thenReturn(savingPageBuilder);
-        when(savingPageBuilderFactory.create(any(Event.EventBuilder.class), eq(enforceTheOrder)))
-            .thenReturn(savingPageBuilder);
-        setEventUnderTest(new EnforcementOrderEvent(enforcementOrderService, addressFormatter,
-                                violentAggressiveRiskPage, verbalOrWrittenThreatsRiskPage, protestorGroupRiskPage,
-                                policeOrSocialServicesRiskPage, firearmsPossessionRiskPage,
-                                criminalAntisocialRiskPage, aggressiveAnimalsRiskPage, propertyAccessDetailsPage,
-                                vulnerableAdultsChildrenPage, additionalInformationPage, savingPageBuilderFactory));
+        setPageUnderTest(new DefendantsDetails(defendantValidator));
     }
 
     @Test
-    void shouldReturnCaseDataInStartCallback() {
+    void shouldValidateSingleDefendant() {
         // Given
-        AddressUK propertyAddress = mock(AddressUK.class);
-        String expectedFormattedPropertyAddress = "expected formatted property address";
-        when(addressFormatter.formatAddressWithHtmlLineBreaks(propertyAddress))
-            .thenReturn(expectedFormattedPropertyAddress);
+        DefendantCircumstances defendantCircumstances = mock(DefendantCircumstances.class);
+        DefendantDetails defendant1 = mock(DefendantDetails.class);
 
-        String firstName = "Test";
-        String lastName = "Testing";
-        DefendantDetails defendantDetails = DefendantDetails.builder().firstName(firstName).lastName(lastName).build();
         PCSCase caseData = PCSCase.builder()
-            .allDefendants(List.of(ListValue.<DefendantDetails>builder().value(defendantDetails).build()))
-            .propertyAddress(propertyAddress).build();
+            .defendant1(defendant1)
+            .defendantCircumstances(defendantCircumstances)
+            .addAnotherDefendant(VerticalYesNo.NO)
+            .build();
+
+        List<String> expectedValidationErrors = List.of("error 1", "error 2");
+        when(defendantValidator.validateDefendant1(defendant1, false))
+            .thenReturn(expectedValidationErrors);
 
         // When
-        PCSCase result = callStartHandler(caseData);
+        AboutToStartOrSubmitResponse<PCSCase, State> response = callMidEventHandler(caseData);
 
         // Then
-        assertThat(result).isNotNull();
-        assertThat(result.getFormattedPropertyAddress()).isEqualTo(expectedFormattedPropertyAddress);
-        assertThat(result.getAllDefendants()).hasSize(1);
-        assertThat(result.getDefendant1().getFirstName()).isEqualTo(firstName);
-        assertThat(result.getDefendant1().getLastName()).isEqualTo(lastName);
+        assertThat(response.getErrors()).isEqualTo(expectedValidationErrors);
+        verify(defendantValidator, never()).validateAdditionalDefendants(anyList());
     }
 
     @Test
-    void shouldCreateEnforcementDataInSubmitCallback() {
+    void shouldValidateMultipleDefendants() {
         // Given
-        EnforcementOrder enforcementOrder = EnforcementOrder.builder().build();
-        PCSCase pcsCase = PCSCase.builder().enforcementOrder(enforcementOrder).build();
+        DefendantCircumstances defendantCircumstances = mock(DefendantCircumstances.class);
+        DefendantDetails defendant1 = mock(DefendantDetails.class);
+        DefendantDetails additionalDefendant1 = mock(DefendantDetails.class);
+        DefendantDetails additionalDefendant2 = mock(DefendantDetails.class);
+
+        List<ListValue<DefendantDetails>> additionalDefendants = wrapListItems(List.of(
+            additionalDefendant1,
+            additionalDefendant2
+        ));
+
+        PCSCase caseData = PCSCase.builder()
+            .defendant1(defendant1)
+            .defendantCircumstances(defendantCircumstances)
+            .addAnotherDefendant(VerticalYesNo.YES)
+            .additionalDefendants(additionalDefendants)
+            .build();
+
+        String errorMessage1 = "error 1";
+        String errorMessage2 = "error 2";
+        String errorMessage3 = "error 3";
+
+        when(defendantValidator.validateDefendant1(defendant1, true))
+            .thenReturn(List.of(errorMessage1, errorMessage2));
+
+        when(defendantValidator.validateAdditionalDefendants(additionalDefendants))
+            .thenReturn(List.of(errorMessage3));
 
         // When
-        callSubmitHandler(pcsCase);
+        AboutToStartOrSubmitResponse<PCSCase, State> response = callMidEventHandler(caseData);
 
         // Then
-        verify(enforcementOrderService).createEnforcementOrder(TEST_CASE_REFERENCE, enforcementOrder);
+        assertThat(response.getErrors()).containsExactly(errorMessage1, errorMessage2, errorMessage3);
     }
+
+    @ParameterizedTest
+    @MethodSource("defendantTermScenarios")
+    void shouldSetDefendantTermPossessive(VerticalYesNo additionalDefendants, String expectedTermPossessive) {
+        // Given
+        DefendantCircumstances defendantCircumstances = new DefendantCircumstances();
+        DefendantDetails defendant1 = mock(DefendantDetails.class);
+
+        PCSCase caseData = PCSCase.builder()
+            .defendant1(defendant1)
+            .defendantCircumstances(defendantCircumstances)
+            .addAnotherDefendant(additionalDefendants)
+            .additionalDefendants(List.of())
+            .build();
+
+        // When
+        AboutToStartOrSubmitResponse<PCSCase, State> response = callMidEventHandler(caseData);
+
+        // Then
+        assertThat(response.getData()
+                       .getDefendantCircumstances().getDefendantTermPossessive()).isEqualTo(expectedTermPossessive);
+    }
+
+    private static Stream<Arguments> defendantTermScenarios() {
+        return Stream.of(
+            argumentSet("No additional defendants", VerticalYesNo.NO, "defendant's"),
+            argumentSet("Additional defendants", VerticalYesNo.YES, "defendants'")
+        );
+    }
+
 }
