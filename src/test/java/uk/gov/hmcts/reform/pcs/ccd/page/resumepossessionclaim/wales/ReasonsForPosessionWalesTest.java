100fafd12d6aa8227644d4a9333b4fd91be4851a
Alex Buzea <alex.buzea@hmcts.net>
2025-11-26 14:43:28 +0000

    adding tests for textarea validation introduced

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/page/resumepossessionclaim/wales/ReasonsForPosessionWalesTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/page/resumepossessionclaim/wales/ReasonsForPosessionWalesTest.java
@@ -6,6 +6,9 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -17,6 +20,7 @@ import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
 import uk.gov.hmcts.reform.pcs.ccd.domain.State;
 import uk.gov.hmcts.reform.pcs.ccd.domain.wales.DiscretionaryGroundWales;
+import uk.gov.hmcts.reform.pcs.ccd.domain.wales.GroundsReasonsWales;
 import uk.gov.hmcts.reform.pcs.ccd.domain.wales.SecureContractDiscretionaryGroundsWales;
 import uk.gov.hmcts.reform.pcs.ccd.page.BasePageTest;
 import uk.gov.hmcts.reform.pcs.ccd.service.TextAreaValidationService;
@@ -119,6 +123,162 @@ class ReasonsForPosessionWalesTest extends BasePageTest {
                         YesOrNo.NO
                 )
         );
+    }
+
+    @Nested
+    @DisplayName("Validation Integration Tests")
+    class ValidationIntegrationTests {
+
+        @Test
+        @DisplayName("Should validate all Wales grounds reasons when provided")
+        void shouldValidateAllWalesGroundsReasonsWhenProvided() {
+            PCSCase caseData = PCSCase.builder()
+                .groundsReasonsWales(GroundsReasonsWales.builder()
+                    .failToGiveUpS170Reason("Failure to give up S170 reason")
+                    .landlordNoticePeriodicS178Reason("Landlord notice periodic S178 reason")
+                    .seriousArrearsPeriodicS181Reason("Serious arrears periodic S181 reason")
+                    .landlordNoticeFtEndS186Reason("Landlord notice FT end S186 reason")
+                    .seriousArrearsFixedTermS187Reason("Serious arrears fixed term S187 reason")
+                    .failToGiveUpBreakNoticeS191Reason("Failure to give up break notice S191 reason")
+                    .landlordBreakClauseS199Reason("Landlord break clause S199 reason")
+                    .convertedFixedTermSch1225B2Reason("Converted fixed term Sch12 25B2 reason")
+                    .otherBreachSection157Reason("Other breach section 157 reason")
+                    .buildingWorksReason("Building works reason")
+                    .redevelopmentSchemesReason("Redevelopment schemes reason")
+                    .charitiesReason("Charities reason")
+                    .disabledSuitableDwellingReason("Disabled suitable dwelling reason")
+                    .housingAssociationsAndTrustsReason("Housing associations and trusts reason")
+                    .specialNeedsDwellingsReason("Special needs dwellings reason")
+                    .reserveSuccessorsReason("Reserve successors reason")
+                    .jointContractHoldersReason("Joint contract holders reason")
+                    .otherEstateManagementReasonsReason("Other estate management reasons reason")
+                    .secureFailureToGiveUpPossessionSection170Reason("Secure failure S170 reason")
+                    .secureLandlordNoticeSection186Reason("Secure landlord notice S186 reason")
+                    .secureFailureToGiveUpPossessionSection191Reason("Secure failure S191 reason")
+                    .secureLandlordNoticeSection199Reason("Secure landlord notice S199 reason")
+                    .secureOtherBreachOfContractReason("Secure other breach reason")
+                    .secureBuildingWorksReason("Secure building works reason")
+                    .secureRedevelopmentSchemesReason("Secure redevelopment schemes reason")
+                    .secureCharitiesReason("Secure charities reason")
+                    .secureDisabledSuitableDwellingReason("Secure disabled suitable dwelling reason")
+                    .secureHousingAssociationsAndTrustsReason("Secure housing associations reason")
+                    .secureSpecialNeedsDwellingsReason("Secure special needs dwellings reason")
+                    .secureReserveSuccessorsReason("Secure reserve successors reason")
+                    .secureJointContractHoldersReason("Secure joint contract holders reason")
+                    .secureOtherEstateManagementReasonsReason("Secure other estate management reason")
+                    .build())
+                .build();
+
+            AboutToStartOrSubmitResponse<PCSCase, State> response = callMidEventHandler(caseData);
+
+            assertThat(response.getData()).isEqualTo(caseData);
+            assertThat(response.getErrors()).isNullOrEmpty();
+        }
+
+        @Test
+        @DisplayName("Should handle null grounds reasons Wales gracefully")
+        void shouldHandleNullGroundsReasonsWalesGracefully() {
+            PCSCase caseData = PCSCase.builder()
+                .groundsReasonsWales(null)
+                .build();
+
+            AboutToStartOrSubmitResponse<PCSCase, State> response = callMidEventHandler(caseData);
+
+            assertThat(response.getData()).isEqualTo(caseData);
+            assertThat(response.getErrors()).isNullOrEmpty();
+        }
+
+        @Test
+        @DisplayName("Should handle empty grounds reasons Wales gracefully")
+        void shouldHandleEmptyGroundsReasonsWalesGracefully() {
+            PCSCase caseData = PCSCase.builder()
+                .groundsReasonsWales(GroundsReasonsWales.builder().build())
+                .build();
+
+            AboutToStartOrSubmitResponse<PCSCase, State> response = callMidEventHandler(caseData);
+
+            assertThat(response.getData()).isEqualTo(caseData);
+            assertThat(response.getErrors()).isNullOrEmpty();
+        }
+
+        @Test
+        @DisplayName("Should handle partial grounds reasons Wales gracefully")
+        void shouldHandlePartialGroundsReasonsWalesGracefully() {
+            PCSCase caseData = PCSCase.builder()
+                .groundsReasonsWales(GroundsReasonsWales.builder()
+                    .failToGiveUpS170Reason("Only failure to give up S170 reason")
+                    .build())
+                .build();
+
+            AboutToStartOrSubmitResponse<PCSCase, State> response = callMidEventHandler(caseData);
+
+            assertThat(response.getData()).isEqualTo(caseData);
+            assertThat(response.getErrors()).isNullOrEmpty();
+        }
+
+        @Test
+        @DisplayName("Should validate standard contract mandatory grounds reasons")
+        void shouldValidateStandardContractMandatoryGroundsReasons() {
+            PCSCase caseData = PCSCase.builder()
+                .groundsReasonsWales(GroundsReasonsWales.builder()
+                    .failToGiveUpS170Reason("Failure to give up S170 reason")
+                    .landlordNoticePeriodicS178Reason("Landlord notice periodic S178 reason")
+                    .seriousArrearsPeriodicS181Reason("Serious arrears periodic S181 reason")
+                    .landlordNoticeFtEndS186Reason("Landlord notice FT end S186 reason")
+                    .seriousArrearsFixedTermS187Reason("Serious arrears fixed term S187 reason")
+                    .failToGiveUpBreakNoticeS191Reason("Failure to give up break notice S191 reason")
+                    .landlordBreakClauseS199Reason("Landlord break clause S199 reason")
+                    .convertedFixedTermSch1225B2Reason("Converted fixed term Sch12 25B2 reason")
+                    .build())
+                .build();
+
+            AboutToStartOrSubmitResponse<PCSCase, State> response = callMidEventHandler(caseData);
+
+            assertThat(response.getData()).isEqualTo(caseData);
+            assertThat(response.getErrors()).isNullOrEmpty();
+        }
+
+        @Test
+        @DisplayName("Should validate secure contract grounds reasons")
+        void shouldValidateSecureContractGroundsReasons() {
+            PCSCase caseData = PCSCase.builder()
+                .groundsReasonsWales(GroundsReasonsWales.builder()
+                    .secureFailureToGiveUpPossessionSection170Reason("Secure failure S170 reason")
+                    .secureLandlordNoticeSection186Reason("Secure landlord notice S186 reason")
+                    .secureFailureToGiveUpPossessionSection191Reason("Secure failure S191 reason")
+                    .secureLandlordNoticeSection199Reason("Secure landlord notice S199 reason")
+                    .secureOtherBreachOfContractReason("Secure other breach reason")
+                    .build())
+                .build();
+
+            AboutToStartOrSubmitResponse<PCSCase, State> response = callMidEventHandler(caseData);
+
+            assertThat(response.getData()).isEqualTo(caseData);
+            assertThat(response.getErrors()).isNullOrEmpty();
+        }
+
+        @Test
+        @DisplayName("Should validate estate management grounds reasons")
+        void shouldValidateEstateManagementGroundsReasons() {
+            PCSCase caseData = PCSCase.builder()
+                .groundsReasonsWales(GroundsReasonsWales.builder()
+                    .buildingWorksReason("Building works reason")
+                    .redevelopmentSchemesReason("Redevelopment schemes reason")
+                    .charitiesReason("Charities reason")
+                    .disabledSuitableDwellingReason("Disabled suitable dwelling reason")
+                    .housingAssociationsAndTrustsReason("Housing associations and trusts reason")
+                    .specialNeedsDwellingsReason("Special needs dwellings reason")
+                    .reserveSuccessorsReason("Reserve successors reason")
+                    .jointContractHoldersReason("Joint contract holders reason")
+                    .otherEstateManagementReasonsReason("Other estate management reasons reason")
+                    .build())
+                .build();
+
+            AboutToStartOrSubmitResponse<PCSCase, State> response = callMidEventHandler(caseData);
+
+            assertThat(response.getData()).isEqualTo(caseData);
+            assertThat(response.getErrors()).isNullOrEmpty();
+        }
     }
 }
 
