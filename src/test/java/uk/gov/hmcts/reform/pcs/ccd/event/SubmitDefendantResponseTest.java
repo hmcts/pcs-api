eda677090f8ad8a18aec0556dc0e361ba512fc41
Toby Plunkett <toby.plunkett@HMCTS.NET>
2025-12-03 16:57:08 +0000

    HDPI-3210 Add Test

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/event/SubmitDefendantResponseTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/event/SubmitDefendantResponseTest.java
@@ -5,8 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
+import uk.gov.hmcts.reform.pcs.ccd.domain.DefendantResponse;
+import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
 import uk.gov.hmcts.reform.pcs.ccd.service.DraftCaseDataService;
 import uk.gov.hmcts.reform.pcs.ccd.service.PcsCaseService;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class SubmitDefendantResponseTest extends BaseEventTest {
@@ -23,7 +29,27 @@ class SubmitDefendantResponseTest extends BaseEventTest {
     }
 
     @Test
-    void shouldUpdateCaseOnSubmit() {
+    void shouldPatchUnsubmittedEventData() {
+        // Given Defendant response
+        DefendantResponse defendantResponse = DefendantResponse.builder()
+            .defendantResponseFinalSubmit(YesOrNo.NO)
+            .contactByPhone(YesOrNo.YES)
+            .build();
+
+        PCSCase caseData = PCSCase.builder()
+            .defendantResponse(defendantResponse)
+            .build();
+
+        // When
+        callSubmitHandler(caseData);
+
+        //Then
+        verify(draftCaseDataService).patchUnsubmittedEventData(
+            eq(TEST_CASE_REFERENCE),
+            eq(defendantResponse),
+            eq(EventId.submitDefendantResponse)
+        );
+
     }
 
 }
