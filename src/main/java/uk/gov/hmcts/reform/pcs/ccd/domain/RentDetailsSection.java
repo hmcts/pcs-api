d237879d2c8891da46d005345be325a085f2923b
Andrew Ajube <andrew.ajube@hmcts.net>
2025-12-05 09:26:33 +0000

    chore: remove unused import

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/RentDetailsSection.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/RentDetailsSection.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.FieldType;
-import uk.gov.hmcts.reform.pcs.ccd.domain.VerticalYesNo;
 
 /**
  * CCD domain complex type for rent details.
