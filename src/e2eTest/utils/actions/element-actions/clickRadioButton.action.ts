0fa0414e10aced901b36b6877d35de80c704db8a
Gautham059 <gautham.bhikkumalla@hmcts.net>
2025-12-08 16:58:44 +0000

    Fixed radio button

--- src/e2eTest/utils/actions/element-actions/clickRadioButton.action.ts
+++ src/e2eTest/utils/actions/element-actions/clickRadioButton.action.ts
@@ -11,13 +11,21 @@ export class ClickRadioButtonAction implements IAction {
     const { question, option, index } = params as actionRecord;
     const idx = index !== undefined ? Number(index) : 0;
 
-    const radioButton1 = page.locator(`legend:has-text("${question}")`).nth(idx).locator('..').getByRole('radio', { name: option as string, exact: true });
+    const radioButton1 = page.locator(`legend:has-text("${question}")`)
+      .nth(idx)
+      .locator('..')
+      .getByRole('radio', { name: option as string, exact: true });
 
     try {
       await radioButton1.waitFor({ state: 'visible', timeout: 1000 });
       await radioButton1.click();
     } catch {
-      const radioButton2 = page.locator(`fieldset:has-text("${question}")`).nth(idx).locator(`div.multiple-choice:has-text("${option}") input[type="radio"]`);
+      const radioButton2 = page.locator(`fieldset:has-text("${question}")`)
+        .nth(idx)
+        .locator('div.multiple-choice')
+        .filter({ has: page.locator('label.form-label', { hasText: new RegExp(`^${option}$`, 'i') }) })
+        .locator('input[type="radio"]');
+
       await radioButton2.click();
     }
   }
