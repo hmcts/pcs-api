40ebe42df8441b3671a35868b0cda9e751f7fd82
guygrewal77 <163517291+guygrewal77@users.noreply.github.com>
2025-12-10 16:16:00 +0000

    HDPI-3301 - Fixed missing json annotations

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/enforcetheorder/warrant/LegalCosts.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/enforcetheorder/warrant/LegalCosts.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.pcs.ccd.domain.enforcetheorder.warrant;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
@@ -12,6 +13,7 @@ import uk.gov.hmcts.reform.pcs.ccd.domain.VerticalYesNo;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.UpperCamelCaseStrategy.class)
--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/enforcetheorder/warrant/PeopleToEvict.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/enforcetheorder/warrant/PeopleToEvict.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.pcs.ccd.domain.enforcetheorder.warrant;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
@@ -15,6 +16,7 @@ import uk.gov.hmcts.reform.pcs.ccd.domain.VerticalYesNo;
  */
 @Builder
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.UpperCamelCaseStrategy.class)
