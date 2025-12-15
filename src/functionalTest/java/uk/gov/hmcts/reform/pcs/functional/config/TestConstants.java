675fe0a54a6caa6ba62d29973d60694c702b4949
Arunabha Chowdhury <90389022+ArunabhaChowdhury@users.noreply.github.com>
2025-12-15 13:35:02 +0000

    Removing unused methods and variables.

--- src/functionalTest/java/uk/gov/hmcts/reform/pcs/functional/config/TestConstants.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/pcs/functional/config/TestConstants.java
@@ -10,25 +10,4 @@ public class TestConstants {
     public static final String SERVICE_AUTHORIZATION = "ServiceAuthorization";
     public static final String EXPIRED_S2S_TOKEN = getEnv("S2S_EXPIRED_TOKEN");
     public static final String EXPIRED_IDAM_TOKEN = getEnv("IDAM_EXPIRED_USER_TOKEN");
-
-//    // Expired tokens for negative testing scenarios
-//    public static final String EXPIRED_S2S_TOKEN =
-//        "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwY3NfYXBpIiwiZXhwIjoxNzQ0MjI0NTgyfQ."
-//            + "5vd6i9FgZOXaOcWnlz4qAUN4Zutf4wyjoIU0DjmA_1G2FZm2uR_zKkl6lz4jc9_Trrf_cqU3Wi2B9GK5vD8LpQ";
-//
-//    public static final String EXPIRED_IDAM_TOKEN =
-//        "eyJ0eXAiOiJKV1QiLCJraWQiOiIxZXIwV1J3Z0lPVEFGb2pFNHJDL2ZiZUt1M0k9IiwiYWxnIjoiUlMyNTYifQ."
-//            + "eyJzdWIiOiJwY3Mtc3lzdGVtLXVwZGF0ZUBobWN0cy5uZXQiLCJjdHMiOiJPQVVUSDJfU1RBVEVMRVNTX0dSQU5UIiwiYXV0aF9"
-//            + "sZXZlbCI6MCwiYXVkaXRUcmFja2luZ0lkIjoiNzdmYzNjZjgtMTk2My00YTEzLWEwZGMtODZhNjg2MGEyNzU5LTMxMTUxMTQiLC"
-//            + "JzdWJuYW1lIjoicGNzLXN5c3RlbS11cGRhdGVAaG1jdHMubmV0IiwiaXNzIjoiaHR0cHM6Ly9mb3JnZXJvY2stYW0uc2Vydmlj"
-//            + "ZS5jb3JlLWNvbXB1dGUtaWRhbS1hYXQyLmludGVybmFsOjg0NDMvb3BlbmFtL29hdXRoMi9yZWFsbXMvcm9vdC9yZWFsbXMvaG"
-//            + "1jdHMiLCJ0b2tlbk5hbWUiOiJhY2Nlc3NfdG9rZW4iLCJ0b2tlbl90eXBlIjoiQmVhcmVyIiwiYXV0aEdyYW50SWQiOiJFNUhz"
-//            + "QlpMeVlvLWEwYkQzb29EWG9GOURGa2ciLCJhdWQiOiJjaXZpbF9jaXRpemVuX3VpIiwibmJmIjoxNzQ2MDQ3MjgwLCJncmFudF"
-//            + "90eXBlIjoicGFzc3dvcmQiLCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwicm9sZXMiXSwiYXV0aF90aW1lIjoxNzQ2MDQ3"
-//            + "MjgwLCJyZWFsbSI6Ii9obWN0cyIsImV4cCI6MTc0NjA3NjA4MCwiaWF0IjoxNzQ2MDQ3MjgwLCJleHBpcmVzX2luIjoyODgwMC"
-//            + "wianRpIjoicUFVQzk2SHBDbUR5aTdZLVB1bUpZUVNHeFM0In0.jFcZmIgfELRJFRhuFk7d8ZIsbJeZA21lyVW-06HBQlXM"
-//            + "-1yKAT8b52UsOojY1v7GyaTorj1i0CoFpDn9Ao0eLB6DCEETKE6HI8Fq1PflA-mwBmR0F1nUGWHxPfWp4a2TaTkvhKiUxMk"
-//            + "CCwfvTJLjBndJi3iCNezRzvxoHMJtKCOVR8PvNQu6pnxNha0xIC9DSBBFt0VjXKma_P3Pijxi_Ce69kZ3B6ZertwCy4Elmb"
-//            + "PQhJgCjbQHimxSa-Omh-zcQG1MD1h1WPYN3OQLS4anvsNFWlDiBmaexN5C7Kru_LTTGLa5wp604m_FAF2nmpgZdsP7o4MQ"
-//            + "6Wd00DXlgY1fAw";
 }
