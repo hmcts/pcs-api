ba3ebee2653210a4caf8c662cb8723200d4f440a
Rehan Ayub <Rehan.Ayub@HMCTS.NET>
2025-12-18 16:51:53 +0000

    HDPI-3073 Refactoring RentArrearsGroundsForPossessionReasons page fields

---
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/GroundsForPossession.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.reform.pcs.ccd.domain;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.ccd.sdk.api.CCD;
+import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
+
+import java.util.Set;
+
+import static uk.gov.hmcts.ccd.sdk.type.FieldType.MultiSelectList;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.UpperCamelCaseStrategy.class)
+public class ArrearsGroundsForPossession {
+
+    @CCD(
+        typeOverride = MultiSelectList,
+        typeParameterOverride = "RentArrearsGround"
+    )
+    private Set<RentArrearsGround> copyOfRentArrearsGrounds;
+
+    // Rent arrears grounds checkboxes
+    @CCD(
+        label = "What are your grounds for possession?",
+        hint = "Select all that apply",
+        typeOverride = MultiSelectList,
+        typeParameterOverride = "RentArrearsGround"
+    )
+    private Set<RentArrearsGround> rentArrearsGrounds;
+
+    @CCD(
+        label = "Do you have any other additional grounds for possession?"
+    )
+    private YesOrNo hasOtherAdditionalGrounds;
+
+}
