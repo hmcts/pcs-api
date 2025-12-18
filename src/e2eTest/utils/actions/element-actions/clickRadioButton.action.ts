3d944502eaa0f66e11187ac9162c0fc8754afafc
Gautham059 <gautham.bhikkumalla@hmcts.net>
2025-12-18 15:03:33 +0000

    logic 2

--- src/e2eTest/utils/actions/element-actions/clickRadioButton.action.ts
+++ src/e2eTest/utils/actions/element-actions/clickRadioButton.action.ts
@@ -7,43 +7,42 @@ export class ClickRadioButtonAction implements IAction {
     const question = params.question as string;
     const option = params.option as string;
 
-    const patterns = [
-      () => {
-        const questionSpan = page.locator(`span.form-label:has-text("${question}")`).nth(idx);
-        return questionSpan.locator('xpath=ancestor::div[contains(@class, "form-group")][1]')
-          .locator('div.multiple-choice')
-          .filter({ has: page.locator('label.form-label').filter({ hasText: new RegExp(`^${this.escapeRegex(option)}$`) }) })
-          .locator('input[type="radio"]');
-      },
-      () => {
-        const escapedOption = option.replace(/'/g, "', \"'\", '");
-        return page.locator(`span.form-label:has-text("${question}")`)
-          .nth(idx)
-          .locator(`xpath=ancestor::fieldset[1]//div.multiple-choice[.//label.form-label[normalize-space()='${escapedOption}']]/input[@type='radio']`);
-      },
-      () => {
-        return page.locator(`legend:has-text("${question}")`)
-          .nth(idx)
-          .locator('xpath=ancestor::fieldset[1]')
-          .locator('div.multiple-choice')
-          .filter({ has: page.locator('label.form-label').filter({ hasText: option }) })
-          .locator('input[type="radio"]');
-      },
-      () => {
-        return page.locator(`legend:has-text("${question}")`)
-          .nth(idx)
-          .locator('..')
-          .getByRole('radio', { name: option as string, exact: true });
-      }
-    ];
+    if (await this.clickRadioButton(this.radioPattern1(page, question, option, idx))) return;
+    if (await this.clickRadioButton(this.radioPattern2(page, question, option, idx))) return;
+    if (await this.clickRadioButton(this.radioPattern3(page, question, option, idx))) return;
+  }
 
-    for (const getPattern of patterns) {
-      const locator = getPattern();
-      if ((await locator.count()) === 1 && await locator.isVisible()) {
-        await locator.check();
-        return;
-      }
+  private async clickRadioButton(locator: any): Promise<boolean> {
+    const count = await locator.count();
+    if (count === 1) {
+      await locator.click();
+      return true;
     }
+    return false;
+  }
+
+  private radioPattern1(page: Page, question: string, option: string, idx: number) {
+    const questionSpan = page.locator(`span.form-label:has-text("${question}")`).nth(idx);
+    return questionSpan.locator('xpath=ancestor::div[contains(@class, "form-group")][1]')
+      .locator('div.multiple-choice')
+      .filter({ has: page.locator('label.form-label').filter({ hasText: new RegExp(`^${this.escapeRegex(option)}$`) }) })
+      .locator('input[type="radio"]');
+  }
+
+  private radioPattern2(page: Page, question: string, option: string, idx: number) {
+    const escapedOption = option.replace(/'/g, "', \"'\", '");
+    return page.locator(`span.form-label:has-text("${question}")`)
+      .nth(idx)
+      .locator(`xpath=ancestor::fieldset[1]//div.multiple-choice[.//label.form-label[normalize-space()='${escapedOption}']]/input[@type='radio']`);
+  }
+
+  private radioPattern3(page: Page, question: string, option: string, idx: number) {
+    return page.locator(`legend:has-text("${question}")`)
+      .nth(idx)
+      .locator('xpath=ancestor::fieldset[1]')
+      .locator('div.multiple-choice')
+      .filter({ has: page.locator('label.form-label').filter({ hasText: option }) })
+      .locator('input[type="radio"]');
   }
 
   private escapeRegex(text: string): string {
