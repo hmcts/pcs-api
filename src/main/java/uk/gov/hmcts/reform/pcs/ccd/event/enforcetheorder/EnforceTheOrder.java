cefbca3cc7a5bfda52e6984fea76e18571f3cf10
Aqib <aqib.hussain@solirius.com>
2025-12-18 13:33:27 +0000

    Missed during the merge

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/enforcetheorder/EnforceTheOrder.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/enforcetheorder/EnforceTheOrder.java
@@ -27,7 +27,7 @@ import uk.gov.hmcts.reform.pcs.ccd.util.AddressFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import uk.gov.hmcts.reform.pcs.ccd.util.FeeApplier;
-import uk.gov.hmcts.reform.pcs.feesandpay.model.FeeTypes;
+import uk.gov.hmcts.reform.pcs.feesandpay.model.FeeType;
 
 import static uk.gov.hmcts.reform.pcs.ccd.event.EventId.enforceTheOrder;
 import static uk.gov.hmcts.reform.pcs.ccd.util.AddressFormatter.BR_DELIMITER;
@@ -96,7 +96,7 @@ public class EnforceTheOrder implements CCDConfig<PCSCase, State, UserRole> {
     private void applyWarrantFeeAmount(PCSCase pcsCase) {
         feeApplier.applyFeeAmount(
             pcsCase,
-            FeeTypes.ENFORCEMENT_WARRANT_FEE,
+            FeeType.ENFORCEMENT_WARRANT_FEE,
             (caseData, fee) -> caseData.getEnforcementOrder().setWarrantFeeAmount(fee)
         );
     }
@@ -104,7 +104,7 @@ public class EnforceTheOrder implements CCDConfig<PCSCase, State, UserRole> {
     private void applyWritFeeAmount(PCSCase pcsCase) {
         feeApplier.applyFeeAmount(
             pcsCase,
-            FeeTypes.ENFORCEMENT_WRIT_FEE,
+            FeeType.ENFORCEMENT_WRIT_FEE,
             (caseData, fee) -> caseData.getEnforcementOrder().setWritFeeAmount(fee)
         );
     }
