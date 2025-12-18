bd4577a7bbf4b6fc39349726a8a03edeb86d9d7e
madhavi1203 <Madhavi.Atmakuri1@HMCTS.NET>
2025-12-18 18:37:36 +0000

    HDPI-3102-QA (#1128)

--- src/e2eTest/data/page-data/claimType.page.data.ts
+++
@@ -1,9 +0,0 @@
-export const claimType =
-  {
-    title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
-    mainHeader: 'Claim type',
-    isThisAClaimAgainstTrespassersQuestion: 'Is this a claim against trespassers?',
-    yes: 'Yes',
-    no: 'No',
-    continue: 'Continue'
-  }
