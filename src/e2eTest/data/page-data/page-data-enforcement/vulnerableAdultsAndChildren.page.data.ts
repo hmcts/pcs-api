f192f9e84fb0ba86d1349f56d69a76c374510c34
A Hussain <85311661+aqibh25@users.noreply.github.com>
2026-01-07 15:07:22 +0000

    HDPI-3038: Implemented the RepaymentPage to show a breakdown of all costs (#1104)
    Co-authored-by: Aqib <aqib.hussain@solirius.com>
    Co-authored-by: Jake Gowler <jakecgowler@gmail.com>
    Co-authored-by: srinijg <srinivasan.joghee@hmcts.net>

--- src/e2eTest/data/page-data/page-data-enforcement/vulnerableAdultsAndChildren.page.data.ts
+++ src/e2eTest/data/page-data/page-data-enforcement/vulnerableAdultsAndChildren.page.data.ts
@@ -4,12 +4,27 @@ export const vulnerableAdultsAndChildren = {
   IsAnyOneLivingAtThePropertyQuestion: 'Is anyone living at the property vulnerable?',
   yesRadioOption: 'Yes',
   noRadioOption: 'No',
-  notSureRadioOption:'I’m not sure',
+  notSureRadioOption: 'I’m not sure',
   confirmVulnerablePeopleQuestion: 'Confirm if the vulnerable people in the property are adults, children, or both adults and children',
   vulnerableAdultsRadioOption: 'Vulnerable adults',
   vulnerableChildrenRadioOption: 'Vulnerable children',
   vulnerableAdultsAndChildrenRadioOption: 'Vulnerable adults and children',
   howAreTheyVulnerableTextLabel: 'How are they vulnerable?',
   howAreTheyVulnerableTextInput: 'Test Input How are they vulnerable',
-  continueButton: 'Continue'
+  continueButton: 'Continue',
+  errors: 'Errors',
+  errorValidation: 'NO',//set it to 'NO' before raising a PR
+  errorValidationType: { one: 'moneyField', two: 'textField', three: 'radioOptions', four: 'checkBox', five: 'moneyFieldAndRadioOption' },
+  errorValidationField: {
+    errorRadioOption1: [
+      { type: 'multi', input: '', errMessage: 'Is anyone living at the property vulnerable? is required' },
+    ],
+    errorRadioOption2: [
+      { type: 'none', input: '', errMessage: 'Confirm if the vulnerable people in the property are adults, children, or both adults and children is required' },
+    ],
+    errorTextField: [
+      { type: 'moreThanMax', input: 'MAXPLUS', errMessage: 'In ‘How are they vulnerable?’, you have entered more than the maximum number of characters (6,800)' },
+      { type: 'empty', input: 'EMPTY', errMessage: 'How are they vulnerable? is required' },
+    ]
+  }
 }
