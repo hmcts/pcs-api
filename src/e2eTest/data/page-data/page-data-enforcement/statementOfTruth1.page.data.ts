f34d8d5cac3762744503d0863926def229d01bd2
srinijg <srinivasan.joghee@hmcts.net>
2026-01-09 17:02:55 +0000

    suspended order automation added

---
+++ src/e2eTest/data/page-data/page-data-enforcement/statementOfTruth1.page.data.ts
@@ -0,0 +1,5 @@
+export const statementOfTruthOne = {
+  title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
+  mainHeader: 'Statement of truth (place holder)',
+  continueButton: 'Continue'
+};
