f8dd7b38465ca5145a4d84f1698d8a6e3827303e
guygrewal77 <guygrewal7@gmail.com>
2026-01-12 09:34:54 +0000

    HDPI-3469 - Minor visual changes

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/enforcetheorder/warrant/WarrantDetails.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/enforcetheorder/warrant/WarrantDetails.java
@@ -109,8 +109,8 @@ public class WarrantDetails {
     @CCD(
             label = "Is your order a suspended order?",
             hint = "If your order is suspended, you will see a different version of the statement of truth on the "
-                    + "next page. If you do not know if your order is suspended, save your application as a draft, "
-                    + "return to the case summary page, and then check the tab named 'Case File View'."
+                    + "next page. If you do not know if your order is suspended: save your application as a draft, "
+                    + "return to the case summary page, and then check the tab named ‘Case File View’"
     )
     private VerticalYesNo isSuspendedOrder;
 }
