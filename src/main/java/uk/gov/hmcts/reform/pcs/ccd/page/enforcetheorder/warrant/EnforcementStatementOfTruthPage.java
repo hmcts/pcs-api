8ef4734c3d17c77ea1e86ff9feb9804a8afd7fe1
Andrew Ajube <andrew.ajube@hmcts.net>
2026-01-14 17:21:38 +0000

    refactor: Remove readonly condition and adjust list formatting in EnforcementStatementOfTruthPage

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcetheorder/warrant/EnforcementStatementOfTruthPage.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcetheorder/warrant/EnforcementStatementOfTruthPage.java
@@ -36,7 +36,6 @@ public class EnforcementStatementOfTruthPage implements CcdPageConfiguration {
             .page("enforcementStatementOfTruth", this::midEvent)
             .pageLabel("Statement of truth")
             .showCondition("selectEnforcementType=\"WARRANT\"")
-            .readonly(PCSCase::getFormattedPropertyAddress, NEVER_SHOW)
             .complex(PCSCase::getEnforcementOrder)
                 .complex(EnforcementOrder::getWarrantDetails)
                     .label("enforcementStatementOfTruth-lineSeparator", "---")
@@ -48,13 +47,13 @@ public class EnforcementStatementOfTruthPage implements CcdPageConfiguration {
                         .label("enforcementSOT-cert-suspended",
                             """
                             <ul class="govuk-list govuk-list--bullet">
-                            <li>the defendant has not vacated the land as ordered (* and that the whole or part of any
-                            instalments due under the judgment or order have not been paid) (and the balance now due is
-                            as shown)</li>
-                            <li>notice has been given in accordance with The Dwelling Houses (Execution of Possession
-                            Order by Mortgagees) Regulations 2010.</li>
-                            <li>a statement of the payments due and made under the judgment or order is attached to this
-                            request</li>
+                                <li>the defendant has not vacated the land as ordered (* and that the whole or part of any
+                                instalments due under the judgment or order have not been paid) (and the balance now due is
+                                as shown)</li>
+                                <li>notice has been given in accordance with The Dwelling Houses (Execution of Possession
+                                Order by Mortgagees) Regulations 2010.</li>
+                                <li>a statement of the payments due and made under the judgment or order is attached to this
+                                request</li>
                             </ul>
                             """,
                             "isSuspendedOrder=\"YES\""
@@ -62,11 +61,11 @@ public class EnforcementStatementOfTruthPage implements CcdPageConfiguration {
                         .label("enforcementSOT-cert-not-suspended",
                             """
                             <ul class="govuk-list govuk-list--bullet">
-                            <li>the defendant has not vacated the land as ordered (* and that the whole or part of any
-                            instalments due under the judgment or order have not been paid) (and the balance now due is
-                            as shown)</li>
-                            <li>notice has been given in accordance with The Dwelling Houses (Execution of Possession
-                            Order by Mortgagees) Regulations 2010.</li>
+                                <li>the defendant has not vacated the land as ordered (* and that the whole or part of any
+                                instalments due under the judgment or order have not been paid) (and the balance now due is
+                                as shown)</li>
+                                <li>notice has been given in accordance with The Dwelling Houses (Execution of Possession
+                                Order by Mortgagees) Regulations 2010.</li>
                             </ul>
                             """,
                             "isSuspendedOrder=\"NO\""
