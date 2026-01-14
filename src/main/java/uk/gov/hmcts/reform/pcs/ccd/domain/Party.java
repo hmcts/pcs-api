c7e9e54bb1239ee65298cf90062a03154362acbb
scottstewart-sl <scott.stewart@solirius.com>
2026-01-14 08:27:10 +0000

    HDPI-3220: Party persistence (#1133)
    Co-authored-by: jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
    Co-authored-by: Jake Gowler <jakecgowler@gmail.com>
    Co-authored-by: AshaJayaprakash <asha.jayaprakash@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/Party.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/Party.java
@@ -4,40 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.AddressUK;
-import uk.gov.hmcts.ccd.sdk.type.FieldType;
-
-import java.util.UUID;
-
-import static uk.gov.hmcts.reform.pcs.ccd.ShowConditions.NEVER_SHOW;
 
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Party {
-    @CCD(label = "Party’s forename")
-    private String forename;
 
-    @CCD(label = "Party’s surname")
-    private String surname;
+    private String firstName;
 
-    @CCD(label = "Organisation name")
+    private String lastName;
+
     private String orgName;
 
-    @CCD(typeOverride = FieldType.Email)
-    private String contactEmail;
+    private VerticalYesNo nameKnown;
 
-    private AddressUK contactAddress;
+    private String emailAddress;
 
-    @CCD(typeOverride = FieldType.PhoneUK)
-    private String contactPhone;
+    private AddressUK address;
 
-    @CCD(typeOverride = FieldType.Text, showCondition = NEVER_SHOW)
-    private UUID idamId;
+    private VerticalYesNo addressKnown;
 
-    @CCD(typeOverride = FieldType.Text, showCondition = NEVER_SHOW)
-    private UUID pcqId;
+    private VerticalYesNo addressSameAsProperty;
+
+    private String phoneNumber;
 
 }
