dde5d18338ea8424e8bab01f1bd4ab0cc88063d1
guygrewal77 <guygrewal7@gmail.com>
2026-01-15 12:11:01 +0000

    HDPI-3261 - Minor change

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcetheorder/ShowConditionsWarrantOrWrit.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcetheorder/ShowConditionsWarrantOrWrit.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.reform.pcs.ccd.page.enforcetheorder;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel. PRIVATE)
 public final class ShowConditionsWarrantOrWrit {
 
     public static final String WARRANT_FLOW = "selectEnforcementType=\"WARRANT\"";
     public static final String WRIT_FLOW = "selectEnforcementType=\"WRIT\"";
 
-    private ShowConditionsWarrantOrWrit() {
-    }
 }
