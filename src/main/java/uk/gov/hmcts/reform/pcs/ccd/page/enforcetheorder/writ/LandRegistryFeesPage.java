155ac502da9e3bf1bea439fec0c0f7b315db50c9
Andrew Ajube <andrew.ajube@hmcts.net>
2026-01-19 09:08:11 +0000

    feat: Create LandRegistryFeesPage for writ flow

---
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcetheorder/writ/LandRegistryFeesPage.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.reform.pcs.ccd.page.enforcetheorder.writ;
+
+import uk.gov.hmcts.reform.pcs.ccd.common.CcdPageConfiguration;
+import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
+import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
+import uk.gov.hmcts.reform.pcs.ccd.domain.enforcetheorder.EnforcementOrder;
+import uk.gov.hmcts.reform.pcs.ccd.domain.enforcetheorder.warrant.LandRegistryFees;
+import uk.gov.hmcts.reform.pcs.ccd.domain.enforcetheorder.writ.WritDetails;
+import uk.gov.hmcts.reform.pcs.ccd.page.CommonPageContent;
+import uk.gov.hmcts.reform.pcs.ccd.page.enforcetheorder.ShowConditionsWarrantOrWrit;
+
+import static uk.gov.hmcts.reform.pcs.ccd.page.CommonPageContent.SAVE_AND_RETURN;
+
+public class LandRegistryFeesPage implements CcdPageConfiguration {
+
+    @Override
+    public void addTo(PageBuilder pageBuilder) {
+        pageBuilder
+                .page("landRegistryFeesWrit")
+                .pageLabel("Land Registry fees")
+                .showCondition(ShowConditionsWarrantOrWrit.WRIT_FLOW)
+                .label("landRegistryFeesWrit-content", "---")
+                .complex(PCSCase::getEnforcementOrder)
+                .complex(EnforcementOrder::getWritDetails)
+                .complex(WritDetails::getLandRegistryFees)
+                .mandatory(LandRegistryFees::getHaveLandRegistryFeesBeenPaid)
+                .mandatory(LandRegistryFees::getAmountOfLandRegistryFees, "writHaveLandRegistryFeesBeenPaid=\"YES\"")
+                    .done()
+                .done()
+                .label("landRegistryFeesWrit-save-and-return", SAVE_AND_RETURN);
+    }
+}
