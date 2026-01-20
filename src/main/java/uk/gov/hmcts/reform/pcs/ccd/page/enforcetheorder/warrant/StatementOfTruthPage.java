138895554ce58f2097abc374bb65903ffb089c74
Andrew Ajube <andrew.ajube@hmcts.net>
2026-01-20 12:07:47 +0000

    refactor: fix StatementOfTruthPage typos

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcetheorder/warrant/StatementOfTruthPage.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcetheorder/warrant/StatementOfTruthPage.java
@@ -43,11 +43,11 @@ public class StatementOfTruthPage implements CcdPageConfiguration {
                         .label("statementOfTruth-cert-suspended",
                             """
                             <ul class="govuk-list govuk-list--bullet">
-                                <li>the defendant has not vacated the land as ordered (* and that the whole or part of
+                                <li>the defendant has not vacated the land as ordered †(*and that the whole or part of
                                 any instalments due under the judgment or order have not been paid) (and the balance now
                                 due is as shown)</li>
                                 <li>notice has been given in accordance with The Dwelling Houses (Execution of
-                                Possession Order by Mortgagees) Regulations 2010.</li>
+                                Possession Orders by Mortgagees) Regulations 2010.</li>
                                 <li>a statement of the payments due and made under the judgment or order is attached to
                                 this request</li>
                             </ul>
@@ -57,11 +57,11 @@ public class StatementOfTruthPage implements CcdPageConfiguration {
                         .label("statementOfTruth-cert-not-suspended",
                             """
                             <ul class="govuk-list govuk-list--bullet">
-                                <li>the defendant has not vacated the land as ordered (* and that the whole or part of
+                                <li>the defendant has not vacated the land as ordered †(*and that the whole or part of
                                 any instalments due under the judgment or order have not been paid) (and the balance now
                                 due is as shown)</li>
                                 <li>notice has been given in accordance with The Dwelling Houses (Execution of
-                                Possession Order by Mortgagees) Regulations 2010.</li>
+                                Possession Orders by Mortgagees) Regulations 2010.</li>
                             </ul>
                             """,
                             "isSuspendedOrder=\"NO\""
