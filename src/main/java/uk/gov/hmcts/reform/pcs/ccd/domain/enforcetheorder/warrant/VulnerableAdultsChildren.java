7425a22c955ad95ad393fb22e466501a4fe9c240
guygrewal77 <163517291+guygrewal77@users.noreply.github.com>
2026-01-21 14:37:47 +0000

    HDPI-3261 - Review and confirm defendant name(s) and address(s) (Writ) (#1218)
    Co-authored-by: Andrew Ajube <andrew.ajube@hmcts.net>
    Co-authored-by: srinijg <srinivasan.joghee@hmcts.net>
    Co-authored-by: SameenaHMCTS <sameena.javeed@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/enforcetheorder/warrant/VulnerableAdultsChildren.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/enforcetheorder/warrant/VulnerableAdultsChildren.java
@@ -6,24 +6,25 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
+
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
 
-@Data
 @Builder
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class VulnerableAdultsChildren {
 
     @CCD(
-            label = "Confirm if the vulnerable people in the property are adults, children, or both adults and children"
+        label = "Confirm if the vulnerable people in the property are adults, children, or both adults and children"
     )
     private VulnerableCategory vulnerableCategory;
 
     @CCD(
-            label = "How are they vulnerable?",
-            hint = "You can enter up to 6,800 characters",
-            typeOverride = TextArea
+        label = "How are they vulnerable?",
+        hint = "You can enter up to 6,800 characters",
+        typeOverride = TextArea
     )
     private String vulnerableReasonText;
 }
