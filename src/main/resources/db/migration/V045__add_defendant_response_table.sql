cc08a0783a504b086ae3c5ec2e1c63e616ad10df
arun <arun.kumar@hmcts.net>
2026-01-26 09:35:09 +0000

    HDPI-3829: Add NOT NULL constraints and remove unnecessary columns from defendant_response table

--- src/main/resources/db/migration/V045__add_defendant_response_table.sql
+++ src/main/resources/db/migration/V045__add_defendant_response_table.sql
@@ -1,14 +1,9 @@
 CREATE TABLE defendant_response (
                     id UUID PRIMARY KEY,
-                    claim_id UUID REFERENCES claim(id),
-                    party_id UUID REFERENCES party(id),
+                    claim_id UUID NOT NULL REFERENCES claim(id),
+                    party_id UUID NOT NULL REFERENCES party(id),
                     response_type  VARCHAR(50) NOT NULL,
                     possession_order_type VARCHAR(30),
                     defence_submitted BOOLEAN NOT NULL,
-                    counterclaim_submitted BOOLEAN NOT NULL,
-                    additional_information VARCHAR(6800),
-                    created_at TIMESTAMP Default NOW(),
-                    updated_at TIMESTAMP Default NOW()
+                    counterclaim_submitted BOOLEAN NOT NULL
 );
-
-CREATE INDEX idx_defendant_response_id ON defendant_response(id);
