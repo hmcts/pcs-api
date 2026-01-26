3dec08692b59411f34eef65eb6e5101492e5c535
MoLala1 <mohammed.lala1@hmcts.net>
2026-01-26 12:06:38 +0000

    Update V045__add_defendant_response_table.sql
    Updating to reflect revised model in absence of figma screens

--- src/main/resources/db/migration/V045__add_defendant_response_table.sql
+++ src/main/resources/db/migration/V045__add_defendant_response_table.sql
@@ -2,8 +2,5 @@ CREATE TABLE defendant_response (
                     id UUID PRIMARY KEY,
                     claim_id UUID NOT NULL REFERENCES claim(id),
                     party_id UUID NOT NULL REFERENCES party(id),
-                    response_type  VARCHAR(50) NOT NULL,
-                    possession_order_type VARCHAR(30),
-                    defence_submitted BOOLEAN NOT NULL,
-                    counterclaim_submitted BOOLEAN NOT NULL
+                    possession_order_type VARCHAR(30)
 );
