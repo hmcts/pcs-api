3b43aba23bd74e9b1c5ca82a1ab3666c3c17d5c5
Andrew Ajube <andrew.ajube@hmcts.net>
2026-01-27 11:47:29 +0000

    feat: implement heading, as per UI designs

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcetheorder/writ/MoneyOwedPage.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcetheorder/writ/MoneyOwedPage.java
@@ -24,6 +24,9 @@ public class MoneyOwedPage implements CcdPageConfiguration {
             .label(
                 "amountDefendantOwes-amount-label",
                 """
+                    <p class="govuk-body govuk-!-font-weight-bold">
+                        What is the total amount that the defendants owe you?
+                    </p>
                     <p class="govuk-body govuk-!-margin-bottom-0">
                         You can include:
                         <ul class="govuk-list govuk-list--bullet">
