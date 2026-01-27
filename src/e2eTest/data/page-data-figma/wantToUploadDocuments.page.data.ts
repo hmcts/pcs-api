c4aef6cc03dd81da36362845348d122a0a3c0bef
SameenaHMCTS <sameena.javeed@hmcts.net>
2026-01-27 22:03:32 +0000

    Corrected the page data

--- src/e2eTest/data/page-data-figma/wantToUploadDocuments.page.data.ts
+++ src/e2eTest/data/page-data-figma/wantToUploadDocuments.page.data.ts
@@ -1,8 +1,11 @@
 export const wantToUploadDocuments = {
-  title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
+  makeAClaimCaption: `Make a claim`, // New record
   mainHeader: 'Upload additional documents',
-  continue: 'Continue',
   uploadAnyAdditionalDocumentsLabel: 'Do you want to upload any additional documents?',
-  yes: 'Yes',
-  no: 'No'
-}
+  instructionalText: `You can either upload documents now or closer to the hearing date. Any documents you upload now will be included in the pack of documents a judge will receive before the hearing (the bundle).`, // New record
+  yesRadioOption: 'Yes',
+  noRadioOption: 'No',
+  continueButton: 'Continue',
+  previousButton: `Previous`, // New record
+  cancelLink: `Cancel`, // New record
+};
