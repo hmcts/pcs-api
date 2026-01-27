f12cdf270fec3f4a654d460ac0aa5cf7b65ffc6c
SameenaHMCTS <sameena.javeed@hmcts.net>
2026-01-27 22:05:20 +0000

    Corrected the page data

--- src/e2eTest/data/page-data-figma/uploadAdditionalDocuments.page.data.ts
+++ src/e2eTest/data/page-data-figma/uploadAdditionalDocuments.page.data.ts
@@ -1,9 +1,18 @@
 export const uploadAdditionalDocuments = {
-  title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
+  makeAClaimCaption: `Make a claim`, // New record
   mainHeader: 'Upload additional documents',
-  continue: 'Continue',
+  instructionalText: `You must select the type of document you’re uploading and give it a short description.`, // New record
+  beforeYouUploadSubHeader: `Before you upload your documents`, // New record
+  namingHintText: `Give your document a name that explains what it is.`, // New record
+  addDocumentSubHeader: `Add document`, // New record
+  uploadSystemInstruction: `Upload a document to the system`, // New record
+  addNewButton: `Add new`, // New record
   typeOfDocument: 'Type of document',
-  shortDescriptionLabel: 'Short Description',
-  tenancyAgreementOption: 'Tenancy Agreement',
-  shortDescriptionInput: 'This is a tenancy agreement'
-}
+  documentFieldLabel: `Document`, // New record
+  chooseFileButton: `Choose file`, // New record
+  cancelUploadButton: `Cancel upload`, // New record
+  shortDescriptionLabel: 'Short description', // Updated casing
+  previousButton: `Previous`, // New record
+  continueButton: 'Continue',
+  cancelLink: `Cancel`, // New record
+};
