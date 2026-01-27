18562324e0ba4da6bf1de783780f9ba6f01463ad
Rehan Ayub <Rehan.Ayub@HMCTS.NET>
2026-01-27 16:24:36 +0000

    HDPI-3859 adding tests

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/event/SubmitDefendantResponseTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/event/SubmitDefendantResponseTest.java
@@ -8,10 +8,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.reform.pcs.ccd.domain.DefendantResponse;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
+import uk.gov.hmcts.reform.pcs.ccd.service.DefendantContactPreferencesService;
 import uk.gov.hmcts.reform.pcs.ccd.service.DraftCaseDataService;
-import uk.gov.hmcts.reform.pcs.ccd.service.PcsCaseService;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 @ExtendWith(MockitoExtension.class)
 class SubmitDefendantResponseTest extends BaseEventTest {
@@ -20,11 +21,14 @@ class SubmitDefendantResponseTest extends BaseEventTest {
     private DraftCaseDataService draftCaseDataService;
 
     @Mock
-    private PcsCaseService pcsCaseService;
+    private DefendantContactPreferencesService defendantContactPreferencesService;
 
     @BeforeEach
     void setUp() {
-        setEventUnderTest(new SubmitDefendantResponse(draftCaseDataService, pcsCaseService));
+        setEventUnderTest(new SubmitDefendantResponse(
+            draftCaseDataService,
+            defendantContactPreferencesService
+        ));
     }
 
     @Test
@@ -48,7 +52,68 @@ class SubmitDefendantResponseTest extends BaseEventTest {
             defendantResponse,
             EventId.submitDefendantResponse
         );
+        verifyNoInteractions(defendantContactPreferencesService);
+    }
 
+    @Test
+    void shouldSaveContactPreferencesWhenSubmitDraftIsYes() {
+        // Given
+        DefendantResponse defendantResponse = DefendantResponse.builder()
+            .contactByPhone(YesOrNo.YES)
+            .contactByEmail(YesOrNo.NO)
+            .contactByText(YesOrNo.YES)
+            .contactByPost(YesOrNo.NO)
+            .phoneNumber("07123456789")
+            .email("defendant@example.com")
+            .build();
+
+        PCSCase caseData = PCSCase.builder()
+            .defendantResponse(defendantResponse)
+            .submitDraftAnswers(YesOrNo.YES)
+            .build();
+
+        // When
+        callSubmitHandler(caseData);
+
+        // Then
+        verify(defendantContactPreferencesService).saveContactPreferences(defendantResponse);
+        verifyNoInteractions(draftCaseDataService);
+    }
+
+    @Test
+    void shouldNotCallServicesWhenDefendantResponseIsNull() {
+        // Given
+        PCSCase caseData = PCSCase.builder()
+            .defendantResponse(null)
+            .submitDraftAnswers(YesOrNo.YES)
+            .build();
+
+        // When
+        callSubmitHandler(caseData);
+
+        // Then
+        verifyNoInteractions(defendantContactPreferencesService);
+        verifyNoInteractions(draftCaseDataService);
+    }
+
+    @Test
+    void shouldNotCallServicesWhenSubmitDraftAnswersIsNull() {
+        // Given
+        DefendantResponse defendantResponse = DefendantResponse.builder()
+            .contactByPhone(YesOrNo.YES)
+            .build();
+
+        PCSCase caseData = PCSCase.builder()
+            .defendantResponse(defendantResponse)
+            .submitDraftAnswers(null)
+            .build();
+
+        // When
+        callSubmitHandler(caseData);
+
+        // Then
+        verifyNoInteractions(defendantContactPreferencesService);
+        verifyNoInteractions(draftCaseDataService);
     }
 
 }
