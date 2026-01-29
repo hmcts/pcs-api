3cc1c8619a45a88cd29d482c5fca837fa94fb7a0
SameenaHMCTS <sameena.javeed@hmcts.net>
2026-01-29 12:15:16 +0000

    Workers 2 and Retries 1

--- src/e2eTest/playwright.config.ts
+++ src/e2eTest/playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
-  workers: 4,
+  workers: 2,
   timeout: 600 * 1000,
   expect: { timeout: 30 * 1000 },
   use: { actionTimeout: 40 * 1000,  navigationTimeout: 40 * 1000, ...storageStateConfig },
