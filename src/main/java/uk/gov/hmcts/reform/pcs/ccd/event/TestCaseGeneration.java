91aa14fe9dd634a5315218a32b8f7ba36eb91d55
sstewart <scott.stewart@solirius.com>
2026-02-02 12:45:42 +0000

    Temporarily ignore test case creation event

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/TestCaseGeneration.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/TestCaseGeneration.java
@@ -54,7 +54,7 @@ public class TestCaseGeneration implements CCDConfig<PCSCase, State, UserRole> {
     @Override
     public void configureDecentralised(DecentralisedConfigBuilder<PCSCase, State, UserRole> configBuilder) {
         log.info("Configuring non-production support event: {}", EVENT_NAME);
-        if ("preview".equalsIgnoreCase(System.getenv().get("ENVIRONMENT"))
+        if ("zzzpreview".equalsIgnoreCase(System.getenv().get("ENVIRONMENT"))
             || Boolean.parseBoolean(System.getenv().get("ENABLE_TESTING_SUPPORT"))) {
             log.info("Test support enabled, configuring event: {}", EVENT_NAME);
             configure(configBuilder);
