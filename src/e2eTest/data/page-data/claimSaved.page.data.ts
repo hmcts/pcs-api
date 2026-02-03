ae61cc910132ffcaf5a7a9ecc2bd89251629dee7
SameenaHMCTS <sameena.javeed@hmcts.net>
2026-02-03 09:14:23 +0000

    Hdpi 3639 extend content validation part3 (#1321)

--- src/e2eTest/data/page-data/claimSaved.page.data.ts
+++
@@ -1,7 +0,0 @@
-export const claimSaved = {
-  title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
-  mainHeader: 'Make a claim',
-  closeAndReturnButton: 'Close and Return to case details',
-  claimSavedBanner: 'Claim saved',
-  draftOfClaimSavedParagraph: 'A draft of your claim has been saved. To sign, submit and pay for your claim: 1. Resume your claim. 2. Click through the questions. 3. Choose the ‘Submit and pay for my claim now’ option when asked how you’d like to complete your claim. 4. Select the ‘Pay the claim fee’ link on the confirmation screen.'
-}
