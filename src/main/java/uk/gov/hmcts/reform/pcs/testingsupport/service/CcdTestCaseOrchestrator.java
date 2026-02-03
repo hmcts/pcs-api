11e1f3d9ee2b458dbb66e70aee27b17f7fd5a7b3
marianadpereira <mariana.pereira@HMCTS.NET>
2026-02-03 11:26:58 +0000

    amending endpoint

--- src/main/java/uk/gov/hmcts/reform/pcs/testingsupport/service/CcdTestCaseOrchestrator.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/testingsupport/service/CcdTestCaseOrchestrator.java
@@ -13,9 +13,13 @@ import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.Event;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CaseResource;
 import uk.gov.hmcts.reform.pcs.ccd.CaseType;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
 import uk.gov.hmcts.reform.pcs.postcodecourt.model.LegislativeCountry;
+
+import java.io.InputStream;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -29,12 +33,12 @@ public class CcdTestCaseOrchestrator {
 
     private final AuthTokenGenerator s2sAuthTokenGenerator;
     private final CoreCaseDataApi coreCaseDataApi;
+    private final ObjectMapper objectMapper;
 
-    public Long createCase(String idamToken, LegislativeCountry legislativeCountry, JsonNode payloadOverride) {
+    public Map<String, Object> createCase(String idamToken, LegislativeCountry legislativeCountry,
+                                          JsonNode payloadMerge) {
 
         String s2sToken = s2sAuthTokenGenerator.generate();
-
-        log.info("About to start CCD event '{}'", CREATE_EVENT);
 
         PCSCase caseData = PCSCase.builder()
             .propertyAddress(addressFor(legislativeCountry))
@@ -47,7 +51,6 @@ public class CcdTestCaseOrchestrator {
             CASE_TYPE,
             CREATE_EVENT
         );
-        log.info("Started CCD event '{}' – token received", CREATE_EVENT);
 
         CaseDataContent createContent = CaseDataContent.builder()
             .event(Event.builder().id(CREATE_EVENT).build())
@@ -63,7 +66,6 @@ public class CcdTestCaseOrchestrator {
         );
 
         String caseId = caseDetails.getId().toString();
-        log.info("CCD case created successfully with caseId={}", caseId);
 
         StartEventResponse resumeEvent = coreCaseDataApi.startEvent(
             idamToken,
@@ -72,7 +74,7 @@ public class CcdTestCaseOrchestrator {
             RESUME_EVENT
         );
 
-        JsonNode resumePossessionClaimPayload = buildResumePossessionClaimPayload(payloadOverride);
+        JsonNode resumePossessionClaimPayload = buildResumePossessionClaimPayload(payloadMerge);
 
         CaseDataContent resumeContent = CaseDataContent.builder()
             .event(Event.builder().id(RESUME_EVENT).build())
@@ -80,7 +82,7 @@ public class CcdTestCaseOrchestrator {
             .data(resumePossessionClaimPayload)
             .build();
 
-        coreCaseDataApi.createEvent(
+        CaseResource caseResource = coreCaseDataApi.createEvent(
             idamToken,
             s2sToken,
             caseId,
@@ -88,22 +90,25 @@ public class CcdTestCaseOrchestrator {
         );
 
         log.info("Created CCD test case {}", caseId);
-        return Long.valueOf(caseId);
+
+        return Map.of(
+            "caseId", Long.valueOf(caseId),
+            "caseDetails", caseResource.getData()
+        );
     }
 
-
-    private JsonNode buildResumePossessionClaimPayload(JsonNode payloadOverride
+    private JsonNode buildResumePossessionClaimPayload(JsonNode payloadMerge
     ) {
-        ObjectNode base = baseResumePossessionClaimPayload();
+        ObjectNode base = getBasePayload();
 
-        if (payloadOverride != null && payloadOverride.isObject()) {
-            deepMerge(base, (ObjectNode) payloadOverride);
+        if (payloadMerge != null && payloadMerge.isObject()) {
+            mergePayload(base, (ObjectNode) payloadMerge);
         }
 
         return base;
     }
 
-    private void deepMerge(ObjectNode target, ObjectNode source) {
+    private void mergePayload(ObjectNode target, ObjectNode source) {
         source.fields().forEachRemaining(entry -> {
             String fieldName = entry.getKey();
             JsonNode sourceValue = entry.getValue();
@@ -113,7 +118,7 @@ public class CcdTestCaseOrchestrator {
                 && targetValue.isObject()
                 && sourceValue.isObject()) {
 
-                deepMerge((ObjectNode) targetValue, (ObjectNode) sourceValue);
+                mergePayload((ObjectNode) targetValue, (ObjectNode) sourceValue);
 
             } else {
                 target.set(fieldName, sourceValue);
@@ -121,114 +126,15 @@ public class CcdTestCaseOrchestrator {
         });
     }
 
-    private ObjectNode baseResumePossessionClaimPayload() {
-        try {
-            String json = """
-                {
-                        "legislativeCountry": "England",
-                        "claimantType": {
-                            "value": {
-                                "code": "PROVIDER_OF_SOCIAL_HOUSING",
-                                "label": "Registered provider of social housing"
-                            },
-                            "list_items": [
-                                {
-                                    "code": "PRIVATE_LANDLORD",
-                                    "label": "Private landlord"
-                                },
-                                {
-                                    "code": "PROVIDER_OF_SOCIAL_HOUSING",
-                                    "label": "Registered provider of social housing"
-                                },
-                                {
-                                    "code": "MORTGAGE_LENDER",
-                                    "label": "Mortgage lender"
-                                },
-                                {
-                                    "code": "OTHER",
-                                    "label": "Other"
-                                }
-                            ],
-                            "valueCode": "PROVIDER_OF_SOCIAL_HOUSING"
-                        },
-                        "claimAgainstTrespassers": "NO",
-                        "claimantName": "Possession Claims Solicitor Org",
-                        "isClaimantNameCorrect": "YES",
-                        "claimantContactEmail": "pcs-solicitor1@test.com",
-                        "isCorrectClaimantContactEmail": "YES",
-                        "orgAddressFound": "Yes",
-                        "organisationAddress": {
-                            "AddressLine1": "Ministry Of Justice",
-                            "AddressLine2": "Seventh Floor 102 Petty France",
-                            "PostTown": "London",
-                            "PostCode": "SW1H 9AJ",
-                            "Country": "United Kingdom"
-                        },
-                        "formattedClaimantContactAddress": "Ministry Of Justice<br>Seventh Floor 102 Petty \
-                        France<br>London<br>SW1H 9AJ",
-                        "isCorrectClaimantContactAddress": "YES",
-                        "claimantProvidePhoneNumber": "NO",
-                        "defendant1": {
-                            "nameKnown": "YES",
-                            "firstName": "John",
-                            "lastName": "doe",
-                            "addressKnown": "NO",
-                            "addressSameAsPossession": null,
-                            "correspondenceAddress": {
-                                "AddressLine1": null,
-                                "AddressLine2": null,
-                                "AddressLine3": null,
-                                "PostTown": null,
-                                "County": null,
-                                "Country": null,
-                                "PostCode": null
-                            }
-                        },
-                        "addAnotherDefendant": "NO",
-                        "tenancy_TypeOfTenancyLicence": "ASSURED_TENANCY",
-                        "tenancy_TenancyLicenceDate": null,
-                        "tenancy_TenancyLicenceDocuments": [],
-                        "claimDueToRentArrears": "No",
-                        "showRentSectionPage": "No",
-                        "noRentArrears_ShowGroundReasonPage": "Yes",
-                        "noRentArrears_MandatoryGrounds": [
-                            "REPOSSESSION_GROUND2"
-                        ],
-                        "noRentArrears_DiscretionaryGrounds": [],
-                        "repossessionByLenderTextArea": "test",
-                        "preActionProtocolCompleted": "NO",
-                        "mediationAttempted": "NO",
-                        "settlementAttempted": "NO",
-                        "noticeServed": "No",
-                        "claimantNamePossessiveForm": "Possession Claims Solicitor Org’s",
-                        "claimantCircumstancesSelect": "NO",
-                        "hasDefendantCircumstancesInfo": "NO",
-                        "suspensionOfRTB_ShowHousingActsPage": "Yes",
-                        "demotionOfTenancy_ShowHousingActsPage": "No",
-                        "suspensionToBuyDemotionOfTenancyPages": "No",
-                        "alternativesToPossession": [
-                            "SUSPENSION_OF_RIGHT_TO_BUY"
-                        ],
-                        "suspensionOfRTB_HousingAct": "SECTION_82A_2",
-                        "suspensionOfRTB_Reason": "test",
-                        "claimingCostsWanted": "NO",
-                        "additionalReasonsForPossession": {
-                            "hasReasons": "NO",
-                            "reasons": null
-                        },
-                        "hasUnderlesseeOrMortgagee": "NO",
-                        "wantToUploadDocuments": "NO",
-                        "applicationWithClaim": "NO",
-                        "languageUsed": "ENGLISH",
-                        "completionNextStep": "SAVE_IT_FOR_LATER"
-                    }
-                """;
+    private ObjectNode getBasePayload() {
+        try (InputStream basePayload = getClass()
+            .getClassLoader()
+            .getResourceAsStream("testcasesupport/Create-Case-Base.json")) {
 
-            ObjectMapper mapper = new ObjectMapper();
-            return (ObjectNode) mapper.readTree(json);
+            return (ObjectNode) objectMapper.readTree(basePayload);
 
         } catch (Exception e) {
-            throw new RuntimeException("Failed to build resumePossessionClaimPayload JSON", e);
+            throw new RuntimeException("Failed to read base payload JSON", e);
         }
     }
 
