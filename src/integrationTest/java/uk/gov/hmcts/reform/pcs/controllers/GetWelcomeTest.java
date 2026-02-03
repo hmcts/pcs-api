c8fdb317e8c943fede9023874ebc2e8c789afee1
sstewart <scott.stewart@solirius.com>
2026-02-03 18:23:42 +0000

    Revert "Trivial change to trigger build"
    This reverts commit fff113efea29e98d527b6ab05a1e21f1e4c43757.

--- src/integrationTest/java/uk/gov/hmcts/reform/pcs/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pcs/controllers/GetWelcomeTest.java
@@ -19,7 +19,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("integration")
 class GetWelcomeTest extends AbstractPostgresContainerIT {
 
-
     @Autowired
     private transient MockMvc mockMvc;
 
