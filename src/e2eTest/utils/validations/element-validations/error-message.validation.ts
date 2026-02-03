b215c9a6e851257f5239483b7aafaefc3818d57b
srinijg <srinivasan.joghee@hmcts.net>
2026-02-03 21:57:23 +0000

    error validation updated

--- src/e2eTest/utils/validations/element-validations/error-message.validation.ts
+++ src/e2eTest/utils/validations/element-validations/error-message.validation.ts
@@ -5,13 +5,13 @@ export class ErrorMessageValidation implements IValidation {
   async validate(page: Page, validation: string, fieldName: string, error: string | validationRecord): Promise<void> {
     let errorMessage;
     if (typeof error === 'string') {
-      errorMessage = page.locator(`a.validation-error:has-text("${error}")`);
+      errorMessage = page.locator(`a.validation-error:text-is("${error}")`);
     } else {
       errorMessage = page.locator(`
-        h3.error-summary-heading:has-text("${error.header}") + p:has-text("${error.message}"),
-        h3.error-summary-heading:has-text("${error.header}") ~ #errors li:has-text("${error.message}"),
-        h2#error-summary-title:has-text("${error.header}") ~ div ul li a:has-text("${error.message}"),
-        h3#edit-case-event_error-summary-heading ~ ul li:has-text("${error.message}")
+        h3.error-summary-heading:text-is("${error.header}") + p:text-is("${error.message}"),
+        h3.error-summary-heading:text-is("${error.header}") ~ #errors li:text-is("${error.message}"),
+        h2#error-summary-title:text-is("${error.header}") ~ div ul li a:text-is("${error.message}"),
+        h3#edit-case-event_error-summary-heading ~ ul li:text-is("${error.message}")
       `);
     }
     await expect(errorMessage).toBeVisible();
