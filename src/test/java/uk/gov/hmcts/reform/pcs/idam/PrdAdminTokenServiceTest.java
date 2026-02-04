aa14bf6edcc16d7d50ce10647c88e63b414fd55d
arun <arun.kumar@hmcts.net>
2026-02-04 08:57:24 +0000

    Fix checkstyle violation - replace wildcard Mockito import with explicit imports
    Replace 'import static org.mockito.Mockito.*;' with explicit imports:
    - mock
    - when
    - verify
    - times
    - atLeastOnce
    
    Fixes Jenkins build failure: AvoidStarImport checkstyle rule

--- src/test/java/uk/gov/hmcts/reform/pcs/idam/PrdAdminTokenServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/idam/PrdAdminTokenServiceTest.java
@@ -18,7 +18,11 @@ import java.time.Instant;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PrdAdminTokenServiceTest {
