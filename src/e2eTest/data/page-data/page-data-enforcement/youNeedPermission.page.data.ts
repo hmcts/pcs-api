d9595abf874af5fa2c787d2d931c02cb841e1290
ashajayaprakash <asha.jayaprakash@hmcts.net>
2026-02-05 13:51:02 +0000

    HDPI-1484-Automation

--- src/e2eTest/data/page-data/page-data-enforcement/youNeedPermission.page.data.ts
+++ src/e2eTest/data/page-data/page-data-enforcement/youNeedPermission.page.data.ts
@@ -2,7 +2,7 @@ export const youNeedPermission = {
   title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
   mainHeader: 'You need permission from a judge to change the name and address for the eviction',
   askTheJudgeLink: 'Ask the judge if you can change the name or address for the eviction (GOV.UK, opens in a new tab).',
-  errors: 'The event could not be created',
+  eventCouldNotBeCreated: 'The event could not be created',
   errMessage: 'You cannot continue with this application until you ask the judge for permission to change the name and address.',
   continueButton: 'Continue'
 }
