2f7949d51eb733ec080da3c72ea1e95169aef659
sstewart <scott.stewart@solirius.com>
2026-02-06 18:15:26 +0000

    HDPI-3553: Don't override DataSource for config-gen profile

--- src/main/java/uk/gov/hmcts/reform/pcs/config/DataSourceConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/config/DataSourceConfiguration.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.pcs.config;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 
 import javax.sql.DataSource;
@@ -11,6 +12,7 @@ import javax.sql.DataSource;
 public class DataSourceConfiguration {
 
     @Bean
+    @Profile("!config-gen")
     public DataSource dataSource(DataSourceProperties properties) {
         // We use a transaction-aware proxy so that DB Scheduler joins the active transaction
         // when scheduling tasks
