02124663737561051417a53667f70f42c87f1a9d
sstewart <scott.stewart@solirius.com>
2026-02-06 15:18:53 +0000

    HDPI-3553: Use TransactionAwareDataSourceProxy so that DB Scheduler connections are transaction-aware
    See the second point in the DB Scheduler README gotchas:
    
    https://github.com/kagkarlsson/db-scheduler?tab=readme-ov-file#things-to-note--gotchas

---
+++ src/main/java/uk/gov/hmcts/reform/pcs/config/DataSourceConfiguration.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.pcs.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Bean
+    public DataSource dataSource(DataSourceProperties properties) {
+        // We use a transaction-aware proxy so that DB Scheduler joins the active transaction
+        // when scheduling tasks
+
+        DataSource dataSource = properties.initializeDataSourceBuilder().build();
+        return new TransactionAwareDataSourceProxy(dataSource);
+    }
+
+}
