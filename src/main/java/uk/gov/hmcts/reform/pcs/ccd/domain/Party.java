bbdb0467c4261d65f7d0d89999e10fdf1a35e01e
arun <arun.kumar@hmcts.net>
2026-02-09 09:03:46 +0000

    HDPI-4099: Remove field-level access control annotations from Party class

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/Party.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/Party.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.AddressUK;
-import uk.gov.hmcts.reform.pcs.ccd.accesscontrol.CitizenAccess;
 
 @Builder
 @Data
@@ -14,34 +12,25 @@ import uk.gov.hmcts.reform.pcs.ccd.accesscontrol.CitizenAccess;
 @AllArgsConstructor
 public class Party {
 
-    @CCD(access = {CitizenAccess.class})
     private String firstName;
 
-    @CCD(access = {CitizenAccess.class})
     private String lastName;
 
-    @CCD(access = {CitizenAccess.class})
     private String orgName;
 
-    @CCD(access = {CitizenAccess.class})
     private VerticalYesNo nameKnown;
 
     // emailAddress: NO CitizenAccess annotation - stored in DB but NOT exposed to citizens
     private String emailAddress;
 
-    @CCD(access = {CitizenAccess.class})
     private AddressUK address;
 
-    @CCD(access = {CitizenAccess.class})
     private VerticalYesNo addressKnown;
 
-    @CCD(access = {CitizenAccess.class})
     private VerticalYesNo addressSameAsProperty;
 
-    @CCD(access = {CitizenAccess.class})
     private String phoneNumber;
 
-    @CCD(access = {CitizenAccess.class})
     private VerticalYesNo phoneNumberProvided;
 
 }
