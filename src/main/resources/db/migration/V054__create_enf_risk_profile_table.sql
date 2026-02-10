2b9472fc2ab9368fd7dac58b14fea3ccf95a83cb
Andrew Ajube <andrew.ajube@hmcts.net>
2026-02-10 15:46:02 +0000

    refactor: Update enf_risk_profile table to use VARCHAR for vulnerable category and details fields

--- src/main/resources/db/migration/V054__create_enf_risk_profile_table.sql
+++ src/main/resources/db/migration/V054__create_enf_risk_profile_table.sql
@@ -1,25 +1,19 @@
 CREATE TYPE YES_NO_NOT_SURE AS ENUM ('YES', 'NO', 'NOT_SURE');
 
-CREATE TYPE VULNERABLE_CATEGORY AS ENUM (
-    'VULNERABLE_ADULTS',
-    'VULNERABLE_CHILDREN',
-    'VULNERABLE_ADULTS_AND_CHILDREN'
-);
-
 CREATE TABLE enf_risk_profile (
     id UUID PRIMARY KEY,
     enf_case_id UUID NOT NULL REFERENCES enf_case(id) ON DELETE CASCADE,
     any_risk_to_bailiff YES_NO_NOT_SURE,
     vulnerable_people_present YES_NO_NOT_SURE,
-    vulnerable_category VULNERABLE_CATEGORY,
-    vulnerable_reason_text TEXT,
-    violent_details TEXT,
-    firearms_details TEXT,
-    criminal_details TEXT,
-    verbal_threats_details TEXT,
-    protest_group_details TEXT,
-    police_social_services_details TEXT,
-    animals_details TEXT,
+    vulnerable_category VARCHAR(100),
+    vulnerable_reason_text VARCHAR(6400),
+    violent_details VARCHAR(6400),
+    firearms_details VARCHAR(6400),
+    criminal_details VARCHAR(6400),
+    verbal_threats_details VARCHAR(6400),
+    protest_group_details VARCHAR(6400),
+    police_social_services_details VARCHAR(6400),
+    animals_details VARCHAR(6400),
     CONSTRAINT unique_risk_profile_per_case UNIQUE(enf_case_id)
 );
 
