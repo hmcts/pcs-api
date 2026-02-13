15cd2d07ac558b50d77d5061839a82ec1a6368f3
sstewart <scott.stewart@solirius.com>
2026-02-13 12:15:08 +0000

    HDPI-4135: Normalise postcode spacing and case when saving

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/util/AddressMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/util/AddressMapperTest.java
@@ -1,152 +1,86 @@
 package uk.gov.hmcts.reform.pcs.ccd.util;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 import uk.gov.hmcts.ccd.sdk.type.AddressUK;
 import uk.gov.hmcts.reform.pcs.ccd.entity.AddressEntity;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class AddressMapperTest {
 
-    private final AddressMapper underTest = new AddressMapper();
+    @Mock
+    private ModelMapper modelMapper;
 
-    @Test
-    void shouldReturnAddressUKWithAllNullFieldsWhenAddressEntityIsNull() {
-        // When
-        AddressUK result = underTest.toAddressUK(null);
+    private AddressMapper underTest;
 
-        // Then
-        assertThat(result).isNotNull();
-        assertThat(result.getAddressLine1()).isNull();
-        assertThat(result.getAddressLine2()).isNull();
-        assertThat(result.getAddressLine3()).isNull();
-        assertThat(result.getPostTown()).isNull();
-        assertThat(result.getCounty()).isNull();
-        assertThat(result.getPostCode()).isNull();
-        assertThat(result.getCountry()).isNull();
+    @BeforeEach
+    void setUp() {
+        underTest = new AddressMapper(modelMapper);
     }
 
     @Test
-    void shouldMapAllFieldsCorrectlyWhenAddressEntityHasAllFieldsPopulated() {
+    void shouldMapAddressEntityToAddressUK() {
         // Given
-        AddressEntity addressEntity = AddressEntity.builder()
-            .addressLine1("123 Test Street")
-            .addressLine2("Flat 4B")
-            .addressLine3("Building Name")
-            .postTown("Manchester")
-            .county("Greater Manchester")
-            .postcode("M1 1AA")
-            .country("United Kingdom")
-            .build();
+        AddressEntity addressEntity = mock(AddressEntity.class);
+        AddressUK expectedAddressUK = mock(AddressUK.class);
+        when(modelMapper.map(addressEntity, AddressUK.class)).thenReturn(expectedAddressUK);
 
         // When
         AddressUK result = underTest.toAddressUK(addressEntity);
 
         // Then
-        assertThat(result).isNotNull();
-        assertThat(result.getAddressLine1()).isEqualTo("123 Test Street");
-        assertThat(result.getAddressLine2()).isEqualTo("Flat 4B");
-        assertThat(result.getAddressLine3()).isEqualTo("Building Name");
-        assertThat(result.getPostTown()).isEqualTo("Manchester");
-        assertThat(result.getCounty()).isEqualTo("Greater Manchester");
-        assertThat(result.getPostCode()).isEqualTo("M1 1AA");
-        assertThat(result.getCountry()).isEqualTo("United Kingdom");
+        assertThat(result).isEqualTo(expectedAddressUK);
     }
 
-    @Test
-    void shouldMapNullFieldsCorrectlyWhenAddressEntityHasPartialFields() {
-        // Given - only mandatory fields populated, optional fields are null
-        AddressEntity addressEntity = AddressEntity.builder()
-            .addressLine1("5 Second Avenue")
-            .addressLine2(null)
-            .addressLine3(null)
+    @ParameterizedTest
+    @MethodSource("postcodeScenarios")
+    void shouldMapAddressUKToEntityAndNormalise(String rawPostcode, String expectedNormalisedPostcode) {
+        // Given
+        AddressUK addressUK = mock(AddressUK.class);
+        AddressEntity mappedAddressEntity = AddressEntity.builder()
+            .addressLine1("10 High Street")
             .postTown("London")
-            .county(null)
-            .postcode("W3 7RX")
-            .country("United Kingdom")
+            .postcode(rawPostcode)
             .build();
 
+        when(modelMapper.map(addressUK, AddressEntity.class)).thenReturn(mappedAddressEntity);
+
         // When
-        AddressUK result = underTest.toAddressUK(addressEntity);
+        AddressEntity normalisedAddressEntity = underTest.toAddressEntityAndNormalise(addressUK);
 
         // Then
-        assertThat(result).isNotNull();
-        assertThat(result.getAddressLine1()).isEqualTo("5 Second Avenue");
-        assertThat(result.getAddressLine2()).isNull();
-        assertThat(result.getAddressLine3()).isNull();
-        assertThat(result.getPostTown()).isEqualTo("London");
-        assertThat(result.getCounty()).isNull();
-        assertThat(result.getPostCode()).isEqualTo("W3 7RX");
-        assertThat(result.getCountry()).isEqualTo("United Kingdom");
+        assertThat(normalisedAddressEntity.getPostcode()).isEqualTo(expectedNormalisedPostcode);
     }
 
-    @Test
-    void shouldMapPostcodeFieldCorrectly() {
-        // Given - Testing the field name mapping: AddressEntity.postcode → AddressUK.postCode
-        AddressEntity addressEntity = AddressEntity.builder()
-            .addressLine1("Test Address")
-            .postTown("Test Town")
-            .postcode("TEST123")
-            .build();
-
-        // When
-        AddressUK result = underTest.toAddressUK(addressEntity);
-
-        // Then - Verify postcode (lowercase 'c') maps to PostCode (uppercase 'C')
-        assertThat(result.getPostCode()).isEqualTo("TEST123");
+    private static Stream<Arguments> postcodeScenarios() {
+        return Stream.of(
+            arguments("B12CD", "B1 2CD"),
+            arguments("B1 2CD", "B1 2CD"),
+            arguments("B 12 CD", "B1 2CD"),
+            arguments("B12   CD", "B1 2CD"),
+            arguments("AB12CD", "AB1 2CD"),
+            arguments("  AB12CD  ", "AB1 2CD"),
+            arguments(" AB1 2CD  ", "AB1 2CD"),
+            arguments("AB1 2CD", "AB1 2CD"),
+            arguments("AB12 CD", "AB1 2CD"),
+            arguments("A B12CD", "AB1 2CD"),
+            arguments("a12Cd", "A1 2CD"),
+            arguments("W1a 1a A", "W1A 1AA")
+        );
     }
 
-    @Test
-    void shouldConvertEmptyStringsToNullInAddressEntity() {
-        // Given
-        AddressEntity addressEntity = AddressEntity.builder()
-            .addressLine1("")
-            .addressLine2("")
-            .addressLine3("")
-            .postTown("")
-            .county("")
-            .postcode("")
-            .country("")
-            .build();
-
-        // When
-        AddressUK result = underTest.toAddressUK(addressEntity);
-
-        // Then - Empty strings should be converted to null for consistency
-        assertThat(result).isNotNull();
-        assertThat(result.getAddressLine1()).isNull();
-        assertThat(result.getAddressLine2()).isNull();
-        assertThat(result.getAddressLine3()).isNull();
-        assertThat(result.getPostTown()).isNull();
-        assertThat(result.getCounty()).isNull();
-        assertThat(result.getPostCode()).isNull();
-        assertThat(result.getCountry()).isNull();
-    }
-
-    @Test
-    void shouldHandleMixOfEmptyStringsAndNullValues() {
-        // Given
-        AddressEntity addressEntity = AddressEntity.builder()
-            .addressLine1("123 Test Street")
-            .addressLine2("")  // Empty string
-            .addressLine3(null)  // Null
-            .postTown("London")
-            .county("")  // Empty string
-            .postcode("W1A 1AA")
-            .country(null)  // Null
-            .build();
-
-        // When
-        AddressUK result = underTest.toAddressUK(addressEntity);
-
-        // Then - Both empty strings and nulls should be normalized to null
-        assertThat(result).isNotNull();
-        assertThat(result.getAddressLine1()).isEqualTo("123 Test Street");
-        assertThat(result.getAddressLine2()).isNull();
-        assertThat(result.getAddressLine3()).isNull();
-        assertThat(result.getPostTown()).isEqualTo("London");
-        assertThat(result.getCounty()).isNull();
-        assertThat(result.getPostCode()).isEqualTo("W1A 1AA");
-        assertThat(result.getCountry()).isNull();
-    }
 }
