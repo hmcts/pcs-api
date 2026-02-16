694bc36563aa624fda5e804259b2a2877517e564
arun <arun.kumar@hmcts.net>
2026-02-16 20:31:05 +0000

    HDPI-4099: Remove duplicate enum creation - reuse YES_NO_NOT_SURE from V055

--- src/main/resources/db/migration/V056__add_defendant_response_columns.sql
+++ src/main/resources/db/migration/V056__add_defendant_response_columns.sql
@@ -1,14 +1,7 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'yes_no_not_sure') THEN
-        CREATE TYPE yes_no_not_sure AS ENUM ('YES', 'NO', 'NOT_SURE');
-    END IF;
-END$$;
-
 ALTER TABLE defendant_response
-    ADD COLUMN tenancy_type_correct yes_no_not_sure,
-    ADD COLUMN tenancy_start_date_correct yes_no_not_sure,
-    ADD COLUMN owe_rent_arrears yes_no_not_sure,
+    ADD COLUMN tenancy_type_correct YES_NO_NOT_SURE,
+    ADD COLUMN tenancy_start_date_correct YES_NO_NOT_SURE,
+    ADD COLUMN owe_rent_arrears YES_NO_NOT_SURE,
     ADD COLUMN rent_arrears_amount NUMERIC(19, 2),
-    ADD COLUMN notice_received yes_no_not_sure,
+    ADD COLUMN notice_received YES_NO_NOT_SURE,
     ADD COLUMN notice_received_date DATE;
