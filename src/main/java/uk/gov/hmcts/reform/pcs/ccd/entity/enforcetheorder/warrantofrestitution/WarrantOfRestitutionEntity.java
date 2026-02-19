db8981005a9e168d07f4a9af315167f3b7a5b865
guygrewal77 <guygrewal7@gmail.com>
2026-02-19 21:17:28 +0000

    HDPI-3966 - Fixed entity class

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/entity/enforcetheorder/warrantofrestitution/WarrantOfRestitutionEntity.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/entity/enforcetheorder/warrantofrestitution/WarrantOfRestitutionEntity.java
@@ -6,17 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-import uk.gov.hmcts.reform.pcs.ccd.entity.ClaimEntity;
 import uk.gov.hmcts.reform.pcs.ccd.entity.enforcetheorder.EnforcementOrderEntity;
 
 import java.util.UUID;
 
-import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Table(name = "enf_warrant_of_restitution")
@@ -27,11 +24,6 @@ public class WarrantOfRestitutionEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "claim_id", nullable = false)
-    @JsonBackReference
-    private ClaimEntity claim;
 
     @OneToOne
     @JoinColumn(name = "enf_case_id", nullable = false)
