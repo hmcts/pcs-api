7b1210c6679110d54c6776035216117b5d25ff88
SameenaHMCTS <sameena.javeed@hmcts.net>
2026-02-25 16:57:19 +0000

    Fixed based on failures wrt page data and locators

--- src/e2eTest/data/page-data-figma/preActionProtocol.page.data.ts
+++ src/e2eTest/data/page-data-figma/preActionProtocol.page.data.ts
@@ -1,19 +1,19 @@
-// Below commented content will be enabled once wales specific content issue also fixed  https://tools.hmcts.net/jira/browse/HDPI-3491 bug
+//Below commented content needs to be verified as part of bug https://tools.hmcts.net/jira/browse/HDPI-4637 - Wales journey
 export const preactionProtocol = {
-  mainHeader: `Pre-action protocol`,
-  //appliedForArrearsToBeParagraph: `applied for arrears to be paid by the Department for Work and Pensions (DWP) by deductions from the defendants’ benefits`,
-  cancelLink: `Cancel`,
-  //contactedOrAttemptedToContactParagraph: `contacted, or attempted to contact, the defendants`,
-  continueButton: `Continue`,
-  haveYouFollowedThePreactionQuestion: `Have you followed the pre-action protocol?`,
-  ifYourClaimIsOnDynamicParagraph: `If your claim is on the grounds of rent arrears, you should have:`,
   makeAClaimCaption: `Make a claim`,
-  noRadioOption: `No`,
-  //offeredToAssistTheDefendantsParagraph: `offered to assist the defendants in a claim for housing benefit or Universal Credit`,
-  previousButton: `Previous`,
+  mainHeader: `Pre-action protocol`,
   registeredProvidersDynamicParagraph: `Registered providers of social housing should follow the pre-action protocol before making a housing possession claim. You should have:`,
   communityLandlordsDynamicParagraph: `Community landlords should follow the pre-action protocol before making a housing possession claim. You should have:`,
+  //contactedOrAttemptedToContactParagraph: `contacted, or attempted to contact, the defendants`,
+  ifYourClaimIsOnDynamicParagraph: `If your claim is on the grounds of rent arrears, you should have:`,
   //triedToAgreeARepaymentParagraph: `tried to agree a repayment plan`,
-  yesRadioOption: `Yes`,
+  //appliedForArrearsToBeParagraph: `applied for arrears to be paid by the Department for Work and Pensions (DWP) by deductions from the defendants’ benefits`,
+  //offeredToAssistTheDefendantsParagraph: `offered to assist the defendants in a claim for housing benefit or Universal Credit`,
   //yourCaseCouldBeDelayedParagraph: `Your case could be delayed or rejected if you have not followed the  pre-action protocol and completed all the steps.`
+  haveYouFollowedThePreactionQuestion: `Have you followed the pre-action protocol?`,
+  yesRadioOption: `Yes`,
+  noRadioOption: `No`,
+  previousButton: `Previous`,
+  continueButton: `Continue`,
+  cancelLink: `Cancel`,
 };
