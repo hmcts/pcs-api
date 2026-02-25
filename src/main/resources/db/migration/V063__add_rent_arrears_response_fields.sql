3bca28129e41b7f18a9fd34ed758f91818dcc24b
arun <arun.kumar@hmcts.net>
2026-02-25 14:43:58 +0000

    HDPI-3495: Add test coverage for rent arrears fields (oweRentArrears and rentArrearsAmount)

--- src/main/resources/db/migration/V063__add_rent_arrears_response_fields.sql
+++ src/main/resources/db/migration/V063__add_rent_arrears_response_fields.sql
@@ -1,3 +1,3 @@
 ALTER TABLE defendant_response
-    ADD COLUMN owe_rent_arrears YES_NO_NOT_SURE,
-    ADD COLUMN rent_arrears_amount NUMERIC(19, 2);
+    ADD COLUMN IF NOT EXISTS owe_rent_arrears YES_NO_NOT_SURE,
+    ADD COLUMN IF NOT EXISTS rent_arrears_amount NUMERIC(19, 2);
