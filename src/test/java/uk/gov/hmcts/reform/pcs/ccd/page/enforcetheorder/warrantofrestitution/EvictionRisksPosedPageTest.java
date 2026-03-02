0b29d52bdfd4324631fba6c32dc536711d346c8f
Andrew Ajube <andrew.ajube@hmcts.net>
2026-03-02 11:07:51 +0000

    test: ensure restitution risk details initialised

---
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcetheorder/warrantofrestitution/EvictionRisksPosedPageTest.java
@@ -0,0 +1,57 @@
+package uk.gov.hmcts.reform.pcs.ccd.page.enforcetheorder.warrantofrestitution;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
+import uk.gov.hmcts.reform.pcs.ccd.domain.State;
+import uk.gov.hmcts.reform.pcs.ccd.domain.enforcetheorder.EnforcementOrder;
+import uk.gov.hmcts.reform.pcs.ccd.domain.enforcetheorder.warrant.EnforcementRiskDetails;
+import uk.gov.hmcts.reform.pcs.ccd.domain.enforcetheorder.warrant.RiskCategory;
+import uk.gov.hmcts.reform.pcs.ccd.domain.enforcetheorder.warrant.WarrantDetails;
+import uk.gov.hmcts.reform.pcs.ccd.page.BasePageTest;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("EvictionRisksPosedPage (warrant of restitution) tests")
+class EvictionRisksPosedPageTest extends BasePageTest {
+
+    @BeforeEach
+    void setUp() {
+        setPageUnderTest(new EvictionRisksPosedPage());
+    }
+
+    @Test
+    @DisplayName("Should initialise riskDetails when null")
+    void shouldInitialiseRiskDetailsWhenNull() {
+        // Given
+        PCSCase caseData = PCSCase.builder()
+            .enforcementOrder(
+                EnforcementOrder.builder()
+                    .warrantDetails(
+                        WarrantDetails.builder()
+                            .riskCategories(Set.of(RiskCategory.VIOLENT_OR_AGGRESSIVE))
+                            .riskDetails(null)
+                            .build()
+                    )
+                    .build()
+            )
+            .build();
+
+        // When
+        AboutToStartOrSubmitResponse<PCSCase, State> response = callMidEventHandler(caseData);
+
+        // Then
+        assertThat(response.getErrors()).isNullOrEmpty();
+        assertThat(response.getData()
+                       .getEnforcementOrder()
+                       .getWarrantDetails()
+                       .getRiskDetails())
+            .isNotNull()
+            .isInstanceOf(EnforcementRiskDetails.class);
+    }
+}
+
