0198afc95cf76a2b7f2caadd855986efa70c12f1
tvr-solirius <trevor.hinson@solirius.com>
2026-03-04 16:07:31 +0000

    Add submission_date to enforcement tables, add language_used and show_change_name_address_page columns, drop obsolete show_people_who_will_be_evicted_page from Writ, and add enforcement_risk_categories to risk_details.

--- src/main/resources/db/migration/V063__enforcement_data_model_updates.sql
+++ src/main/resources/db/migration/V063__enforcement_data_model_updates.sql
@@ -1,3 +1,9 @@
+-- Add Timestamps submission date to each of the existing tables !!!
+ALTER TABLE public.enf_warrant ADD COLUMN submission_date DATE;
+ALTER TABLE public.enf_writ ADD COLUMN submission_date DATE;
+ALTER TABLE public.enf_warrant_of_restitution ADD COLUMN submission_date DATE;
+ALTER TABLE public.enf_writ_of_restitution ADD COLUMN submission_date DATE;
+
 -- Add Language used to each existing table
 ALTER TABLE public.enf_warrant ADD COLUMN language_used VARCHAR(30);
 ALTER TABLE public.enf_writ ADD COLUMN language_used VARCHAR(30);
@@ -5,9 +11,6 @@ ALTER TABLE public.enf_warrant_of_restitution ADD COLUMN language_used VARCHAR(3
 ALTER TABLE public.enf_writ_of_restitution ADD COLUMN language_used VARCHAR(30);
 
 ALTER TABLE public.enf_warrant ADD COLUMN show_change_name_address_page YES_NO;
-
--- Not needed within Writ
-ALTER TABLE public.enf_writ DROP COLUMN show_people_who_will_be_evicted_page;
 
 ALTER TABLE public.enf_writ ADD COLUMN was_general_application_to_transfer_to_high_court_successful YES_NO;
 ALTER TABLE public.enf_writ ADD COLUMN completed_by STATEMENT_OF_TRUTH_COMPLETED_BY;
@@ -18,11 +21,10 @@ ALTER TABLE public.enf_writ ADD COLUMN certification TEXT;
   amount_of_repayment_costs               NUMERIC(10, 2),
 
 
-
-
 ALTER TABLE public.enf_risk_details ADD COLUMN enforcement_risk_categories VARCHAR(6800);
 
-
+-- Not needed within Writ
+ALTER TABLE public.enf_writ DROP COLUMN show_people_who_will_be_evicted_page;
 
 
 
