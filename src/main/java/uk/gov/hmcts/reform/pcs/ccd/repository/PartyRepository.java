aa0ad801f5abdaf72169f7cad879d0d214ec268c
sstewart <scott.stewart@solirius.com>
2026-03-05 19:02:40 +0000

    HDPI-5180: Refactor code that gets a party by IDAM ID
    Moving the party lookup code into PartyService so it can be shared.
    
    Updating the query to join to the pcs_case table so that a single query
    can be made with the case reference.

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/repository/PartyRepository.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/repository/PartyRepository.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.pcs.ccd.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.pcs.ccd.entity.party.PartyEntity;
 
@@ -9,5 +11,8 @@ import java.util.UUID;
 
 @Repository
 public interface PartyRepository extends JpaRepository<PartyEntity, UUID> {
-    Optional<PartyEntity> findByIdamIdAndPcsCaseId(UUID idamId, UUID caseId);
+
+    @Query("SELECT p FROM PartyEntity p WHERE p.pcsCase.caseReference = :caseReference")
+    Optional<PartyEntity> findByIdamId(@Param("idamId") UUID idamId, @Param("caseReference") long caseReference);
+
 }
