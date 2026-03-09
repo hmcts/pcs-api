03adbd7a7252964fbdc27c81f8f3904fac9c8fb1
ankita <ankita.kumbhare@hmcts.net>
2026-03-09 12:10:49 +0000

    Adding Figma Pages and controller logic for page content validation

---
+++ src/e2eTest/data/page-data-figma/page-data-enforcement-figma/confirmHCEOfficer.page.data.ts
@@ -0,0 +1,12 @@
+export const enforceTheOrderconfirmHCEOfficer = {
+  //title: `Create a case - HM Courts & Tribunals Service - GOV.UK`,
+  mainHeader: `Confirm if you have already hired a High Court Enforcement Officer`,
+  haveYouHiredHCEOQuestion: `Have you hired a High Court Enforcement Officer?`,
+  yesRadioOption: `Yes`,
+  noRadioOption: `No`, 
+  summarySaveApplicationLink: `I want to save this application and return to it later`,
+  previousButton: `Previous`,
+  continueButton: `Continue`,
+  cancelLink: `Cancel`,
+  iDoNotKnowLink: `I do not know if I need to hire a High Court Enforcement Officer`
+}
