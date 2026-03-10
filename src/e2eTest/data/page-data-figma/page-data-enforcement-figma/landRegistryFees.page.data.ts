6899e72f0261a0fe03c8b813916ca2900a5e9d4e
ankita <ankita.kumbhare@hmcts.net>
2026-03-10 10:26:15 +0000

    Adding Figma Pages and controller logic for page content validation

--- src/e2eTest/data/page-data-figma/page-data-enforcement-figma/landRegistryFees.page.data.ts
+++ src/e2eTest/data/page-data-figma/page-data-enforcement-figma/landRegistryFees.page.data.ts
@@ -3,8 +3,8 @@ export const landRegistryFees = {
   EnforceTheOrderCaption: `Enforce the order`,
   mainHeader: `Land Registry fees`,
   landRegistryFeeQuestion: `Have you paid any Land Registry fees?`,
-  forExampleHintText: `For example, if you paid the Land Registry a fee to view the property boundary. 
-If you have paid a Land Registry fee, but you do not want the defendant to repay it, you can choose ‘No’`,
+  forExampleHintText: `For example, if you paid the Land Registry a fee to view the property boundary. If you have paid a Land Registry fee, but you do not want the defendant to repay it, you can choose ‘No’`,
+  //Updated above hint text to be consistent with the application. Defect HDPI-5199 has been raised.
   yesRadioOption: `Yes`,
   noRadioOption: `No`,
   howMuchYouSpendOnLandRegistryFeeTextLabelHidden: `How much did you spend on Land Registry fees?`,
