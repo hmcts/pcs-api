de135cf2f591e2d8d5bad6baf4aaad7a05e1b6cd
Andrew Ajube <andrew.ajube@hmcts.net>
2026-03-10 16:36:31 +0000

    feat: add save and return label to VulnerableAdultsChildrenPlaceholderPage

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcetheorder/warrantofrestitution/VulnerableAdultsChildrenPlaceholderPage.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/enforcetheorder/warrantofrestitution/VulnerableAdultsChildrenPlaceholderPage.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.pcs.ccd.common.CcdPageConfiguration;
 import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
 
+import static uk.gov.hmcts.reform.pcs.ccd.page.CommonPageContent.SAVE_AND_RETURN;
 import static uk.gov.hmcts.reform.pcs.ccd.page.enforcetheorder.ShowConditionsEnforcementType.WARRANT_OF_RESTITUTION_FLOW;
 
 @Component
@@ -15,7 +16,8 @@ public class VulnerableAdultsChildrenPlaceholderPage implements CcdPageConfigura
             .page("warrantOfRestitutionVulnerableAdultsChildren")
             .pageLabel("Vulnerable adults and children at the property")
             .showCondition(WARRANT_OF_RESTITUTION_FLOW)
-            .label("warrantOfRestitutionVulnerableAdultsChildren-line-separator", "---");
+            .label("warrantOfRestitutionVulnerableAdultsChildren-line-separator", "---")
+            .label("warrantOfRestitutionVulnerableAdultsChildren-saveAndReturn", SAVE_AND_RETURN);
     }
 }
 
