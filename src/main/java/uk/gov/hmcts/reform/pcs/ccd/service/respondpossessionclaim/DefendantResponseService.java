09682e333b4fc5fb0c9bfb5216b1f97aced14e0b
Liban Abdirahman <liban.abdirahman@hmcts.net>
2026-03-10 12:08:40 +0000

    HDPI-5191:Hardcoding statement of truth entity values for testing

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/service/respondpossessionclaim/DefendantResponseService.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/service/respondpossessionclaim/DefendantResponseService.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.reform.pcs.ccd.domain.respondpossessionclaim.DefendantResponses;
 import uk.gov.hmcts.reform.pcs.ccd.domain.respondpossessionclaim.PossessionClaimResponse;
+import uk.gov.hmcts.reform.pcs.ccd.domain.statementoftruth.StatementOfTruthCompletedBy;
 import uk.gov.hmcts.reform.pcs.ccd.entity.ClaimEntity;
 import uk.gov.hmcts.reform.pcs.ccd.entity.claim.StatementOfTruthEntity;
 import uk.gov.hmcts.reform.pcs.ccd.entity.party.PartyEntity;
@@ -107,7 +109,12 @@ public class DefendantResponseService {
         //set bidirectional relationship with the pcs case
         claimRef.getPcsCase().addDefendantResponse(defendantResponse);
         //empty for now to satisfy not null constraint
-        defendantResponse.setStatementOfTruth(new StatementOfTruthEntity());
+        defendantResponse.setStatementOfTruth(StatementOfTruthEntity.builder()
+                                                   .completedBy(StatementOfTruthCompletedBy.LEGAL_REPRESENTATIVE)
+                                                   .accepted(YesOrNo.YES)
+                                                   .fullName("John test")
+                                                   .build());
+
         buildAndLinkChildEntities(defendantResponse, possessionClaimResponse);
 
         defendantResponseRepository.save(defendantResponse);
