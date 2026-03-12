efa25af2fa831fd55d9775adab63330a7c6ee9e6
arun <arun.kumar@hmcts.net>
2026-03-12 08:37:03 +0000

    HDPI-3495: Fix migration naming and support 1 billion max

--- src/main/resources/db/migration/V063__add_rent_arrears_response.sql
+++ src/main/resources/db/migration/V063__add_rent_arrears_response.sql
@@ -1,3 +1,3 @@
 ALTER TABLE defendant_response
     ADD COLUMN rent_arrears_amount_confirmation YES_NO_NOT_SURE,
-    ADD COLUMN rent_arrears_amount NUMERIC(10, 2);
+    ADD COLUMN rent_arrears_amount NUMERIC(12, 2);
