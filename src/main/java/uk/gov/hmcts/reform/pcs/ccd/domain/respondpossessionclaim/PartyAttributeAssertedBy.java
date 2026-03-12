b5c4c4d9c4d00bde43e93c088d2b7b6860c82c6c
Liban Abdirahman <liban.abdirahman@hmcts.net>
2026-03-12 11:44:44 +0000

    HDPI-5191:Updated party attribute enums

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/respondpossessionclaim/PartyAttributeAssertedBy.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/respondpossessionclaim/PartyAttributeAssertedBy.java
@@ -1,12 +1,20 @@
 package uk.gov.hmcts.reform.pcs.ccd.domain.respondpossessionclaim;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import uk.gov.hmcts.ccd.sdk.api.HasLabel;
+
 /**
  * Who asserted a correction to a party attribute.
  */
-public enum PartyAttributeAssertedBy {
-    CLAIMANT,
-    DEFENDANT,
-    JUDGE,
-    COURT_STAFF,
-    CASE_WORKER
+@AllArgsConstructor
+@Getter
+public enum PartyAttributeAssertedBy implements HasLabel {
+    CLAIMANT("Claimant"),
+    DEFENDANT("Defendant"),
+    JUDGE("Judge"),
+    COURT_STAFF("Court staff"),
+    CASE_WORKER("Case worker");
+
+    private final String label;
 }
