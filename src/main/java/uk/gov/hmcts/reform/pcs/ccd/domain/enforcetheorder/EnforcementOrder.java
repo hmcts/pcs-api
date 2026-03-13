c1d1c3b66f051d385def53964986b832b8b8093a
guygrewal77 <guygrewal7@gmail.com>
2026-03-13 15:34:38 +0000

    HDPI-4596 - SelectEnforcementType field changed back to enum plus other other domain changes to display the right details

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/enforcetheorder/EnforcementOrder.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/enforcetheorder/EnforcementOrder.java
@@ -31,10 +31,9 @@ public class EnforcementOrder {
     // Redundant field to support the existing CCD definition, will be removed in the future once the
     // CCD definition is updated to use the new field
     @CCD(
-        label = "What do you want to apply for?",
-        typeOverride = DynamicRadioList
+        label = "What do you want to apply for?"
     )
-    private DynamicStringList selectEnforcementType;
+    private SelectEnforcementType selectEnforcementType;
 
     @CCD(
         label = "What do you want to apply for?",
