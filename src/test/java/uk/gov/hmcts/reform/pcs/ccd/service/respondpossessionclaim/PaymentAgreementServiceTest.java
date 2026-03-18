f9a3f90408dcb564e2921a18ffc65f2551376c1e
Andrew Ajube <andrew.ajube@hmcts.net>
2026-03-18 13:36:08 +0000

    test: update PaymentAgreementServiceTest to include additional fields and assertions for payment agreement mapping

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/service/respondpossessionclaim/PaymentAgreementServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/service/respondpossessionclaim/PaymentAgreementServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
+import uk.gov.hmcts.reform.pcs.ccd.domain.YesNoNotSure;
 import uk.gov.hmcts.reform.pcs.ccd.domain.respondpossessionclaim.PaymentAgreement;
 import uk.gov.hmcts.reform.pcs.ccd.entity.respondpossessionclaim.PaymentAgreementEntity;
 
@@ -21,10 +22,17 @@ class PaymentAgreementServiceTest {
     }
 
     @Test
-    void shouldMapAnyPaymentsMadeField() {
+    void shouldMapAllFields() {
         //Given
         PaymentAgreement paymentAgreement = PaymentAgreement.builder()
             .anyPaymentsMade(YesOrNo.YES)
+            .paymentDetails("Some details")
+            .paidMoneyToHousingOrg(YesOrNo.NO)
+            .repaymentPlanAgreed(YesNoNotSure.YES)
+            .repaymentAgreedDetails("Agreed to pay weekly")
+            .repayArrearsInstalments(YesOrNo.YES)
+            .additionalRentContribution(new java.math.BigDecimal("123.45"))
+            .additionalContributionFrequency("WEEKLY")
             .build();
 
         //When
@@ -32,6 +40,13 @@ class PaymentAgreementServiceTest {
 
         //Then
         assertThat(entity.getAnyPaymentsMade()).isEqualTo(YesOrNo.YES);
+        assertThat(entity.getPaymentDetails()).isEqualTo("Some details");
+        assertThat(entity.getPaidMoneyToHousingOrg()).isEqualTo(YesOrNo.NO);
+        assertThat(entity.getRepaymentPlanAgreed()).isEqualTo(YesNoNotSure.YES);
+        assertThat(entity.getRepaymentAgreedDetails()).isEqualTo("Agreed to pay weekly");
+        assertThat(entity.getRepayArrearsInstalments()).isEqualTo(YesOrNo.YES);
+        assertThat(entity.getAdditionalRentContribution()).isEqualByComparingTo("123.45");
+        assertThat(entity.getAdditionalContributionFrequency()).isEqualTo("WEEKLY");
     }
 
     @Test
