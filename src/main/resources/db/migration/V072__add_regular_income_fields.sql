9be81e191f6a4943a1e9a6be0014be070508ffd7
arun <arun.kumar@hmcts.net>
2026-03-26 13:25:11 +0000

    HDPI-3764: Revert VARCHAR size to 10 for frequency columns

--- src/main/resources/db/migration/V072__add_regular_income_fields.sql
+++ src/main/resources/db/migration/V072__add_regular_income_fields.sql
@@ -1,14 +1,14 @@
 ALTER TABLE household_circumstances
     ADD COLUMN income_from_jobs YES_NO,
     ADD COLUMN income_from_jobs_amount DECIMAL(18,2),
-    ADD COLUMN income_from_jobs_frequency VARCHAR(5),
+    ADD COLUMN income_from_jobs_frequency VARCHAR(10),
     ADD COLUMN pension YES_NO,
     ADD COLUMN pension_amount DECIMAL(18,2),
-    ADD COLUMN pension_frequency VARCHAR(5),
+    ADD COLUMN pension_frequency VARCHAR(10),
     ADD COLUMN universal_credit_amount DECIMAL(18,2),
-    ADD COLUMN universal_credit_frequency VARCHAR(5),
+    ADD COLUMN universal_credit_frequency VARCHAR(10),
     ADD COLUMN other_benefits YES_NO,
     ADD COLUMN other_benefits_amount DECIMAL(18,2),
-    ADD COLUMN other_benefits_frequency VARCHAR(5),
+    ADD COLUMN other_benefits_frequency VARCHAR(10),
     ADD COLUMN money_from_elsewhere YES_NO,
     ADD COLUMN money_from_elsewhere_details VARCHAR(500);
