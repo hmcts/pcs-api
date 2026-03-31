016f01620cc1e0f7fcc8898a8d1419eb94af2cae
ankita <ankita.kumbhare@hmcts.net>
2026-03-31 14:51:34 +0100

    Adding files to figma folder and removing same form page-data folder

---
+++ src/e2eTest/data/page-data-figma/page-data-enforcement-figma/shareEvidenceWithJudge.page.data.ts
@@ -0,0 +1,7 @@
+export const shareEvidenceWithJudge = {
+  mainHeader: 'On the next few questions, we will ask you to share evidence with a judge',
+  judgeWillReadParagraph: `The judge will read this evidence and either approve or refuse your request for a warrant of restitution.`,
+  previousButton: `Previous`,
+  continueButton: `Continue`,
+  cancelLink: `Cancel`
+};
