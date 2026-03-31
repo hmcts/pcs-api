3fe812666a6ddf743b7ae2b8e1ebadaeccb78d63
Andrew Ajube <andrew.ajube@hmcts.net>
2026-03-31 09:45:00 +0100

    feat: add fields for financial details and expenses, in HouseholdCircumstances

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/respondpossessionclaim/HouseholdCircumstances.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/respondpossessionclaim/HouseholdCircumstances.java
@@ -6,7 +6,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
+import uk.gov.hmcts.ccd.sdk.type.FieldType;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
+import uk.gov.hmcts.reform.pcs.ccd.annotation.JacksonMoneyGBP;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Builder
 @Data
@@ -16,5 +21,41 @@ public class HouseholdCircumstances {
 
     @CCD
     private YesOrNo dependantChildren;
+
+    @CCD
+    private YesOrNo shareIncomeExpenseDetails;
+
+    @CCD
+    private String regularIncome;
+
+    @CCD
+    private YesOrNo universalCredit;
+
+    @CCD
+    private LocalDate ucApplicationDate;
+
+    @CCD
+    private YesOrNo priorityDebts;
+
+    @CCD(typeOverride = FieldType.MoneyGBP)
+    @JacksonMoneyGBP
+    private BigDecimal debtTotal;
+
+    @CCD(typeOverride = FieldType.MoneyGBP)
+    @JacksonMoneyGBP
+    private BigDecimal debtContribution;
+
+    @CCD
+    private String debtContributionFrequency;
+
+    @CCD
+    private String regularExpenses;
+
+    @CCD(typeOverride = FieldType.MoneyGBP)
+    @JacksonMoneyGBP
+    private BigDecimal expenseAmount;
+
+    @CCD
+    private String expenseFrequency;
 
 }
