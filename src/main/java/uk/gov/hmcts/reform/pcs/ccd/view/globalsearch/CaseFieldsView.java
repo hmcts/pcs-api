a2ae320bdda35c37108ea4844323c322730198ec
Toby Plunkett <toby.plunkett@HMCTS.NET>
2026-04-07 14:23:31 +0100

    HDPI-4063 Styling and tests

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/view/globalsearch/CaseFieldsView.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/view/globalsearch/CaseFieldsView.java
@@ -8,7 +8,6 @@ import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
 import uk.gov.hmcts.reform.pcs.ccd.domain.Party;
 import uk.gov.hmcts.reform.pcs.ccd.domain.VerticalYesNo;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
