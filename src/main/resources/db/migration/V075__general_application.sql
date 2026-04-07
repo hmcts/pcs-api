f2fcc3424ce42efe7621c11d6260388683a83480
sstewart <scott.stewart@solirius.com>
2026-04-07 10:06:53 +0100

    HDPI-5180: Update DB migration version number

--- src/main/resources/db/migration/V075__general_application.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE general_application (
-  id       UUID PRIMARY KEY,
-  case_id  UUID REFERENCES public.pcs_case (id),
-  type     VARCHAR(50),
-  state    VARCHAR(30),
-  party_id UUID REFERENCES public.party (id)
-)
