454afd6e848cbda1aa2732deace16c5d98e83349
sstewart <scott.stewart@solirius.com>
2026-04-07 21:01:19 +0100

    HDPI-5180: Add missing NOT NULL constraints

--- src/main/resources/db/migration/V076__general_application.sql
+++ src/main/resources/db/migration/V076__general_application.sql
@@ -1,16 +1,16 @@
 CREATE TABLE help_with_fees (
   id            UUID PRIMARY KEY,
-  hwf_reference VARCHAR(16)
+  hwf_reference VARCHAR(16) NOT NULL
 );
 
 CREATE TABLE general_application (
   id                    UUID PRIMARY KEY,
-  case_id               UUID REFERENCES public.pcs_case (id),
+  case_id               UUID NOT NULL REFERENCES public.pcs_case (id),
   sot_id                UUID REFERENCES public.statement_of_truth (id),
   hwf_id                UUID REFERENCES public.help_with_fees (id),
-  type                  VARCHAR(50),
+  type                  VARCHAR(50) NOT NULL,
   state                 VARCHAR(30),
-  party_id              UUID REFERENCES public.party (id),
+  party_id              UUID NOT NULL REFERENCES public.party (id),
   within_14_days        YES_NO,
   need_hwf              YES_NO,
   applied_for_hwf       YES_NO,
