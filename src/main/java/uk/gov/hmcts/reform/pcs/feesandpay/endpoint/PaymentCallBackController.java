d84823bf6e1900a8f657f5fd480e315c5a079227
tvr-solirius <trevor.hinson@solirius.com>
2026-04-10 11:08:36 +0100

    Update to PUT mapping and added payment to whitelist

--- src/main/java/uk/gov/hmcts/reform/pcs/feesandpay/endpoint/PaymentCallBackController.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/feesandpay/endpoint/PaymentCallBackController.java
@@ -2,7 +2,7 @@ package uk.gov.hmcts.reform.pcs.feesandpay.endpoint;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +19,7 @@ public class PaymentCallBackController {
 
     private final PaymentService paymentService;
 
-    @PostMapping(path = "/service-request-update", consumes = APPLICATION_JSON_VALUE)
+    @PutMapping(path = "/service-request-update", consumes = APPLICATION_JSON_VALUE)
     public void processPaymentCallback(
         @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String authorisation,
         @RequestHeader(value = SERVICE_AUTHORIZATION, required = false) String s2sToken,
