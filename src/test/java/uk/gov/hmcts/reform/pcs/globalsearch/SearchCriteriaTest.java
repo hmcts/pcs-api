7eacfa91bc79d4b63b62ed30f40e1f37e5c87d92
adusumillipraveen <praveen.adusumilli@hmcts.net>
2026-04-13 15:58:42 +0100

    add tests

--- src/test/java/uk/gov/hmcts/reform/pcs/globalsearch/SearchCriteriaTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/globalsearch/SearchCriteriaTest.java
@@ -7,8 +7,9 @@ import static org.mockito.Mockito.when;
 
 import uk.gov.hmcts.ccd.sdk.api.DecentralisedConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.SearchCriteria.SearchCriteriaBuilder;
+import uk.gov.hmcts.ccd.sdk.api.SearchCriteriaField;
 
-import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,6 +28,10 @@ class SearchCriteriaTest {
     void shouldConfigureSearchCriteria() {
         var configBuilder = mock(DecentralisedConfigBuilder.class);
         var searchCriteriaBuilder = mock(SearchCriteriaBuilder.class);
+        var expectedFields = List.of(
+            SearchCriteriaField.builder().otherCaseReference("caseNameHmctsInternal").build(),
+            SearchCriteriaField.builder().otherCaseReference("caseNamePublic").build()
+        );
 
         when(configBuilder.searchCriteria()).thenReturn(searchCriteriaBuilder);
         when(searchCriteriaBuilder.fields(anyList())).thenReturn(searchCriteriaBuilder);
@@ -34,6 +39,6 @@ class SearchCriteriaTest {
         underTest.configureDecentralised(configBuilder);
 
         verify(configBuilder).searchCriteria();
-        verify(searchCriteriaBuilder).fields(Collections.emptyList());
+        verify(searchCriteriaBuilder).fields(expectedFields);
     }
 }
