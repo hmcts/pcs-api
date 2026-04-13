db7ef83e101084a5670fafb06b9628c47d1c72d4
adusumillipraveen <praveen.adusumilli@hmcts.net>
2026-04-13 16:48:40 +0100

    fix

--- src/test/java/uk/gov/hmcts/reform/pcs/globalsearch/SearchCriteriaTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/globalsearch/SearchCriteriaTest.java
@@ -1,18 +1,15 @@
 package uk.gov.hmcts.reform.pcs.globalsearch;
 
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.ccd.sdk.api.DecentralisedConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.SearchCriteria.SearchCriteriaBuilder;
 import uk.gov.hmcts.ccd.sdk.api.SearchCriteriaField;
 
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.*;
 
 class SearchCriteriaTest {
 
