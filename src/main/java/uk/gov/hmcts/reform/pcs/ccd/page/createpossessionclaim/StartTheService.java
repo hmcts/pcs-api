89b4b239f770cb44b29bf8b5edecfa207f68cf96
taleb <tech@benouaer.com>
2026-04-20 13:03:01 +0100

    HDPI-3344: fix checkstyle issues

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/createpossessionclaim/StartTheService.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/createpossessionclaim/StartTheService.java
@@ -27,51 +27,44 @@ public class StartTheService implements CcdPageConfiguration {
         pageBuilder
             .page("startTheService")
             .readonly(PCSCase::getFeeAmount, NEVER_SHOW, true)
-            .label("mainContentTop", """
-                <h1 class="%s">Make a housing possession claim online</h1>
-                <p class="%s">You can use this online service if you're a registered provider of social housing or a \
-                community landlord and the property you want to claim possession of is in England or Wales.</p>
-                <p class="%s">This service is also available <a href="javascript:void(0)" class="%s">in Welsh (Cymraeg)</a>.</p>
-                """.formatted(HEADING_L, BODY, BODY, LINK)
+            .label("mainContentTop",
+                "<h1 class=\"" + HEADING_L + "\">Make a housing possession claim online</h1>"
+                + "<p class=\"" + BODY + "\">You can use this online service if you're a registered provider of "
+                + "social housing or a community landlord and the property you want to claim possession of is in "
+                + "England or Wales.</p>"
+                + "<p class=\"" + BODY + "\">This service is also available "
+                + "<a href=\"javascript:void(0)\" class=\"" + LINK + "\">in Welsh (Cymraeg)</a>.</p>"
             )
-            .label("feePaymentWithAmount", """
-                <p class="%s">The claim fee is ${feeAmount}. You can pay by card or through Payment By Account (PBA).</p>
-                """.formatted(BODY),
-                   FEE_AMOUNT_KNOWN
+            .label("feePaymentWithAmount",
+                "<p class=\"" + BODY + "\">The claim fee is ${feeAmount}. "
+                + "You can pay by card or through Payment By Account (PBA).</p>",
+                FEE_AMOUNT_KNOWN
             )
-            .label("feePaymentWithoutAmount", """
-                <p class="%s">You can pay by card or through Payment By Account (PBA).</p>
-                """.formatted(BODY),
+            .label("feePaymentWithoutAmount",
+                "<p class=\"" + BODY + "\">You can pay by card or through Payment By Account (PBA).</p>",
                 FEE_AMOUNT_UNKNOWN
             )
-            .label("mainContentBottom", """
-                <p class="%s">Your claim will be saved as you answer the questions, so you'll be able to close and return to your draft.</p>
-                <h2 class="%s %s">What you'll need</h2>
-                <p class="%s">Before you start, make sure you have the following information:</p>
-                <ul>
-                  <li class="%s">details of the tenancy, contract, licence or mortgage agreement</li>
-                  <li class="%s">the defendants' details (the people you're making the claim against)</li>
-                  <li class="%s">your reasons for making a possession claim</li>
-                  <li class="%s">copies of any relevant documents. \
-                You can either upload documents now or closer to the hearing date. \
-                Any documents you upload now will be included in the pack of documents \
-                that a judge will receive before the hearing (the bundle)</li>
-                </ul>
-                <h2 class="%s">Completing your claim</h2>
-                <p class="%s">Once you've finished answering the questions, you can either:</p>
-                <ul>
-                  <li class="%s">sign, submit and pay for your claim now, or</li>
-                  <li class="%s">save it as a draft. You or someone else can then return to sign, submit and pay at a later date</li>
-                </ul>
-                """.formatted(
-                    BODY,
-                    HEADING_M, PADDING_TOP_0,
-                    BODY,
-                    LIST_ITEM, LIST_ITEM, LIST_ITEM, LIST_ITEM,
-                    HEADING_M,
-                    BODY,
-                    LIST_ITEM, LIST_ITEM
-                )
+            .label("mainContentBottom",
+                "<p class=\"" + BODY + "\">Your claim will be saved as you answer the questions, so you'll be "
+                + "able to close and return to your draft.</p>"
+                + "<h2 class=\"" + HEADING_M + " " + PADDING_TOP_0 + "\">What you'll need</h2>"
+                + "<p class=\"" + BODY + "\">Before you start, make sure you have the following information:</p>"
+                + "<ul>"
+                + "<li class=\"" + LIST_ITEM + "\">details of the tenancy, contract, licence or mortgage agreement</li>"
+                + "<li class=\"" + LIST_ITEM + "\">the defendants' details "
+                + "(the people you're making the claim against)</li>"
+                + "<li class=\"" + LIST_ITEM + "\">your reasons for making a possession claim</li>"
+                + "<li class=\"" + LIST_ITEM + "\">copies of any relevant documents. You can either upload documents "
+                + "now or closer to the hearing date. Any documents you upload now will be included in the pack of "
+                + "documents that a judge will receive before the hearing (the bundle)</li>"
+                + "</ul>"
+                + "<h2 class=\"" + HEADING_M + "\">Completing your claim</h2>"
+                + "<p class=\"" + BODY + "\">Once you've finished answering the questions, you can either:</p>"
+                + "<ul>"
+                + "<li class=\"" + LIST_ITEM + "\">sign, submit and pay for your claim now, or</li>"
+                + "<li class=\"" + LIST_ITEM + "\">save it as a draft. You or someone else can then return to "
+                + "sign, submit and pay at a later date</li>"
+                + "</ul>"
             );
     }
 }
