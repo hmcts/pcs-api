f152068c35f8f78ebb21593e0b16357a8ba83523
danielmillard <dmillard91@hotmail.co.uk>
2026-04-21 09:22:47 +0100

    HDPI-6100: refactor tests to reduce duplication and remove NoC roles as not yet used and minimial effort to add in when needed

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/accesscontrol/UserRole.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/accesscontrol/UserRole.java
@@ -24,10 +24,7 @@ public enum UserRole implements HasRole {
     DEFENDANT("[DEFENDANT]", Permission.CRU, RAS),
     PCS_CASE_WORKER("caseworker-pcs", Set.of(R), IDAM),
     PCS_SOLICITOR("caseworker-pcs-solicitor", Permission.CRU, IDAM),
-    DEFENDANT_SOLICITOR("[DEFENDANTSOLICITOR]", Permission.CRU, RAS),
-
-    NOC_APPROVER("[NOCAPPROVER]", Permission.CRU, RAS),
-    CASEWORKER_CASE_ACCESS_ADMINISTRATOR("caseworker-caa", Permission.CRU, IDAM);
+    DEFENDANT_SOLICITOR("[DEFENDANTSOLICITOR]", Permission.CRU, RAS);
 
     @JsonValue
     private final String role;
