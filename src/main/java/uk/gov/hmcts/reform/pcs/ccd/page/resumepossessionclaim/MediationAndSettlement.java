bdd8071cc0c9dcb70f57a040f2a66c764c764b7f
gmmagruder <gabrielle.magruder@gmail.com>
2026-04-22 14:28:44 +0100

    HDPI-5727: remove unused imports

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/resumepossessionclaim/MediationAndSettlement.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/resumepossessionclaim/MediationAndSettlement.java
@@ -2,14 +2,10 @@ package uk.gov.hmcts.reform.pcs.ccd.page.resumepossessionclaim;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.reform.pcs.ccd.common.CcdPageConfiguration;
 import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
-import uk.gov.hmcts.reform.pcs.ccd.domain.State;
 import uk.gov.hmcts.reform.pcs.ccd.page.CommonPageContent;
-import uk.gov.hmcts.reform.pcs.ccd.service.TextAreaValidationService;
 
 import java.util.List;
 
