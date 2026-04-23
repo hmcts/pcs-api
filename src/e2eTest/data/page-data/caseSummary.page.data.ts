f5c595cca448544bd8b41cbf62707a7b5838bf0c
ankita <ankita.kumbhare@hmcts.net>
2026-04-23 14:52:33 +0100

    Adding/Updating Scenarios for Case Linking

--- src/e2eTest/data/page-data/caseSummary.page.data.ts
+++ src/e2eTest/data/page-data/caseSummary.page.data.ts
@@ -2,6 +2,8 @@ export const caseSummary = {
   title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
   mainHeader: 'Case Summary',
   nextStepEventList: 'Next step',
+  linkCaseEvent: 'Link cases',
+  manageCaseEvent: 'Manage case links',
   enforceTheOrderEvent: 'Enforce the order',
   go: "Go"
 }
---
+++ src/e2eTest/data/page-data/selectCaseToLink.page.data.ts
@@ -0,0 +1,21 @@
+import { label } from "allure-js-commons";
+
+export const selectCasesToLink = {
+  title: 'Create a case - HM Courts & Tribunals Service - GOV.UK',
+  mainHeader: 'Select a case you want to link to this case',
+  caseRefLabel: 'Enter case reference',
+  whyToLinkQuestion: 'Why should these cases be linked?',
+  caseConsolidateCheckbox: 'Case consolidated',
+  linkForHearingCheckbox: 'Linked for a hearing',
+  progressedCheckbox: 'Progressed as part of this lead case',
+  relatedAppealCheckbx:'Related appeal',
+  relatedProceedingsCheckbox: 'Related proceedings',
+  samePartyCheckbox: 'Same Party',
+  otherCheckbox: 'Other',
+  CommentsLabelHidden: 'Comments',
+  otherDescriptionInputLabel: 'otherDescription',
+  otherDescriptionInputText: 'Input for other reason testing purpose',
+  proposeLinkButton: 'Propose case link',
+  submitButton: 'Submit',
+  cancel: 'Cancel',
+}
