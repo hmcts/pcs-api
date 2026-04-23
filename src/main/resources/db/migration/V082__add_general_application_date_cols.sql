008d5ae7ab2bba5394b2bf806fb0b7c1521ec23d
sstewart <scott.stewart@solirius.com>
2026-04-23 17:48:28 +0100

    HDPI-4272: Make hwf_reference column 60 chars

--- src/main/resources/db/migration/V082__add_general_application_date_cols.sql
+++ src/main/resources/db/migration/V082__add_general_application_date_cols.sql
@@ -6,3 +6,6 @@ ALTER TABLE general_application
 ALTER TABLE statement_of_truth
   ALTER COLUMN completed_by DROP NOT NULL,
   ADD COLUMN completed_date TIMESTAMP;
+
+ALTER TABLE help_with_fees
+  ALTER COLUMN hwf_reference TYPE VARCHAR(60)
