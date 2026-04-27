ca21731ba6679783e23cef87cedddcc8bc726e2a
bytesize-hq <bytesize-hq@gmail.com>
2026-04-27 12:23:51 +0100

    dedup

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/service/dashboard/task/DocumentsTaskGroupEvaluator.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/service/dashboard/task/DocumentsTaskGroupEvaluator.java
@@ -22,7 +22,7 @@ public class DocumentsTaskGroupEvaluator implements TaskGroupEvaluator {
     @Override
     public TaskGroup evaluate(DashboardContext ctx) {
         return TaskGroup.builder()
-            .groupId(TaskGroupId.DOCUMENTS)
+            .groupId(groupId())
             .tasks(ListValueUtils.wrapListItems(List.of(
                 Task.builder()
                     .templateId("Defendant.UploadDocuments")
