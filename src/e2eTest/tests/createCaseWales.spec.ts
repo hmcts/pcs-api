0d87b13df2838dfb0b7cf96cd48294fa86e2d8af
ankita <ankita.kumbhare@hmcts.net>
2026-04-29 08:26:33 +0100

    Changing CREATOR role with CLAIMANTSOLICITOR

--- src/e2eTest/tests/createCaseWales.spec.ts
+++ src/e2eTest/tests/createCaseWales.spec.ts
@@ -57,7 +57,7 @@ test.beforeEach(async ({ page }) => {
 
 test.afterEach(async () => {
   if (caseNumber) {
-    await performAction('deleteCaseRole', '[CREATOR]');
+    await performAction('deleteCaseRole', '[CLAIMANTSOLICITOR]');
   }
   PageContentValidation.finaliseTest();
 });
--- src/e2eTest/tests/eligibilityCheck.spec.ts
+++ src/e2eTest/tests/eligibilityCheck.spec.ts
@@ -30,7 +30,7 @@ test.beforeEach(async ({ page }) => {
 
 test.afterEach(async () => {
   if (caseNumber) {
-    await performAction('deleteCaseRole', '[CREATOR]');
+    await performAction('deleteCaseRole', '[CLAIMANTSOLICITOR]');
   }
   PageContentValidation.finaliseTest();
 });
