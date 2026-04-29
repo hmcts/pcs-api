fa332422749a469704b098a34ac752f841b01014
aschalew <aschalew.tessemahmcts.net@Aschalews-MacBook-Pro.local>
2026-04-29 12:28:23 +0100

    Changed service ID back to Civil(AAA7) for testing purpose

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/CaseType.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/CaseType.java
@@ -56,7 +56,7 @@ public class CaseType implements CCDConfig<PCSCase, State, UserRole> {
 
         builder.caseType(getCaseType(), getCaseTypeName(), CASE_TYPE_DESCRIPTION);
         builder.jurisdiction(JURISDICTION_ID, JURISDICTION_NAME, JURISDICTION_DESCRIPTION);
-        builder.hmctsServiceId(hmctsServiceId);
+        builder.hmctsServiceId(alternativeHmctsOrgId);
 
         builder.searchInputFields()
             .caseReferenceField();
