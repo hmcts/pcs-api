69557c0e763e5f0f34471f01923f2fa70afa1c99
Toby Plunkett <toby.plunkett@HMCTS.NET>
2026-04-30 10:36:46 +0100

    HDPI-4161 add gs_profile access profile to config

--- src/cftlib/java/uk/gov/hmcts/reform/pcs/CftlibConfig.java
+++ src/cftlib/java/uk/gov/hmcts/reform/pcs/CftlibConfig.java
@@ -70,6 +70,7 @@ public class CftlibConfig implements CFTLibConfigurer {
 
         roleNames.add("caseworker");
         roleNames.add("caseworker-ras-validation");
+        roleNames.add("GS_profile");
 
         lib.createRoles(roleNames.toArray(new String[0]));
     }
