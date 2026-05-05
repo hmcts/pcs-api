447f5a096d643bf521920d09e8a596fd2130a496
srinijg <srinivasan.joghee@hmcts.net>
2026-05-05 19:52:34 +0100

    content check

--- src/e2eTest/data/page-data-figma/checkingNotice.page.data.ts
+++ src/e2eTest/data/page-data-figma/checkingNotice.page.data.ts
@@ -3,7 +3,7 @@ export const checkingNotice = {
   mainHeader: `Notice of your intention to begin possession proceedings`,
   youMayHaveAlreadyServedParagraph: `You may have already served the defendants with notice of your intention to begin possession proceedings. Notice periods vary between grounds and some do not require any notice to be served. You should read the guidance on possession notice periods (opens in new tab) to make sure your claim is valid.`,
   guidanceOnPossessionLink: `guidance on possession notice periods (opens in new tab)`,
-  mainHeaderEnglandNewTab: `Understanding the possession action process: guidance for landlords and tenants - GOV.UK`,
+  mainHeaderEnglandNewTab: `Possession guidance for tenants in social housing - GOV.UK`,
   mainHeaderWalesNewTab: `Understanding the possession process: guidance for private landlords | GOV.WALES`,
   aJudgeMightNotGrantParagraph: `A judge might not grant a possession order if you have not followed the correct notice procedure`,
   haveYouServedNoticeToQuestion: `Have you served notice to the defendants?`,
