df5a2ebe7c2d97e234ea1ec5f6347e8d5e0a7947
srinijg <srinivasan.joghee@hmcts.net>
2026-05-05 09:36:13 +0100

    new tests added

--- src/e2eTest/tests/caseTabs.spec.ts
+++ src/e2eTest/tests/caseTabs.spec.ts
@@ -1,25 +1,18 @@
 import { expect, test } from '@utils/test-fixtures';
 import { getCaseTypeId } from '@utils/common/caseType.utils';
-import { initializeExecutor,performAction } from '@utils/controller';
+import { initializeExecutor, performAction } from '@utils/controller';
 import { createCaseApiData, submitCaseApiData } from '@data/api-data';
 import { caseInfo } from '@utils/actions/custom-actions/createCaseAPI.action';
-import { VERY_LONG_TIMEOUT } from 'playwright.config';
+import { LONG_TIMEOUT } from 'playwright.config';
 import { PageContentValidation } from '@utils/validations/element-validations/pageContent.validation';
+import { home } from '@data/page-data';
 
 test.beforeEach(async ({ page }) => {
   initializeExecutor(page);
-
-
   await performAction('createCaseAPI', { data: createCaseApiData.createCasePayload });
   await performAction('submitCaseAPI', { data: submitCaseApiData.submitCasePayloadCaseTab });
-
   await performAction('navigateToUrl', `${process.env.MANAGE_CASE_BASE_URL}/cases/case-details/PCS/${getCaseTypeId()}/${process.env.CASE_NUMBER}#Case%20Parties`);
-  // Login and cookie consent are handled globally via storageState in global-setup.config.ts
-  await expect(async () => {
-    await page.waitForURL(`${process.env.MANAGE_CASE_BASE_URL}/**/**/**/**/**#Case%20Parties`);
-  }).toPass({
-    timeout: VERY_LONG_TIMEOUT,
-  });
+  
 });
 
 test.afterEach(async () => {
@@ -28,10 +21,26 @@ test.afterEach(async () => {
   }
   PageContentValidation.finaliseTest();
 });
-// Skipping this test case as the feature is not part of Release 1 to save execution time.
-// It will be enabled once the feature is included in the execution scope.
+
 test.describe('[Case tabs - England Journey] @nightly', async () => {
   test('Case tabs - Case parties tab test', async () => {
+    await expect(async () => {
+      await performAction('clickTab', home.caseParties);
+    }).toPass({
+      timeout: LONG_TIMEOUT,
+    });
+
+    await performAction('validateDefendantDetails', {
+      defendant1NameKnown: submitCaseApiData.submitCasePayloadCaseTab.defendant1.nameKnown,
+      additionalDefendants: submitCaseApiData.submitCasePayloadCaseTab.addAnotherDefendant,
+      payLoad: submitCaseApiData.submitCasePayloadCaseTab,
+      table: 'Defendant'
+    });
+
+    await performAction('validateClaimantDetails', {
+      payLoad: submitCaseApiData.submitCasePayloadCaseTab,
+      table: 'Claimant'
+    });
   });
 
 });
