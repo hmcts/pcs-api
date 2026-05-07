c36b140005f52ca683fc386051ea30ffdddf3ebc
lucas-phillips28 <lucas.phillips@hmcts.net>
2026-05-07 12:50:56 +0100

    feat: update `case_notification` table

---
+++ src/main/java/uk/gov/hmcts/reform/pcs/notify/model/NotificationClaimType.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.reform.pcs.notify.model;
+
+import lombok.Getter;
+
+@Getter
+public enum NotificationClaimType {
+    COUNTER_CLAIM("counter_claim"),
+    POSSESSION_CLAIM("possession_claim"),
+    GENERAL_APPLICATION_CLAIM("general_application_claim");
+
+    private final String value;
+
+    NotificationClaimType(String value) {
+        this.value = value;
+    }
+
+    public static NotificationClaimType fromString(String status) {
+        for (NotificationClaimType notificationClaimType : NotificationClaimType.values()) {
+            if (notificationClaimType.value.equalsIgnoreCase(status)) {
+                return notificationClaimType;
+            }
+        }
+        throw new IllegalArgumentException("Unknown status: " + status);
+    }
+}
