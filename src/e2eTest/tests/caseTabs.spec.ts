4cfbb434658e972e1f113a2390a6e14b98943816
srinijg <srinivasan.joghee@hmcts.net>
2026-05-07 10:51:52 +0100

    Tag added

--- src/e2eTest/tests/caseTabs.spec.ts
+++ src/e2eTest/tests/caseTabs.spec.ts
@@ -3,7 +3,7 @@ import { getCaseTypeId } from '@utils/common/caseType.utils';
 import { initializeExecutor, performAction } from '@utils/controller';
 import { createCaseApiData, submitCaseApiData } from '@data/api-data';
 import { caseInfo } from '@utils/actions/custom-actions/createCaseAPI.action';
-import { LONG_TIMEOUT } from 'playwright.config';
+import { VERY_LONG_TIMEOUT } from 'playwright.config';
 import { PageContentValidation } from '@utils/validations/element-validations/pageContent.validation';
 import { home } from '@data/page-data';
 
@@ -11,8 +11,12 @@ test.beforeEach(async ({ page }) => {
   initializeExecutor(page);
   await performAction('createCaseAPI', { data: createCaseApiData.createCasePayload });
   await performAction('submitCaseAPI', { data: submitCaseApiData.submitCasePayloadCaseTab });
-  await performAction('navigateToUrl', `${process.env.MANAGE_CASE_BASE_URL}/cases/case-details/PCS/${getCaseTypeId()}/${process.env.CASE_NUMBER}#Case%20Parties`);
-  
+  await performAction('navigateToUrl', `${process.env.MANAGE_CASE_BASE_URL}/cases/case-details/PCS/${getCaseTypeId()}/${process.env.CASE_NUMBER}#Summary`);
+  await expect(async () => {
+    await page.waitForURL(`${process.env.MANAGE_CASE_BASE_URL}/**/**/**/**/**#Summary`);
+  }).toPass({
+    timeout: VERY_LONG_TIMEOUT,
+  });
 });
 
 test.afterEach(async () => {
@@ -23,13 +27,8 @@ test.afterEach(async () => {
 });
 
 test.describe('[Case tabs - England Journey] @nightly', async () => {
-  test('Case tabs - Case parties tab test', async () => {
-    await expect(async () => {
-      await performAction('clickTab', home.caseParties);
-    }).toPass({
-      timeout: LONG_TIMEOUT,
-    });
-
+  test('Case tabs - Case parties tab test @MAC', async () => {
+    await performAction('clickTab', home.caseParties);
     await performAction('validateDefendantDetails', {
       defendant1NameKnown: submitCaseApiData.submitCasePayloadCaseTab.defendant1.nameKnown,
       additionalDefendants: submitCaseApiData.submitCasePayloadCaseTab.addAnotherDefendant,
