427338c94090b3dea3bbe61d7a84c2f917f0a475
Aqib <aqib.hussain@solirius.com>
2026-05-12 23:23:48 +0100

    Initial commit of legal rep document upload event

---
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/legalrepdocumentupload/LegalRepDocumentUpload.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.reform.pcs.ccd.event.legalrepdocumentupload;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.DecentralisedConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.ccd.sdk.api.EventPayload;
+import uk.gov.hmcts.ccd.sdk.api.Permission;
+import uk.gov.hmcts.ccd.sdk.api.callback.SubmitResponse;
+import uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole;
+import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
+import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
+import uk.gov.hmcts.reform.pcs.ccd.domain.State;
+
+import static uk.gov.hmcts.reform.pcs.ccd.event.EventId.legalRepDocumentUpload;
+
+@Component
+@AllArgsConstructor
+public class LegalRepDocumentUpload implements CCDConfig<PCSCase, State, UserRole> {
+
+    @Override
+    public void configureDecentralised(DecentralisedConfigBuilder<PCSCase, State, UserRole> configBuilder) {
+        Event.EventBuilder<PCSCase, UserRole, State> eventBuilder =
+            configBuilder
+                .decentralisedEvent(legalRepDocumentUpload.name(), this::submit)
+                .forAllStates()
+                .name("Upload legal rep document")
+                .grant(Permission.CRUD, UserRole.PCS_SOLICITOR)
+                .showSummary();
+        //Placeholder screen. This will be decentralised and replaced with a screen in FE
+        new PageBuilder(eventBuilder)
+            .add(pageBuilder -> pageBuilder
+                .page("legalRepDocumentUpload")
+                .pageLabel("Legal Representative Document Upload Placeholder")
+                .label("legalRepDocumentUpload-separator", "---"));
+    }
+
+    private SubmitResponse<State> submit(EventPayload<PCSCase, State> eventPayload) {
+        return SubmitResponse.defaultResponse();
+    }
+
+}
