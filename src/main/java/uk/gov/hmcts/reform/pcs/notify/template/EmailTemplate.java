128714c6e9fd5255c7a074d7614bbefcb9b6137a
lucas-phillips28 <lucas.phillips@hmcts.net>
2026-05-14 13:03:25 +0100

    feat: add template for counterclaim made by defendant

--- src/main/java/uk/gov/hmcts/reform/pcs/notify/template/EmailTemplate.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/notify/template/EmailTemplate.java
@@ -4,11 +4,15 @@ import lombok.Getter;
 
 @Getter
 public enum EmailTemplate {
+    // Respond to Claim Email Templates
     RESPONSE_NO_COUNTERCLAIM("response-no-counterclaim"),
     RESPONSE_WITH_COUNTERCLAIM_PAYMENT_REQUIRED("counterclaim-payment-required"),
     COUNTERCLAIM_PAYMENT_SUCCESS("counterclaim-payment-success"),
     RESPONSE_WITH_COUNTERCLAIM_NO_PAYMENT_REQUIRED("counterclaim-no-payment-required"),
-    MAKE_A_CLAIM_CLAIM_SAVED_FOR_LATER("make-a-claim-claim-saved-for-later");
+
+    // Make a Claim Email Templates
+    MAKE_A_CLAIM_CLAIM_SAVED_FOR_LATER("make-a-claim-claim-saved-for-later"),
+    MAKE_A_CLAIM_DEFENDANT_MADE_COUNTERCLAIM("make-a-claim-defendant-made-counterclaim");
 
     private final String templateKey;
 
