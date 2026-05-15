ac08e29bb2b0d77d664f20b043480f3337a922b8
Liban Abdirahman <liban.abdirahman@hmcts.net>
2026-05-15 13:12:50 +0100

    HDPI-5485:Increasing migration script number

--- src/main/resources/db/migration/V101__update_party_attribute_assertation_table.sql
+++ src/main/resources/db/migration/V101__update_party_attribute_assertation_table.sql
@@ -2,8 +2,6 @@ ALTER TABLE party_attribute_assertion
   RENAME COLUMN decided_at TO last_updated_at;
 
 ALTER TABLE party_attribute_assertion
-  ALTER COLUMN evidence_document_id DROP NOT NULL; 
-
-ALTER TABLE party_attribute_assertion
+  ALTER COLUMN evidence_document_id DROP NOT NULL,
   ADD COLUMN created_by UUID NOT NULL REFERENCES party(id),
   ADD COLUMN last_updated_by UUID NOT NULL REFERENCES party(id);
