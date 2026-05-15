9901843a4092ed2cd25e892fd0b0c857f4678b6b
Gabrielle Magruder <51362727+gmmagruder@users.noreply.github.com>
2026-05-15 09:18:51 +0100

    HDPI-5744: content changes to make a claim screens (#1667)
    Co-authored-by: jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
    Co-authored-by: PriyadharsiniRathinavel <Priyadarshini.Rathinavel@HMCTS.NET>
    Co-authored-by: srinijg <srinivasan.joghee@hmcts.net>
    Co-authored-by: Priyadharsini Rathinavel <priyadharsini@Scrumconnect-PR-000.local>

--- src/e2eTest/data/page-data-figma/claimLanguageUsed.page.data.ts
+++ src/e2eTest/data/page-data-figma/claimLanguageUsed.page.data.ts
@@ -1,8 +1,8 @@
 export const claimLanguageUsed = {
   makeAClaimCaption: `Make a claim`,
   mainHeader: `Language used`,
-  whichLanguageDidYouUseQuestion: `Which language did you use to complete this service?`,
-  ifSomeoneElseHelpedYouHintText: `If someone else helped you to answer a question in this service, ask them if they answered any questions in Welsh. We’ll use this to make sure your claim is processed correctly`,
+  whichLanguageDidYouUseQuestion: `Which language did you use to complete this claim?`,
+  ifSomeoneElseHelpedYouHintText: `If someone else helped you to answer a question in this claim, ask them if they answered any questions in Welsh. We’ll use this to make sure your claim is processed correctly`,
   englishLRadioOption: `English`,
   welshRadioOption: `Welsh`,
   englishAndWelshRadioOption: `English and Welsh`,
