056af0c7abbae225c3e610fba5ed15c82d46a5e0
arun <arun.kumar@hmcts.net>
2026-05-18 20:36:37 +0100

    HDPI-6576: remove unused MockitoBean import in PaymentCallBackControllerIT

--- src/integrationTest/java/uk/gov/hmcts/reform/pcs/controllers/PaymentCallBackControllerIT.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pcs/controllers/PaymentCallBackControllerIT.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.reform.payments.client.models.FeeDto;
 import uk.gov.hmcts.reform.pcs.ccd.entity.ClaimEntity;
