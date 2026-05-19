dfc39ba5573478c32471a2458b78f3f102162e83
gmmagruder <gabrielle.magruder@gmail.com>
2026-05-19 12:03:33 +0100

    HDPI-2983: reverse changes

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/grounds/AssuredRentArrearsGround.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/grounds/AssuredRentArrearsGround.java
@@ -1,24 +1,18 @@
 package uk.gov.hmcts.reform.pcs.ccd.domain.grounds;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 
 /**
  * Enum representing rent arrears grounds for possession claims.
  */
-@AllArgsConstructor
-@Getter
 public enum AssuredRentArrearsGround {
 
     @CCD(label = "Serious rent arrears (ground 8)")
-    SERIOUS_RENT_ARREARS_GROUND8("Serious rent arrears (ground 8)"),
+    SERIOUS_RENT_ARREARS_GROUND8,
 
     @CCD(label = "Rent arrears (ground 10)")
-    RENT_ARREARS_GROUND10("Rent arrears (ground 10)"),
+    RENT_ARREARS_GROUND10,
 
     @CCD(label = "Persistent delay in paying rent (ground 11)")
-    PERSISTENT_DELAY_GROUND11("Persistent delay in paying rent (ground 11)");
-
-    private final String label;
+    PERSISTENT_DELAY_GROUND11;
 }
