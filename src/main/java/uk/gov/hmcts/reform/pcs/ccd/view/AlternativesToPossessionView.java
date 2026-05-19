c57c1b95fdb2e0bbb0f13b210bb00be967385bec
gmmagruder <gabrielle.magruder@gmail.com>
2026-05-19 15:58:23 +0100

    HDPI-2983: fix bug where demotion of tenancy and suspension wasn't displayed on submit

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/view/AlternativesToPossessionView.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/view/AlternativesToPossessionView.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.pcs.ccd.view;
 
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
+import uk.gov.hmcts.reform.pcs.ccd.domain.AlternativesToPossession;
 import uk.gov.hmcts.reform.pcs.ccd.domain.DemotionOfTenancy;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
 import uk.gov.hmcts.reform.pcs.ccd.domain.SuspensionOfRightToBuy;
@@ -9,8 +10,12 @@ import uk.gov.hmcts.reform.pcs.ccd.entity.ClaimEntity;
 import uk.gov.hmcts.reform.pcs.ccd.entity.PcsCaseEntity;
 import uk.gov.hmcts.reform.pcs.ccd.entity.claim.PossessionAlternativesEntity;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
+import static uk.gov.hmcts.reform.pcs.ccd.domain.AlternativesToPossession.DEMOTION_OF_TENANCY;
+import static uk.gov.hmcts.reform.pcs.ccd.domain.AlternativesToPossession.SUSPENSION_OF_RIGHT_TO_BUY;
 import static uk.gov.hmcts.reform.pcs.ccd.util.YesOrNoConverter.toVerticalYesNo;
 
 @Component
@@ -30,12 +35,15 @@ public class AlternativesToPossessionView {
     private static void setPossessionAlternativesFields(PCSCase pcsCase,
                                                         PossessionAlternativesEntity possessionAlternatives) {
 
+        Set<AlternativesToPossession> alternativesToPossession = new HashSet<>();
+
         if (possessionAlternatives.getSuspensionOfRTB() == YesOrNo.YES) {
             SuspensionOfRightToBuy suspensionOfRightToBuy = SuspensionOfRightToBuy.builder()
                 .housingAct(possessionAlternatives.getSuspensionOfRTBHousingActSection())
                 .reason(possessionAlternatives.getSuspensionOfRTBReason())
                 .build();
             pcsCase.setSuspensionOfRightToBuy(suspensionOfRightToBuy);
+            alternativesToPossession.add(SUSPENSION_OF_RIGHT_TO_BUY);
         }
 
         if (possessionAlternatives.getDotRequested() == YesOrNo.YES) {
@@ -47,7 +55,10 @@ public class AlternativesToPossessionView {
                 .statementOfExpressTermsDetails(possessionAlternatives.getDotStatementDetails())
                 .build();
             pcsCase.setDemotionOfTenancy(demotionOfTenancy);
+            alternativesToPossession.add(DEMOTION_OF_TENANCY);
         }
+
+        pcsCase.setAlternativesToPossession(alternativesToPossession);
     }
 
 }
