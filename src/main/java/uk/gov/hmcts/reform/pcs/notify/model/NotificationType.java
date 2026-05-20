722b35b154992f2674997e20b4d51af371b5d4df
lucas-phillips28 <lucas.phillips@hmcts.net>
2026-05-20 10:26:22 +0100

    refactor: simplify NotificationType by removing unused methods and tests

--- src/main/java/uk/gov/hmcts/reform/pcs/notify/model/NotificationType.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/notify/model/NotificationType.java
@@ -1,24 +1,5 @@
 package uk.gov.hmcts.reform.pcs.notify.model;
 
-import lombok.RequiredArgsConstructor;
-
-import java.util.Arrays;
-
-@RequiredArgsConstructor
 public enum NotificationType {
-    EMAIL("Email");
-
-    private final String value;
-
-    @Override
-    public String toString() {
-        return value;
-    }
-
-    public static NotificationType fromString(String status) {
-        return Arrays.stream(values())
-            .filter(s -> s.value.equalsIgnoreCase(status))
-            .findFirst()
-            .orElseThrow(() -> new IllegalArgumentException("Unknown status: " + status));
-    }
+    EMAIL
 }
