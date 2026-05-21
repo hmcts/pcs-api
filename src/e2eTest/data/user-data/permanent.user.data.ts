75bade5cbc79bed90c386fed250094f63a7f2841
SameenaHMCTS <sameena.javeed@hmcts.net>
2026-05-21 21:37:02 +0100

    Replaced hardcoded email with env var from azure key vault

--- src/e2eTest/data/user-data/permanent.user.data.ts
+++ src/e2eTest/data/user-data/permanent.user.data.ts
@@ -1,6 +1,6 @@
 export const user = {
   claimantSolicitor:  {
-    email: 'pcs-solicitor-automation@test.com',
+    email: process.env.PCS_SOLICITOR_AUTOMATION_EMAIL,
     password: process.env.IDAM_PCS_USER_PASSWORD,
     uid: process.env.PCS_SOLICITOR_AUTOMATION_UID
   },
