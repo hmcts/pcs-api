f46ca9303fbfb7974ac141fc3fd821c89c34b3bd
gmmagruder <gabrielle.magruder@gmail.com>
2026-05-22 07:39:10 +0100

    HDPI-5685: build wales tab details

---
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/tabs/details/ProhibitedConductStandardContractTabDetails.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.pcs.ccd.domain.tabs.details;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.ccd.sdk.api.CCD;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProhibitedConductStandardContractTabDetails {
+
+    @CCD(label = "Are you seeking an order imposing a prohibited conduct standard contract?")
+    private String seekingProhibitedConductStandardContract;
+
+    @CCD(
+        label = "Have you and the contract holder agreed terms of the periodic standard contract"
+            +" in addition to those incorporated by statute?"
+    )
+    private String agreedTerms;
+
+    @CCD(label = "Details of terms")
+    private String termDetails;
+
+    @CCD(label = "Why are you making this claim?")
+    private String whyMakingClaim;
+
+}
