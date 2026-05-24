15bb81eb322b882411ba3b0146d5a2b22e4553bc
sstewart <202802468+scottstewart-sl@users.noreply.github.com>
2026-05-24 20:35:14 +0100

    HDPI-6033: Refactor statement of truth model
    This is to make it suitable for use with a legal representative of a defendant in a subsequent PR

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/statementoftruth/AgreementClaimantLegalRep.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/statementoftruth/AgreementClaimantLegalRep.java
@@ -2,14 +2,14 @@ package uk.gov.hmcts.reform.pcs.ccd.domain.statementoftruth;
 
 import uk.gov.hmcts.ccd.sdk.api.HasLabel;
 
-public enum StatementOfTruthAgreementLegalRep implements HasLabel {
+public enum AgreementClaimantLegalRep implements HasLabel {
 
     AGREED("The claimant believes that the facts stated in this claim form are true. "
         + "I am authorised by the claimant to sign this statement.");
 
     private final String label;
 
-    StatementOfTruthAgreementLegalRep(String label) {
+    AgreementClaimantLegalRep(String label) {
         this.label = label;
     }
 
