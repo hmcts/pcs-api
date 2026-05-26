8154663b39f1eaa666960af120d6cc2067d2bf1d
padmakumari <padma.kumari@hmcts.net>
2026-05-26 15:19:40 +0100

    updated the spec files

--- src/e2eTest/tests/test-genApps/makeAnApplication.askToAdjourn.spec.ts
+++ src/e2eTest/tests/test-genApps/makeAnApplication.askToAdjourn.spec.ts
@@ -122,11 +122,6 @@ test('Select an Application - Ask to Adjourn journey - Court hearing 14 days[No]
     question: isTheCourtHearingInTheNext14Days.isTheCourtHearingInTheNext14DaysQuestion,
     option: isTheCourtHearingInTheNext14Days.noRadioOption,
   });
-  await performValidation('mainHeader', helpPayingTheFee.mainHeader);
-  await performAction('doYouNeedHelpPayingFee', {
-    question: helpPayingTheFee.doYouNeedHelpPayingTheFeeQuestion,
-    option: helpPayingTheFee.noRadioOption,
-  });
   await performAction('mainHeader',hasTheDefendantAskedTheOtherPartiesAgreedToThisApplication.mainHeader);
   await performAction('confirmOtherPartiesAgreed', {
     question: hasTheDefendantAskedTheOtherPartiesAgreedToThisApplication.haveTheOtherPartiesAgreedQuestion,
