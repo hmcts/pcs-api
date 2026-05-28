fe28621c56ffefd012354d6ede31bf422fe57725
gmmagruder <gabrielle.magruder@gmail.com>
2026-05-28 08:43:37 +0100

    HDPI-2983: change correspondence address to service address

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/tabs/shared/DefendantInformationTabDetails.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/tabs/shared/DefendantInformationTabDetails.java
@@ -22,7 +22,7 @@ public class DefendantInformationTabDetails {
     @CCD(label = "Last name")
     private String lastName;
 
-    @CCD(label = "Defendant 1’s correspondence address known")
+    @CCD(label = "Defendant 1’s address for service known?")
     private String addressKnown;
 
     @CCD(label = "Defendant 1 address for service")
