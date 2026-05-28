d154b842b11ca4ae2be886c1b204bf9ac12e51b4
tvr-solirius <trevor.hinson@solirius.com>
2026-05-28 17:28:02 +0100

    Merging to update from master.

--- src/main/java/uk/gov/hmcts/reform/pcs/location/service/api/LocationReferenceApi.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/location/service/api/LocationReferenceApi.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import uk.gov.hmcts.reform.pcs.location.model.CourtLocation;
 import uk.gov.hmcts.reform.pcs.location.model.CourtVenue;
 
 import java.util.List;
@@ -16,6 +17,8 @@ public interface LocationReferenceApi {
     String SERVICE_AUTHORIZATION = "ServiceAuthorization";
     String COURT_VENUES_ENDPOINT = "/refdata/location/court-venues";
 
+    String COURT_LOCATIONS_ENDPOINT = "/refdata/location/court-locations";
+
     @GetMapping(COURT_VENUES_ENDPOINT)
     List<CourtVenue> getCountyCourts(
         @RequestHeader(AUTHORIZATION) String authorisation,
@@ -23,4 +26,10 @@ public interface LocationReferenceApi {
         @RequestParam(name = "epimms_id", required = false) String epimmsId,
         @RequestParam(name = "court_type_id", required = false) Integer courTypeId
     );
+
+    @GetMapping(COURT_LOCATIONS_ENDPOINT)
+    List<CourtLocation> getCourtLocations(@RequestHeader(AUTHORIZATION) String authorisation,
+                                          @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
+                                          @RequestParam(name = "epimms_id", required = false) String epimmsId);
+
 }
