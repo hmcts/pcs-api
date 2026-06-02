893bffc95f335db0648bc22247ac3cd0c399c090
scottstewart-sl <202802468+scottstewart-sl@users.noreply.github.com>
2026-06-02 12:00:57 +0100

    HDPI-6031: Make method private

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/service/genapp/GenAppService.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/service/genapp/GenAppService.java
@@ -110,10 +110,10 @@ public class GenAppService {
         return genAppRepository.save(genAppEntity);
     }
 
-    public List<DocumentEntity> createDocumentEntities(List<ListValue<UploadedDocument>> uploadedDocuments,
-                                                       PcsCaseEntity pcsCaseEntity,
-                                                       GenAppEntity genAppEntity,
-                                                       UUID applicantPartyId) {
+    private List<DocumentEntity> createDocumentEntities(List<ListValue<UploadedDocument>> uploadedDocuments,
+                                                        PcsCaseEntity pcsCaseEntity,
+                                                        GenAppEntity genAppEntity,
+                                                        UUID applicantPartyId) {
 
         if (uploadedDocuments == null) {
             return List.of();
