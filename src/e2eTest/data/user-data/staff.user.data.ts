37a78addbfffe1e3fabfb5cbff3ef92f6d69ddf7
SameenaHMCTS <sameena.javeed@hmcts.net>
2026-06-02 18:41:59 +0100

    AM role based testing of case flags for staff and judicial users

--- src/e2eTest/data/user-data/staff.user.data.ts
+++ src/e2eTest/data/user-data/staff.user.data.ts
@@ -9,15 +9,16 @@ export const staff = {
   pcs_ctsc_team_leader_ts_email: 'pcs-ctsc-team-leader-ts-01@justice.gov.uk',
   pcs_ctsc_team_leader_ts_ca_email: 'pcs-ctsc-team-leader-ts-ca-01@justice.gov.uk',
 
-  pcs_hearing_centre_team_leader_email: 'pcs-hearing-centre-team-leader-01@justice.gov.uk',
-  pcs_hearing_centre_team_leader_ca_email: 'pcs-hearing-centre-team-leader-ca-01@justice.gov.uk',
-  pcs_hearing_centre_team_leader_ts_email: 'pcs-hearing-centre-team-leader-ts-01@justice.gov.uk',
-  pcs_hearing_centre_team_leader_ts_ca_email: 'pcs-hearing-centre-team-leader-ts-ca-01@justice.gov.uk',
-
-  pcs_hearing_centre_administrator_email: 'pcs-hearing-centre-administrator-01@justice.gov.uk',
-  pcs_hearing_centre_administrator_ca_email: 'pcs-hearing-centre-administrator-ca-01@justice.gov.uk',
-  pcs_hearing_centre_administrator_ts_email: 'pcs-hearing-centre-administrator-ts-01@justice.gov.uk',
-  pcs_hearing_centre_administrator_ts_ca_email: 'pcs-hearing-centre-administrator-ts-ca-01@justice.gov.uk',
+  //These users are not able to access case flags currently, bug is in place to fix this issue HDPI-6843
+  // pcs_hearing_centre_team_leader_email: 'pcs-hearing-centre-team-leader-01@justice.gov.uk',
+  // pcs_hearing_centre_team_leader_ca_email: 'pcs-hearing-centre-team-leader-ca-01@justice.gov.uk',
+  // pcs_hearing_centre_team_leader_ts_email: 'pcs-hearing-centre-team-leader-ts-01@justice.gov.uk',
+  // pcs_hearing_centre_team_leader_ts_ca_email: 'pcs-hearing-centre-team-leader-ts-ca-01@justice.gov.uk',
+  //
+  // pcs_hearing_centre_administrator_email: 'pcs-hearing-centre-administrator-01@justice.gov.uk',
+  // pcs_hearing_centre_administrator_ca_email: 'pcs-hearing-centre-administrator-ca-01@justice.gov.uk',
+  // pcs_hearing_centre_administrator_ts_email: 'pcs-hearing-centre-administrator-ts-01@justice.gov.uk',
+  // pcs_hearing_centre_administrator_ts_ca_email: 'pcs-hearing-centre-administrator-ts-ca-01@justice.gov.uk',
 
   pcs_wlu_administrator_email: 'pcs-wlu-administrator-01@justice.gov.uk',
   pcs_wlu_administrator_ca_email: 'pcs-wlu-administrator-ca-01@justice.gov.uk',
