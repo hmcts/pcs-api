0ee3ebfc2c1ab4bb5b3783bba5105d0f47a8ba88
SameenaHMCTS <sameena.javeed@hmcts.net>
2026-06-03 19:27:37 +0100

    AM role based testing of case flags for staff and judicial users

--- src/e2eTest/data/user-data/judicial.user.data.ts
+++ src/e2eTest/data/user-data/judicial.user.data.ts
@@ -2,10 +2,10 @@ export const judicialEmails = {
   // Appointment: Recorder, Type: Fee-paid, Mapping: Possession Fee Paid
   recorder_tyler_murphy_email: 'Recorder.TylerMurphy@ejudiciary.net',
   // Appointment: Insolvency and Companies Court Judge, Type: Salaried / SPTW, Mapping: Possession Salaried
-  icc_judge_barry_email: 'ICCJudge.Barry@ejudiciary.net',
+  //icc_judge_barry_email: 'ICCJudge.Barry@ejudiciary.net',
   // Appointment: Court of Appeal Judge, Type: Salaried / SPTW, Mapping: Possession Circuit Judge Salaried
-  lady_justice_butler_email: 'LadyJustice.Butler@ejudiciary.net',
+  //lady_justice_butler_email: 'LadyJustice.Butler@ejudiciary.net',
   // Appointment: Chief Master, Type: Salaried / SPTW, Mapping: Possession Leadership Judge Salaried
-  master_mendoza_email: 'Master.Mendoza@ejudiciary.net',
+  //master_mendoza_email: 'Master.Mendoza@ejudiciary.net',
 
 };
