96b257a0a491d76add5923e09ef9f976215ba9c7
guygrewal77 <guygrewal7@gmail.com>
2026-06-10 11:03:51 +0100

    HDPI-7064 - document id needs to be set where applicable

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/view/TenancyLicenceView.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/view/TenancyLicenceView.java
@@ -79,6 +79,7 @@ public class TenancyLicenceView {
 
     private static ListValue<Document> toDocument(DocumentEntity documentEntity) {
         return ListValue.<Document>builder()
+            .id(documentEntity.getId().toString())
             .value(
                 Document.builder()
                     .url(documentEntity.getUrl())
