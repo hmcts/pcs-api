125c592b1659c1652affe949cfc4cbb0ff74b214
sstewart <202802468+scottstewart-sl@users.noreply.github.com>
2026-06-26 16:37:16 +0100

    HDPI-7213: Add CftLibTest for gen app creation
    Includes shared wiremock stubs for document services and also a wiremock test for emails

---
+++ src/cftlibTest/java/uk/gov/hmcts/reform/pcs/CitizenMakeAnApplicationTest.java
@@ -0,0 +1,111 @@
+package uk.gov.hmcts.reform.pcs;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
+import uk.gov.hmcts.reform.pcs.ccd.domain.VerticalYesNo;
+import uk.gov.hmcts.reform.pcs.ccd.domain.genapp.CitizenGenAppRequest;
+import uk.gov.hmcts.reform.pcs.ccd.domain.genapp.GenAppType;
+import uk.gov.hmcts.reform.pcs.ccd.event.EventId;
+import uk.gov.hmcts.reform.pcs.client.CcdClient;
+import uk.gov.hmcts.reform.pcs.client.TestingSupportClient;
+import uk.gov.hmcts.reform.pcs.model.PartyAccessCode;
+import uk.gov.hmcts.reform.pcs.notification.CapturedNotification;
+import uk.gov.hmcts.reform.pcs.notification.MockNotificationServer;
+import uk.gov.hmcts.reform.pcs.service.AccessCodeService;
+import uk.gov.hmcts.reform.pcs.service.CaseCreationService;
+import uk.gov.hmcts.reform.pcs.testingsupport.model.PartyEmail;
+import uk.gov.hmcts.rse.ccd.lib.test.CftlibTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CitizenMakeAnApplicationTest extends CftlibTest {
+
+    private static final String CITIZEN_EMAIL_ADDRESS = "test@test.com";
+    private static final String GEN_APP_NOTIFICATION_TEMPLATE_ID = "e5daba0d-5c66-4c76-9e0f-06646200dc89";
+
+    @Autowired
+    private CcdClient ccdClient;
+    @Autowired
+    private IdamClient idamClient;
+    @Autowired
+    private CaseCreationService caseCreationService;
+    @Autowired
+    private AccessCodeService accessCodeService;
+    @Autowired
+    private TestingSupportClient testingSupportClient;
+
+    private final MockNotificationServer mockNotificationServer = new MockNotificationServer();
+
+    private String solicitorToken;
+    private String citizenToken;
+
+    @BeforeAll
+    void setup() {
+        solicitorToken = idamClient.getAccessToken("pcs-solicitor1@test.com", "password");
+        citizenToken = idamClient.getAccessToken("citizen@pcs.com", "password");
+
+        mockNotificationServer.startServer();
+    }
+
+    @AfterAll
+    void tearDown() {
+        mockNotificationServer.stopServer();
+    }
+
+    @BeforeEach
+    void setUp() {
+        mockNotificationServer.resetServer();
+    }
+
+    @Test
+    void makeAnApplication() {
+        long caseReference = caseCreationService.createMinimalCase(solicitorToken);
+
+        List<PartyAccessCode> partyAccessCodes = accessCodeService.waitForAccessCodes(caseReference);
+        PartyAccessCode partyAccessCode = partyAccessCodes.getFirst();
+        accessCodeService.linkUserToCase(caseReference, partyAccessCode.getAccessCode(), citizenToken);
+
+        PartyEmail partyEmail = PartyEmail.builder()
+            .partyId(partyAccessCode.getPartyId())
+            .emailAddress(CITIZEN_EMAIL_ADDRESS)
+            .build();
+        testingSupportClient.setPartyEmail(partyEmail, citizenToken);
+
+        CitizenGenAppRequest citizenGenAppRequest = CitizenGenAppRequest.builder()
+            .applicationType(GenAppType.ADJOURN)
+            .needHwf(VerticalYesNo.NO)
+            .withoutNotice(VerticalYesNo.NO)
+            .hasSupportingDocuments(VerticalYesNo.NO)
+            .sotAccepted(VerticalYesNo.YES)
+            .sotFullName("Test party name")
+            .build();
+
+        PCSCase caseData = PCSCase.builder()
+            .citizenGenAppRequest(citizenGenAppRequest)
+            .build();
+
+        ccdClient.updateCase(EventId.makeAnApplication, caseReference, caseData, citizenToken);
+
+        List<CapturedNotification> sentNotifications = mockNotificationServer.getSentNotifications(1);
+
+        CapturedNotification sentNotification = sentNotifications.getFirst();
+        assertThat(sentNotification.getTemplateId()).isEqualTo(GEN_APP_NOTIFICATION_TEMPLATE_ID);
+        assertThat(sentNotification.getEmailAddress()).isEqualTo(CITIZEN_EMAIL_ADDRESS);
+        assertThat(sentNotification.getPersonalisation())
+            .containsKeys("caseNumber", "claimantName", "firstName", "lastName", "primaryDefendantName");
+        assertThat(sentNotification.getReference()).isNotBlank();
+    }
+
+}
