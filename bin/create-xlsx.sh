fa2bb1fa0857e0523ceb8eabde668526b11056d5
taleb <tech@benouaer.com>
2026-06-26 17:25:22 +0100

    HDPI-7052: update ccd def processor image with GroupAccess changes

--- bin/create-xlsx.sh
+++ bin/create-xlsx.sh
@@ -26,7 +26,7 @@ for case_dir in "$run_dir"/build/definitions/*/; do
 docker run --rm --name "json2xlsx" \
   -v "$run_dir/build/definitions/${case_type}:/tmp/ccd-input" \
   -v "$run_dir/build/definitions:/tmp/ccd-output" \
-  hmctsprod.azurecr.io/ccd/definition-processor:pr-348-b9a81c6 \
+  hmctsprod.azurecr.io/ccd/definition-processor:pr-348-64dd53f \
   json2xlsx -D /tmp/ccd-input -o /tmp/ccd-output/"${ccd_definition_file}"
 
 done
