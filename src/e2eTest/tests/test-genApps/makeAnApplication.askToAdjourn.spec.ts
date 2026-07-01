4fb2fae927bd53df4c3199e60ceedc8ebb8f345c
padmakumari <padma.kumari@hmcts.net>
2026-07-01 17:40:31 +0100

    removed @PR tag from spec files

--- src/e2eTest/tests/test-genApps/makeAnApplication.askToAdjourn.spec.ts
+++ src/e2eTest/tests/test-genApps/makeAnApplication.askToAdjourn.spec.ts
@@ -75,7 +75,7 @@ test.afterEach(async () => {
 });
 
 test.describe('Make an Application - e2e Journey @nightly', async () => {
-  test('Select an Application - Ask to Adjourn journey - Court hearing in 14 days[Yes] @regression @PR @smoke', async () => {
+  test('Select an Application - Ask to Adjourn journey - Court hearing in 14 days[Yes] @regression @smoke', async () => {
     await performAction('select', caseSummary.nextStepEventList, caseSummary.makeAnApplication);
     await performAction('clickButton', caseSummary.go);
     await performAction('chooseAnApplication', {
@@ -148,7 +148,7 @@ test.describe('Make an Application - e2e Journey @nightly', async () => {
   });
 
 
-test('Select an Application - Ask to Adjourn journey - Help paying the Fee[No] @PR', async () => {
+test('Select an Application - Ask to Adjourn journey - Help paying the Fee[No]', async () => {
   await performAction('select', caseSummary.nextStepEventList, caseSummary.makeAnApplication);
   await performAction('clickButton', caseSummary.go);
   await performAction('chooseAnApplication', {
