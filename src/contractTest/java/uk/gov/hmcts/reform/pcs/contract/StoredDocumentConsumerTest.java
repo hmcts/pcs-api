72714768fcfb8234500067e4918c80109cb77514
Arunabha Chowdhury <90389022+ArunabhaChowdhury@users.noreply.github.com>
2026-07-02 13:15:05 +0100

    empty commit

--- src/contractTest/java/uk/gov/hmcts/reform/pcs/contract/StoredDocumentConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/pcs/contract/StoredDocumentConsumerTest.java
@@ -43,7 +43,7 @@ public class StoredDocumentConsumerTest {
 
         given()
             .accept(APPLICATION)
-            .header("ServiceAuthorization", SERVICE_AUTHORIZATION)
+            .header("ServiceAuthorization",SERVICE_AUTHORIZATION)
             .when()
             .get("/documents/" + DOCUMENT_ID)
             .then()
