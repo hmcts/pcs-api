d25253f45618c51e762b854ee092ccf8abd1f01a
sstewart <202802468+scottstewart-sl@users.noreply.github.com>
2026-07-03 19:08:29 +0100

    Test 1

---
+++ src/main/resources/db/migration/V133__add_document_original_name.sql
@@ -0,0 +1,10 @@
+ALTER TABLE document
+  ADD COLUMN original_file_name TEXT;
+
+-- Get original filename from patterns like:
+--   * General Application GA1 - Defendant 3.pdf
+--   * rentStatement - Claimant 1.pdf
+UPDATE document
+SET original_file_name =  regexp_replace(file_name, '(.+?)(?: GA\d+)*(?:(?: - Defendant)|(?: - Claimant)) \d+(\..+)', '\1\2')
+WHERE original_file_name IS NULL AND file_name IS NOT NULL;
+
