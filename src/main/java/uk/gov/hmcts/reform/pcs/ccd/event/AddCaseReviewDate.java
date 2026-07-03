b69d6a90f3e2a4ea19c17562713b2edf8176f958
gmmagruder <gabrielle.magruder@gmail.com>
2026-07-03 10:09:54 +0100

    HDPI-6501: fix checkstyle

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/AddCaseReviewDate.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/AddCaseReviewDate.java
@@ -8,7 +8,6 @@ import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.EventPayload;
 import uk.gov.hmcts.ccd.sdk.api.Permission;
 import uk.gov.hmcts.ccd.sdk.api.callback.SubmitResponse;
-import uk.gov.hmcts.ccd.sdk.type.AddressUK;
 import uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole;
 import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
