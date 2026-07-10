10579b345c4fd30482a39e7025c6b8280c869a0d
gmmagruder <gabrielle.magruder@gmail.com>
2026-07-10 16:35:18 +0100

    HDPI-6505: fix checkstyle

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/entity/HearingEntity.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/entity/HearingEntity.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.reform.pcs.ccd.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -11,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
