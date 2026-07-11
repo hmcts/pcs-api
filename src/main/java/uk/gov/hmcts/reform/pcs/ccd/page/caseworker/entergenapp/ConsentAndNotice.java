bdd96fd9238037dc91838d7824285684ef8cb4c7
sstewart <202802468+scottstewart-sl@users.noreply.github.com>
2026-07-11 11:39:34 +0100

    HDPI-6638: Remove unused placeholder text

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/caseworker/entergenapp/ConsentAndNotice.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/caseworker/entergenapp/ConsentAndNotice.java
@@ -10,17 +10,6 @@ import static uk.gov.hmcts.reform.pcs.ccd.ShowConditions.fieldEquals;
 
 public class ConsentAndNotice implements CcdPageConfiguration {
 
-    private static final String PLACEHOLDER_BANNER = """
-        <div class="govuk-notification-banner" role="region"
-        aria-labelledby="govuk-notification-banner-title" data-module="govuk-notification-banner">
-          <div class="govuk-notification-banner__content">
-            <p class="govuk-notification-banner__heading">
-              Placeholder
-            </p>
-          </div>
-        </div>
-        """;
-
     @Override
     public void addTo(PageBuilder pageBuilder) {
         pageBuilder
