497c19b8850d5e2cccc08bb95fe4870c26f25f8b
guygrewal77 <guygrewal7@gmail.com>
2026-07-16 15:51:07 +0100

    HDPI-3460 - Delete Case page change reverting to original.

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/deletecase/DeleteThisCasePage.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/deletecase/DeleteThisCasePage.java
@@ -2,16 +2,10 @@ package uk.gov.hmcts.reform.pcs.ccd.page.deletecase;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.reform.pcs.ccd.common.CcdPageConfiguration;
 import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
-import uk.gov.hmcts.reform.pcs.ccd.domain.State;
 import uk.gov.hmcts.reform.pcs.ccd.page.CcdPage;
-
-import java.util.List;
 
 @AllArgsConstructor
 @Component
@@ -22,30 +16,14 @@ public class DeleteThisCasePage implements CcdPageConfiguration, CcdPage {
         String pageKey = getPageKey();
 
         pageBuilder
-            .page(pageKey, this::midEvent)
+            .page(pageKey)
             .pageLabel("Delete this case")
             .label(pageKey + "-separator", "---")
             .mandatory(PCSCase::getDeleteUnsubmittedClaim);
-    }
-
-    // The event carries a TTLIncrement, so submitting it always stamps the retention TTL.
-    // Block anything but an explicit Yes from reaching submission.
-    private AboutToStartOrSubmitResponse<PCSCase, State> midEvent(CaseDetails<PCSCase, State> details,
-                                                                  CaseDetails<PCSCase, State> detailsBefore) {
-        if (!YesOrNo.YES.equals(details.getData().getDeleteUnsubmittedClaim())) {
-            return AboutToStartOrSubmitResponse.<PCSCase, State>builder()
-                .errors(List.of("Select Yes to delete this case, or cancel to keep it"))
-                .build();
-        }
-
-        return AboutToStartOrSubmitResponse.<PCSCase, State>builder()
-            .data(details.getData())
-            .build();
     }
 
     @Override
     public String getPageKey() {
         return CcdPage.derivePageKey(this.getClass());
     }
-
 }
