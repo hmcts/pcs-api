1e9979077131fe637596624d7c14d5122e6a8253
Avi Singh <avi.singh@solirius.com>
2026-07-16 11:17:30 +0100

    Created initial test file

---
+++ src/test/java/DataTest.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.pcs;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DataTest {
+}
