0995c3a2fa1b5eee4a490aaaad7e67adf816d5ea
sstewart <202802468+scottstewart-sl@users.noreply.github.com>
2026-07-17 14:07:14 +0100

    HDPI-6505: Ensure party list is hidden when turning off notice

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/managehearing/AddHearingPage.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/managehearing/AddHearingPage.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.reform.pcs.ccd.ShowConditions;
 import uk.gov.hmcts.reform.pcs.ccd.common.CcdPageConfiguration;
 import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
+import uk.gov.hmcts.reform.pcs.ccd.domain.VerticalYesNo;
 import uk.gov.hmcts.reform.pcs.ccd.domain.hearing.Hearing;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
 import uk.gov.hmcts.reform.pcs.ccd.domain.State;
@@ -15,10 +17,16 @@ import uk.gov.hmcts.reform.pcs.ccd.service.TextAreaValidationService;
 import java.util.List;
 
 import static uk.gov.hmcts.reform.pcs.ccd.ShowConditions.NEVER_SHOW;
+import static uk.gov.hmcts.reform.pcs.ccd.ShowConditions.fieldEquals;
 
 @AllArgsConstructor
 @Component
 public class AddHearingPage implements CcdPageConfiguration, CcdPage {
+
+    private static final String PARTYLIST_SHOW_CONDITION = ShowConditions.and(
+            fieldEquals("hearing_IssueNotice", VerticalYesNo.YES),
+            fieldEquals("hearing_IsWithoutNotice", VerticalYesNo.YES)
+    );
 
     private final TextAreaValidationService textAreaValidationService;
 
@@ -53,7 +61,7 @@ public class AddHearingPage implements CcdPageConfiguration, CcdPage {
             .mandatory(Hearing::getIssueNotice)
             .mandatory(Hearing::getIsWithoutNotice, "hearing_IssueNotice=\"YES\"")
             .done()
-            .mandatory(PCSCase::getPartyMultiSelectionList, "hearing_IsWithoutNotice=\"YES\"", null,
+            .mandatory(PCSCase::getPartyMultiSelectionList, PARTYLIST_SHOW_CONDITION, null,
                        "Who should receive the hearing notice?", "Select all that apply")
             .complex(PCSCase::getHearing)
             .optional(Hearing::getAdditionalInformation)
