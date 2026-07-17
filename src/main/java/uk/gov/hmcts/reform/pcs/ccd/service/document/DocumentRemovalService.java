e03982f62f3c04f3c5cf6626c91dcf73de4e42c7
Liban Abdirahman <liban.abdirahman@hmcts.net>
2026-07-17 14:10:13 +0100

    HDPI-6454:Adding migration script and document removal service

---
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/service/document/DocumentRemovalService.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.reform.pcs.ccd.service.document;
+
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.pcs.ccd.entity.DocumentEntity;
+import uk.gov.hmcts.reform.pcs.ccd.repository.DocumentRepository;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class DocumentRemovalService {
+
+    private final DocumentRepository documentRepository;
+    private final DocumentImportService documentImportService;
+
+    public DocumentRemovalService(DocumentRepository documentRepository,
+                                   DocumentImportService documentImportService) {
+        this.documentRepository = documentRepository;
+        this.documentImportService = documentImportService;
+    }
+
+    public void removeDocument(UUID documentEntityId, String reason) {
+        DocumentEntity documentEntity = documentRepository.findById(documentEntityId).orElseThrow();
+
+        documentImportService.deleteDocument(documentEntity.getUrl());
+
+        documentEntity.setRemoved(true);
+        documentEntity.setRemovalReason(reason);
+        documentEntity.setRemovedAt(LocalDateTime.now());
+        documentRepository.save(documentEntity);
+    }
+}
