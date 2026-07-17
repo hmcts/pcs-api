73e7462e32197c9bb1f61288ef631ecde7de6571
sstewart <202802468+scottstewart-sl@users.noreply.github.com>
2026-07-17 14:08:48 +0100

    HDPI-6505: Fix checkstyle

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/accesscontrol/AccessGrants.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/accesscontrol/AccessGrants.java
@@ -15,7 +15,6 @@ import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.FEE_PAID_JUDGE;
 import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.HEARING_CENTRE_ADMIN;
 import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.JUDGE;
 import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.LEADERSHIP_JUDGE;
-import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.PCS_CASE_WORKER;
 import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.PCS_SOLICITOR;
 import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.WLU_ADMIN;
 
