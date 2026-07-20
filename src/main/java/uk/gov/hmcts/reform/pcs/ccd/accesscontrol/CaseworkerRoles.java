ca4c3dbc7590b3a0caa9128b5c15e28b84e59937
sadmanrahman <sadman.rahman@HMCTS.NET>
2026-07-20 11:12:32 +0100

    HDPI-6453: fix roles

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/accesscontrol/CaseworkerRoles.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/accesscontrol/CaseworkerRoles.java
@@ -5,10 +5,6 @@ public final class CaseworkerRoles {
     public static final UserRole[] CASEWORKER_ROLES = {
         UserRole.HEARING_CENTRE_TEAM_LEADER,
         UserRole.HEARING_CENTRE_ADMIN,
-        UserRole.CTSC_TEAM_LEADER,
-        UserRole.CTSC_ADMIN,
-        UserRole.WLU_TEAM_LEADER,
-        UserRole.WLU_ADMIN
     };
 
     private CaseworkerRoles() {
