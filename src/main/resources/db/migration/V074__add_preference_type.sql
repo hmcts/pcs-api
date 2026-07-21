39502320e49f474ea86c9db2a2177deb919cac73
arun-hmcts <73122505+arun-hmcts@users.noreply.github.com>
2026-07-21 21:53:29 +0100

    HDPI-7834: convert internal UUID primary keys to bigint identity (#2227)

--- src/main/resources/db/migration/V074__add_preference_type.sql
+++
@@ -1,6 +0,0 @@
-CREATE TYPE CONTACT_PREFERENCE_TYPE AS ENUM ('EMAIL', 'POST');
-
-ALTER TABLE contact_preferences
-    ADD COLUMN preference_type CONTACT_PREFERENCE_TYPE,
-    DROP COLUMN contact_by_email,
-    DROP COLUMN contact_by_post;
