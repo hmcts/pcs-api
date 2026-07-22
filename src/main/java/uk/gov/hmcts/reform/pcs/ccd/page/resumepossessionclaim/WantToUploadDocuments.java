bb0db8e365bd303d0454570b15cb93a0621c4029
shazanm-soli <shazan.miah@solirius.com>
2026-07-22 15:07:39 +0100

    removed country to main doc type logic as not needed as draft stores country doc type

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/resumepossessionclaim/WantToUploadDocuments.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/resumepossessionclaim/WantToUploadDocuments.java
@@ -4,18 +4,13 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.reform.pcs.ccd.common.CcdPageConfiguration;
 import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
-import uk.gov.hmcts.reform.pcs.ccd.domain.AdditionalDocument;
-import uk.gov.hmcts.reform.pcs.ccd.domain.AdditionalDocumentTypeEngland;
-import uk.gov.hmcts.reform.pcs.ccd.domain.AdditionalDocumentTypeWales;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
 import uk.gov.hmcts.reform.pcs.ccd.domain.State;
 import uk.gov.hmcts.reform.pcs.ccd.event.EventId;
 import uk.gov.hmcts.reform.pcs.ccd.page.CommonPageContent;
 import uk.gov.hmcts.reform.pcs.ccd.service.DraftCaseDataService;
-import uk.gov.hmcts.reform.pcs.postcodecourt.model.LegislativeCountry;
 
 import java.util.Optional;
 
@@ -52,26 +47,6 @@ public class WantToUploadDocuments implements CcdPageConfiguration {
 
         if (draftCaseData.isPresent() && draftCaseData.get().getAdditionalDocuments() != null) {
             caseData.setAdditionalDocuments(draftCaseData.get().getAdditionalDocuments());
-
-            for (ListValue<AdditionalDocument> additionalDocumentListValue : caseData.getAdditionalDocuments()) {
-                AdditionalDocument additionalDocument = additionalDocumentListValue.getValue();
-                if (additionalDocument == null || additionalDocument.getDocumentType() == null) {
-                    continue;
-                }
-
-                String label = additionalDocument.getDocumentType().getValueLabel();
-                if (label == null) {
-                    continue;
-                }
-
-                if (caseData.getLegislativeCountry() == LegislativeCountry.WALES) {
-                    additionalDocument.setDocumentTypeWales(AdditionalDocumentTypeWales.getValueFromLabel(label));
-                } else {
-                    additionalDocument.setDocumentTypeEngland(AdditionalDocumentTypeEngland.getValueFromLabel(label));
-                }
-
-                additionalDocument.setDocumentType(null);
-            }
         }
     }
 
