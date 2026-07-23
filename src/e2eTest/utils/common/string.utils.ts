2b8470ffcbe2aa2b7e74afa697358d53b1434034
arun <arun.kumar@hmcts.net>
2026-07-23 18:23:34 +0100

    Revert "HDPI-7878: add additional postcodes for nightly global search (#2249)"
    This reverts commit 0207c752ea554511a9a3b70e3e3841b7028ae80f.

--- src/e2eTest/utils/common/string.utils.ts
+++ src/e2eTest/utils/common/string.utils.ts
@@ -154,6 +154,6 @@ export function formatCaseStateText(input: string): string {
 
 export function randomPostcode()
 {
-  const postcodes = ['M1 1AE', 'B1 1AA', 'CF10 1EP'];
+  const postcodes = ['W3 6RS', 'W3 6RT', 'CF61 1ZH'];
   return postcodes[Math.floor(Math.random() * postcodes.length)];
 }
