09d37899412a2f88b1805fdf74fa6a56fc83262d
gmmagruder <gabrielle.magruder@gmail.com>
2026-07-28 14:24:09 +0100

    HDPI-6585: fix checkstyle

--- src/test/java/uk/gov/hmcts/reform/pcs/camunda/CamundaServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/camunda/CamundaServiceTest.java
@@ -319,8 +319,7 @@ public class CamundaServiceTest {
         camundaService.handleRequest(taskData);
 
         // Then
-        ArgumentCaptor<SendMessageRequest> requestArgumentCaptor =
-        ArgumentCaptor.forClass(SendMessageRequest.class);
+        ArgumentCaptor<SendMessageRequest> requestArgumentCaptor = ArgumentCaptor.forClass(SendMessageRequest.class);
         verify(camundaApi).sendMessage(eq("authToken"), requestArgumentCaptor.capture());
         SendMessageRequest sendMessageRequest = requestArgumentCaptor.getValue();
 
@@ -349,8 +348,7 @@ public class CamundaServiceTest {
         camundaService.handleRequest(taskData);
 
         // Then
-        ArgumentCaptor<SendMessageRequest> requestArgumentCaptor =
-        ArgumentCaptor.forClass(SendMessageRequest.class);
+        ArgumentCaptor<SendMessageRequest> requestArgumentCaptor = ArgumentCaptor.forClass(SendMessageRequest.class);
         verify(camundaApi).sendMessage(eq("authToken"), requestArgumentCaptor.capture());
         SendMessageRequest sendMessageRequest = requestArgumentCaptor.getValue();
 
@@ -379,8 +377,7 @@ public class CamundaServiceTest {
         camundaService.handleRequest(taskData);
 
         // Then
-        ArgumentCaptor<SendMessageRequest> requestArgumentCaptor =
-        ArgumentCaptor.forClass(SendMessageRequest.class);
+        ArgumentCaptor<SendMessageRequest> requestArgumentCaptor = ArgumentCaptor.forClass(SendMessageRequest.class);
         verify(camundaApi).sendMessage(eq("authToken"), requestArgumentCaptor.capture());
         SendMessageRequest sendMessageRequest = requestArgumentCaptor.getValue();
 
