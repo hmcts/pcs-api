6cb1ce48ccba3662d3e91fd8e274ef337043aa20
Nicole-Samuels <nicole.samuels@solirius.com>
2026-07-28 15:47:26 +0100

    updated content and removed done() from page builder

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/legalrepdocumentupload/ExistingApplicationPage.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/legalrepdocumentupload/ExistingApplicationPage.java
@@ -19,7 +19,7 @@ public class ExistingApplicationPage implements CcdPageConfiguration, CcdPage {
                         the defendant’s landlord, housing association or mortgage provider.
                     </p>
                      <p class="govuk-body">
-                        If your application is without ‘notice’ (where you have asked us to consider your application
+                        If your application is ‘without notice’ (where you have asked us to consider your application
                         without telling the other party) we will not share anything with them.
                     </p>
                     """;
@@ -40,8 +40,7 @@ public class ExistingApplicationPage implements CcdPageConfiguration, CcdPage {
             .label(pageKey + "-line-separator", "---")
             .label(pageKey + "-content", WITHOUT_NOTICE_APPLICATION_GUIDANCE)
             .complex(PCSCase::getLegalRepDocumentUploadDetails)
-            .mandatory(LegalRepDocumentUploadDetails::getValidCategories)
-            .done();
+            .mandatory(LegalRepDocumentUploadDetails::getValidCategories);
     }
 
     @Override
