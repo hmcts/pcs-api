e38f2e9e2a9e50730a9e680f22dd21cb07776296
Nicole-Samuels <nicole.samuels@solirius.com>
2026-07-28 12:48:26 +0100

    updated ExistingApplicationPage with a midEvent to deal with the error messaging and also updated content with curly apostrophes

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/legalrepdocumentupload/ExistingApplicationPage.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/legalrepdocumentupload/ExistingApplicationPage.java
@@ -1,8 +1,11 @@
 package uk.gov.hmcts.reform.pcs.ccd.page.legalrepdocumentupload;
 
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.reform.pcs.ccd.common.CcdPageConfiguration;
 import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
+import uk.gov.hmcts.reform.pcs.ccd.domain.State;
 import uk.gov.hmcts.reform.pcs.ccd.domain.legalrepdocumentupload.LegalRepDocumentUploadDetails;
 import uk.gov.hmcts.reform.pcs.ccd.page.CcdPage;
 
@@ -16,16 +19,19 @@ public class ExistingApplicationPage implements CcdPageConfiguration, CcdPage {
                         the defendant’s landlord, housing association or mortgage provider.
                     </p>
                      <p class="govuk-body">
-                        If your application is 'without notice' (where you have asked us to consider your application
+                        If your application is without ‘notice’ (where you have asked us to consider your application
                         without telling the other party) we will not share anything with them.
                     </p>
                     """;
+
+    static final String ERROR_MESSAGE =
+        "Confirm if these documents relate to an existing application";
 
     @Override
     public void addTo(PageBuilder pageBuilder) {
         String pageKey = getPageKey();
         pageBuilder
-            .page(pageKey)
+            .page(pageKey, this::midEvent)
             .pageLabel("Confirm if these documents relate to an existing application")
             .showCondition("showExistingApplicationPage=\"Yes\"")
             .complex(PCSCase::getLegalRepDocumentUploadDetails)
@@ -41,5 +47,14 @@ public class ExistingApplicationPage implements CcdPageConfiguration, CcdPage {
     @Override
     public String getPageKey() {
         return CcdPage.derivePageKey(this.getClass());
+    }
+
+    public AboutToStartOrSubmitResponse<PCSCase, State> midEvent(CaseDetails<PCSCase, State> details,
+                                                                 CaseDetails<PCSCase, State> before) {
+        PCSCase data = details.getData();
+        return AboutToStartOrSubmitResponse.<PCSCase, State>builder()
+            .data(data)
+            .errorMessageOverride(ERROR_MESSAGE)
+            .build();
     }
 }
