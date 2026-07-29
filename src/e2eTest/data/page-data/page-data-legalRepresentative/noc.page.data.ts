af414d16034813cdf86d238795da61b05955028c
ashajayaprakash <asha.jayaprakash@hmcts.net>
2026-07-29 17:10:53 +0100

    Automation-HDPI-4176

---
+++ src/e2eTest/data/page-data/page-data-legalRepresentative/noc.page.data.ts
@@ -0,0 +1,9 @@
+export const noc = {
+  mainHeader: `Notice of change`,
+  youCanUseThisNoticeParagraph: ` You can use this notice of change (sometimes called a 'notice of acting') to get access to the digital case file in place of:`,
+  aClientActingInPersonListItem: `a client acting in person`,
+  aLegalRepresentativeListItem: `a legal representative previously acting on your client's behalf`,
+  onlineCaseReferenceNumberTextLabel: `Online case reference number`,
+  thisIsHintText: `This is a 16-digit number from MyHMCTS, for example 1111-2222-3333-4444`,
+  continueButton: `Continue`,
+};
---
+++ src/e2eTest/tests/noticeOfChangeLR.spec.ts
@@ -0,0 +1,52 @@
+import { createCaseApiData, submitCaseApiData } from '@data/api-data';
+
+
+import { initializeExecutor } from '@utils/controller';
+import test from '@playwright/test';
+import { FieldsStore } from '@utils/actions/custom-actions/custom-actions-genApps/recordAnsweredFields.action';
+import { initializeGenAppsExecutor, performAction } from '@utils/controller-genApps';
+import { user } from '@data/user-data';
+import { dismissCookieBanner } from '@config/cookie-banner';
+import { caseInfo } from '@utils/actions/custom-actions';
+import { PageContentValidation } from '@utils/validations/element-validations/pageContent.validation';
+import { defendantDetails } from '@utils/actions/custom-actions/custom-actions-genApps/genApps.action';
+import { home } from '@data/page-data';
+import {noc} from '@data/page-data/page-data-legalRepresentative/noc.page.data';
+
+
+test.use({ storageState: undefined })
+
+test.beforeEach(async ({ page, context }) => {
+  await context.clearCookies();
+  initializeExecutor(page);
+  initializeGenAppsExecutor(page);
+  defendantDetails.length = 0;
+  FieldsStore.clear();
+  await performAction('createCaseAPI', { data: createCaseApiData.createCasePayload });
+  await performAction('submitCaseAPI', { data: submitCaseApiData.submitCasePayload });
+  await performAction('getDefendantDetails', {
+    defendant1NameKnown: submitCaseApiData.submitCasePayload.defendant1.nameKnown,
+    additionalDefendants: submitCaseApiData.submitCasePayload.addAnotherDefendant,
+    payLoad: submitCaseApiData.submitCasePayload
+  });
+  await performAction('navigateToUrl', process.env.MANAGE_CASE_BASE_URL);
+  await dismissCookieBanner(page, 'additional');
+  await performAction('login', user.defendantSolicitor);
+  await dismissCookieBanner(page, 'analytics');
+  await performAction('clickTab', home.noticeOfChangeTab);
+});
+
+test.afterEach(async () => {
+  if (caseInfo.id) {
+    await performAction('deleteCaseRole', '[CLAIMANTSOLICITOR]');
+  }
+  PageContentValidation.finaliseTest();
+
+});
+
+test.describe('Make an Application - LR - e2e Journey @nightly', async () => {
+  test('Notice of change - LR @regression @smoke', async () => {
+    await performAction('inputText', noc.onlineCaseReferenceNumberTextLabel, caseInfo.id);
+    await performAction('clickButton', noc.continueButton);
+  });
+});
