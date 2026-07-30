fb25936eef2ed1fd361f6871fddcb394b2731114
srinijg <srinivasan.joghee@hmcts.net>
2026-07-30 11:51:53 +0100

    Delete src/e2eTest/data/page-data-figma/page-data-caseManagement-figma/enterGenAppUploadGeneralApplication.page.data.ts

--- src/e2eTest/data/page-data-figma/page-data-caseManagement-figma/enterGenAppUploadGeneralApplication.page.data.ts
+++
@@ -1,9 +0,0 @@
-export const enterGenAppUploadGeneralApplication = {
-  mainHeader: `Upload general application`,
-  uploadADocumentParagraph: `Upload a document to the system`,
-  uploadDocHiddenOption:[`legalAidCertificate.pdf`,`possessionNotice.pdf`,`certificateOfService.pdf`,`GenApp.pdf`],
-  dateTypeHiddenUserInput: `past`,
-  continueButton: `Continue`,
-  previousButton: `Previous`,
-  cancelLink: `Cancel`,
-};
