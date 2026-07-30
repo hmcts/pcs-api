39dec5f1e9fa932cac6b0a30d4b5c378cc71d59e
Toby Plunkett <toby.plunkett@HMCTS.NET>
2026-07-30 15:09:32 +0100

    Add test

--- src/test/java/uk/gov/hmcts/reform/pcs/config/DefendantFlagsDraftSerialisationTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/config/DefendantFlagsDraftSerialisationTest.java
@@ -8,6 +8,7 @@ import uk.gov.hmcts.ccd.sdk.type.Flags;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
+import uk.gov.hmcts.reform.pcs.ccd.domain.respondpossessionclaim.PossessionClaimResponse;
 
 import java.time.LocalDateTime;
 
@@ -51,7 +52,104 @@ class DefendantFlagsDraftSerialisationTest {
         }
         """;
 
+    /**
+     * A complete draft as stored in draft.draft_case_data, with the flags alongside the answers the
+     * defendant had already given.
+     */
+    private static final String STORED_DRAFT = """
+        {
+          "possessionClaimResponse": {
+            "defendantFlags": {
+              "details": [
+                {
+                  "value": {
+                    "name": "Accessible toilet",
+                    "path": [
+                      { "value": "Party" },
+                      { "value": "Reasonable adjustment" },
+                      { "value": "I need adjustments to get to, into and around our buildings" }
+                    ],
+                    "status": "Active",
+                    "name_cy": "Toiled hygyrch",
+                    "flagCode": "RA0022",
+                    "flagComment": "",
+                    "flagComment_cy": "",
+                    "dateTimeCreated": "2026-07-29T16:23:53.169Z",
+                    "hearingRelevant": "YES",
+                    "availableExternally": "YES"
+                  }
+                }
+              ],
+              "partyName": "2 2",
+              "roleOnCase": "Defendant"
+            },
+            "defendantResponses": {
+              "dateOfBirth": "2000-01-01",
+              "disputeClaim": "NO",
+              "hasSolicitor": "NO",
+              "languageUsed": "ENGLISH",
+              "contactByPost": "YES",
+              "contactByEmail": "NO",
+              "contactByPhone": "NO",
+              "freeLegalAdvice": "NO",
+              "makeCounterClaim": "NO",
+              "statementOfTruth": { "accepted": "YES", "fullName": "q" },
+              "completedSections": [
+                "START_NOW_AND_DETAILS",
+                "PERSONAL_DETAILS",
+                "DISPUTE_AND_TENANCY",
+                "SITUATION_AND_CIRCUMSTANCES",
+                "INCOME_AND_EXPENDITURE",
+                "UPLOAD_FILES"
+              ],
+              "defendantDocuments": [],
+              "otherConsiderations": "NO",
+              "householdCircumstances": {
+                "otherTenants": "NO",
+                "otherDependants": "NO",
+                "dependantChildren": "NO",
+                "exceptionalHardship": "NO",
+                "alternativeAccommodation": "NO",
+                "shareIncomeExpenseDetails": "NO",
+                "shareAdditionalCircumstances": "NO"
+              },
+              "tenancyTypeConfirmation": "YES",
+              "defendantNameConfirmation": "YES",
+              "propertyAddressConfirmation": "YES"
+            },
+            "defendantContactDetails": {
+              "party": {
+                "address": {
+                  "County": "",
+                  "Country": "United Kingdom",
+                  "PostCode": "W3 7RX",
+                  "PostTown": "London",
+                  "AddressLine1": "1 Second Avenue",
+                  "AddressLine2": "",
+                  "AddressLine3": ""
+                }
+              }
+            }
+          }
+        }
+        """;
+
     private final ObjectMapper underTest = new JacksonConfiguration().draftCaseDataObjectMapper();
+
+    @Test
+    void shouldReadDefendantFlagsFromAStoredDraftAlongsideTheAnswers() throws JsonProcessingException {
+        // When
+        PossessionClaimResponse response =
+            underTest.readValue(STORED_DRAFT, PCSCase.class).getPossessionClaimResponse();
+
+        // Then
+        assertThat(response.getDefendantResponses()).isNotNull();
+        assertThat(response.getDefendantContactDetails()).isNotNull();
+        assertThat(response.getDefendantFlags()).isNotNull();
+        assertThat(response.getDefendantFlags().getDetails())
+            .extracting(detail -> detail.getValue().getFlagCode())
+            .containsExactly("RA0022");
+    }
 
     @Test
     void shouldReadDefendantFlagsSuppliedByReasonableAdjustmentsService() throws JsonProcessingException {
