48b0bffad31e174bed5aad46280f10802b42dcb0
jordan.aitken <jordan.aitken@hmcts.net>
2026-08-03 21:12:20 +0100

    HDPI-7982 | remove wildcard imports

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/event/RespondPossessionClaimTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/event/RespondPossessionClaimTest.java
@@ -72,7 +72,9 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
