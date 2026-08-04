28af967da8209cfabbb62e4d7deedaaca18c52d8
taleb <20416858+benouaer@users.noreply.github.com>
2026-08-04 12:43:37 +0100

    Update Docker image version in create-xlsx.sh

--- bin/create-xlsx.sh
+++ bin/create-xlsx.sh
@@ -27,7 +27,7 @@ az acr login --name hmctsprod --subscription DCD-CNP-PROD
 docker run --rm --name "json2xlsx" \
   -v "$run_dir/build/definitions/${case_type}:/tmp/ccd-input" \
   -v "$run_dir/build/definitions:/tmp/ccd-output" \
-  hmctsprod.azurecr.io/ccd/definition-processor:pr-348-64dd53f \
+  hmctsprod.azurecr.io/ccd/definition-processor:pr-348-7561002 \
   json2xlsx -D /tmp/ccd-input -o /tmp/ccd-output/"${ccd_definition_file}"
 
 done
