e45924365a08433948d148e72895ad63f62b2042
taleb <tech@benouaer.com>
2026-08-04 16:10:18 +0100

    add GA roles to CCD

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/config/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/config/HighLevelDataSetupApp.java
@@ -37,6 +37,8 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
         new CcdRoleConfig("circuit-judge", "PUBLIC"),
         new CcdRoleConfig("leadership-judge", "PUBLIC"),
         new CcdRoleConfig("pcs-system-update", "PUBLIC"),
+        new CcdRoleConfig("PCS_Solicitor_Org", "PUBLIC"),
+        new CcdRoleConfig("PCS_Solicitor_Group", "PUBLIC"),
         new CcdRoleConfig("[SOLICITOR]", "PUBLIC"),
         new CcdRoleConfig("professional-user", "PUBLIC")
     };
