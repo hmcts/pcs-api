64907c0ba5fced8068d66c4066e54438cd431354
srinijg <srinivasan.joghee@hmcts.net>
2026-08-05 15:37:50 +0100

    address info method update

--- src/e2eTest/tests/test-caseManagement/caseManagement.caseWorkerGenApps.spec.ts
+++ src/e2eTest/tests/test-caseManagement/caseManagement.caseWorkerGenApps.spec.ts
@@ -27,6 +27,7 @@ test.beforeEach(async ({ page, context }) => {
   allPartyDetails.length = 0;
   await performAction('createCaseAPI', { data: createCaseApiData.createCasePayload });
   await performAction('submitCaseAPI', { data: submitCaseApiData.submitCasePayloadCaseFileView });
+  await performAction('getAddressInfo', { data: createCaseApiData.createCasePayload });
   console.log(`Case created with case number: ${process.env.CASE_NUMBER}`);
   await performAction('updatePaymentAPI');
   await performAction('getCaseAPI', 'Link Solicitor');
