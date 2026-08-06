1f82a601b9f6d11798a16106199f60801bd69429
taleb <tech@benouaer.com>
2026-08-06 17:18:03 +0100

    HDPI-7052: remove duplicate state authorisation

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/accesscontrol/SolicitorAccess.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/accesscontrol/SolicitorAccess.java
@@ -6,14 +6,14 @@ import uk.gov.hmcts.ccd.sdk.api.HasAccessControl;
 import uk.gov.hmcts.ccd.sdk.api.HasRole;
 import uk.gov.hmcts.ccd.sdk.api.Permission;
 
-import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.CLAIMANT_SOLICITOR_ORG;
+import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.PCS_SOLICITOR;
 
 public class SolicitorAccess implements HasAccessControl {
 
     @Override
     public SetMultimap<HasRole, Permission> getGrants() {
         SetMultimap<HasRole, Permission> grants = HashMultimap.create();
-        grants.putAll(CLAIMANT_SOLICITOR_ORG, Permission.CRUD);
+        grants.putAll(PCS_SOLICITOR, Permission.CRUD);
 
         return grants;
     }
