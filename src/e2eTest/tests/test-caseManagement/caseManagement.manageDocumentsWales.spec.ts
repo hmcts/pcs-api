e20a63f31ac5df473b8532d7f2aa3c57d184aedd
srinijg <srinivasan.joghee@hmcts.net>
2026-08-06 18:11:03 +0100

    test title update

--- src/e2eTest/tests/test-caseManagement/caseManagement.manageDocumentsWales.spec.ts
+++ src/e2eTest/tests/test-caseManagement/caseManagement.manageDocumentsWales.spec.ts
@@ -54,7 +54,7 @@ test.afterEach(async () => {
 });
 
 test.describe('Case management - Manage documents Wales Journey @nightly', async () => {
-  test('Case management - Manage documents - Amend @CM @regression', async () => {
+  test('Case management - Manage documents - Amend Wales Journey @CM @regression', async () => {
       let date = CaseManagementCommonUtils.getRandomDate(uploadADocument.dateTypeHiddenUserInput);
       let appType = CaseManagementCommonUtils.getGenApplicationType(defendantUserDetails.length)[0];
       let party = allPartyDetails[0];
@@ -89,7 +89,7 @@ test.describe('Case management - Manage documents Wales Journey @nightly', async
       });
     });
   
-    test('Case management - Manage documents - Amend Document not related to any App or Counterclaim @CM @regression', async () => {
+    test('Case management - Manage documents - Amend Document not related to any App or Counterclaim Wales Journey @CM', async () => {
       let date = CaseManagementCommonUtils.getRandomDate(uploadADocument.dateTypeHiddenUserInput);
       let appType = amendDocumentDetails.notRelatedToAppRadioOption;
       let party = allPartyDetails[1];
@@ -125,7 +125,7 @@ test.describe('Case management - Manage documents Wales Journey @nightly', async
       });
     });
   
-    test('Case management - Manage documents - Amend Document without any Issue date @CM', async () => {
+    test('Case management - Manage documents - Amend Document without any Issue date Wales Journey @CM', async () => {
       let date = '';
       let appType = amendDocumentDetails.notRelatedToAppRadioOption;
       let party = allPartyDetails[0];
