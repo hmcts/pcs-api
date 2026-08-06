07907e004044faef5d2c498f77c0e17ee3ed70ec
jordan.aitken <jordan.aitken@hmcts.net>
2026-08-06 14:58:29 +0100

    HDPI-5823 | fix checkstyle

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/event/RespondPossessionClaimTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/event/RespondPossessionClaimTest.java
@@ -74,7 +74,8 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
