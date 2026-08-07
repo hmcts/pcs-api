b3fd0c0a1efb3743c487a18241a6a8598051e8e9
arun <arun.kumar@hmcts.net>
2026-08-07 17:14:52 +0100

    HDPI-7052: drop unused OrganisationField from group access fields

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/GroupAccessFields.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/GroupAccessFields.java
@@ -7,9 +7,8 @@ import lombok.Data;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.api.HasRole;
 import uk.gov.hmcts.ccd.sdk.type.CaseAccessGroup;
-import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.ccd.sdk.type.ChangeOrganisationRequest;
-import uk.gov.hmcts.ccd.sdk.type.Organisation;
+import uk.gov.hmcts.ccd.sdk.type.ListValue;
 
 @Data
 @Builder
@@ -18,10 +17,6 @@ public class GroupAccessFields<R extends HasRole> {
     @JsonProperty("CaseAccessGroups")
     @CCD
     private List<ListValue<CaseAccessGroup>> caseAccessGroups;
-
-    @JsonProperty("OrganisationField")
-    @CCD
-    private Organisation organisationField;
 
     @JsonProperty("ChangeOrganisationRequestField")
     @CCD
