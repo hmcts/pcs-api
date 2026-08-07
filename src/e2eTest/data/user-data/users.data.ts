2c1ef749eab49c9c68a62edfceae798e2875776e
srinijg <srinivasan.joghee@hmcts.net>
2026-08-07 16:00:10 +0100

    user roles updated

--- src/e2eTest/data/user-data/users.data.ts
+++ src/e2eTest/data/user-data/users.data.ts
@@ -20,31 +20,31 @@ export const users = [{
   user: 'County Court Judge',
   email: 'TribunalMember.Brandon.Kshlerin-Hartmann@ejudiciary.net',
   password: process.env.IDAM_PCS_USER_PASSWORD,
-  tabAccess: ['Case Parties', 'Case Details', 'Case File View', 'Summary', 'History', 'Service Request', 'Notes', 'Linked Cases', 'Case flags']
+  tabAccess: ['Tasks', 'Roles and access', 'Case Parties', 'Case Details', 'Case File View', 'Summary', 'History', 'Service Request', 'Notes', 'Linked Cases', 'Case flags']
 },
 {
   user: 'CTSC Team Leader',
   email: 'pcs-ctsc-team-leader-01@justice.gov.uk',
   password: process.env.IDAM_PCS_USER_PASSWORD,
-  tabAccess: ['Case Parties', 'Case Details', 'Case File View', 'Summary', 'History', 'Service Request', 'Notes', 'Linked Cases', 'Case flags']
+  tabAccess: ['Tasks', 'Roles and access', 'Case Parties', 'Case Details', 'Case File View', 'Summary', 'History', 'Service Request', 'Notes', 'Linked Cases', 'Case flags']
 },
 {
   user: 'CTSC Administrator',
   email: 'pcs-ctsc-admin-01@justice.gov.uk',
   password: process.env.IDAM_PCS_USER_PASSWORD,
-  tabAccess: ['Case Parties', 'Case Details', 'Case File View', 'Summary', 'History', 'Service Request', 'Notes', 'Linked Cases', 'Case flags']
+  tabAccess: ['Tasks', 'Roles and access', 'Case Parties', 'Case Details', 'Case File View', 'Summary', 'History', 'Service Request', 'Notes', 'Linked Cases', 'Case flags']
 },
 {
   user: 'WLU Team Leader',
   email: 'pcs-wlu-team-leader-01@justice.gov.uk',
   password: process.env.IDAM_PCS_USER_PASSWORD,
-  tabAccess: ['Case Parties', 'Case Details', 'Case File View', 'Summary', 'History', 'Service Request', 'Notes', 'Linked Cases', 'Case flags']
+  tabAccess: ['Tasks', 'Roles and access', 'Case Parties', 'Case Details', 'Case File View', 'Summary', 'History', 'Service Request', 'Notes', 'Linked Cases', 'Case flags']
 },
 {
   user: 'WLU Administrator',
   email: 'pcs-wlu-administrator-01@justice.gov.uk',
   password: process.env.IDAM_PCS_USER_PASSWORD,
-  tabAccess: ['Case Parties', 'Case Details', 'Case File View', 'Summary', 'History', 'Service Request', 'Notes', 'Linked Cases', 'Case flags']
+  tabAccess: ['Tasks', 'Roles and access', 'Case Parties', 'Case Details', 'Case File View', 'Summary', 'History', 'Service Request', 'Notes', 'Linked Cases', 'Case flags']
 },
 
 ];
