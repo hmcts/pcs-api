cedd73c8af850e42c2266a69a233f18af63409f5
arun <arun.kumar@hmcts.net>
2026-08-11 09:39:14 +0100

    HDPI-7052: declare defendant_solicitor access profile referenced by RoleToAccessProfiles

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/accesscontrol/AccessProfile.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/accesscontrol/AccessProfile.java
@@ -21,6 +21,7 @@ public enum AccessProfile implements HasRole {
     CLAIMANT_SOLICITOR("[CLAIMANTSOLICITOR]", CRU),
     DEFENDANT_SOLICITOR("[DEFENDANTSOLICITOR]", CRU),
     GA_CLAIMANT_SOLICITOR("claimant_solicitor", CRU),
+    GA_DEFENDANT_SOLICITOR("defendant_solicitor", CRU),
     PCS_CASE_WORKER("caseworker-pcs", Set.of(R)),
     PCS_SOLICITOR("caseworker-pcs-solicitor", CRU),
 
