6b1155438b78acdc185614ba346fcd91670e210c
arun <arun.kumar@hmcts.net>
2026-08-11 10:35:13 +0100

    HDPI-7052: create duty-advisor-request role in definition store

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/config/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/config/HighLevelDataSetupApp.java
@@ -40,6 +40,7 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
         new CcdRoleConfig("pcs-system-update", "PUBLIC"),
         new CcdRoleConfig("claimant_solicitor", "PUBLIC"),
         new CcdRoleConfig("defendant_solicitor", "PUBLIC"),
+        new CcdRoleConfig("duty-advisor-request", "PUBLIC"),
         new CcdRoleConfig("[SOLICITOR]", "PUBLIC")
     };
 
