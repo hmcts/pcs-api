0c4e531bfb885fba774c2eec5fe7b2a16262d6e8
gmmagruder <gabrielle.magruder@gmail.com>
2026-08-11 14:04:57 +0100

    HDPI-6505: fix application hearing type label

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/hearing/HearingType.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/domain/hearing/HearingType.java
@@ -8,7 +8,7 @@ import uk.gov.hmcts.ccd.sdk.api.HasLabel;
 @Getter
 public enum HearingType implements HasLabel {
     POSSESSION("Possession first hearing"),
-    APPLICATION("Application"),
+    APPLICATION("Application hearing"),
     ADJOURNED("Adjourned first hearing"),
     OTHER("Other");
 
