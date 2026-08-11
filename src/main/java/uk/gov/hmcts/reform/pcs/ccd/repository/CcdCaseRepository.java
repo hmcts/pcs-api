59d6702b8159c7eab7074abb4b4bad2da64f91bd
guygrewal77 <guygrewal7@gmail.com>
2026-08-11 16:50:05 +0100

    HDPI-3404 - ccd case data delete sql fix

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/repository/CcdCaseRepository.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/repository/CcdCaseRepository.java
@@ -44,23 +44,24 @@ public class CcdCaseRepository {
 
     public void deleteCcdCaseData(long caseReference) {
         String sql = """
-            WITH deleted_case_data AS (
-                DELETE FROM ccd.case_data
-                WHERE reference = ?
-                RETURNING id
+            WITH target_case AS (
+                SELECT id FROM ccd.case_data WHERE reference = ?
             ),
             deleted_case_event AS (
                 DELETE FROM ccd.case_event
-                WHERE case_data_id IN (SELECT id FROM deleted_case_data)
+                WHERE case_data_id IN (SELECT id FROM target_case)
                 RETURNING id
             ),
             deleted_case_event_audit AS (
                 DELETE FROM ccd.case_event_audit
                 WHERE case_event_id IN (SELECT id FROM deleted_case_event)
-                RETURNING case_event_id
+            ),
+            deleted_significant_items AS (
+                DELETE FROM ccd.case_event_significant_items
+                WHERE case_event_id IN (SELECT id FROM deleted_case_event)
             )
-            DELETE FROM ccd.case_event_significant_items
-            WHERE case_event_id IN (SELECT case_event_id FROM deleted_case_event_audit);
+            DELETE FROM ccd.case_data
+            WHERE id IN (SELECT id FROM target_case);
             """;
         jdbcTemplate.update(sql, caseReference);
     }
