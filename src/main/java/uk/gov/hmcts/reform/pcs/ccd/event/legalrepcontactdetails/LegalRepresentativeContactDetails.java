a8ae8f1de7df80db53c1ef5f6c300e94cf85125b
danielmillard <dmillard91@hotmail.co.uk>
2026-08-12 17:06:56 +0100

    HDPI-5794: update apostrophe

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/legalrepcontactdetails/LegalRepresentativeContactDetails.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/legalrepcontactdetails/LegalRepresentativeContactDetails.java
@@ -73,8 +73,8 @@ public class LegalRepresentativeContactDetails implements CCDConfig<PCSCase, Sta
         return """
             ---
             <div class="govuk-panel govuk-panel--confirmation govuk-!-padding-top-3 govuk-!-padding-bottom-3">
-            <span class="govuk-panel__title govuk-!-font-size-36">You have updated a defendant's
-                    legal representative's information</span>
+            <span class="govuk-panel__title govuk-!-font-size-36">You have updated a defendant’s
+                    legal representative’s information</span>
             </div>
             """;
     }
