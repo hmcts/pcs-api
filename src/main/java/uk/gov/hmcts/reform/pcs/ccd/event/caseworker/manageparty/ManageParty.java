f746a36d13353513d8bcb911344b346c8731cb8e
gmmagruder <gabrielle.magruder@gmail.com>
2026-08-12 09:31:02 +0100

    HDPI-6429: add feature flags to manage parties event

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/caseworker/manageparty/ManageParty.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/caseworker/manageparty/ManageParty.java
@@ -6,6 +6,7 @@ import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.DecentralisedConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.Permission;
+import uk.gov.hmcts.reform.pcs.ccd.ShowConditions;
 import uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole;
 import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
@@ -18,6 +19,8 @@ import uk.gov.hmcts.reform.pcs.ccd.page.caseworker.manageparty.ManagePartyOption
 import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.CaseworkerRoles.CASEWORKER_ROLES;
 import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.JudicialHistoryRoles.JUDICIAL_HISTORY_ROLES;
 import static uk.gov.hmcts.reform.pcs.ccd.event.EventId.manageParties;
+import static uk.gov.hmcts.reform.pcs.service.FeatureFlag.CASEWORKER_EVENTS;
+import static uk.gov.hmcts.reform.pcs.service.FeatureFlag.RELEASE_1_DOT_3;
 
 @Component
 public class ManageParty implements CCDConfig<PCSCase, State, UserRole> {
@@ -42,6 +45,7 @@ public class ManageParty implements CCDConfig<PCSCase, State, UserRole> {
             .name("Manage parties")
             .grant(Permission.CRUD, CASEWORKER_ROLES)
             .grantHistoryOnly(JUDICIAL_HISTORY_ROLES)
+            .showCondition(ShowConditions.featureFlagsEnabled(RELEASE_1_DOT_3, CASEWORKER_EVENTS))
             .showSummary()
             .endButtonLabel("Submit");
 
