6e71062dcae78461c8060a765732172399a4c4e8
gmmagruder <gabrielle.magruder@gmail.com>
2026-08-14 12:30:10 +0100

    HDPI-7772: fix template

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/service/workallocation/TaskDescriptionService.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/service/workallocation/TaskDescriptionService.java
@@ -88,6 +88,7 @@ public class TaskDescriptionService {
             .map(ListValue::getValue)
             .filter(TaskDescriptionService::isCaseFlagActive)
             .map(FlagDetail::getName)
+            .map((name) -> name.replace("&#39;", "’"))
             .toList();
 
         Map<String, Object> context = Map.of(
