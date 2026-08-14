fb050a14d35f219019b2836f2a76c49b0174454d
sadmanrahman <sadman.rahman@HMCTS.NET>
2026-08-14 13:11:17 +0100

    HDPI-7659: missing doc fix

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/view/ClaimView.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/view/ClaimView.java
@@ -111,6 +111,7 @@ public class ClaimView {
 
         return pcsCaseEntity.getDocuments().stream()
             .filter(ClaimView::isEnergyPerformanceCertificate)
+            .filter(ClaimView::isClaimDocument)
             .filter(DocumentsView::isDescriptionEmpty)
             .filter(DocumentsView::isNotRemoved)
             .map(ClaimView::toDocument)
@@ -124,6 +125,7 @@ public class ClaimView {
 
         return pcsCaseEntity.getDocuments().stream()
             .filter(ClaimView::isGasSafetyReport)
+            .filter(ClaimView::isClaimDocument)
             .filter(DocumentsView::isDescriptionEmpty)
             .filter(DocumentsView::isNotRemoved)
             .map(ClaimView::toDocument)
@@ -137,6 +139,7 @@ public class ClaimView {
 
         return pcsCaseEntity.getDocuments().stream()
             .filter(ClaimView::isElectricalInstallationCondition)
+            .filter(ClaimView::isClaimDocument)
             .filter(DocumentsView::isDescriptionEmpty)
             .filter(DocumentsView::isNotRemoved)
             .map(ClaimView::toDocument)
@@ -153,6 +156,10 @@ public class ClaimView {
 
     private static boolean isElectricalInstallationCondition(DocumentEntity documentEntity) {
         return documentEntity.getType() == DocumentType.EICR_REPORT;
+    }
+
+    private static boolean isClaimDocument(DocumentEntity documentEntity) {
+        return documentEntity.getGeneralApplication() == null;
     }
 
     private static ListValue<Document> toDocument(DocumentEntity documentEntity) {
