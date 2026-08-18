9208c0ab3a104541b341e732adb6f9545676bee9
srinijg <srinivasan.joghee@hmcts.net>
2026-08-18 18:12:59 +0100

    login method update along with add hearing tests

--- src/e2eTest/utils/registry/registry-caseManagement/action-caseManagement.registry.ts
+++ src/e2eTest/utils/registry/registry-caseManagement/action-caseManagement.registry.ts
@@ -74,6 +74,7 @@ export class ActionCMRegistry {
     ['errorValidationCancelHearing', new ErrorValidationAction()],
     ['errorValidationManageHearing', new ErrorValidationAction()],
     ['errorValidationUploadGenAppsFile', new ErrorValidationAction()],
+    ['errorValidationEnterAddAHearingPage', new ErrorValidationAction()],
     ['inputErrorValidation', new CaseManagementAction()],
     ['reTryOnCallBackError', new RetryOnCallBackError()],
     ['getAddressInfo', new CaseManagementAction()],
