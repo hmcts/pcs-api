a73a4380efeaac4a994e1e139479a083155c6548
tvr-solirius <trevor.hinson@solirius.com>
2026-08-21 22:52:23 +0100

    Missed unused import.

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/task/CaseDeletionScheduledTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/task/CaseDeletionScheduledTaskTest.java
@@ -29,7 +29,6 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.inOrder;
