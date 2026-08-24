5f474b40cf2c480439a30aa67cb880a320d62a6e
ankita <ankita.kumbhare@hmcts.net>
2026-08-24 16:57:04 +0100

    Running GA scenario

--- src/e2eTest/data/user-data/users.data.ts
+++ src/e2eTest/data/user-data/users.data.ts
@@ -1,18 +1,18 @@
 export const users = [{
   user: 'Creator',
-  email: 'pcs-solicitor2@test.com',
+  email: 'pcs-solicitor2@test.com',  //
   password: process.env.IDAM_PCS_USER_PASSWORD,
   tabAccess: ['Case Parties', 'Case Details', 'Case File View', 'Summary', 'Service Request']
 },
 {
-  user: 'Claimant Solicitor',
-  email: 'pcs-solicitor-user01@test.com',
+  user: 'Claimant Solicitor',  // local auth. pcs.local.auth1user1@hmcts.net
+  email: 'pcs-solicitor-user01@test.com',  
   password: process.env.IDAM_PCS_USER_PASSWORD,
   tabAccess: ['Case Parties', 'Case Details', 'Case File View', 'Summary', 'Service Request']
 },
 {
   user: 'Defendant Solicitor',
-  email: 'pcs-org1-solicitor2@test.com',
+  email: 'pcs-org1-solicitor2@test.com', // pcs.solicitor.orguser3@hmcts.net
   password: process.env.IDAM_PCS_USER_PASSWORD,
   tabAccess: ['Case Parties', 'Case Details', 'Case File View', 'Summary', 'Service Request']
 },
