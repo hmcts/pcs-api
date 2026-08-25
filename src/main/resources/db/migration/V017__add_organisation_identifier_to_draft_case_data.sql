59e1867e3868ca5f41969cf022629fe13e6ce10f
Marianne-Azzopardi <marianne.azzopardi@solirius.com>
2026-08-25 15:30:17 +0100

    remove stale flyway migrations

--- src/main/resources/db/migration/V017__add_organisation_identifier_to_draft_case_data.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE draft.draft_case_data
-  ADD COLUMN organisation_id varchar(64);
-
-CREATE UNIQUE INDEX draft_case_data_unique_by_organisation_idx
-  ON draft.draft_case_data(case_reference, event_id, organisation_id, party_id);
