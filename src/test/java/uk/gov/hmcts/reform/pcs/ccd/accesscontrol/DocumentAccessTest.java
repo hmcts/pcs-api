d348f4588c68e5bbde27daa6f1b13bbe7a45f6e9
arun <arun.kumar@hmcts.net>
2026-08-26 15:26:06 +0100

    HDPI-7052: grant document access to claimant/defendant solicitor org roles under group access

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/accesscontrol/DocumentAccessTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/accesscontrol/DocumentAccessTest.java
@@ -12,6 +12,8 @@ import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.CITIZEN;
 import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.CLAIMANT_SOLICITOR;
 import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.DEFENDANT;
 import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.DEFENDANT_SOLICITOR;
+import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.GA_CLAIMANT_SOLICITOR;
+import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.GA_DEFENDANT_SOLICITOR;
 import static uk.gov.hmcts.reform.pcs.ccd.accesscontrol.UserRole.PCS_SOLICITOR;
 
 class DocumentAccessTest {
@@ -21,7 +23,9 @@ class DocumentAccessTest {
         CITIZEN,
         DEFENDANT,
         CLAIMANT_SOLICITOR,
-        DEFENDANT_SOLICITOR
+        DEFENDANT_SOLICITOR,
+        GA_CLAIMANT_SOLICITOR,
+        GA_DEFENDANT_SOLICITOR
     };
 
     private final DocumentAccess underTest = new DocumentAccess();
