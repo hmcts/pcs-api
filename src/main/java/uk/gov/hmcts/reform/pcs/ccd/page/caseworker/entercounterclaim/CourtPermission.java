9a0af10508941c1087a6afd8b8a4b1d32a282c81
Liban Abdirahman <liban.abdirahman@hmcts.net>
2026-08-27 09:14:30 +0100

    HDPI-6451:Adding validation on permissionOrderDate and claimReceivedDate

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/caseworker/entercounterclaim/CourtPermission.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/page/caseworker/entercounterclaim/CourtPermission.java
@@ -1,12 +1,22 @@
 package uk.gov.hmcts.reform.pcs.ccd.page.caseworker.entercounterclaim;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.reform.pcs.ccd.ShowConditions;
 import uk.gov.hmcts.reform.pcs.ccd.common.CcdPageConfiguration;
 import uk.gov.hmcts.reform.pcs.ccd.common.PageBuilder;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
+import uk.gov.hmcts.reform.pcs.ccd.domain.State;
 import uk.gov.hmcts.reform.pcs.ccd.domain.VerticalYesNo;
 import uk.gov.hmcts.reform.pcs.ccd.domain.caseworker.EnterCounterClaimDetails;
+import uk.gov.hmcts.reform.pcs.ccd.util.StringUtils;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static uk.gov.hmcts.reform.pcs.ccd.ShowConditions.fieldEquals;
 
@@ -20,10 +30,21 @@ public class CourtPermission implements CcdPageConfiguration {
         fieldEquals(COURT_PERMISSION_GRANTED_FIELD, VerticalYesNo.NO)
     );
 
+    private static final String PERMISSION_ORDER_DATE_ERROR =
+        "Date the order was made must be in the past";
+    private static final String CLAIM_RECEIVED_DATE_ERROR =
+        "Date the counterclaim was received must be in the past";
+
+    private final Clock ukClock;
+
+    public CourtPermission(@Qualifier("ukClock") Clock ukClock) {
+        this.ukClock = ukClock;
+    }
+
     @Override
     public void addTo(PageBuilder pageBuilder) {
         pageBuilder
-            .page("courtPermission")
+            .page("courtPermission", this::midEvent)
             .pageLabel("Court permission")
             .label("courtPermission-lineSeparator", "---")
             .label(
@@ -36,7 +57,8 @@ public class CourtPermission implements CcdPageConfiguration {
                     You should check whether permission is required. If it is, you must advise the party \
                 to get permission from the court to make their counterclaim.
                 </strong>
-                </div>""")
+                </div>""",
+                COURT_PERMISSION_ANSWERED)
             .complex(PCSCase::getEnterCounterClaim)
                 .mandatory(EnterCounterClaimDetails::getCourtPermissionGranted)
                 .mandatory(
@@ -47,5 +69,31 @@ public class CourtPermission implements CcdPageConfiguration {
             .complex(PCSCase::getEnterCounterClaim)
                 .mandatory(EnterCounterClaimDetails::getClaimReceivedDate, COURT_PERMISSION_ANSWERED)
             .done();
+    }
+
+    private AboutToStartOrSubmitResponse<PCSCase, State> midEvent(CaseDetails<PCSCase, State> details,
+                                                                  CaseDetails<PCSCase, State> detailsBefore) {
+        PCSCase caseData = details.getData();
+        EnterCounterClaimDetails counterClaimDetails = caseData.getEnterCounterClaim();
+
+        List<String> validationErrors = new ArrayList<>();
+        LocalDate currentDate = LocalDate.now(ukClock);
+
+        if (counterClaimDetails != null) {
+            LocalDate permissionOrderDate = counterClaimDetails.getPermissionOrderDate();
+            if (permissionOrderDate != null && !permissionOrderDate.isBefore(currentDate)) {
+                validationErrors.add(PERMISSION_ORDER_DATE_ERROR);
+            }
+
+            LocalDate claimReceivedDate = counterClaimDetails.getClaimReceivedDate();
+            if (claimReceivedDate != null && !claimReceivedDate.isBefore(currentDate)) {
+                validationErrors.add(CLAIM_RECEIVED_DATE_ERROR);
+            }
+        }
+
+        return AboutToStartOrSubmitResponse.<PCSCase, State>builder()
+            .data(caseData)
+            .errorMessageOverride(StringUtils.joinIfNotEmpty("\n", validationErrors))
+            .build();
     }
 }
