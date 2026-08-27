e77300d4e05175ea8ba7b9cafc5cb6b8d50f19dd
Liban Abdirahman <liban.abdirahman@hmcts.net>
2026-08-27 09:35:19 +0100

    HDPI-6451:Updating existing and adding new tests

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/event/caseworker/entergenapp/EnterGenAppTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/event/caseworker/entergenapp/EnterGenAppTest.java
@@ -10,11 +10,9 @@ import uk.gov.hmcts.ccd.sdk.type.DynamicList;
 import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
 import uk.gov.hmcts.reform.pcs.ccd.domain.State;
-import uk.gov.hmcts.reform.pcs.ccd.domain.VerticalYesNo;
 import uk.gov.hmcts.reform.pcs.ccd.domain.caseworker.EnterGenAppRequest;
 import uk.gov.hmcts.reform.pcs.ccd.entity.ClaimEntity;
 import uk.gov.hmcts.reform.pcs.ccd.entity.PcsCaseEntity;
-import uk.gov.hmcts.reform.pcs.ccd.entity.party.ClaimPartyEntity;
 import uk.gov.hmcts.reform.pcs.ccd.entity.party.PartyEntity;
 import uk.gov.hmcts.reform.pcs.ccd.entity.party.PartyRole;
 import uk.gov.hmcts.reform.pcs.ccd.event.BaseEventTest;
@@ -59,52 +57,18 @@ class EnterGenAppTest extends BaseEventTest {
     }
 
     @Test
-    void shouldBuildApplicantPartyListFromClaimantAndDefendants() {
+    void shouldPopulatePartyRadioListFromClaimantsAndDefendants() {
         // Given
-        UUID claimantId = UUID.randomUUID();
-        UUID defendant1Id = UUID.randomUUID();
-        UUID defendant2Id = UUID.randomUUID();
-        UUID underlesseeId = UUID.randomUUID();
-
-        PartyEntity claimant = PartyEntity.builder()
-            .id(claimantId)
-            .firstName("John")
-            .lastName("Smith")
-            .nameKnown(VerticalYesNo.YES)
-            .build();
-
-        PartyEntity defendant1 = PartyEntity.builder()
-            .id(defendant1Id)
-            .firstName("Jane")
-            .lastName("Doe")
-            .nameKnown(VerticalYesNo.YES)
-            .build();
-
-        PartyEntity defendant2 = PartyEntity.builder()
-            .id(defendant2Id)
-            .nameKnown(VerticalYesNo.NO)
-            .build();
-
-        PartyEntity underlesseeOrMortgagee = PartyEntity.builder()
-            .id(underlesseeId)
-            .orgName("Bank Ltd")
-            .build();
-
         when(pcsCaseService.loadCase(TEST_CASE_REFERENCE)).thenReturn(pcsCaseEntity);
         when(pcsCaseEntity.getClaims()).thenReturn(List.of(claimEntity));
-        when(claimEntity.getClaimParties()).thenReturn(List.of(
-            ClaimPartyEntity.builder().party(claimant).role(PartyRole.CLAIMANT).rank(1).build(),
-            ClaimPartyEntity.builder().party(defendant1).role(PartyRole.DEFENDANT).rank(1).build(),
-            ClaimPartyEntity.builder().party(defendant2).role(PartyRole.DEFENDANT).rank(2).build(),
-            ClaimPartyEntity.builder().party(underlesseeOrMortgagee).role(PartyRole.UNDERLESSEE_OR_MORTGAGEE)
-                .rank(1).build()
-        ));
 
-        when(partyService.getPartyName(claimant)).thenReturn("John Smith");
-        when(partyService.getPartyName(defendant1)).thenReturn("Jane Doe");
-        when(partyService.getPartyLabel(claimEntity, claimantId)).thenReturn("Claimant 1");
-        when(partyService.getPartyLabel(claimEntity, defendant1Id)).thenReturn("Defendant 1");
-        when(partyService.getPartyLabel(claimEntity, defendant2Id)).thenReturn("Defendant 2");
+        DynamicList partyList = DynamicList.builder()
+            .listItems(List.of(
+                DynamicListElement.builder().code(UUID.randomUUID()).label("John Smith - Claimant 1").build(),
+                DynamicListElement.builder().code(UUID.randomUUID()).label("Jane Doe - Defendant 1").build()))
+            .build();
+        when(partyService.buildPartyDynamicList(claimEntity, PartyRole.CLAIMANT, PartyRole.DEFENDANT))
+            .thenReturn(partyList);
 
         PCSCase caseData = PCSCase.builder()
             .enterGenAppRequest(EnterGenAppRequest.builder().build())
@@ -114,13 +78,7 @@ class EnterGenAppTest extends BaseEventTest {
         PCSCase result = callStartHandler(caseData);
 
         // Then
-        List<DynamicListElement> listItems = result.getPartyRadioList().getListItems();
-
-        assertThat(listItems).containsExactly(
-            DynamicListElement.builder().code(claimantId).label("John Smith - Claimant 1").build(),
-            DynamicListElement.builder().code(defendant1Id).label("Jane Doe - Defendant 1").build(),
-            DynamicListElement.builder().code(defendant2Id).label("Person unknown - Defendant 2").build()
-        );
+        assertThat(result.getPartyRadioList()).isEqualTo(partyList);
     }
 
     @Test
