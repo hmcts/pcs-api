997e652655d262c616ba91c14122c170e31fcc3c
Liban Abdirahman <liban.abdirahman@hmcts.net>
2026-09-01 16:05:24 +0100

    HDPI-6625:Adding tests for new midEvent and submit helper

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/event/caseworker/entercounterclaim/EnterCounterClaimTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/event/caseworker/entercounterclaim/EnterCounterClaimTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.type.DynamicList;
 import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
+import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.reform.pcs.ccd.domain.PCSCase;
 import uk.gov.hmcts.reform.pcs.ccd.domain.VerticalYesNo;
 import uk.gov.hmcts.reform.pcs.ccd.domain.caseworker.EnterCounterClaimDetails;
@@ -21,11 +22,16 @@ import uk.gov.hmcts.reform.pcs.ccd.event.BaseEventTest;
 import uk.gov.hmcts.reform.pcs.ccd.page.caseworker.entercounterclaim.CounterClaimAmount;
 import uk.gov.hmcts.reform.pcs.ccd.page.caseworker.entercounterclaim.CourtPermission;
 import uk.gov.hmcts.reform.pcs.ccd.page.caseworker.entercounterclaim.HelpWithFees;
+import uk.gov.hmcts.reform.pcs.ccd.page.caseworker.entercounterclaim.PartyCounterClaimAgainst;
 import uk.gov.hmcts.reform.pcs.ccd.page.caseworker.entercounterclaim.TypeOfCounterClaim;
+import uk.gov.hmcts.reform.pcs.ccd.page.caseworker.entercounterclaim.UploadCounterClaimForm;
 import uk.gov.hmcts.reform.pcs.ccd.service.PcsCaseService;
 import uk.gov.hmcts.reform.pcs.ccd.service.party.PartyService;
 import uk.gov.hmcts.reform.pcs.ccd.service.respondpossessionclaim.CounterClaimService;
+import uk.gov.hmcts.reform.pcs.ccd.type.DynamicMultiSelectStringList;
+import uk.gov.hmcts.reform.pcs.ccd.type.DynamicStringListElement;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
@@ -57,12 +63,17 @@ class EnterCounterClaimTest extends BaseEventTest {
     private CounterClaimAmount counterClaimAmount;
     @Mock
     private HelpWithFees helpWithFees;
+    @Mock
+    private PartyCounterClaimAgainst partyCounterClaimAgainst;
+    @Mock
+    private UploadCounterClaimForm uploadCounterClaimForm;
 
     @BeforeEach
     void setUp() {
         EnterCounterClaim enterCounterClaim = new EnterCounterClaim(
             pcsCaseService, partyService, counterClaimService,
-            courtPermission, typeOfCounterClaim, counterClaimAmount, helpWithFees);
+            courtPermission, typeOfCounterClaim, counterClaimAmount, helpWithFees,
+            partyCounterClaimAgainst, uploadCounterClaimForm);
         setEventUnderTest(enterCounterClaim);
     }
 
@@ -99,12 +110,70 @@ class EnterCounterClaimTest extends BaseEventTest {
 
         LocalDate permissionOrderDate = LocalDate.of(2026, 1, 15);
         LocalDate claimReceivedDate = LocalDate.of(2026, 1, 20);
+        BigDecimal counterClaimAmount = new BigDecimal("250.00");
 
         EnterCounterClaimDetails enterCounterClaimDetails = EnterCounterClaimDetails.builder()
             .claimTypeOption(CounterClaimType.PAYMENT_OR_COMPENSATION)
             .courtPermissionGranted(VerticalYesNo.YES)
             .permissionOrderDate(permissionOrderDate)
             .claimReceivedDate(claimReceivedDate)
+            .counterClaimAmount(counterClaimAmount)
+            .appliedForHwf(VerticalYesNo.YES)
+            .hwfReferenceNumber("HWF-123")
+            .build();
+
+        DynamicList submittingPartyList = DynamicList.builder()
+            .value(DynamicListElement.builder().code(submittingPartyId).build())
+            .build();
+
+        UUID againstPartyId = UUID.randomUUID();
+        DynamicMultiSelectStringList partyMultiSelectionList = DynamicMultiSelectStringList.builder()
+            .value(List.of(DynamicStringListElement.builder().code(againstPartyId.toString()).build()))
+            .build();
+
+        PCSCase caseData = PCSCase.builder()
+            .enterCounterClaim(enterCounterClaimDetails)
+            .partyRadioList(submittingPartyList)
+            .partyMultiSelectionList(partyMultiSelectionList)
+            .build();
+
+        // When
+        callSubmitHandler(caseData);
+
+        // Then
+        ArgumentCaptor<CounterClaim> counterClaimCaptor = ArgumentCaptor.forClass(CounterClaim.class);
+        verify(counterClaimService).saveCounterClaim(
+            eq(TEST_CASE_REFERENCE),
+            counterClaimCaptor.capture(),
+            eq(submittingParty));
+
+        CounterClaim savedCounterClaim = counterClaimCaptor.getValue();
+        assertThat(savedCounterClaim.getClaimType()).isEqualTo(CounterClaimType.PAYMENT_OR_COMPENSATION);
+        assertThat(savedCounterClaim.getCourtPermissionGranted()).isEqualTo(VerticalYesNo.YES);
+        assertThat(savedCounterClaim.getPermissionOrderDate()).isEqualTo(permissionOrderDate);
+        assertThat(savedCounterClaim.getClaimReceivedDate()).isEqualTo(claimReceivedDate);
+        assertThat(savedCounterClaim.getIsClaimAmountKnown()).isEqualTo(VerticalYesNo.YES);
+        assertThat(savedCounterClaim.getClaimAmount()).isEqualTo(counterClaimAmount);
+        assertThat(savedCounterClaim.getAppliedForHwf()).isEqualTo(VerticalYesNo.YES);
+        assertThat(savedCounterClaim.getHwfReferenceNumber()).isEqualTo("HWF-123");
+        assertThat(savedCounterClaim.getCounterClaimAgainst())
+            .extracting(ListValue::getId)
+            .containsExactly(againstPartyId.toString());
+    }
+
+    @Test
+    void shouldNullHwfReferenceNumberWhenNotAppliedForHwf() {
+        // Given
+        UUID submittingPartyId = UUID.randomUUID();
+        PartyEntity submittingParty = mock(PartyEntity.class);
+
+        when(partyService.getPartyEntityByEntityId(submittingPartyId, TEST_CASE_REFERENCE))
+            .thenReturn(submittingParty);
+
+        EnterCounterClaimDetails enterCounterClaimDetails = EnterCounterClaimDetails.builder()
+            .claimTypeOption(CounterClaimType.SOMETHING_ELSE)
+            .appliedForHwf(VerticalYesNo.NO)
+            .hwfReferenceNumber("stale-reference")
             .build();
 
         DynamicList submittingPartyList = DynamicList.builder()
@@ -126,10 +195,54 @@ class EnterCounterClaimTest extends BaseEventTest {
             counterClaimCaptor.capture(),
             eq(submittingParty));
 
-        CounterClaim savedCounterClaim = counterClaimCaptor.getValue();
-        assertThat(savedCounterClaim.getClaimType()).isEqualTo(CounterClaimType.PAYMENT_OR_COMPENSATION);
-        assertThat(savedCounterClaim.getCourtPermissionGranted()).isEqualTo(VerticalYesNo.YES);
-        assertThat(savedCounterClaim.getPermissionOrderDate()).isEqualTo(permissionOrderDate);
-        assertThat(savedCounterClaim.getClaimReceivedDate()).isEqualTo(claimReceivedDate);
+        assertThat(counterClaimCaptor.getValue().getAppliedForHwf()).isEqualTo(VerticalYesNo.NO);
+        assertThat(counterClaimCaptor.getValue().getHwfReferenceNumber()).isNull();
+    }
+
+    @Test
+    void shouldSaveAllSelectedCounterClaimAgainstParties() {
+        // Given
+        UUID submittingPartyId = UUID.randomUUID();
+        PartyEntity submittingParty = mock(PartyEntity.class);
+
+        when(partyService.getPartyEntityByEntityId(submittingPartyId, TEST_CASE_REFERENCE))
+            .thenReturn(submittingParty);
+
+        EnterCounterClaimDetails enterCounterClaimDetails = EnterCounterClaimDetails.builder()
+            .claimTypeOption(CounterClaimType.SOMETHING_ELSE)
+            .build();
+
+        DynamicList submittingPartyList = DynamicList.builder()
+            .value(DynamicListElement.builder().code(submittingPartyId).build())
+            .build();
+
+        UUID claimantId = UUID.randomUUID();
+        UUID defendantId = UUID.randomUUID();
+        DynamicMultiSelectStringList partyMultiSelectionList = DynamicMultiSelectStringList.builder()
+            .value(List.of(
+                DynamicStringListElement.builder().code(claimantId.toString()).build(),
+                DynamicStringListElement.builder().code(defendantId.toString()).build()
+            ))
+            .build();
+
+        PCSCase caseData = PCSCase.builder()
+            .enterCounterClaim(enterCounterClaimDetails)
+            .partyRadioList(submittingPartyList)
+            .partyMultiSelectionList(partyMultiSelectionList)
+            .build();
+
+        // When
+        callSubmitHandler(caseData);
+
+        // Then
+        ArgumentCaptor<CounterClaim> counterClaimCaptor = ArgumentCaptor.forClass(CounterClaim.class);
+        verify(counterClaimService).saveCounterClaim(
+            eq(TEST_CASE_REFERENCE),
+            counterClaimCaptor.capture(),
+            eq(submittingParty));
+
+        assertThat(counterClaimCaptor.getValue().getCounterClaimAgainst())
+            .extracting(ListValue::getId)
+            .containsExactly(claimantId.toString(), defendantId.toString());
     }
 }
