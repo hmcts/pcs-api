6bd49469c262a83fcefd0887e937d5278dfe48a7
shazanm-soli <shazan.miah@solirius.com>
2026-09-02 19:36:08 +0100

    added code to fix failing test

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/ShowConditions.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/ShowConditions.java
@@ -52,6 +52,7 @@ public class ShowConditions {
         return switch (featureFlag) {
             case RELEASE_1_DOT_2 -> "release1dot2Enabled";
             case RELEASE_1_DOT_3 -> "release1dot3Enabled";
+            case RELEASE_1_DOT_4 -> "release1dot4Enabled";
             case CASEWORKER_EVENTS -> "caseWorkerEventsEnabled";
             case WALES_MAKE_A_CLAIM -> "walesMakeAClaimEnabled";
             case EXEMPT_LANDLORD_QUESTION -> "exemptLandlordQuestionEnabled";
