4fe418ab093b64f1fa70221ebd0a38540001fecd
gmmagruder <gabrielle.magruder@gmail.com>
2026-09-04 10:26:00 +0100

    HDPI-8731: add migration sql script

--- src/main/resources/db/migration/V023__create_hearing_notice_parties_table.sql
+++ src/main/resources/db/migration/V023__create_hearing_notice_parties_table.sql
@@ -5,3 +5,11 @@ CREATE TABLE public.hearing_notice_party
   party_id UUID NOT NULL REFERENCES public.party (id),
   CONSTRAINT hearing_notice_party_pkey PRIMARY KEY (hearing_id, party_id)
 );
+
+-- Migrate existing hearing notice party data
+INSERT INTO public.hearing_notice_party (hearing_id, party_id)
+SELECT
+  h.id,
+  p.party_id
+FROM public.hearing h
+       CROSS JOIN LATERAL unnest(h.notice_parties) AS p(party_id);
