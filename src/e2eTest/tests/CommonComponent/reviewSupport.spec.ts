a6039401b7c2578e742abce1243d66084dfe38fa
Hemu31 <chalasani.hemu@gmail.com>
2026-09-07 12:32:02 +0100

    Approve support request

--- src/e2eTest/tests/CommonComponent/reviewSupport.spec.ts
+++ src/e2eTest/tests/CommonComponent/reviewSupport.spec.ts
@@ -2,11 +2,12 @@ import {initializeExecutor, performAction, performValidation} from '@utils/contr
 import {caseNumber} from '@utils/actions/custom-actions/createCase.action';
 import {test} from '@utils/test-fixtures';
 import {createCaseApiData, submitCaseApiData} from '@data/api-data';
-import {caseSummary, user} from '@data/page-data';
+import {caseSummary, home, user} from '@data/page-data';
 import {reviewSupport} from '../../data/page-data-figma/page-data-common-component/reviewSupport.page.data';
 import {dismissCookieBanner} from '@config/cookie-banner';
 import {BrowserContext, Page} from '@playwright/test';
 import { staff } from '@data/user-data/staff.user.data';
+import { caseInfo } from '@utils/actions/custom-actions/createCaseAPI.action';
 
 async function clearBrowserSession(page: Page, context: BrowserContext): Promise<void> {
   await context.clearCookies();
@@ -42,42 +43,64 @@ test.afterEach(async () => {
 test.describe('[Review support request] - Solicitor user - @nightly @CC @supportEvents', async () => {
 
   test('Approve support request @smoke', async ({page}) => {
-    //await performAction('login', {email: user.claimantSolicitor.email, password: user.claimantSolicitor.password});
-    await performAction('login', { email: staff.pcs_ctsc_admin_email, password: process.env.IDAM_PCS_USER_PASSWORD });
+    await performAction('login', {email: user.claimantSolicitorForGATest.email, password: user.claimantSolicitorForGATest.password});
     await dismissCookieBanner(page, 'analytics');
-    
+
+    await performAction('clickTab', home.noticeOfChangeTab);
+    await performAction('noticeOfChange', { caseRefNo: caseInfo.id } );
+    await performAction('clientDetails', { firstName: 'Peter' , lastName: 'Parker' });
+    await performAction('checkAndSubmit', { caseRefNo: caseInfo.id, firstName: 'Peter' , lastName: 'Parker' } );
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    await performAction('noticeOfChangeSuccessful', { caseRefNo: caseInfo.fid } );
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    await performAction('clickLink', reviewSupport.viewThisCase);
+    await performValidation('mainHeader', reviewSupport.casePartiesHeader);
+
     await performAction('navigateToCaseSummary');
-    await performAction('select', caseSummary.nextStepEventList, caseSummary.requestSupport);
-    await performAction('clickButton', caseSummary.go);
+    await performAction('validateTabAccess', { user: user, tabs: ['Case Parties', 'Case Details', 'Case File View', 'Summary', 'Service Request', 'Support'] });
+    await performAction('selectAnEvent', { eventType: caseSummary.requestSupport });
     await performValidation('mainHeader', reviewSupport.mainHeader);
-    await performAction('clickRadioButton', { option: reviewSupport.optionOne });
-    await performAction('clickButton', reviewSupport.continueButton);
+    await performAction('selectRadioButtonInYourSupport', {
+      optionToSelect: reviewSupport.whoIsTheSupportForOption,
+      continueButton: reviewSupport.continueButton,
+      headerToCheck: reviewSupport.supporTypeHeader
+    });
     await performValidation('mainHeader', reviewSupport.mainHeader);
-    await performAction('clickRadioButton', { option: reviewSupport.optionTwo });
-    await performAction('clickButton', reviewSupport.continueButton);
+    await performAction('selectRadioButtonInYourSupport', {
+      optionToSelect: reviewSupport.specialMeasureOption,
+      continueButton: reviewSupport.continueButton,
+      headerToCheck: reviewSupport.specialMeasureHeader
+    });
     await performValidation('mainHeader', reviewSupport.mainHeader);
-    await performAction('clickRadioButton', { option: reviewSupport.optionThree });
-    await performAction('clickButton', reviewSupport.continueButton);
-    await performValidation('mainHeader', reviewSupport.mainHeader);
-    await performAction('clickRadioButton', { option: reviewSupport.optionFour });
-    await performAction('clickButton', reviewSupport.continueButton);
+    await performAction('selectRadioButtonInYourSupport', {
+      optionToSelect: reviewSupport.evidenceByLiveLinkOption,
+      continueButton: reviewSupport.continueButton,
+      headerToCheck: reviewSupport.addCommentLabel
+    });
     await performValidation('mainHeader', reviewSupport.mainHeader);
     await performAction('inputText', reviewSupport.addCommentLabel, reviewSupport.addCommentText);
     await performAction('clickButton', reviewSupport.continueButton);
-    await performAction('clickButton', reviewSupport.submitButton);
+    await performAction('clickButton', 'Submit');
     await performValidation('bannerAlert', `Case #.* has been updated with event: Request support`);
+    await performAction('signOut');
 
-
-    //Approve Support Request
+    await performAction('navigateToUrl', process.env.MANAGE_CASE_BASE_URL);
+    await performAction('login', {email: user.staffAdmin.email, password: user.staffAdmin.password});
+    await dismissCookieBanner(page, 'analytics');
     await performAction('navigateToCaseSummary');
     await performAction('select', caseSummary.nextStepEventList, caseSummary.reviewSupport);
     await performAction('clickButton', caseSummary.go);
     await performValidation('mainHeader', reviewSupport.reviewSupportHeader);
+    await performAction('clickRadioButton', { option: 'Peter Parker (Defendant) - Special measure, Evidence by live link (Claimant Test Create Support)' });
     await performAction('clickButton', reviewSupport.continueButton);
     await performValidation('mainHeader', reviewSupport.reviewSupportHeader);
     await performAction('inputText', reviewSupport.reviewCommentLabel, reviewSupport.reviewCommentText);
+    await performAction('clickRadioButton', { question: reviewSupport.reviewStatusLabel, option: reviewSupport.activeOption });
+    await performAction('inputText', reviewSupport.reasonForStatusChangeLabel, reviewSupport.reasonForStatusChangeText);
+    await performAction('clickButton', reviewSupport.continueButton);
+    await performValidation('mainHeader', reviewSupport.reviewSupportHeader);
     await performAction('clickButton', reviewSupport.submitButton);
-    await performValidation('bannerAlert', `Case #.* has been updated with event: Review support`);
+    await performValidation('bannerAlert', `Case #.* has been updated with event: Review support request`);
   });
 
 });
