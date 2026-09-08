91b7a4d44b186efd1f158405cb881464275d36f5
marianadpereira <71711509+marianadpereira@users.noreply.github.com>
2026-09-08 17:11:58 +0100

    removing permissions of cuts users

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/accesscontrol/SupportReviewRoles.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/accesscontrol/SupportReviewRoles.java
@@ -3,8 +3,6 @@ package uk.gov.hmcts.reform.pcs.ccd.accesscontrol;
 public final class SupportReviewRoles {
 
     public static final UserRole[] SUPPORT_REVIEW_ROLES = {
-        UserRole.CTSC_TEAM_LEADER,
-        UserRole.CTSC_ADMIN,
         UserRole.HEARING_CENTRE_TEAM_LEADER,
         UserRole.HEARING_CENTRE_ADMIN
     };
