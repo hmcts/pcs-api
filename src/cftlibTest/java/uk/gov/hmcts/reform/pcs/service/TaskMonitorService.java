5dfb42340ada0452fa45e99bc17e0d5f3895c213
gmmagruder <gabrielle.magruder@gmail.com>
2026-09-09 10:51:43 +0100

    HDPI-8037: fix checkstyle

--- src/cftlibTest/java/uk/gov/hmcts/reform/pcs/service/TaskMonitorService.java
+++ src/cftlibTest/java/uk/gov/hmcts/reform/pcs/service/TaskMonitorService.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.pcs.service;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
