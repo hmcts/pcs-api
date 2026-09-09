c2ca9c085a8f41427e2cd64dd0c959d11777e793
Ankita-Kumbhare <kumbhare.ankita33@gmail.com>
2026-09-09 10:45:41 +0100

    Nightly fix for review support (#2670)

--- src/e2eTest/tests/CommonComponent/reviewSupport.spec.ts
+++ src/e2eTest/tests/CommonComponent/reviewSupport.spec.ts
@@ -40,7 +40,7 @@ test.afterEach(async () => {
   }
 });
 
-test.describe('[Review support request] - Solicitor user - @nightly @CC @supportEvents', async () => {
+test.describe('[Review support request] - HearingCenterAdmin user - @nightly @CC @supportEvents', async () => {
 
   test('Approve the support request', async ({page}) => {
     await performAction('login', {email: user.claimantSolicitorForGATest.email, password: user.claimantSolicitorForGATest.password});
@@ -85,7 +85,7 @@ test.describe('[Review support request] - Solicitor user - @nightly @CC @support
     await performAction('signOut');
 
     await performAction('navigateToUrl', process.env.MANAGE_CASE_BASE_URL);
-    await performAction('login', {email: user.staffAdmin.email, password: user.staffAdmin.password});
+    await performAction('login', {email: user.hearingCenterAdmin.email, password: user.hearingCenterAdmin.password});
     await dismissCookieBanner(page, 'analytics');
     await performAction('navigateToCaseSummary');
     await performAction('select', caseSummary.nextStepEventList, caseSummary.reviewSupport);
@@ -145,7 +145,7 @@ test.describe('[Review support request] - Solicitor user - @nightly @CC @support
     await performAction('signOut');
 
     await performAction('navigateToUrl', process.env.MANAGE_CASE_BASE_URL);
-    await performAction('login', {email: user.staffAdmin.email, password: user.staffAdmin.password});
+    await performAction('login', {email: user.hearingCenterAdmin.email, password: user.hearingCenterAdmin.password});
     await dismissCookieBanner(page, 'analytics');
     await performAction('navigateToCaseSummary');
     await performAction('select', caseSummary.nextStepEventList, caseSummary.reviewSupport);
@@ -205,7 +205,7 @@ test.describe('[Review support request] - Solicitor user - @nightly @CC @support
     await performAction('signOut');
 
     await performAction('navigateToUrl', process.env.MANAGE_CASE_BASE_URL);
-    await performAction('login', {email: user.staffAdmin.email, password: user.staffAdmin.password});
+    await performAction('login', {email: user.hearingCenterAdmin.email, password: user.hearingCenterAdmin.password});
     await dismissCookieBanner(page, 'analytics');
     await performAction('navigateToCaseSummary');
     await performAction('select', caseSummary.nextStepEventList, caseSummary.reviewSupport);
