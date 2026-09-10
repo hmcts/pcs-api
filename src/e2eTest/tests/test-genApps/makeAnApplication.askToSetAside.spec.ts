d56946ad57ab259b8391bd424b0375a2195c3730
Linus Norton <linusnorton@gmail.com>
2026-09-10 09:47:15 +0000

    Bound the seven spinner waits in spec files too
    The first pass only swept utils/. Five specs held seven more copies of the
    same unbounded throwing wait, including makeAnApplication.somethingElse and
    feeAndPay, both of which appear in the known-flake list.

--- src/e2eTest/tests/test-genApps/makeAnApplication.askToSetAside.spec.ts
+++ src/e2eTest/tests/test-genApps/makeAnApplication.askToSetAside.spec.ts
@@ -5,6 +5,7 @@ import { initializeExecutor } from '@utils/controller';
 import test, { expect } from '@playwright/test';
 import { FieldsStore } from '@utils/actions/custom-actions/custom-actions-genApps/recordAnsweredFields.action';
 import { initializeGenAppsExecutor, performAction, performValidation } from '@utils/controller-genApps';
+import { waitForSpinner } from '@utils/common/locator.utils';
 import { getCaseTypeId } from '@utils/common/caseType.utils';
 import { VERY_LONG_TIMEOUT } from 'playwright.config';
 import { caseSummary } from '@data/page-data/caseSummary.page.data';
@@ -60,7 +61,7 @@ test.beforeEach(async ({ page, context }) => {
     timeout: VERY_LONG_TIMEOUT,
   });
   await page.waitForLoadState();
-  await page.locator('.spinner-container').waitFor({ state: 'detached' });
+  await waitForSpinner(page);
   await performValidation('mainHeader', home.caseSummary);
 });
 
