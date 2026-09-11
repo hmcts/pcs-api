93da0bb258aa992a07cd89d228b1d1ba74f31480
gmmagruder <gabrielle.magruder@gmail.com>
2026-09-11 14:04:59 +0100

    HDPI-6625: remove unused imports

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/service/respondpossessionclaim/CounterClaimServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/service/respondpossessionclaim/CounterClaimServiceTest.java
@@ -29,7 +29,6 @@ import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
