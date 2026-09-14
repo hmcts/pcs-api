d3288bb6b60e95cab3d77ff31247317b075aec7d
gmmagruder <gabrielle.magruder@gmail.com>
2026-09-14 09:50:45 +0100

    HDPI-8142: remove unused import

--- src/test/java/uk/gov/hmcts/reform/pcs/ccd/service/respondpossessionclaim/CounterClaimServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pcs/ccd/service/respondpossessionclaim/CounterClaimServiceTest.java
@@ -33,7 +33,6 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
