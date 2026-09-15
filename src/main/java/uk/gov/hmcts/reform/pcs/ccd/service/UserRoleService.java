6680d2208a1be4b365b1a2a1c259cf29ed2bfb8b
gmmagruder <gabrielle.magruder@gmail.com>
2026-09-15 14:31:50 +0100

    HDPI-8867: add log for getRasRoles

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/service/UserRoleService.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/service/UserRoleService.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.pcs.ccd.service;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.CaseAssignmentApi;
@@ -18,6 +19,7 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class UserRoleService {
 
@@ -61,6 +63,8 @@ public class UserRoleService {
             List.of(String.valueOf(cacheKey.caseReference())),
             List.of(cacheKey.userId())
         );
+
+        log.info("getRasRoles userRoles: {}", userRoles);
 
         if (userRoles == null || userRoles.getCaseAssignmentUserRoles() == null) {
             return Set.of();
