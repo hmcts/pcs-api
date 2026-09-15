a377e6cefedd11cf97833372825e57a60d251227
Matt Searle <mgsearle@proton.me>
2026-09-15 11:31:31 +0100

    Simplify migration to only add column, defer backfill to later migration

--- src/main/resources/db/migration/V028__add_counter_claim_rank.sql
+++ src/main/resources/db/migration/V028__add_counter_claim_rank.sql
@@ -1,31 +1,6 @@
--- Add nullable column
+-- Add nullable rank column to counter_claim table
 ALTER TABLE public.counter_claim
 ADD COLUMN rank integer;
 
--- Backfill existing data
-WITH ranked_claims AS (
-    SELECT 
-        id,
-        ROW_NUMBER() OVER (
-            PARTITION BY case_id 
-            ORDER BY 
-                claim_submitted_date NULLS LAST,
-                id
-        ) as new_rank
-    FROM public.counter_claim
-)
-UPDATE public.counter_claim cc
-SET rank = rc.new_rank
-FROM ranked_claims rc
-WHERE cc.id = rc.id;
-
--- Verify backfill succeeded
-DO $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM counter_claim WHERE rank IS NULL) THEN
-        RAISE EXCEPTION 'Some counterclaims still have NULL rank after backfill!';
-    END IF;
-END $$;
-
--- Note: NOT NULL constraint will be added in a future migration
+-- Note: Backfill of existing data and NOT NULL constraint will be added in a future migration
 -- after the application code that sets rank on new counterclaims is deployed.
