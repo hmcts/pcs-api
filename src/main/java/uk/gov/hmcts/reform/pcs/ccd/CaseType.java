0fc8ee5ce367f8f62a45e03db415ad6651894ddf
shazanm-soli <shazan.miah@solirius.com>
2026-09-23 17:18:24 +0100

    making changes to judge roles

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/CaseType.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/CaseType.java
@@ -37,18 +37,12 @@ public class CaseType implements CCDConfig<PCSCase, State, AccessProfile> {
         AccessProfile.PCS_SOLICITOR,
         AccessProfile.GA_CLAIMANT_SOLICITOR,
         AccessProfile.JUDGE,
-        AccessProfile.FEE_PAID_JUDGE,
-        AccessProfile.CIRCUIT_JUDGE,
-        AccessProfile.LEADERSHIP_JUDGE,
         AccessProfile.HEARING_CENTRE_ADMIN,
         AccessProfile.CTSC_ADMIN,
         AccessProfile.WLU_ADMIN
     };
     static final AccessProfile[] INTERNAL_TAB_ROLES = {
         AccessProfile.JUDGE,
-        AccessProfile.FEE_PAID_JUDGE,
-        AccessProfile.CIRCUIT_JUDGE,
-        AccessProfile.LEADERSHIP_JUDGE,
         AccessProfile.HEARING_CENTRE_ADMIN,
         AccessProfile.CTSC_ADMIN,
         AccessProfile.WLU_ADMIN
@@ -56,9 +50,6 @@ public class CaseType implements CCDConfig<PCSCase, State, AccessProfile> {
 
     static final AccessProfile[] CASE_NOTE_TAB_ROLES = {
         AccessProfile.JUDGE,
-        AccessProfile.FEE_PAID_JUDGE,
-        AccessProfile.CIRCUIT_JUDGE,
-        AccessProfile.LEADERSHIP_JUDGE,
         AccessProfile.HEARING_CENTRE_ADMIN,
         AccessProfile.CTSC_ADMIN,
         AccessProfile.WLU_ADMIN
