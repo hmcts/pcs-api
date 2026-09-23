dbe97e85f821039ed5c88f3a472c50b3a29f8aae
jordan.aitken <jordan.aitken@hmcts.net>
2026-09-23 13:53:15 +0100

    HDPI-7755 | fix checkstyle

--- src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/legalrepdocumentupload/LegalRepDocumentUpload.java
+++ src/main/java/uk/gov/hmcts/reform/pcs/ccd/event/legalrepdocumentupload/LegalRepDocumentUpload.java
@@ -122,7 +122,7 @@ public class LegalRepDocumentUpload implements CCDConfig<PCSCase, State, UserRol
             caseReference
         );
 
-         boolean representingMultipleParties = representedPartyNames.getListItems().size() > 1;
+        boolean representingMultipleParties = representedPartyNames.getListItems().size() > 1;
         caseData.setMultipleRepresentedParties(VerticalYesNo.from(representingMultipleParties));
         caseData.setRepresentedPartyNames(representedPartyNames);
 
